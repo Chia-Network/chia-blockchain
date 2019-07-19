0bbc8ba90d82225b8c64a5ae893839cf6f222962
Mariano Sorgente <sorgente711@gmail.com>
2019-07-19 12:30:31 +0900

    Some basic boilerplate, and start with plotter.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # chia-blockchain
+
+### Install
+
+```bash
+git submodule update --init --recursive
+python3 -m venv .venv
+. .venv/bin/activate
+pip3 install .
+pip install lib/chiapos
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from setuptools import setup
+
+dependencies = ['pytest', 'flake8', 'blspy==0.1.9']
+
+setup(
+    name='chiablockchain',
+    version='0.2.1',
+    author='Mariano Sorgente',
+    author_email='mariano@chia.net',
+    description='Chia proof of space plotting, proving, and verifying (wraps C++)',
+    license='Apache License',
+    python_requires='>=3.7',
+    keywords='chia blockchain node',
+    install_requires=dependencies,
+    long_description=open('README.md').read(),
+    zip_safe=False,
+)
