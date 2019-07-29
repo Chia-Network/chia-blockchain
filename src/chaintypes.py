852045e027824621980923084717a476a02bcd31
Alex Wice <alexwice@gmail.com>
2019-07-30 07:41:30 +0800

    types wip

---
+++ src/chaintypes.py
@@ -0,0 +1,108 @@
+import dataclasses
+import json
+
+
+def sizedclass(cls, size):
+    return dataclasses.field(
+        default_factory = cls,
+        metadata = {'size': size}
+    )
+
+
+def with_slots(cls):
+    D = dict(cls.__dict__)
+    D['__slots__'] = tuple(f.name for f in dataclasses.fields(cls))
+    for f in dataclasses.fields(cls):
+        D.pop(f.name, None)
+    D.pop('__dict__', None)
+    return type(cls)(cls.__name__, cls.__bases__, D)
+
+
+class Transportable:
+    @classmethod
+    def import(cls, compact: bytes):
+        args = []
+        i = 0
+        for field in dataclasses.fields(cls):
+            size = field.metadata['size']
+            args.append(field.default_factory(compact[i : i + size]))
+            i += size
+        return cls(*args)
+    
+    def export(self):
+        ans = []
+        for field in dataclasses.fields(self):
+            ans.extend(getattr(self, field.name))
+        return bytes(ans)
+
+    def export_json(self):
+        return json.dumps(asdict(self))
+
+
+@with_slots
+@dataclasses.dataclass
+class Header(Transportable):
+    timestamp: bytes = sizedclass(bytes, 8)
+    prevhash: bytes = sizedclass(bytes, 32)
+    filterhash: bytes = sizedclass(bytes, 32)
+    poshash: bytes = sizedclass(bytes, 32)
+    bodyhash: bytes = sizedclass(bytes, 32)
+    extdata: bytes = sizedclass(bytes, 32)
+    onetimesig: bytes = sizedclass(bytes, 32)
+
+
+@with_slots
+@dataclasses.dataclass
+class Body(Transportable):
+    coinbase_hash: bytes = sizedclass(bytes, 32)
+    coinbase_amt: bytes = sizedclass(bytes, 8)
+    coinbase_sig: bytes = sizedclass(bytes, 32)
+    fees_hash: bytes = sizedclass(bytes, 32)
+    fees_amt: bytes = sizedclass(bytes, 8)
+    soln_gen: bytes = sizedclass(bytes, 32)
+    cost: bytes = sizedclass(bytes, 8)
+    aggsig: bytes = sizedclass(bytes, 32)
+
+
+@with_slots
+@dataclasses.dataclass
+class ProofOfTimeOutput(Transportable):
+    chall_hash: bytes = sizedclass(bytes, 32)
+    num_iters: bytes = sizedclass(bytes, 8)
+    final_power: bytes = sizedclass(bytes, 32)
+
+
+@with_slots
+@dataclasses.dataclass
+class ProofOfTime(Transportable):
+    output: ProofOfTimeOutput = sizedclass(bytes, 72)
+    witness_type: bytes = sizedclass(bytes, 8)
+    witness: bytes = sizedclass(bytes, 32)
+
+
+@with_slots
+@dataclasses.dataclass
+class ProofOfSpace(Transportable):
+    pool_pk: bytes = sizedclass(bytes, 32)
+    plot_pk: bytes = sizedclass(bytes, 32)
+    param_k: bytes = sizedclass(bytes, 8)
+    proof: bytes = sizedclass(bytes, 512)
+
+
+@with_slots
+@dataclasses.dataclass
+class Challenge(Transportable):
+    pot_hash: bytes = sizedclass(bytes, 32)
+    pos_hash: bytes = sizedclass(bytes, 32)
+    height: bytes = sizedclass(bytes, 8)
+    tot_weight: bytes = sizedclass(bytes, 8)
+
+
+@with_slots
+@dataclasses.dataclass
+class Block(Transportable):
+    header: Header = sizedclass(Header.import, 200)
+    body: Body = sizedclass(Body.import, 184)
+    proof_time: ProofOfTime = sizedclass(ProofOfTime.import, 112)
+    proof_space: ProofOfSpace = sizedclass(ProofOfSpace.import, 584)
+    challenge: Challenge = sizedclass(Challenge.import, 80)
