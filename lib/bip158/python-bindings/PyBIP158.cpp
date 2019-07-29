9d2d0095398a306f868f34dc3829530904dbed3d
Bill Blanke <wjb2002@flex.com>
2019-07-28 18:05:07 -0700

    reorg python bindings. copyright

--- lib/bip158/python-bindings/PyBIP158.cpp
+++ lib/bip158/python-bindings/PyBIP158.cpp
@@ -1,8 +1,18 @@
-// Copyright (c) 2018 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php./Users/bill/downloads/gene/chia-blockchain/lib/bip158
+// Copyright 2018 Chia Network Inc
+  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 
-#include <PyBIP158.h>
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "PyBIP158.h"
 
 PyBIP158::PyBIP158(std::vector< std::vector< unsigned char > >& hashes)
 {
