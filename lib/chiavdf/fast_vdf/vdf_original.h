a66fe8c18495d14d104ca8ea8df95724b236efa3
Bill Blanke <wjb2002@flex.com>
2019-10-04 13:12:14 -0700

    made vdf_original thread safe since it is called by squaring thread in addition to proof threads

--- lib/chiavdf/fast_vdf/vdf_original.h
+++ lib/chiavdf/fast_vdf/vdf_original.h
@@ -14,7 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ***/
 
-namespace vdf_original {
+class vdf_original
+{
+public:
+    
     struct form {
         // y = ax^2 + bxy + y^2
         mpz_t a;
@@ -24,15 +27,11 @@ namespace vdf_original {
         //mpz_t d; // discriminant
     };
 
-    ostream& operator<<(ostream& os, const form& f) {
-        return os << "a: " <<  f.a << endl << "b: " << f.b << endl << "c: " << f.c << endl;
-    }
-
     mpz_t negative_a, r, denom, old_b, ra, s, x, old_a, g, d, e, q, w, u, a,
         b, m, k, mu, v, sigma, lambda, h, t, l, j;
     form f3;
 
-    inline void normalize(form& f) {
+    void normalize(form& f) {
         mpz_neg(negative_a, f.a);
         if (mpz_cmp(f.b, negative_a) > 0 && mpz_cmp(f.b, f.a) <= 0) {
             // Already normalized
@@ -65,7 +64,7 @@ namespace vdf_original {
         mpz_add(f.c, f.c, ra);
     }
 
-    inline void reduce(form& f) {
+    void reduce(form& f) {
         normalize(f);
         while ((mpz_cmp(f.a, f.c) > 0) ||
             (mpz_cmp(f.a, f.c) == 0 && mpz_cmp_si(f.b, 0) < 0)) {
@@ -105,7 +104,7 @@ namespace vdf_original {
         normalize(f);
     }
 
-    inline form generator_for_discriminant(mpz_t* d) {
+    form generator_for_discriminant(mpz_t* d) {
         form x;
         mpz_init_set_ui(x.a, 2);
         mpz_init_set_ui(x.b, 1);
@@ -127,7 +126,7 @@ namespace vdf_original {
     // Returns mu and v, solving for x:  ax = b mod m
     // such that x = u + vn (n are all integers). Assumes that mu and v are initialized.
     // Returns 0 on success, -1 on failure
-    inline int solve_linear_congruence(mpz_t& mu, mpz_t& v, mpz_t& a, mpz_t& b, mpz_t& m) {
+    int solve_linear_congruence(mpz_t& mu, mpz_t& v, mpz_t& a, mpz_t& b, mpz_t& m) {
         // g = gcd(a, m), and da + em = g
         mpz_gcdext(g, d, e, a, m);
 
@@ -148,7 +147,7 @@ namespace vdf_original {
     }
 
     // Faster version without check, and without returning v
-    inline int solve_linear_congruence(mpz_t& mu, mpz_t& a, mpz_t& b, mpz_t& m) {
+    int solve_linear_congruence(mpz_t& mu, mpz_t& a, mpz_t& b, mpz_t& m) {
         mpz_gcdext(g, d, e, a, m);
         mpz_fdiv_q(q, b, g);
         mpz_mul(mu, q, d);
@@ -157,12 +156,12 @@ namespace vdf_original {
     }
 
     // Takes the gcd of three numbers
-    inline void three_gcd(mpz_t& ret, mpz_t& a, mpz_t& b, mpz_t& c) {
+    void three_gcd(mpz_t& ret, mpz_t& a, mpz_t& b, mpz_t& c) {
         mpz_gcd(ret, a, b);
         mpz_gcd(ret, ret, c);
     }
 
-    inline form* multiply(form &f1, form &f2) {
+    form* multiply(form &f1, form &f2) {
         //assert(mpz_cmp(f1.d, f2.d) == 0);
 
         // g = (b1 + b2) / 2
@@ -280,7 +279,7 @@ namespace vdf_original {
     *     C = mu^2 - (b * mu - c)/a
     * 3. reduce f(A, B, C)
     **/
-    inline form* square(form &f1) {
+    form* square(form &f1) {
         int ret = solve_linear_congruence(mu, f1.b, f1.c, f1.a);
         assert(ret == 0);
 
@@ -306,16 +305,16 @@ namespace vdf_original {
     }
 
     // Performs the VDF squaring iterations
-    inline form repeated_square(form *f, uint64_t iterations) {
+    form repeated_square(form *f, uint64_t iterations) {
         for (uint64_t i=0; i < iterations; i++) {
             f = square(*f);
         }
         return *f;
     }
 
-    void init() {
+    vdf_original() {
         mpz_inits(negative_a, r, denom, old_a, old_b, ra, s, x, g, d, e, q, w, m,
                 u, a, b, k, mu, v, sigma, lambda, f3.a, f3.b, f3.c, //f3.d,
                 NULL);
     }
-}
+};
