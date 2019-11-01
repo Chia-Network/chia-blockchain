1a227b6f3d3f42006e6ce77e43ab4ab5a42a637c
Richard Kiss <him@richardkiss.com>
2019-11-01 11:56:46 -0700

    Get rid of "bits" class variable.

--- src/util/ints.py
+++ src/util/ints.py
@@ -4,42 +4,34 @@ from typing import Any, BinaryIO
 
 class int8(StructStream):
     PACK = "!b"
-    bits = 8
 
 
 class uint8(StructStream):
     PACK = "!B"
-    bits = 8
 
 
 class int16(StructStream):
     PACK = "!h"
-    bits = 16
 
 
 class uint16(StructStream):
     PACK = "!H"
-    bits = 16
 
 
 class int32(StructStream):
     PACK = "!l"
-    bits = 32
 
 
 class uint32(StructStream):
     PACK = "!L"
-    bits = 32
 
 
 class int64(StructStream):
     PACK = "!q"
-    bits = 64
 
 
 class uint64(StructStream):
     PACK = "!Q"
-    bits = 64
 
 
 class int1024(int):
