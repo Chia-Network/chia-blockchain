5380df8e736bb1c0e74b5e47fa504904de4b91ed
fchirica <fchirica96@gmail.com>
2019-11-09 00:33:29 +0200

    Refactor

--- src/timelord.py
+++ src/timelord.py
@@ -25,10 +25,8 @@ log = logging.getLogger(__name__)
 class Timelord:
     def __init__(self):
         self.config = safe_load(open("src/config/timelord.yaml", "r"))
-        #self.free_servers = self.config["vdf_server_ports"]
         self.lock: Lock = Lock()
-        self.free_servers: List[(str, str)] = [('127.0.0.1', '8889'), ('127.0.0.1', '8890')]
-        #self.free_servers: List[(str, str)] = [('18.206.54.21', 8889), ('18.206.54.21', 8890), ('18.206.54.21', 8891), ('18.206.54.21', 8892), ('18.206.54.21', 8893)]
+        self.free_servers: List[str, str] = list(zip(self.config["vdf_server_ips"], self.config["vdf_server_ports"]))
         self.server_count: int = len(self.free_servers)
         self.active_discriminants: Dict = {}
         self.best_height_three_proofs: int = -1
@@ -51,7 +49,7 @@ class Timelord:
         disc: int = create_discriminant(challenge_start.challenge_hash, constants["DISCRIMINANT_SIZE_BITS"])
         async with self.lock:
             if (challenge_start.challenge_hash in self.seen_discriminants):
-                log.info("Already seen this challenge hash {challenge_start.challenge_hash}. Ignoring.")
+                log.info(f"Already seen this challenge hash {challenge_start.challenge_hash}. Ignoring.")
                 return
             self.seen_discriminants.append(challenge_start.challenge_hash)
             self.active_heights.append(challenge_start.height)
@@ -61,65 +59,42 @@ class Timelord:
                 log.info("Not starting challenge, already three proofs at that height")
                 return 
             if (len(self.active_discriminants) == self.server_count):
-                worst_height_active = None
-                for active_disc in self.active_discriminants:
-                    _, current_height, _ = self.active_discriminants[active_disc]
-                    if (worst_height_active == None or worst_height_active > current_height):
-                        worst_height_active = current_height
-                # If I have a better height than what's running, stop the newest process.
+                worst_height_active = min([h for (_, h, _) in self.active_discriminants.values()])
+                if (worst_height_active >= challenge_start.height):
+                    return
+                # We need to stop one process having worst_height_active.
                 log.info(f"Worst height running {worst_height_active}")
                 log.info(f"Challenge start height {challenge_start.height}")
-                if (worst_height_active < challenge_start.height):
-                    newest_process_time = None
-                    latest_finish_time = None
-                    stop_writer = None
-                    stop_discriminant = None
-
-                    # Firstly find processes without iters and pick the newest one (since it did the least number of iters)
-                    for active_disc in self.active_discriminants:
-                        writer, current_height, _ = self.active_discriminants[active_disc]
-                        if (current_height != worst_height_active):
-                            continue
-                        process_time = self.active_discriminants_start_time[active_disc]
-                        if (active_disc not in self.pending_iters or len(self.pending_iters[active_disc]) == 0):
-                            if (newest_process_time == None or process_time > newest_process_time):
-                                stop_writer = writer
-                                stop_discriminant = active_disc
-                                newest_process_time = process_time
-                    
-                    # If all processes have iters, stop the one that finish one proof the latest.
-                    if (stop_writer is None):
-                        for active_disc in self.active_discriminants:
-                            try:
-                                writer, current_height, ip = self.active_discriminants[active_disc]
-                                if (current_height != worst_height_active):
-                                    continue
-                                best_iter = min(self.pending_iters[active_disc])
-                                time_taken = time.time() - self.active_discriminants_start_time[active_disc]
-                                ips, _ = self.avg_ips[ip]
-                                expected_finish = max(0, (best_iter - time_taken * ips) / ips)
-                                log.info(f"Expected to finish in: {expected_finish}s")
-                                if (latest_finish_time == None or expected_finish > latest_finish_time):
-                                    stop_writer = writer
-                                    stop_discriminant = active_disc
-                                    latest_finish_time = expected_finish
-                            except Exception as e:
-                                e_to_str = str(e)
-                                log.info(f"Exception: {e_to_str}")
-
-                    log.info("Stopping one server as new challenge")
-                    stop_writer.write(b'10')
-                    await stop_writer.drain()
-                    del self.active_discriminants[stop_discriminant]
-                    del self.active_discriminants_start_time[stop_discriminant]
-                    self.done_discriminants.append(stop_discriminant)
+                stop_writer = None
+                stop_discriminant = None
+                
+                low_heights = {k: v for k, v in self.active_discriminants.items() if v[1] == worst_height_active}
+                no_iters = {k: v for k, v in low_heights.items() if k not in self.pending_iters or len(self.pending_iters[k]) == 0}
+                
+                # If we have process(es) with no iters, stop the one that started the latest
+                if len(no_iters) > 0:
+                    latest_start_time = max([self.active_discriminants_start_time[k] for k, _ in no_iters.items()])
+                    stop_discriminant, stop_writer = next((k, v[0]) for k, v in no_iters.items() if self.active_discriminants_start_time[k] == latest_start_time)                    
                 else:
-                    return
+                    #Otherwise, pick the one that finishes one proof the latest.
+                    best_iter = {k: min(self.pending_iters[k]) for k, _ in low_heights.items()}
+                    time_taken = {k: time.time() - self.active_discriminants_start_time[k] for k, _ in low_heights.items()}
+                    expected_finish = {k: max(0, (best_iter[k] - time_taken[k] * self.avg_ips[v[2]][0]) / self.avg_ips[v[2]][0]) for k, v in low_heights.items()}
+                    worst_finish = max([v for v in expected_finish.values()])
+                    log.info(f"Worst finish time: {worst_finish}s")
+                    stop_discriminant, stop_writer = next((k, v[0]) for k, v in low_heights.items() if expected_finish[k] == worst_finish)
+                               
+                log.info("Stopping one server as new challenge")
+                stop_writer.write(b'10')
+                await stop_writer.drain()
+                del self.active_discriminants[stop_discriminant]
+                del self.active_discriminants_start_time[stop_discriminant]
+                self.done_discriminants.append(stop_discriminant)
 
         # Wait for a server to become free.
-        ip: str = "None"
-        port: str = "None"
-        while port == "None":
+        ip: Optional[str] = None
+        port: Optional[str] = None
+        while port == None:
             async with self.lock:
                 if (challenge_start.height <= max(self.active_heights) - 3):
                     self.done_discriminants.append(challenge_start.challenge_hash)
@@ -136,8 +111,8 @@ class Timelord:
                         self.active_heights.remove(challenge_start.height)
 
             # Poll until a server becomes free.
-            if port == "None":
-                await asyncio.sleep(1)
+            if port == None:
+                await asyncio.sleep(0.5)
 
         log.info("Attempting SSH connection")
         #proc = await asyncio.create_subprocess_shell(f"ssh chia@{ip} '~/fast_vdf2/server {port} 2>&1 | tee -a ~/logs.txt'")
@@ -152,7 +127,7 @@ class Timelord:
             except Exception as e:
                 e_to_str = str(e)
                 log.error(f"Connection to VDF server error message: {e_to_str}")
-            await asyncio.sleep(5)
+            await asyncio.sleep(1)
         if not writer or not reader:
             raise Exception("Unable to connect to VDF server")
 
