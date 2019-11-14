59acd66c524e2e479d351b2417127e8869490807
Mariano Sorgente <sorgente711@gmail.com>
2019-11-14 12:08:01 -0500

    Revert "Start removing global connections"
    This reverts commit 15bd64e954425ff6ae05601bb9a525308ac1e117.

--- src/server/repl_client.py
+++ src/server/repl_client.py
@@ -1,32 +1,27 @@
-# import asyncio
-# import logging
-# import sys
-# from src.full_node import FullNode
-# from src.server.server import ChiaServer
-# from src.util.network import parse_host_port, create_node_id
-# from src.server.outbound_message import NodeType, OutboundMessage, Message, Delivery
-# from src.types.peer_info import PeerInfo
-# from src.store.full_node_store import FullNodeStore
-# from src.blockchain import Blockchain
-
-# logging.basicConfig(format='Farmer %(name)-25s: %(levelname)-8s %(asctime)s.%(msecs)03d %(message)s',
-#                     level=logging.INFO,
-#                     datefmt='%H:%M:%S'
-#                     )
-# log = logging.getLogger(__name__)
-
-# async def start_client_to_full_node(host, port):
-#     store = FullNodeStore()
-#     await store.initialize()
-#     blockchain = Blockchain(store)
-#     await blockchain.initialize()
-#     full_node = FullNode(store, blockchain)
-#     server = ChiaServer(9000, full_node, NodeType.FULL_NODE)
-#     res = await server.start_client(PeerInfo(host, port), None)
-#     log.info("ASFd")
-#     m = Message("block", {})
-#     server.push_message(OutboundMessage(NodeType.FULL_NODE, m, Delivery.BROADCAST))
-#     await server.await_closed()
+import asyncio
+import logging
+import sys
+from src.full_node import FullNode
+from src.server.server import ChiaServer
+from src.util.network import parse_host_port, create_node_id
+from src.server.outbound_message import NodeType, OutboundMessage, Message, Delivery
+from src.types.peer_info import PeerInfo
+from src.store.full_node_store import FullNodeStore
+from src.blockchain import Blockchain
 
 
-# asyncio.run(start_client_to_full_node("127.0.0.1", 8444))
+async def start_client_to_full_node(host, port):
+    store = FullNodeStore()
+    await store.initialize()
+    blockchain = Blockchain(store)
+    await blockchain.initialize()
+    full_node = FullNode(store, blockchain)
+    server = ChiaServer(9000, full_node, NodeType.FULL_NODE)
+    res = await server.start_client(PeerInfo(host, port), None)
+    print(res)
+    m = Message("block", {})
+    server.push_message(OutboundMessage(NodeType.FULL_NODE, m, Delivery.BROADCAST))
+    await server.await_closed()
+
+
+asyncio.run(start_client_to_full_node("beast.44monty.chia.net", 8444))
