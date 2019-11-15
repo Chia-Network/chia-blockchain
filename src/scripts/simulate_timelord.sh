b4ff07c521e201774e489a560c79018e74fdafc4
Rostislav <rostislav@users.noreply.github.com>
2019-11-15 19:02:03 +0200

    scripts: Refactor, move duplicate code to common.sh

--- src/scripts/simulate_timelord.sh
+++ src/scripts/simulate_timelord.sh
@@ -1,27 +1,9 @@
 . .venv/bin/activate
+. src/scripts/common.sh
 
 # Starts a timelord, and a full node
 
-_kill_servers() {
-  ps -e | grep python | awk '{print $1}' | xargs -L1  kill
-  ps -e | grep "vdf_server" | awk '{print $1}' | xargs -L1  kill
-}
+_run_bg_cmd python -m src.server.start_timelord
+_run_bg_cmd python -m src.server.start_full_node "127.0.0.1" 8444 -t -u 8222
 
-_kill_servers
-
-python -m src.server.start_timelord &
-P1=$!
-python -m src.server.start_full_node "127.0.0.1" 8444 -t -u 8222 &
-P2=$!
-
-_term() {
-  echo "Caught SIGTERM signal, killing all servers."
-  kill -TERM "$P1" 2>/dev/null
-  kill -TERM "$P2" 2>/dev/null
-  _kill_servers
-}
-
-trap _term SIGTERM
-trap _term SIGINT
-trap _term INT
-wait $P1 $P2
+wait
--- src/scripts/stop_all_servers.sh
+++ src/scripts/stop_all_servers.sh
@@ -1,4 +1,3 @@
 # Stops all python servers and VDF processes running on this machine
 
-ps -e | grep python | awk '{print $1}' | xargs -L1  kill
-ps -e | grep "vdf_server" | awk '{print $1}' | xargs -L1  kill
+. src/scripts/common.sh
