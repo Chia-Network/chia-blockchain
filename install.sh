0e24cbea605519479a8bd90fe8e23ac7e82cf3f0
fchirica <fchirica96@gmail.com>
2019-11-20 22:56:48 +0200

    Simulate network working with old VDF Server exec

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 python3 -m venv .venv
 . .venv/bin/activate
+pip install wheel # For building blspy
 pip install -e .
 pip install -r requirements.txt
+
+#cd lib/chiavdf/fast_vdf
+# Install libgmp, libboost, and libflint, and then run the following
+# sh install.sh
