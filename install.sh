1ac8a0a9693948e15c87416d6a575e9e0eec29e0
Gene Hoffman <30377676+hoffmang9@users.noreply.github.com>
2019-11-26 11:36:14 -0800

    Update install.sh to compile and install flint2

--- install.sh
+++ install.sh
@@ -5,4 +5,12 @@ pip install -e .
 pip install -r requirements.txt
 
 # Install libgmp, libboost, and libflint, and then run the following
+# Check for git clone of flint2 on MacOS and install if found
+if [  -f flint2/configure ]; then
+    cd flint2/
+    make -j4
+    make install
+    cd ../
+fi
+
 cd lib/chiavdf/fast_vdf && sh install.sh
