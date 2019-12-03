ce9294b8399503ba636317e5cc3a13734f7f5d79
Rostislav <rostislav@users.noreply.github.com>
2019-12-03 02:03:38 +0100

    peer_info: Show IP addresses in human-readable form

--- src/types/peer_info.py
+++ src/types/peer_info.py
@@ -3,13 +3,19 @@ from typing import Union
 
 from src.types.sized_bytes import bytes16
 from src.util.ints import uint16
+from src.util.ip_str import ip_to_str
 from src.util.streamable import Streamable, streamable
+
+
+class IPAddr(bytes16):
+    def __repr__(self):
+        return f"'{ip_to_str(self)}'"
 
 
 @dataclass(frozen=True)
 @streamable
 class BinPeerInfo(Streamable):
-    host: bytes16
+    host: IPAddr
     port: uint16
 
 
