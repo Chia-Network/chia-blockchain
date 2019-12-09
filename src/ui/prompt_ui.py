b5293e141199c560cdc4a1e6a297763a9da8a7fa
Mariano Sorgente <sorgente711@gmail.com>
2019-12-09 22:51:25 +0900

    Fix UI issue, remove useless catch

--- src/ui/prompt_ui.py
+++ src/ui/prompt_ui.py
@@ -84,7 +84,7 @@ async def start_ssh_server(
         except ConnectionError:
             log.info("Connection error in ssh UI, exiting.")
             ui.close()
-            # raise
+            raise
 
     ssh_server = await asyncssh.create_server(
         lambda: PromptToolkitSSHServer(interact),
@@ -482,8 +482,6 @@ class FullNodeUI:
                 if self.app and not self.app.invalidated:
                     self.app.invalidate()
                 await asyncio.sleep(1)
-        except concurrent.futures._base.CancelledError as e:
-            log.warn(f"Cancelled error in UI: {type(e)}: {e}")
         except Exception as e:
             log.warn(f"Exception in UI update_ui {type(e)}: {e}")
             raise e
@@ -522,8 +520,6 @@ class FullNodeUI:
                     for pk in self.pool_pks
                 ]
                 await asyncio.sleep(5)
-        except concurrent.futures._base.CancelledError as e:
-            log.warn(f"Cancelled error in UI: {type(e)}: {e}")
         except Exception as e:
             log.warn(f"Exception in UI update_data {type(e)}: {e}")
             raise e
