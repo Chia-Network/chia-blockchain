516047f3c6f58976cc1fe1a936091229f37e011f
Rostislav <rostislav@users.noreply.github.com>
2019-12-18 11:05:43 +0200

    Revert "peer_info: Show IP addresses in human-readable form"
    This reverts commit 4f2ba66577f45057bfd5d85dc8a35d4f6bbd47f0.

--- src/types/peer_info.py
+++ src/types/peer_info.py
@@ -3,19 +3,13 @@ from typing import Union
 
 from src.types.sized_bytes import bytes16
 from src.util.ints import uint16
-from src.util.ip_str import ip_to_str
 from src.util.streamable import Streamable, streamable
-
-
-class IPAddr(bytes16):
-    def __repr__(self):
-        return f"'{ip_to_str(self)}'"
 
 
 @dataclass(frozen=True)
 @streamable
 class BinPeerInfo(Streamable):
-    host: IPAddr
+    host: bytes16
     port: uint16
 
 
