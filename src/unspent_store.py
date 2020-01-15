5fd40ddd6c9fe81db6abf2fbf32f001de3ef8468
Yostra <straya@chia.net>
2020-01-14 23:54:35 -0800

    diffs for tips

--- src/unspent_store.py
+++ src/unspent_store.py
@@ -1,11 +1,12 @@
 import asyncio
-from typing import Dict, Optional, List
+from typing import Dict, Optional, List, Tuple
 
 import aiosqlite
 
 from src.consensus.constants import constants as consensus_constants
 from src.types.full_block import FullBlock
 from src.types.hashable import Hash, Unspent, Coin, CoinName
+from src.types.header_block import HeaderBlock
 from src.util.chain_utils import name_puzzle_conditions_list
 from src.util.consensus import created_outputs_for_conditions_dict
 from src.util.ints import uint32
@@ -16,8 +17,32 @@ def additions_for_npc(npc_list) -> List[Coin]:
     for coin_name, puzzle_hash, conditions_dict in npc_list:
         for coin in created_outputs_for_conditions_dict(conditions_dict, coin_name):
             additions.append(coin)
-
     return additions
+
+
+ def removals_and_additions(block: FullBlock, index: uint32) -> Tuple[List[Hash], List[Coin]]:
+    removals: List[Hash] = []
+    additions: List[Coin] = []
+
+    additions.append(block.body.coinbase)
+    additions.append(block.body.fees_coin)
+
+    if block.body.transactions is not None:
+        # ensure block program generates solutions
+        # This should never throw here, block must be valid if it comes to here
+        npc_list = name_puzzle_conditions_list(block.body.transactions)
+        # build removals list
+        for coin_name, ph, con in npc_list:
+            removals.append(coin_name)
+
+        additions.extend(additions_for_npc(npc_list))
+
+    return removals, additions
+
+
+class DiffStore:
+    header: HeaderBlock
+    diffs: Dict[Hash, Unspent]
 
 
 class UnspentStore:
@@ -28,6 +53,7 @@ class UnspentStore:
     lock: asyncio.Lock
     # TODO set the size limit of ram cache
     lce_unspent_coins: Dict
+    self.head_diffs: Dict[HeaderBlock, DiffStore]
 
     @classmethod
     async def create(cls, db_name: str):
@@ -62,7 +88,7 @@ class UnspentStore:
         # Lock
         self.lock = asyncio.Lock()  # external
         self.lce_unspent_coins = dict()
-        self.head_diffs: Dict[Hash, Dict] = dict()
+        self.head_diffs = dict()
         return self
 
     async def close(self):
@@ -73,33 +99,53 @@ class UnspentStore:
         await self.db.commit()
 
     async def new_lca(self, block: FullBlock):
-        print("Not implemented")
-        coinbase: Unspent = Unspent(block.body.coinbase, block.height, 0, False)
-        fee: Unspent = Unspent(block.body.fees_coin, block.height, 0, False)
+        removals, additions = removals_and_additions(block)
 
-        # Add coinbase and fee coin
-        await self.add_unspent(coinbase)
-        await self.add_unspent(fee)
+        for coin_name in removals:
+            await self.set_spent(coin_name, block.height)
 
-        # Add transacitions
-        if block.body.transactions is not None:
-            # ensure block program generates solutions
-            # This should never throw here, block must be valid if it comes to here
-            npc_list = name_puzzle_conditions_list(block.body.transactions)
-            # build removals list
-            removals = tuple(_[0] for _ in npc_list)
-            additions: List[Coin] = additions_for_npc(npc_list)
-            for coin_name in removals:
-                await self.set_spent(coin_name, block.height)
-            for coin in additions:
-                unspent: Unspent = Unspent(coin, block.height, 0, False)
-                await self.add_unspent(unspent)
+        for coin in additions:
+            unspent: Unspent = Unspent(coin, block.height, 0, False)
+            await self.add_unspent(unspent)
 
-    async def new_head(self, head: FullBlock, old: Hash):
+    # Received new tip, just update diffs
+    async def new_head(self, head: FullBlock, old: Optional[HeaderBlock]):
+        removals, additions = removals_and_additions(head)
+        # New Head nothing is being replaced
+        if old is None:
+            await self.add_diffs(removals, additions, head)
+
         if self.head_diffs[old] is not None:
-            del self.head_diffs[old]
-        print("new head")
+            # Old head is being extended, add diffs
+            if head.prev_header_hash == old.header_hash:
+                old_diff = self.head_diffs.pop(old)
+                await self.add_diffs(removals, additions, head, old_diff)
 
+            # Old head is being replaced
+            else:
+                del self.head_diffs[old]
+                await self.add_diffs(removals, additions, head)
+
+    async def add_diffs(self, removals: List[Hash], additions: List[Coin], head: FullBlock, diff_store: DiffStore = None):
+        if diff_store is None:
+            diff_store: DiffStore = DiffStore(head.header_block, dict())
+
+        for coin_name in removals:
+            removed: Unspent = diff_store.diffs[coin_name]
+            if removed is None:
+                removed = await self.get_unspent(coin_name)
+            removed.spent = True
+            removed.spent_block_index = head.height
+            diff_store.diffs[removed.name()] = removed
+
+        for coin in additions:
+            added: Unspent = Unspent(coin, head.height, 0, False)
+            diff_store.diffs[added.name()] = added
+
+        diff_store.header = head.header_block
+        self.head_diffs[head.header_block] = diff_store
+
+    # Store unspent in DB and ram cache
     async def add_unspent(self, unspent: Unspent) -> None:
         await self.db.execute(
             "INSERT OR REPLACE INTO blocks VALUES(?, ?, ?)",
@@ -110,8 +156,9 @@ class UnspentStore:
              bytes(unspent)),
         )
         await self.db.commit()
-        self.lce_unspent_coins[unspent.coin.name()] = unspent
+        self.lce_unspent_coins[unspent.coin.name().hex()] = unspent
 
+    # Update unspent to be spent
     async def set_spent(self, coin_name: Hash, index: uint32):
         spent: Unspent = await self.get_unspent(coin_name)
         spent.spent = True
@@ -133,6 +180,7 @@ class UnspentStore:
     # TODO figure out if we want to really delete when doing rollback
     async def rollback_to_block(self, block_index):
         # Update memory cache
+        breakpoint()
         for k, v in self.lce_unspent_coins.items():
             if v.spent_block_index > block_index:
                 v.spent_block_index = 0
