f10926c95cfc256099dafd7f1c3a8fc566526070
Yostra <straya@chia.net>
2020-01-26 15:26:29 -0800

    use sorted dic in mempool

--- src/mempool.py
+++ src/mempool.py
@@ -4,11 +4,12 @@ from enum import Enum
 from typing import Dict, Optional, Tuple, List
 
 import clvm
+from numpy.core import double
 
 from src.consensus.constants import constants as consensus_constants
 from src.farming.farming_tools import best_solution_program
 from src.types.full_block import FullBlock
-from src.types.hashable import SpendBundle, CoinName, ProgramHash, Program, Coin, Unspent
+from src.types.hashable import SpendBundle, CoinName, ProgramHash, Program, Coin, Unspent, Hash
 from src.types.hashable.SpendBundle import BundleHash
 from src.types.header_block import HeaderBlock
 from src.types.sized_bytes import bytes32
@@ -16,26 +17,87 @@ from src.unspent_store import UnspentStore
 from src.util.Conditions import ConditionOpcode, ConditionVarPair
 from src.util.ConsensusError import Err
 from src.util.consensus import conditions_dict_for_solution, hash_key_pairs_for_conditions_dict
-from src.util.ints import uint32, uint64
+from src.util.ints import uint64
+from sortedcontainers import SortedDict
 
-
-@dataclass(frozen=True)
-class Pool:
-    header_block: HeaderBlock
-    spends: Dict[BundleHash: SpendBundle]
-    additions: Dict[CoinName: SpendBundle]
-    removals: Dict[CoinName: SpendBundle]
-    conflicting: Dict[BundleHash: BundleHash]
+tx_per_sec = 20
+sec_per_block = 5 * 60
+block_buffer_count = 10
+# 60000
+mempool_size = tx_per_sec * sec_per_block * block_buffer_count
 
 
 @dataclass(frozen=True)
 class MempoolItem:
     spend_bundle: SpendBundle
-    fee_per_cost: uint64
+    fee_per_cost: double
     fee: uint64
+    cost: uint64
 
     def __lt__(self, other):
+        # TODO test to see if it's < or >
         return self.fee_per_cost < other.fee_per_cost
+
+    @property
+    def name(self) -> bytes32:
+        return self.spend_bundle.name()
+
+
+class Pool:
+    header_block: HeaderBlock
+    spends: Dict[BundleHash, MempoolItem]
+    sorted_spends: SortedDict[double, Dict[BundleHash, MempoolItem]]
+    additions: Dict[CoinName, MempoolItem]
+    removals: Dict[CoinName, MempoolItem]
+    min_fee: uint64
+
+    # if new min fee is added
+    @staticmethod
+    async def create(head: HeaderBlock):
+        self = Pool()
+        self.spends = {}
+        self.additions = {}
+        self.removals = {}
+        self.min_fee = 0
+        return self
+
+    def get_min_fee_per_cost(self) -> double:
+        if self.at_full_capacity():
+            fee_per_cost, val = self.sorted_spends.peekitem(index=0)
+            return fee_per_cost
+        else:
+            return 0
+
+    def remove_spend(self, item: MempoolItem):
+        removals: List[Coin] = item.spend_bundle.removals()
+        additions: List[Coin] = item.spend_bundle.additions()
+        for rem in removals:
+            del self.removals[rem.name()]
+        for add in additions:
+            del self.additions[add.name()]
+        del self.spends[item.name]
+        del self.sorted_spends[item.fee_per_cost][item.name]
+        dic = self.sorted_spends[item.fee_per_cost]
+        if len(dic.values) == 0:
+            del self.sorted_spends[item.fee_per_cost]
+
+    def add_to_pool(self, item: MempoolItem, additions: List[Coin], removals_dic: Dict[bytes32, Coin]):
+        if self.at_full_capacity():
+            # Val is Dict[hash, MempoolItem]
+            fee_per_cost, val = self.sorted_spends.peekitem(index=0)
+            to_remove = val.values()[0]
+            self.remove_spend(to_remove)
+
+        self.spends[item.name] = item
+        self.sorted_spends[item.fee_per_cost] = item
+
+        for add in additions:
+            self.additions[add.name()] = item
+        for key in removals_dic.keys():
+            self.removals[key] = item
+
+    def at_full_capacity(self) -> bool:
+        return len(self.spends.keys()) >= mempool_size
 
 
 @dataclass(frozen=True)
@@ -55,7 +117,7 @@ async def get_name_puzzle_conditions(spend_bundle: SpendBundle) -> Tuple[Optiona
     program = best_solution_program(spend_bundle)
     try:
         sexp = clvm.eval_f(clvm.eval_f, program, [])
-    except clvm.EvalError.EvalError:
+    except clvm.EvalError:
         breakpoint()
         return Err.INVALID_COIN_SOLUTION, None
 
@@ -76,7 +138,7 @@ async def get_name_puzzle_conditions(spend_bundle: SpendBundle) -> Tuple[Optiona
             error, conditions_dict = conditions_dict_for_solution(puzzle_solution_program)
             if error:
                 return error, None
-        except clvm.EvalError.EvalError:
+        except clvm.EvalError:
             return Err.INVALID_COIN_SOLUTION, None
         npc: NPC = NPC(coin_name, puzzle_hash, conditions_dict)
         npc_list.append(npc)
@@ -101,8 +163,8 @@ class Mempool:
         self.unspent_store = unspent_store
 
     # TODO implement creating block from mempool
-    # TODO Maximize transaction fees, handle conflicting transactions
     # TODO Aggregate all SpendBundles for the tip and return only one
+    # TODO 6000 cost units
     async def create_bundle_for_tip(self, header_block: HeaderBlock) -> Optional[SpendBundle]:
         """
         Returns aggregated spendbundle that can be used for creating new block
@@ -113,15 +175,13 @@ class Mempool:
         self.allSeen[new_spend.name()] = new_spend.name()
 
         # Calculate the cost and fees
-        cost: uint32 = new_spend.get_signature_count()
-        fees: uint32 = new_spend.fees()
-        fees_per_cost = fees / cost
-
-        # TODO if pools is not full accept even without fee
-        if fees == 0:
-            return False, Err.INVALID_FEE_NO_FEE
-        if fees_per_cost < 1:
-            return False, Err.INVALID_FEE_LOW_FEE
+        cost = new_spend.get_signature_count()
+        fees = new_spend.fees()
+        # TODO Cost is hack, currently it just counts number of signatures (count can be 0)
+        # TODO Remove when real cost function is implemented
+        if cost == 0:
+            return False, Err.UNKNOWN
+        fees_per_cost: double = fees / cost
 
         # npc contains names of the coins removed, puzzle_hashes and their spend conditions
         fail_reason, npc_list = get_name_puzzle_conditions(new_spend)
@@ -145,14 +205,42 @@ class Mempool:
 
         # Spend might be valid for on pool but not for others
         added_count = 0
+        errors: List[Err] = []
         for pool in self.mempools:
+            # Check if more is created than spent
+            if fees < 0:
+                errors.append(Err.MINTING_COIN)
+                continue
+            # If pool is at capacity check the fee, if not then accept even without the fee
+            if pool.full_capacity():
+                if fees == 0:
+                    errors.append(Err.INVALID_FEE_NO_FEE)
+                    continue
+                if fees_per_cost < pool.get_min_fee():
+                    errors.append(Err.INVALID_FEE_LOW_FEE)
+                    continue
 
             # Check removals against UnspentDB + DiffStore + Mempool + SpendBundle
-            # TODO Keep track of Mempool conflicts
-            # TODO Use this information later when constructing a block
+            # Use this information later when constructing a block
             fail_reason, unspents, conflicts = await self.check_removals(new_spend.additions(), new_spend.removals(),
                                                                          pool)
-            if fail_reason:
+            # If there is a mempool conflict check if this spendbundle has a higher fee per cost than all others
+            tmp_error: Optional[Err] = None
+            conflicting_pool_items: Dict[bytes32: MempoolItem] = {}
+            if fail_reason is Err.MEMPOOL_CONFLICT:
+                for conflicting in conflicts:
+                    sb: MempoolItem = pool.removals[conflicting.name()]
+                    conflicting_pool_items[sb.name] = sb
+                for item in conflicting_pool_items.values():
+                    if item.fee_per_cost >= fees_per_cost:
+                        tmp_error = Err.MEMPOOL_CONFLICT
+                        break
+            elif fail_reason:
+                errors.append(fail_reason)
+                continue
+
+            if tmp_error:
+                errors.append(tmp_error)
                 continue
 
             # Check that the revealed removal puzzles actually match the puzzle hash
@@ -163,7 +251,7 @@ class Mempool:
 
             # Verify conditions, create hash_key list for aggsig check
             hash_key_pairs = []
-            error = None
+            error: Optional[Err] = None
             for npc in npc_list:
                 unspent: Unspent = unspents[npc.coin_name]
                 error = self.check_conditions_dict(unspent, new_spend, npc.condition_dict, pool)
@@ -171,18 +259,21 @@ class Mempool:
                     break
                 hash_key_pairs.extend(hash_key_pairs_for_conditions_dict(npc.condition_dict))
             if error:
+                errors.append(error)
                 continue
 
             # Verify aggregated signature
             if not new_spend.aggregated_signature.validate(hash_key_pairs):
                 return False, Err.BAD_AGGREGATE_SIGNATURE
 
-            pool.spends[new_spend.name()] = new_spend
-            for add in additions:
-                pool.additions[add.name()] = new_spend
-            for key in removals_dic.keys():
-                pool.removals[key] = new_spend
-            # TODO mark conflicting
+            # Remove all conflicting Coins and SpendBundles
+            if fail_reason:
+                item: MempoolItem
+                for item in conflicting_pool_items.values():
+                    pool.remove_spend(item)
+
+            new_item = MempoolItem(new_spend, fees_per_cost, uint64(fees), cost)
+            pool.add_to_pool(new_item, additions, removals_dic)
             added_count += 1
 
         return added_count > 0, None
@@ -190,44 +281,40 @@ class Mempool:
     async def check_removals(self, additions: List[Coin], removals: List[Coin],
                              mempool: Pool) -> Tuple[Optional[Err], Dict[bytes32, Unspent], Optional[List[Coin]]]:
         """
-        This function checks for double spends and unknown spends, if it fails returns why
-        0: Checks for double spend inside same spend_bundle
-        1: Checks if removed coin is created in spend_bundle (For ephemeral coins)
-        2: Checks we have it in the unspent_store
-        3: Checks if it's been spent already
-        4: Checks if there's mempool conflict
-        5: If coins can be spent return list of unspents as we see them in local storage
-        Returns Error (if any), Dict of Unspents, List of coins with conflict error (if any any)
+        This function checks for double spends, unknown spends and conflicting transactions in mempool.
+        Returns Error (if any), dictionary of Unspents, list of coins with conflict errors (if any any).
         """
         removals_counter: Dict[CoinName, int] = {}
         unspents: Dict[bytes32, Unspent] = {}
         conflicts: List[Coin] = []
         for removal in removals:
-            # 0:
+            # 0. Checks for double spend inside same spend_bundle
             if not removals_counter[removal.name()]:
                 removals_counter[removal.name()] = 1
             else:
                 return Err.DOUBLE_SPEND, {}, None
-            # 1:
+            # 1. Checks if removed coin is created in spend_bundle (For ephemeral coins)
             if removal in additions:
                 # Setting ephemeral coin confirmed index to current + 1
+                if removal.name() in unspents:
+                    return Err.DOUBLE_SPEND, {}, None
                 unspents[removal.name()] = Unspent(removal, mempool.header_block.height + 1, 0, 0)
                 continue
-            # 2:
+            # 2. Checks we have it in the unspent_store
             unspent: Optional[Unspent] = await self.unspent_store.get_unspent(removal.name(), mempool.header_block)
             if unspent is None:
                 return Err.UNKNOWN_UNSPENT, {}, None
-            # 3:
+            # 3. Checks if it's been spent already
             if unspent.spent == 1:
                 return Err.DOUBLE_SPEND, {}, None
-            # 4:
+            # 4. Checks if there's a mempool conflict
             if removal.name() in mempool.removals:
                 conflicts.append(removal)
 
             unspents[unspent.coin.name()] = unspent
         if len(conflicts) > 0:
             return Err.MEMPOOL_CONFLICT, unspents, conflicts
-        # 5:
+        # 5. If coins can be spent return list of unspents as we see them in local storage
         return None, unspents, None
 
     async def seen(self, bundle_hash: bytes32) -> bool:
