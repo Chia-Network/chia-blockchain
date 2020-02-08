663045d501d2c5468ecc2f96fbcec6e76df3c6fa
Yostra <straya@chia.net>
2020-02-08 00:45:04 -0800

    rename and return false if same block

--- src/blockchain.py
+++ src/blockchain.py
@@ -827,11 +827,11 @@ class Blockchain:
         there is one block per height before the LCA (and use the height_to_hash dict).
         """
         cur: List[Header] = self.tips[:]
-        lca_tmp: Optional[Header]
+        old_lca: Optional[Header]
         try:
-            lca_tmp = self.lca_block
+            old_lca = self.lca_block
         except AttributeError:
-            lca_tmp = None
+            old_lca = None
         while any(b.header_hash != cur[0].header_hash for b in cur):
             heights = [b.height for b in cur]
             i = heights.index(max(heights))
@@ -842,18 +842,17 @@ class Blockchain:
             self._reconsider_heights(self.lca_block, cur[0])
         self.lca_block = cur[0]
 
-        if lca_tmp is None:
+        if old_lca is None:
             full: Optional[FullBlock] = await self.store.get_block(
                 self.lca_block.header_hash
             )
-            if full is None:
-                return
+            assert full is not None
             await self.unspent_store.new_lca(full)
             await self.create_diffs_for_tips(self.lca_block)
         # If LCA changed update the unspent store
-        elif lca_tmp.header_hash != self.lca_block.header_hash:
-            if self.lca_block.height < lca_tmp.height:
-                if self.is_descendant(lca_tmp, self.lca_block):
+        elif old_lca.header_hash != self.lca_block.header_hash:
+            if self.lca_block.height < old_lca.height:
+                if self.is_descendant(old_lca, self.lca_block):
                     # new LCA is lower height than the new LCA (linear REORG)
                     await self.unspent_store.rollback_lca_to_block(
                         self.lca_block.height
@@ -864,7 +863,7 @@ class Blockchain:
                     await self.create_diffs_for_tips(self.lca_block)
                 else:
                     # New LCA is lower height but not the a parent of old LCA (Reorg)
-                    fork_h = self.find_fork_for_lca(lca_tmp)
+                    fork_h = self.find_fork_for_lca(old_lca)
                     # Rollback to fork
                     await self.unspent_store.rollback_lca_to_block(fork_h)
                     # Nuke DiffStore
@@ -875,8 +874,8 @@ class Blockchain:
                     await self._from_fork_to_lca(fork_head, self.lca_block)
                     # Create DiffStore
                     await self.create_diffs_for_tips(self.lca_block)
-            if self.lca_block.height >= lca_tmp.height:
-                if self.lca_block.prev_header_hash == lca_tmp.header_hash:
+            if self.lca_block.height >= old_lca.height:
+                if self.lca_block.prev_header_hash == old_lca.header_hash:
                     # New LCA is a child of the old one, just add it
                     full = await self.store.get_block(self.lca_block.header_hash)
                     if full is None:
@@ -887,16 +886,16 @@ class Blockchain:
                     # Create DiffStore
                     await self.create_diffs_for_tips(self.lca_block)
                 else:
-                    if self.is_descendant(self.lca_block, lca_tmp):
+                    if self.is_descendant(self.lca_block, old_lca):
                         # Add blocks between old and new lca block
-                        await self._from_fork_to_lca(lca_tmp, self.lca_block)
+                        await self._from_fork_to_lca(old_lca, self.lca_block)
                         # Nuke DiffStore
                         self.unspent_store.nuke_diffs()
                         # Create DiffStore
                         await self.create_diffs_for_tips(self.lca_block)
                     else:
                         # Find Fork
-                        fork_h = self.find_fork_for_lca(lca_tmp)
+                        fork_h = self.find_fork_for_lca(old_lca)
                         # Rollback to fork_point
                         await self.unspent_store.rollback_lca_to_block(fork_h)
                         # Add blocks from fork_point to new_lca
@@ -928,6 +927,9 @@ class Blockchain:
 
     def is_descendant(self, child: Header, maybe_parent: Header) -> bool:
         """ Goes backward from potential child until it reaches potential parent or genesis"""
+        if child.header_hash == maybe_parent.header_hash:
+            return False
+
         current = child
 
         while current.header_hash != self.genesis.header_hash:
