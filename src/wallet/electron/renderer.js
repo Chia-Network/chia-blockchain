02d38bb4bd47f3b08f6f664fa6d9464ad8328107
Yostra <straya@chia.net>
2020-02-17 11:14:24 -0800

    get puzzlehash on start

--- src/wallet/electron/renderer.js
+++ src/wallet/electron/renderer.js
@@ -43,3 +43,37 @@ copy.addEventListener("click", () => {
     document.execCommand("copy");
 })
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function get_new_puzzlehash() {
+    //wait for wallet.py to start up
+    await sleep(300)
+    jquery.ajax({
+        type: 'POST',
+        url: 'http://127.0.0.1:9256/get_next_puzzle_hash',
+        dataType: 'json'
+    })
+    .done(function(response) {
+    // Make sure that the formMessages div has the 'success' class.
+        console.log(response)
+        let puzzle_holder = document.querySelector("#puzzle_holder")
+        puzzle_holder.value = response["puzzlehash"]
+        QRCode.toCanvas(canvas, response["puzzlehash"], function (error) {
+        if (error) console.error(error)
+        console.log('success!');
+        })
+    })
+    .fail(function(data) {
+        // Make sure that the formMessages div has the 'error' class.
+        console.log(data)
+
+        get_new_puzzlehash()
+    });
+}
+
+get_new_puzzlehash()
+
