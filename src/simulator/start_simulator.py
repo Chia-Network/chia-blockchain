4fa9a3372322bf6f9a4f87393eef6b0d852f5355
Yostra <straya@chia.net>
2020-03-09 15:36:38 -0700

    start simulator

---
+++ src/simulator/start_simulator.py
@@ -0,0 +1,125 @@
+import asyncio
+import logging
+import logging.config
+import signal
+from pathlib import Path
+from typing import Dict, Any
+
+from src.simulator.full_node_simulator import FullNodeSimulator
+from tests.block_tools import BlockTools
+
+try:
+    import uvloop
+except ImportError:
+    uvloop = None
+
+from src.full_node.blockchain import Blockchain
+from src.consensus.constants import constants
+from src.full_node.store import FullNodeStore
+from src.rpc.rpc_server import start_rpc_server
+from src.full_node.mempool_manager import MempoolManager
+from src.server.server import ChiaServer
+from src.server.connection import NodeType
+from src.types.full_block import FullBlock
+from src.full_node.coin_store import CoinStore
+from src.util.logging import initialize_logging
+from src.util.config import load_config_cli
+from setproctitle import setproctitle
+
+test_constants: Dict[str, Any] = {
+    "DIFFICULTY_STARTING": 5,
+    "DISCRIMINANT_SIZE_BITS": 16,
+    "BLOCK_TIME_TARGET": 10,
+    "MIN_BLOCK_TIME": 2,
+    "DIFFICULTY_FACTOR": 3,
+    "DIFFICULTY_EPOCH": 12,  # The number of blocks per epoch
+    "DIFFICULTY_WARP_FACTOR": 4,  # DELAY divides EPOCH in order to warp efficiently.
+    "DIFFICULTY_DELAY": 3,  # EPOCH / WARP_FACTOR
+    "VDF_IPS_STARTING": 50,
+}
+
+
+bt = BlockTools()
+
+genesis = bt.create_genesis_block(test_constants, bytes([0] * 32), b"0")
+test_constants["GENESIS_BLOCK"] = bytes(genesis)
+
+
+async def main():
+    config = load_config_cli("config.yaml", "full_node")
+    setproctitle("chia_full_node")
+    initialize_logging("FullNode %(name)-23s", config["logging"])
+
+    log = logging.getLogger(__name__)
+    server_closed = False
+
+    db_path = Path(config["database_path"])
+
+    # Create the store (DB) and full node instance
+    store = await FullNodeStore.create(db_path)
+    await store._clear_database()
+
+    genesis: FullBlock = FullBlock.from_bytes(test_constants["GENESIS_BLOCK"])
+    await store.add_block(genesis)
+    unspent_store = await CoinStore.create(db_path)
+
+    log.info("Initializing blockchain from disk")
+    blockchain = await Blockchain.create(unspent_store, store, test_constants)
+
+    mempool_manager = MempoolManager(unspent_store, test_constants)
+    await mempool_manager.new_tips(await blockchain.get_full_tips())
+
+    full_node = FullNodeSimulator(store, blockchain, config, mempool_manager, unspent_store, override_constants=test_constants)
+
+    # Starts the full node server (which full nodes can connect to)
+    server = ChiaServer(config["port"], full_node, NodeType.FULL_NODE)
+    full_node._set_server(server)
+    _ = await server.start_server(config["host"], full_node._on_connect)
+    rpc_cleanup = None
+
+    def master_close_cb():
+        nonlocal server_closed
+        if not server_closed:
+            # Called by the UI, when node is closed, or when a signal is sent
+            log.info("Closing all connections, and server...")
+            full_node._shutdown()
+            server.close_all()
+            server_closed = True
+
+    if config["start_rpc_server"]:
+        # Starts the RPC server
+        rpc_cleanup = await start_rpc_server(
+            full_node, master_close_cb, config["rpc_port"]
+        )
+
+    asyncio.get_running_loop().add_signal_handler(signal.SIGINT, master_close_cb)
+    asyncio.get_running_loop().add_signal_handler(signal.SIGTERM, master_close_cb)
+
+    full_node._start_bg_tasks()
+
+    log.info("Waiting to connect to some peers...")
+    await asyncio.sleep(3)
+    log.info(f"Connected to {len(server.global_connections.get_connections())} peers.")
+
+    # Awaits for server and all connections to close
+    await server.await_closed()
+    log.info("Closed all node servers.")
+
+    # Waits for the rpc server to close
+    if rpc_cleanup is not None:
+        await rpc_cleanup()
+    log.info("Closed RPC server.")
+
+    await store.close()
+    log.info("Closed store.")
+
+    await unspent_store.close()
+    log.info("Closed unspent store.")
+
+    await asyncio.get_running_loop().shutdown_asyncgens()
+    log.info("Node fully closed.")
+
+
+if uvloop is not None:
+    uvloop.install()
+asyncio.run(main())
