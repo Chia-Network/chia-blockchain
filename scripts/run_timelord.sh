bf6f0e4a7690ac0f3ef07a347f58cc6840a80477
Gene Hoffman <hoffmang@hoffmang.com>
2020-03-22 22:58:42 -0700

    Remove UI in run_timelord

--- scripts/run_timelord.sh
+++ scripts/run_timelord.sh
@@ -6,6 +6,5 @@
 _run_bg_cmd python -m src.server.start_timelord
 _run_bg_cmd python -m src.timelord_launcher
 _run_bg_cmd python -m src.server.start_full_node --port=8444 --connect_to_timelord=True --rpc_port=8555
-_run_bg_cmd python -m src.ui.start_ui --port=8222 --rpc_port=8555
 
 wait
