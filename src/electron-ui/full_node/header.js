c363b4183e80a2c36e65708ccae578a7afac1f4c
Mariano Sorgente <sorgente711@gmail.com>
2020-03-26 00:58:59 +0900

    Header hashing and display LCA and tips

--- src/electron-ui/full_node/header.js
+++ src/electron-ui/full_node/header.js
@@ -1,31 +1,36 @@
-function hash_header(header) {
-    function toByteArray(buf, hexString) {
-        hexString = hexString.slice(2);
-        for (var i = 0; i < hexString.length; i += 2) {
-            buf.push(parseInt(hexString.substr(i, 2), 16));
-        }
-        return buf;
-    }
+const { big_int_to_array, hex_to_array, arr_to_hex } = require("../utils");
 
-    function buf2hex(buffer) { // buffer is an ArrayBuffer
-        return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-    }
+async function hash_header(header) {
+    var buf = big_int_to_array(BigInt(header.data.height), 4);
+    buf = buf.concat(hex_to_array(header.data.prev_header_hash));
+    buf = buf.concat(big_int_to_array(BigInt(header.data.timestamp), 8))
+    buf = buf.concat(hex_to_array(header.data.filter_hash));
+    buf = buf.concat(hex_to_array(header.data.proof_of_space_hash));
+    buf = buf.concat(big_int_to_array(BigInt(header.data.weight), 16))
+    buf = buf.concat(big_int_to_array(BigInt(header.data.total_iters), 8))
+    buf = buf.concat(hex_to_array(header.data.additions_root));
+    buf = buf.concat(hex_to_array(header.data.removals_root));
+    buf = buf.concat(hex_to_array(header.data.coinbase.parent_coin_info));
+    buf = buf.concat(hex_to_array(header.data.coinbase.puzzle_hash));
+    buf = buf.concat(big_int_to_array(BigInt(header.data.coinbase.amount), 8))
+    buf = buf.concat(hex_to_array(header.data.coinbase_signature.sig));
+    buf = buf.concat(hex_to_array(header.data.fees_coin.parent_coin_info));
+    buf = buf.concat(hex_to_array(header.data.fees_coin.puzzle_hash));
+    buf = buf.concat(big_int_to_array(BigInt(header.data.fees_coin.amount), 8));
 
-    var buf = [];
-    buf = toByteArray(buf, header.data.prev_header_hash);
-    var ts = BigInt(header.data.timestamp);
-    buf.push(0, 0, 0, 0, 0xff & (ts >> 24), 0xff & (ts >> 16), 0xff & (ts >> 8), 0xff & ts);
-    buf = toByteArray(buf, jsn.header.data.filter_hash);
-    buf = toByteArray(buf, jsn.header.data.proof_of_space_hash);
-    buf = toByteArray(buf, jsn.header.data.body_hash);
-    buf = toByteArray(buf, jsn.header.data.extension_data);
-    buf = toByteArray(buf, jsn.header.harvester_signature);
+    // TODO: handle no aggsig
+    let agg_sig_bytes = hex_to_array(header.data.aggregated_signature.sig);
+    buf.push(1);
+    buf = buf.concat(agg_sig_bytes);
+    buf = buf.concat(big_int_to_array(BigInt(header.data.cost), 8))
+    buf = buf.concat(hex_to_array(header.data.extension_data));
+    buf = buf.concat(hex_to_array(header.data.generator_hash));
+    buf = buf.concat(hex_to_array(header.harvester_signature));
 
-    return window.crypto.subtle.digest("SHA-256", new Uint8Array(buf))
-        .then(function(hash) {
-            hashed_blocks[index] = buf2hex(hash);
-            return makeHash(index + 1, added_blocks);
-        });
+    let hash = await window.crypto.subtle.digest("SHA-256", new Uint8Array(buf));
+    return arr_to_hex(hash);
 }
 
-module.exports = hash_header
+module.exports = {
+    hash_header,
+}
