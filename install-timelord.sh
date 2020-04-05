4c600eba98631ee9a0c0f34ff43d376624653e76
Gene Hoffman <hoffmang@hoffmang.com>
2020-04-05 14:52:21 -0700

    Correct dpkg if statement

--- install-timelord.sh
+++ install-timelord.sh
@@ -10,11 +10,10 @@ else
   if [ -e venv/bin/python ]
   then
     echo "installing chiavdf from source"
-    #if [ `uname` = "Linux" ] && type apt-get]; then
     if [ `uname` = "Linux" ] && type apt-get; then
       echo "Found Ubuntu"
-      if [ ! dpkg -s cmake >/dev/null 2>&1 ]; then
-        echo "Cmake dependencies not found. Installing them."
+      if ! dpkg -s cmake >/dev/null 2>&1; then
+        echo "Cmake requirments not found. Installing them."
         sudo apt-get install cmake libgmp-dev libboost-all-dev -y
       fi
     fi
