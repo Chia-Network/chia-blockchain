82341e8358f5e53b3b52de378015eac0f3856833
Don Kackman <dkackman@gmail.com>
2020-04-05 00:58:30 -0500

    add WiX minimal UI

--- electron-ui/bundle-win32.ps1
+++ electron-ui/bundle-win32.ps1
@@ -2,7 +2,7 @@
 $env:Path += ";C:\Program Files (x86)\WiX Toolset v3.11\bin"
 $env:exename = "chia-wallet" # if you update this make sure to update .gitignore
 $env:electronpackagerdir = $env:exename + "-win32-x64"
-$env:version = "0.1.6"
+$env:version = "0.1.9"
 
 # remove any exisitng outputs
 Write-Host "Cleaning any previous outputs"
@@ -13,16 +13,15 @@ Remove-Item *.msi -Force -ErrorAction Ignore
 Remove-Item electron-packager-files.wxs -Force -ErrorAction Ignore
 
 # package up the electron stuff and sources
+Write-Host "Packaging electron files"
 electron-packager src $env:exename --platform=win32
 
 # compile the installer
 Write-Host "Compiling installer"
-
 # this generates electron-packager-files.wxs from the contents of the electron packager folder
 heat dir $env:electronpackagerdir -cg ChiaFiles -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -out electron-packager-files.wxs
-
 candle electron-packager-files.wxs chia.wxs
 
 # link the installer
 Write-Host "Linking Windows Installer database"
-light electron-packager-files.wixobj chia.wixobj -b $env:electronpackagerdir -o Chia.msi
+light -ext WixUIExtension electron-packager-files.wixobj chia.wixobj -b $env:electronpackagerdir -o Chia.msi
