228ee6491681600f06e4d22abcd2a59aa5632129
Gene Hoffman <hoffmang@hoffmang.com>
2020-04-05 17:05:11 -0700

    check existing logic

--- install-timelord.sh
+++ install-timelord.sh
@@ -1,7 +1,7 @@
 
 THE_PATH=`python -c 'import pkg_resources; print( pkg_resources.get_distribution("chiavdf").location)' 2> /dev/null`/vdf_client
 CHIAVDF_VERSION=`python -c 'from setup import dependencies; t = [_ for _ in dependencies if _.startswith("chiavdf")][0]; print(t)'`
-UBUNTU_BUILD_REQUIREMENTS=("cmake" "libgmp-dev" "libboost-python-dev" "libbost-system-dev")
+#UBUNTU_BUILD_REQUIREMENTS=("cmake" "libgmp-dev" "libboost-python-dev" "libbost-system-dev")
 
 echo "This script assumes it is run from the chia venv - '. ./activate' before running."
 
@@ -17,12 +17,12 @@ else
     if [ `uname` = "Linux" ] && type apt-get;
     then
       echo "Found Ubuntu/Debian"
-      for Packages in "{$UBUNTU_BUILD_REQUIREMENTS[@]}"; do
-        if ! dpkg -s $Packages >/dev/null 2>&1; then
-          echo "Installing $Packages."
-          sudo apt-get install $Packages -y
-        fi
-      done
+      #for Packages in "{$UBUNTU_BUILD_REQUIREMENTS[@]}"; do
+        #if ! dpkg -s $Packages >/dev/null 2>&1; then
+          #echo "Installing $Packages."
+          #sudo apt-get install $Packages -y
+        #fi
+      #done
     fi
     echo venv/bin/python -m pip install --force --no-binary chiavdf $CHIAVDF_VERSION
     venv/bin/python -m pip install --force --no-binary chiavdf $CHIAVDF_VERSION
