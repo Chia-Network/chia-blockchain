ec56d4620eabbd36b44383db2eb2ee4f23640168
Gene Hoffman <hoffmang@hoffmang.com>
2020-04-05 17:16:42 -0700

    move back into if

--- install-timelord.sh
+++ install-timelord.sh
@@ -1,9 +1,8 @@
 
 THE_PATH=`python -c 'import pkg_resources; print( pkg_resources.get_distribution("chiavdf").location)' 2> /dev/null`/vdf_client
 CHIAVDF_VERSION=`python -c 'from setup import dependencies; t = [_ for _ in dependencies if _.startswith("chiavdf")][0]; print(t)'`
-UBUNTU_BUILD_REQUIREMENTS=(cmake libgmp-dev libboost-python-dev libbost-system-dev)
 
-echo "This script assumes it is run from the chia venv - '. ./activate' before running."
+#echo "This script assumes it is run from the chia venv - '. ./activate' before running."
 
 if [ -e $THE_PATH ]
 then
