451b3711ccde2efd000f84f9a9a321f255366cff
Yostra <straya@chia.net>
2020-04-07 13:15:38 -0700

    future assert fee condition

--- src/types/condition_opcodes.py
+++ src/types/condition_opcodes.py
@@ -2,6 +2,7 @@ import enum
 
 
 class ConditionOpcode(bytes, enum.Enum):
+    UNKNOWN = bytes([49])
     AGG_SIG = bytes([50])
     CREATE_COIN = bytes([51])
     ASSERT_COIN_CONSUMED = bytes([52])
@@ -10,3 +11,4 @@ class ConditionOpcode(bytes, enum.Enum):
     ASSERT_BLOCK_INDEX_EXCEEDS = bytes([55])
     ASSERT_BLOCK_AGE_EXCEEDS = bytes([56])
     AGG_SIG_ME = bytes([57])
+    ASSERT_FEE = bytes([58])
