a8f302e551a2312313b68890d7a8f066bda6965d
Don Kackman <dkackman@gmail.com>
2020-04-09 13:43:44 -0500

    refinements to the windows isntaller

--- electron-wix/bundle-win32.ps1
+++ electron-wix/bundle-win32.ps1
@@ -1,8 +1,8 @@
 # add wix to path
-$env:Path += ";C:\Program Files (x86)\WiX Toolset v3.11\bin"
+$env:Path += ";${env:ProgramFiles(x86)}\WiX Toolset v3.11\bin"
 $env:exename = "chia-wallet" # if you update this make sure to update .gitignore
 $env:electronpackagerdir = $env:exename + "-win32-x64"
-$env:version = "0.1.2"
+$env:version = "0.1.5"
 
 # remove any exisitng outputs
 Write-Host "Cleaning any previous outputs"
@@ -20,9 +20,8 @@ Write-Host "Compiling installer"
 
 # this generates package-files.wxs from the contents of the electron packager folder
 heat.exe dir $env:electronpackagerdir -cg ChiaFiles -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -out electron-packager-files.wxs
-
 candle electron-packager-files.wxs chia.wxs
 
 # link the installer
 Write-Host "Linking Windows Installer database"
-light electron-packager-files.wixobj chia.wixobj -b $env:electronpackagerdir -o Chia-$env:version.msi
+light -ext WixUIExtension electron-packager-files.wixobj chia.wixobj -b $env:electronpackagerdir -o Chia.msi
