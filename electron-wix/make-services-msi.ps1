e4b317a08f284ab5e13e1ea592e9abbda7518c60
Don Kackman <dkackman@gmail.com>
2020-04-15 09:28:54 -0500

    more changes to support service bundling

--- electron-wix/make-services-msi.ps1
+++ electron-wix/make-services-msi.ps1
@@ -9,14 +9,14 @@ catch {
 }
 
 # package up the electron stuff and sources
-electron-packager ../electron-ui $env:walletExeName --platform=win32 --arch=x64 --icon="$env:resourceDir\icon.ico" --app-version="$env:version" --win32metadata.CompanyName="Chia Network" --win32metadata.ProductName="Chia Wallet" --app-copyright="Chia Network 2020"
+electron-packager ../electron-ui $env:exename --platform=win32 --arch=x64 --icon="$env:resourceDir\icon.ico" --app-version="$env:version" --win32metadata.CompanyName="Chia Network" --win32metadata.ProductName="Chia Wallet" --app-copyright="Chia Network 2020"
 if ($LastExitCode) { exit $LastExitCode }
 
 $tempName = "electron-packager-files"
-$msiName = "$env:walletExeName-$env:version.msi"
+$msiName = "$env:exename-$env:version.msi"
 
 # this generates package-files.wxs from the contents of the electron packager folder
-heat dir $electronPackagerDir -cg ChiaFiles -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -out "$buildDir\$tempName.wxs"
+heat dir $electronpackagerdir -cg ChiaFiles -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -out "$buildDir\$tempName.wxs"
 if ($LastExitCode) { exit $LastExitCode }
 
 # compile the installer
@@ -24,7 +24,7 @@ candle "$buildDir\$tempName.wxs" "$sourceDir\wallet-msi.wxs" -o "$buildDir\"
 if ($LastExitCode) { exit $LastExitCode }
 
 # link the installer
-light -ext WixUIExtension "$buildDir\$tempName.wixobj" "$buildDir\wallet-msi.wixobj" -b $electronPackagerDir -o "$buildDir\$msiName" -sw1076
+light -ext WixUIExtension "$buildDir\$tempName.wixobj" "$buildDir\wallet-msi.wixobj" -b $electronpackagerdir -o "$buildDir\$msiName" -sw1076
 if ($LastExitCode) { exit $LastExitCode }
 
 Write-Host "Successfully built $msiName"
