3492828ae3a84ba278c6e90bf73440aa19e7d992
Don Kackman <dkackman@gmail.com>
2020-04-14 20:37:56 -0500

    working version

---
+++ electron-wix/clean.ps1
@@ -0,0 +1,15 @@
+# Include required files
+$ScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+try {
+    . ("$ScriptDirectory\config.ps1")
+}
+catch {
+    Write-Host "Error while loading supporting PowerShell Scripts" 
+    exit 1
+}
+
+# remove any exisitng outputs
+Write-Host "Cleaning any previous outputs"
+Remove-Item $electronpackagerdir -Recurse -Force -ErrorAction Ignore
+Remove-Item "$buildDir" -Recurse -Force -ErrorAction Ignore
+New-Item -ItemType Directory -Path "$buildDir"
---
+++ electron-wix/make-bundle.ps1
@@ -0,0 +1,17 @@
+# Include required files
+$ScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+try {
+    . ("$ScriptDirectory\config.ps1")
+}
+catch {
+    Write-Host "Error while loading supporting PowerShell Scripts" 
+    exit 1
+}
+
+candle "$sourceDir\bundle.wxs" "$sourceDir\python-package.wxs" "$sourceDir\wallet-package.wxs" -o "$buildDir\" -ext WixBalExtension -ext WixUtilExtension
+if ($LastExitCode) { exit $LastExitCode }
+
+light "$buildDir\bundle.wixobj" "$buildDir\python-package.wixobj" "$buildDir\wallet-package.wixobj" -o "$buildDir\chia-bundle" -ext WixBalExtension -ext WixUtilExtension
+if ($LastExitCode) { exit $LastExitCode }
+
+Write-Host "Successfully built chia-bundle.exe"
