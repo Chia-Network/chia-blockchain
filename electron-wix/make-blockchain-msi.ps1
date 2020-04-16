8ec5066508ddd7eefa8f3cb523faf9969d71c341
Don Kackman <dkackman@gmail.com>
2020-04-15 22:20:33 -0500

    set blockchain pacakge arch=x64

--- electron-wix/make-blockchain-msi.ps1
+++ electron-wix/make-blockchain-msi.ps1
@@ -13,15 +13,15 @@ $tempName = "blockchain-files"
 $packageName = "$env:blockchainProductName-$env:version.msi"
 
 # this generates package-files.wxs from the contents of the electron packager folder
-heat dir $venvDir -cg ChiaBlockchainFiles -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -out "$buildDir\$tempName.wxs"
+heat dir $venvDir -cg ChiaBlockchainFiles -nologo -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -out "$buildDir\$tempName.wxs"
 if ($LastExitCode) { exit $LastExitCode }
 
 # compile the installer
-candle "$buildDir\$tempName.wxs" "$sourceDir\blockchain-msi.wxs" -o "$buildDir\"
+candle "$buildDir\$tempName.wxs" "$sourceDir\blockchain-msi.wxs" -nologo -arch x64 -o "$buildDir\"
 if ($LastExitCode) { exit $LastExitCode }
 
 # link the installer
-light -ext WixUIExtension "$buildDir\$tempName.wixobj" "$buildDir\blockchain-msi.wixobj" -b $venvDir -o "$buildDir\$packageName"
+light -ext WixUIExtension "$buildDir\$tempName.wixobj" "$buildDir\blockchain-msi.wixobj" -nologo -b $venvDir -o "$buildDir\$packageName"
 if ($LastExitCode) { exit $LastExitCode }
 
 Write-Host "Successfully built $packageName"
