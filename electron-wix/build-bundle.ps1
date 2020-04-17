c1faba8422ac194f1d0d9207a8923b1d4a87d172
Don Kackman <dkackman@gmail.com>
2020-04-17 07:55:55 -0500

    add MSVC 2019 prerequisite installation

--- electron-wix/build-bundle.ps1
+++ electron-wix/build-bundle.ps1
@@ -10,11 +10,11 @@ catch {
 $packageName = "chia-bundle-$env:version.exe"
 
 Write-Host "Compiling $packageName"
-candle "$sourceDir\bundle.wxs" "$sourceDir\blockchain-package.wxs" "$sourceDir\wallet-package.wxs" -nologo -o "$buildDir\" -ext WixBalExtension -ext WixUtilExtension
+candle "$sourceDir\bundle.wxs" "$sourceDir\msvc2019-package.wxs" "$sourceDir\blockchain-package.wxs" "$sourceDir\wallet-package.wxs" -nologo -o "$buildDir\" -ext WixBalExtension -ext WixUtilExtension
 if ($LastExitCode) { exit $LastExitCode }
 
 Write-Host "Building $packageName"
-light "$buildDir\bundle.wixobj" "$buildDir\blockchain-package.wixobj" "$buildDir\wallet-package.wixobj" -nologo -o "$buildDir\$packageName" -ext WixBalExtension -ext WixUtilExtension
+light "$buildDir\bundle.wixobj" "$buildDir\msvc2019-package.wixobj" "$buildDir\blockchain-package.wixobj" "$buildDir\wallet-package.wixobj" -nologo -o "$buildDir\$packageName" -sw1133 -ext WixBalExtension -ext WixUtilExtension
 if ($LastExitCode) { exit $LastExitCode }
 
 Write-Host "Successfully built $packageName"
--- electron-wix/config.ps1
+++ electron-wix/config.ps1
@@ -3,7 +3,9 @@ $env:path += ";${env:ProgramFiles(x86)}\WiX Toolset v3.11\bin" # add wix to path
 $env:walletProductName = "chia-wallet"
 $env:blockchainProductName = "chia-blockchain"
 $env:version = "0.1.5" # all packages share the same version
-$env:resourceDir = ".\resources" # bitmaps, icons etx
+$env:resourceDir = ".\resources" # bitmaps, icons etc
+$env:prereqDir = ".\prerequisites" # location for any pre-req installers
+
 $buildDir = ".\build" # where temp and final build output go
 $sourceDir = ".\src" # the location of wxs files etc
 $electronPackagerDir = $env:walletProductName + "-win32-x64" # the folder that electron-packager creates
