a78849ef501db2e05f4d8a4f1c1e1c774e18b98f
Don Kackman <dkackman@gmail.com>
2020-04-19 16:56:27 -0500

    package up the wheels

--- electron-wix/build-blockchain-msi.ps1
+++ electron-wix/build-blockchain-msi.ps1
@@ -9,24 +9,31 @@ catch {
     Write-Host $_
     exit 1
 }
-$venvDir = "..\venv"
+$blockchainDir = "$buildDir\blockchain"
 
 $tempName = "blockchain-files"
 $packageName = "$env:blockchainProductName-$env:version.msi"
 
+# copy blockchain stuff into the build dir
+New-Item -ItemType Directory -Path "$blockchainDir" -Force
+New-Item -ItemType Directory -Path "$blockchainDir\wheels" -Force
+Copy-Item ".\blockchain\install.ps1" "$blockchainDir" -Force
+Copy-Item ".\blockchain\readme.txt" "$blockchainDir" -Force
+Copy-Item ".\blockchain\*.whl" "$blockchainDir\wheels" -Force
+
 # this generates package-files.wxs from the contents of the electron packager folder
 Write-Host "Creating manifest of python environment files"
-heat dir $venvDir -cg ChiaBlockchainFiles -nologo -gg -scom -sreg -sfrag -srd -dr venvDir -out "$buildDir\$tempName.wxs"
+heat dir $blockchainDir -cg ChiaBlockchainFiles -nologo -gg -scom -sreg -sfrag -srd -dr INSTALLDIR -out "$buildDir\$tempName.wxs"
 if ($LastExitCode) { exit $LastExitCode }
 
 # compile the installer
 Write-Host "Compiling $packageName"
-candle "$buildDir\$tempName.wxs" "$sourceDir\blockchain-msi.wxs" -nologo -arch x64 -o "$buildDir\"
+candle "$buildDir\$tempName.wxs" "$sourceDir\blockchain-msi.wxs" -nologo -arch x64 -o "$buildDir\"  -ext WixUtilExtension
 if ($LastExitCode) { exit $LastExitCode }
 
 # link the installer
 Write-Host "Building $packageName"
-light -ext WixUIExtension "$buildDir\$tempName.wixobj" "$buildDir\blockchain-msi.wixobj" -nologo -b $venvDir -o "$buildDir\$packageName"
+light -ext WixUIExtension "$buildDir\$tempName.wixobj" "$buildDir\blockchain-msi.wixobj" -nologo -b $blockchainDir -o "$buildDir\$packageName"  -ext WixUtilExtension
 if ($LastExitCode) { exit $LastExitCode }
 
 Sign-Item "$buildDir\$packageName"
