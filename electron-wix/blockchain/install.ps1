6c207153c5ca1179570c7edc0be8fe68321e6c98
Don Kackman <dkackman@gmail.com>
2020-04-19 18:49:13 -0500

    drop the wheels, a readme and an install script

--- electron-wix/blockchain/install.ps1
+++ electron-wix/blockchain/install.ps1
@@ -1,9 +1,9 @@
 Remove-Item "./venv" -Recurse -Force -ErrorAction Ignore
 
 Start-Process "$env:HOMEDRIVE$env:HOMEPATH\AppData\Local\Programs\Python\Python37\python.exe" -ArgumentList "-m venv venv" -Wait
-if ($LastExitCode) { exit $LastExitCode }
 
 . .\venv\Scripts\activate.ps1
+
 pip3 install --upgrade pip
-pip install -i https://download.chia.net/simple/ miniupnpc==2.1 setproctitle==1.1.10 cbor2==5.1.0
-pip install -e .
+
+.\wheels.ps1
---
+++ electron-wix/blockchain/readme.txt
@@ -0,0 +1,2 @@
+To complete the install, open powershell, and navigate to `~\AppData\Local\Programs\Chia Network\Chia Blockchain\`
+Execute the `install.ps1` located in that folder.
