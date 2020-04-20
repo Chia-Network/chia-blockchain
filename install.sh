300b4632a048faf464d16d8340d284f326e7e724
Gene Hoffman <30377676+hoffmang9@users.noreply.github.com>
2020-04-20 12:01:43 -0700

    Add nodejs-dev - github's 18.04 runner is broken..

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ if [ `uname` = "Linux" ]; then
   #LINUX=1
   if type apt-get; then
     # Debian/Ubuntu
-    sudo apt-get install -y nodejs node-gyp npm
+    sudo apt-get install -y npm nodejs node-gyp nodejs-dev
   elif type yum && [ ! -f "/etc/redhat-release" ] && [ ! -f "/etc/centos-release" ]; then
     # AMZN 2
     echo "Installing on Amazon Linux 2"
