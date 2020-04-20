28b642a7a8993804cab0aeb053e73041f585e633
Gene Hoffman <hoffmang@hoffmang.com>
2020-04-19 22:57:55 -0700

    Find cbor2 on download.chia

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ if [ `uname` = "Linux" ]; then
   #LINUX=1
   if type apt-get; then
     # Debian/Ubuntu
-    sudo apt-get install -y npm
+    sudo apt-get install -y nodejs
   elif type yum && [ ! -f "/etc/redhat-release" ] && [ ! -f "/etc/centos-release" ]; then
     # AMZN 2
     echo "Installing on Amazon Linux 2"
