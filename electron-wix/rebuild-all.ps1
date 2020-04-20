e671af213f1cc37202077fe074ede32d327c9bcc
Don Kackman <dkackman@gmail.com>
2020-04-19 20:54:11 -0500

    copy installers to final folder with a rebuild

--- electron-wix/rebuild-all.ps1
+++ electron-wix/rebuild-all.ps1
@@ -1,5 +1,17 @@
+$ScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+try {
+    . ("$ScriptDirectory\config.ps1")
+}
+catch {
+    Write-Host "Error while loading supporting PowerShell Scripts" 
+    Write-Host $_    
+    exit 1
+}
+
 # this script cleans and builds all of the packages and bundle
 .\clean.ps1
+
+New-Item -ItemType Directory -Path "$buildDir"
 
 .\build-wallet-msi.ps1
 if ($LastExitCode) { exit $LastExitCode }
@@ -11,3 +23,7 @@ if ($LastExitCode) { exit $LastExitCode }
 if ($LastExitCode) { exit $LastExitCode }
 
 Write-Host "Succesfully built Chia installer for $env:version"
+
+New-Item -ItemType Directory -Path "$finalDir"
+Copy-Item "$buildDir\*.msi" "$finalDir\" -Force
+Copy-Item "$buildDir\*.exe" "$finalDir\" -Force
