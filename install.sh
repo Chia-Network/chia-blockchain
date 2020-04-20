d3939e318d2f3469897287e7e59a458d0395ea4a
Gene Hoffman <30377676+hoffmang9@users.noreply.github.com>
2020-04-20 11:55:28 -0700

    Add node-gyp to install.sh - test ci

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ if [ `uname` = "Linux" ]; then
   #LINUX=1
   if type apt-get; then
     # Debian/Ubuntu
-    sudo apt-get install -y nodejs npm
+    sudo apt-get install -y nodejs node-gyp npm
   elif type yum && [ ! -f "/etc/redhat-release" ] && [ ! -f "/etc/centos-release" ]; then
     # AMZN 2
     echo "Installing on Amazon Linux 2"
