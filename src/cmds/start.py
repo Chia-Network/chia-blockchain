a0fc274f2abae8178816dee88c0ff22929b86055
Richard Kiss <him@richardkiss.com>
2020-04-23 18:36:10 -0700

    Refactor to start_stop.py. Support "--restart" in `chia start`.

--- src/cmds/start.py
+++ src/cmds/start.py
@@ -1,35 +1,49 @@
 import os
-import subprocess
+import time
 from typing import List
 
-from src.util.path import mkdir
-
-SERVICES_FOR_GROUP = {
-    "all": "chia_harvester chia_timelord chia_timelord_launcher chia_farmer chia_full_node".split(),
-    "node": "chia_full_node".split(),
-    "farmer": "chia_harvester chia_farmer chia_full_node".split(),
-    "timelord": "chia_timelord chia_timelord_launcher chia_full_node".split(),
-    "wallet": ["npm run --prefix ./electron-ui start", "chia-wallet"],
-    "introducer": "chia_introducer".split(),
-}
-
-SERVICES_FOR_GROUP["wallet-gui"] = SERVICES_FOR_GROUP["wallet"][:1]
-SERVICES_FOR_GROUP["wallet-server"] = SERVICES_FOR_GROUP["wallet"][1:]
+from .start_stop import (
+    all_groups,
+    pid_path_for_service,
+    services_for_groups,
+    start_service,
+    stop_service,
+)
 
 
 def make_parser(parser):
 
     parser.add_argument(
-        "group", choices=SERVICES_FOR_GROUP.keys(), type=str, nargs="+",
+        "group", choices=all_groups(), type=str, nargs="+",
+    )
+    parser.add_argument(
+        "-r",
+        "--restart",
+        action="store_true",
+        help="Force restart of running processes",
     )
     parser.set_defaults(function=start)
 
 
 def start(args, parser):
+
     processes: List = []
-    for group in args.group:
-        for service in SERVICES_FOR_GROUP[group]:
-            processes.append(start_service(args.root_path, service))
+    for service in services_for_groups(args.group):
+        if pid_path_for_service(args.root_path, service).is_file():
+            if args.restart:
+                print("restarting")
+                stop_service(args.root_path, service)
+                while pid_path_for_service(args.root_path, service).is_file():
+                    # try to avoid race condition
+                    # this is pretty hacky
+                    time.sleep(1)
+            else:
+                print(
+                    f"{service} seems to already be running, use `-r` to force restart"
+                )
+                continue
+        process = start_service(args.root_path, service)
+        processes.append(process)
 
     try:
         for process, pid_path in processes:
@@ -37,36 +51,13 @@ def start(args, parser):
     except KeyboardInterrupt:
         for process, pid_path in processes:
             process.kill()
-    print("Chia start script finished killing servers.")
     for process, pid_path in processes:
-        process.wait()
         try:
+            process.wait()
             pid_path_killed = pid_path.with_suffix(".pid-killed")
             os.rename(pid_path, pid_path_killed)
         except Exception:
             pass
+    if len(processes) > 0:
+        print("chia start complete")
     return 0
-
-
-def pid_path_for_service(root_path, service):
-    return root_path / "run" / f"{service}.pid"
-
-
-def start_service(root_path, service):
-    # set up CHIA_ROOT
-    # invoke correct script
-    # save away PID
-
-    # we need to pass on the possibly altered CHIA_ROOT
-    os.environ["CHIA_ROOT"] = str(root_path)
-
-    process = subprocess.Popen(service, shell=True)
-    pid_path = pid_path_for_service(root_path, service)
-    try:
-        mkdir(pid_path.parent)
-        with open(pid_path, "w") as f:
-            f.write(f"{process.pid}\n")
-        print(f"wrote pid to {pid_path}")
-    except Exception:
-        print(f"can't write PID file for {process} at {pid_path}")
-    return process, pid_path
--- src/cmds/stop.py
+++ src/cmds/stop.py
@@ -1,47 +1,19 @@
-import os
-import signal
-
-from .start import pid_path_for_service, SERVICES_FOR_GROUP
+from .start_stop import all_groups, services_for_groups, stop_service
 
 
 def make_parser(parser):
 
     parser.add_argument(
-        "group", choices=SERVICES_FOR_GROUP.keys(), type=str, nargs="+",
+        "group", choices=all_groups(), type=str, nargs="+",
     )
     parser.set_defaults(function=stop)
 
 
-def find_kill_signal():
-    for signal_name in "SIGHUP CTRL_C_EVENT".split():
-        try:
-            return getattr(signal, signal_name)
-        except AttributeError:
-            pass
-    raise RuntimeError("can't find a kill signal")
-
-
 def stop(args, parser):
-    kill_signal = find_kill_signal()
-
     return_val = 0
 
-    for group in args.group:
-        for service in SERVICES_FOR_GROUP[group]:
-            if stop_service(args.root_path, service, kill_signal):
-                return_val = 1
+    for service in services_for_groups(args.group):
+        if stop_service(args.root_path, service):
+            return_val = 1
 
     return return_val
-
-
-def stop_service(root_path, service, kill_signal):
-    try:
-        pid_path = pid_path_for_service(root_path, service)
-        with open(pid_path) as f:
-            pid = int(f.readline())
-        print(f"sending signal to pid {pid:>5} for {service}")
-        os.kill(pid, kill_signal)
-    except Exception:
-        print(f"can't open PID file {pid_path} for {service}, is it running?")
-        return 1
-    return 0
