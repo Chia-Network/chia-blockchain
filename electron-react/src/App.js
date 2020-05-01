1311bd8b34ff56f6b9c60cd8a5c357e76d1e2426
Yostra <straya@chia.net>
2020-04-30 18:33:32 -0700

    add material ui

--- electron-react/src/App.js
+++ electron-react/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 
 class App extends Component {
   render() {
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
+          <h2>Welcome to React/Electron s</h2>
         </div>
         <p className="App-intro">
           Hello Electron!
