b4bb4129df8d51a46d8f7eebf647c0db32166c41
Yostra <straya@chia.net>
2020-05-03 22:50:51 -0700

    mnemonic v0

---
+++ src/cmds/keys.py
@@ -0,0 +1,106 @@
+import keyring
+from src.util.byte_types import hexstr_to_bytes
+from src.util.config import str2bool
+from src.util.keychain import generate_mnemonic, bytes_to_mnemonic, Keychain, seed_from_mnemonic
+
+
+def make_parser(parser):
+    """
+    Allows replacing keys of farmer, harvester, and pool, all default to True.
+    """
+
+    parser.add_argument(
+        "-m",
+        "--mnemonic",
+        type=str,
+        nargs=24,
+        default=None,
+        help="Enter mnemonic you want to use",
+    )
+
+    parser.add_argument(
+        "command", help='', type=str, nargs=1,
+    )
+    parser.set_defaults(function=handler)
+
+
+keychain: Keychain = Keychain.create(False)
+
+
+def generate():
+    mnemonic = generate_mnemonic()
+    mnemonics_string = mnemonic_to_string(mnemonic)
+    print("Mnemonic:")
+    print(mnemonics_string)
+
+
+def set_key(args, key_type):
+    try:
+        mnemonic = args.mnemonic
+        print(f"Adding mnemonic: {mnemonic_to_string(mnemonic)}")
+        seed = seed_from_mnemonic(mnemonic)
+
+        if key_type == "wallet":
+            keychain.set_wallet_seed(seed)
+            return
+        elif key_type == "pool":
+            keychain.set_pool_seed(seed)
+            return
+        elif key_type == "harvester":
+            keychain.set_harvested_seed(seed)
+            return
+
+        print("Invalid key type")
+    except ValueError as e:
+        print(e)
+        return
+
+def mnemonic_to_string(mnemonic):
+    mnemonics_string = ""
+
+    for i in range(0,24):
+        mnemonics_string += f"{i + 1}) {mnemonic[i]}, "
+        if (i + 1) % 6 == 0:
+            mnemonics_string += "\n"
+
+    return mnemonics_string
+
+
+def show_mnemonics(key):
+    if key == "wallet":
+        entropy = keychain.get_wallet_seed()
+    elif key == "harvester":
+        entropy = keychain.get_harvester_seed()
+    elif key == "pool":
+        entropy = keychain.get_pool_seed()
+    else:
+        raise ValueError("Wrong type of key")
+
+    mnemonic = bytes_to_mnemonic(entropy)
+    mnemonic_string = mnemonic_to_string(mnemonic)
+    print(mnemonic_string)
+
+
+def handler(args, parser):
+    command_list = ["generate", "set_wallet_key", "set_harvester_key", "set_pool_key",
+                    "show_wallet_key", "show_harvester_key", "show_pool_key"]
+
+    command = args.command[0]
+    if command not in command_list:
+        print(f"Available commands: {command_list}")
+        return 1
+
+    if command == "generate":
+        generate()
+    elif command == "set_wallet_key":
+        set_key(args, "wallet")
+    elif command == "set_harvester_key":
+        set_key(args, "harvester")
+    elif command == "set_pool_key":
+        set_key(args, "pool")
+    elif command == "show_wallet_key":
+        show_mnemonics("wallet")
+    elif command == "show_harvester_key":
+        show_mnemonics("harvester")
+    elif command == "show_pool_key":
+        show_mnemonics("pool")
---
+++ src/util/keychain.py
@@ -0,0 +1,151 @@
+import bisect
+from secrets import token_bytes
+from typing import List, Optional
+
+import keyring
+import pkg_resources
+from bitstring import BitArray
+
+from src.types.sized_bytes import bytes32
+from src.util.byte_types import hexstr_to_bytes
+from src.util.hash import std_hash
+
+
+def binary_search(a, x, lo=0, hi=None):
+    hi = hi if hi is not None else len(a)
+    pos = bisect.bisect_left(a, x, lo, hi)
+    return pos if pos != hi and a[pos] == x else -1
+
+
+def bip39_word_list() -> str:
+    return pkg_resources.resource_string("src.util.bip39", f"english.txt").decode()
+
+
+def generate_mnemonic()-> List[str]:
+    seed_bytes = token_bytes(32)
+    mnemonic = bytes_to_mnemonic(seed_bytes)
+    return mnemonic
+
+
+def bytes_to_mnemonic(seed_bytes: bytes):
+    seed_array = bytearray(seed_bytes)
+    word_list = bip39_word_list().splitlines()
+
+    checksum = bytes(std_hash(seed_bytes))
+
+    seed_array.append(checksum[0])
+    bytes_for_mnemonic = bytes(seed_array)
+    bitarray = BitArray(bytes_for_mnemonic)
+    mnemonics = []
+
+    for i in range(0,24):
+        start = i * 11
+        end = start + 11
+        bits = bitarray[start:end]
+        m_word_poition = bits.uint
+        m_word = word_list[m_word_poition]
+        mnemonics.append(m_word)
+
+    return mnemonics
+
+
+def seed_from_mnemonic(mnemonic: List[str]):
+    word_list = bip39_word_list().splitlines()
+    bit_array = BitArray()
+    for i in range(0, 24):
+        word = mnemonic[i]
+        value = binary_search(word_list, word)
+        if value == -1:
+            raise ValueError(f"{word} not in mnemonic list")
+
+        bit_array.append(BitArray(uint=value, length=11))
+
+    all_bytes = bit_array.bytes
+    entropy_bytes = all_bytes[:32]
+    checksum_bytes = all_bytes[32]
+    checksum = std_hash(entropy_bytes)
+
+    if checksum[0] != checksum_bytes:
+        raise ValueError(f"Invalid order of mnemonic words")
+
+    return entropy_bytes
+
+
+class Keychain:
+    testing: bool
+
+    @staticmethod
+    def create(testing: bool):
+        self = Keychain()
+        self.testing = testing
+        return self
+
+    def get_service(self):
+        if self.testing:
+            return "chia-test"
+        else:
+            return "chia"
+
+    def get_wallet_user(self):
+        if self.testing:
+            return "wallet-test"
+        else:
+            return "wallet"
+
+    def get_harvester_user(self):
+        if self.testing:
+            return "harvester-test"
+        else:
+            return "harvester"
+
+    def get_pool_user(self):
+        if self.testing:
+            return "pool-test"
+        else:
+            return "pool"
+
+    def set_wallet_seed(self, seed: bytes):
+        keyring.set_password(self.get_service(), self.get_wallet_user(), seed.hex())
+
+    def get_stored_entropy(self, user: str):
+        return keyring.get_password(self.get_service(), user)
+
+    def get_wallet_seed(self) -> Optional[bytes32]:
+        seed = self.get_stored_entropy(self.get_wallet_user())
+        if seed is None:
+            return None
+        return hexstr_to_bytes(seed)
+
+    def delete_all_keys(self):
+        keyring.delete_password(self.get_service(), self.get_wallet_user())
+        keyring.delete_password(self.get_service(), self.get_pool_user())
+        keyring.delete_password(self.get_service(), self.get_harvester_user())
+
+    def get_harvester_seed(self) -> Optional[bytes32]:
+        stored = self.get_stored_entropy(self.get_harvester_user())
+        if stored is not None:
+            return stored
+
+        wallet_seed = self.get_wallet_seed()
+        if wallet_seed is None:
+            return None
+        default_harvester = std_hash(wallet_seed)
+        return default_harvester
+
+    def set_harvested_seed(self, seed):
+        keyring.set_password(self.get_service(), self.get_harvester_user(), seed.hex())
+
+    def get_pool_seed(self) -> Optional[bytes32]:
+        stored = self.get_stored_entropy(self.get_pool_user())
+        if stored is not None:
+            return stored
+
+        wallet_seed = self.get_wallet_seed()
+        if wallet_seed is None:
+            return None
+
+        default_pool = std_hash(std_hash(wallet_seed))
+        return default_pool
+
+    def set_pool_seed(self, seed):
+        keyring.set_password(self.get_service(), self.get_pool_user(), seed.hex())
