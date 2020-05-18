651401d1131c6cff0eaf9280bc75d2d617d3ff8d
Yostra <straya@chia.net>
2020-05-18 15:38:56 -0700

    gitignore fail..

--- electron-react/src/dev_config.js
+++ electron-react/src/dev_config.js
@@ -1,5 +1,5 @@
 const dev_config = {
-  //redux_tool: null,
+  redux_tool: null,
   react_tool: null
   //react_tool: "/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.6.0_0",
   //redux_tool: "/Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.17.0_0"
