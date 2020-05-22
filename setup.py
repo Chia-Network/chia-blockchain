5453ea046e3c993961a625828fac3a674c5e5f7a
Gene Hoffman <30377676+hoffmang9@users.noreply.github.com>
2020-05-21 20:00:08 -0700

    install-timelord handles cmake, python3.8 - rename and clean build-ubuntu-mac.yml (#234)
    * cleanup and rename build-ubuntu-mac.yml
    * Remove vestiges of GH workaround for ubunutu/node and update Changelog
    * timelord-install now handles cmake installation on Ubuntu b4 20.04LTS
    * Bump chiavdf to fix timelord install, and bump chiapos for a fix

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ dependencies = [
     "aiosqlite==0.13.0",  # asyncio wrapper for sqlite, to store blocks
     "aiohttp==3.6.2",  # HTTP server for full node rpc
     "colorlog==4.1.0",  # Adds color to logs
-    "chiavdf==0.12.15",  # timelord and vdf verification
+    "chiavdf==0.12.16",  # timelord and vdf verification
     "chiabip158==0.14",  # bip158-style wallet filters
-    "chiapos==0.12.16",  # proof of space
+    "chiapos==0.12.17",  # proof of space
     "sortedcontainers==2.1.0",  # For maintaining sorted mempools
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
     "clvm-tools==0.1.1",  # clvm compiler tools
