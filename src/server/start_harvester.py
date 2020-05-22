2ce8e732e7723f673d9ccefd43ac226106e6c50b
Richard Kiss <him@richardkiss.com>
2020-05-22 13:33:08 -0700

    Refactor `start_harvester` into `start_service`.

--- src/server/start_harvester.py
+++ src/server/start_harvester.py
@@ -1,94 +1,44 @@
-import asyncio
-import signal
-import logging
-
-try:
-    import uvloop
-except ImportError:
-    uvloop = None
-
 from src.harvester import Harvester
 from src.server.outbound_message import NodeType
-from src.server.server import ChiaServer
 from src.types.peer_info import PeerInfo
 from src.util.config import load_config, load_config_cli
 from src.util.default_root import DEFAULT_ROOT_PATH
 from src.rpc.harvester_rpc_server import start_harvester_rpc_server
-from src.util.logging import initialize_logging
-from src.util.setproctitle import setproctitle
 
-from .reconnect_task import start_reconnect_task
+from src.server.start_service import run_service
 
 
-async def async_main():
-    root_path = DEFAULT_ROOT_PATH
-    net_config = load_config(root_path, "config.yaml")
-    config = load_config_cli(root_path, "config.yaml", "harvester")
+def service_kwargs_for_harvester(root_path=DEFAULT_ROOT_PATH):
+    service_name = "harvester"
+    config = load_config_cli(root_path, "config.yaml", service_name)
+
     try:
         plot_config = load_config(root_path, "plots.yaml")
     except FileNotFoundError:
         raise RuntimeError("Plots not generated. Run chia-create-plots")
 
-    initialize_logging("Harvester %(name)-22s", config["logging"], root_path)
-    log = logging.getLogger(__name__)
-    setproctitle("chia_harvester")
+    connect_peers = [
+        PeerInfo(config["farmer_peer"]["host"], config["farmer_peer"]["port"])
+    ]
 
-    harvester = await Harvester.create(config, plot_config, root_path)
-    ping_interval = net_config.get("ping_interval")
-    network_id = net_config.get("network_id")
-    assert ping_interval is not None
-    assert network_id is not None
-    server = ChiaServer(
-        config["port"],
-        harvester,
-        NodeType.HARVESTER,
-        ping_interval,
-        network_id,
-        DEFAULT_ROOT_PATH,
-        config,
+    api = Harvester.create(config, plot_config, root_path)
+
+    kwargs = dict(
+        root_path=root_path,
+        api=api,
+        node_type=NodeType.HARVESTER,
+        advertised_port=config["port"],
+        service_name=service_name,
+        server_listen_ports=[config["port"]],
+        connect_peers=connect_peers,
+        rpc_start_callback_port=(start_harvester_rpc_server, config["rpc_port"]),
     )
-
-    peer_info = PeerInfo(
-        config["farmer_peer"]["host"], config["farmer_peer"]["port"]
-    )
-    reconnect_task = start_reconnect_task(server, peer_info, log)
-
-    def stop_all():
-        server.close_all()
-        reconnect_task.cancel()
-
-    try:
-        asyncio.get_running_loop().add_signal_handler(signal.SIGINT, stop_all)
-        asyncio.get_running_loop().add_signal_handler(signal.SIGTERM, stop_all)
-    except NotImplementedError:
-        log.info("signal handlers unsupported")
-
-    rpc_cleanup = None
-    if config["start_rpc_server"]:
-        # Starts the RPC server
-        rpc_cleanup = await start_harvester_rpc_server(
-            harvester, server.close_all, config["rpc_port"]
-        )
-
-    harvester.set_server(server)
-    await asyncio.sleep(1)
-
-    await server.await_closed()
-    harvester._shutdown()
-    await harvester._await_shutdown()
-
-    # Waits for the rpc server to close
-    if rpc_cleanup is not None:
-        await rpc_cleanup()
-    log.info("Closed RPC server.")
-
-    log.info("Harvester fully closed.")
+    return kwargs
 
 
 def main():
-    if uvloop is not None:
-        uvloop.install()
-    asyncio.run(async_main())
+    kwargs = service_kwargs_for_harvester()
+    return run_service(**kwargs)
 
 
 if __name__ == "__main__":
