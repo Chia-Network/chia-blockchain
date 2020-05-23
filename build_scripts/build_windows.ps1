d479b6458de75156e8190dd86df3184d84494bc2
Gene Hoffman <hoffmang@hoffmang.com>
2020-05-22 23:42:49 -0700

    Clean up win, mac installers

--- build_scripts/build_windows.ps1
+++ build_scripts/build_windows.ps1
@@ -2,38 +2,52 @@ cd ..
 mkdir build_scripts\win_build
 cd build_scripts\win_build
 
+Write-Output "curl miniupnpc, setprotitle";
 curl -OL --show-error --fail https://download.chia.net/simple/miniupnpc/miniupnpc-2.1-cp37-cp37m-win_amd64.whl
 curl -OL --show-error --fail https://download.chia.net/simple/setproctitle/setproctitle-1.1.10-cp37-cp37m-win_amd64.whl
-# C:\curl\curl.exe -OL --show-error --fail https://download.chia.net/simple/setproctitle/setproctitle-1.1.10-cp37-cp37m-win_amd64.whl
-# C:\curl\curl.exe -OL --show-error --fail https://download.chia.net/simple/miniupnpc/miniupnpc-2.1-cp37-cp37m-win_amd64.whl
 
-Write-Output "checkpoint 1";
+Write-Output "\n";
+Write-Output "Install pip/python prerequisites\n";
 cd ..\..
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install pep517 wheel
-pip wheel --use-pep517 --only-binary cbor2 --extra-index-url https://download.chia.net/simple/ -f . --wheel-dir=.\build_scripts\win_build .
-Write-Output "checkpoint 2";
 
-python -m venv venv
+Write-Output "\n";
+Write-Output "Build chia-blockchain wheels\n";
+pip wheel --use-pep517 --only-binary cbor2 --extra-index-url https://download.chia.net/simple/ -f . --wheel-dir=.\build_scripts\win_build .
+
+Write-Output "\n";
+Write-Output "\Create venv\n";
+python3 -m venv venv
 . .\venv\Scripts\Activate.ps1
 python -m pip install --upgrade pip
+pip install pywin32 pyinstaller
+
+Write-Output "\n";
+Write-Output "Install chia-blockchain wheels into venv with install_win.py\n";
 cd build_scripts
 python install_win.py
-Write-Output "checkpoint 3";
 
-pip install pywin32
-pip install pyinstaller
+Write-Output "\n";
+Write-Output "Use pyinstaller to create chia .exe's\n";
 pyinstaller daemon_windows.spec
-Write-Output "checkpoint 4";
 
+Write-Output "\n";
+Write-Output "Copy chia executables to electron-react/\n";
 cp -r dist/daemon ../electron-react/
 cd ../electron-react
 
-Write-Output "checkpoint 5";
-
+Write-Output "\n";
+Write-Output "Prepare Electron package\n";
 npm install --save-dev electron-winstaller
 npm install -g electron-packager
 npm install
+
+Write-Output "\n";
+Write-Output "Electron package Windows Installer\n";
 npm run build
 electron-packager . Chia --asar.unpack="**/daemon/**" --overwrite --icon=./src/assets/img/chia.ico
 node winstaller.js
+
+Write-Output "\n";
+Write-Output "Done\n";
