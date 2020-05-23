633cca7d3f4dd11998ef680d01f4498a5066802e
Gene Hoffman <30377676+hoffmang9@users.noreply.github.com>
2020-05-23 13:41:19 -0700

    Clean up installers (#239)
    * Clean up win, mac installers
    * Build Catalina DMG on GitHub Actions
    * gitignore dmg builds
    * Manage mac artifacts
    * Correct Mac artifact, temp disable azure pytest
    * Re-enable pytest on azure pipeline

--- build_scripts/build_macos.sh
+++ build_scripts/build_macos.sh
@@ -1,14 +1,26 @@
+#!/bin/bash
+echo "Installing npm and electron packagers"
 npm install electron-installer-dmg -g
 npm install electron-packager -g
+
+echo "Create dist/"
 sudo rm -rf dist
 mkdir dist
+
+echo "Create executeables with pyinstaller"
+pip install pyinstaller
 sudo pyinstaller daemon.spec
 cp -r dist/daemon ../electron-react
 cd ..
 cd electron-react
+
+echo "npm build"
 npm install
 npm run build
 electron-packager . Chia --overwrite --icon=./src/assets/img/chia.ico
 mv Chia-darwin-x64 ../build_scripts/dist/
 cd ../build_scripts
+
+echo"Create .dmg"
 electron-installer-dmg dist/Chia-darwin-x64/Chia.app Chia --overwrite
+ls -l
--- build_scripts/build_windows.ps1
+++ build_scripts/build_windows.ps1
@@ -2,38 +2,63 @@ cd ..
 mkdir build_scripts\win_build
 cd build_scripts\win_build
 
+Write-Output "   ---";
+Write-Output "curl miniupnpc, setprotitle";
+Write-Output "   ---";
 curl -OL --show-error --fail https://download.chia.net/simple/miniupnpc/miniupnpc-2.1-cp37-cp37m-win_amd64.whl
 curl -OL --show-error --fail https://download.chia.net/simple/setproctitle/setproctitle-1.1.10-cp37-cp37m-win_amd64.whl
-# C:\curl\curl.exe -OL --show-error --fail https://download.chia.net/simple/setproctitle/setproctitle-1.1.10-cp37-cp37m-win_amd64.whl
-# C:\curl\curl.exe -OL --show-error --fail https://download.chia.net/simple/miniupnpc/miniupnpc-2.1-cp37-cp37m-win_amd64.whl
 
-Write-Output "checkpoint 1";
+Write-Output "   ---";
+Write-Output "Install pip/python prerequisites";
+Write-Output "   ---";
 cd ..\..
 python -m pip install --upgrade pip
 pip install pep517 wheel
-pip wheel --use-pep517 --only-binary cbor2 --extra-index-url https://download.chia.net/simple/ -f . --wheel-dir=.\build_scripts\win_build .
-Write-Output "checkpoint 2";
 
+Write-Output "   ---";
+Write-Output "Build chia-blockchain wheels";
+Write-Output "   ---";
+pip wheel --use-pep517 --only-binary cbor2 --extra-index-url https://download.chia.net/simple/ -f . --wheel-dir=.\build_scripts\win_build .
+
+Write-Output "   ---";
+Write-Output "\Create venv - python3.7 or 3.8 is required in PATH";
+Write-Output "   ---";
 python -m venv venv
 . .\venv\Scripts\Activate.ps1
 python -m pip install --upgrade pip
+pip install pywin32 pyinstaller
+
+Write-Output "   ---";
+Write-Output "Install chia-blockchain wheels into venv with install_win.py";
+Write-Output "   ---";
 cd build_scripts
 python install_win.py
-Write-Output "checkpoint 3";
 
-pip install pywin32
-pip install pyinstaller
+Write-Output "   ---";
+Write-Output "Use pyinstaller to create chia .exe's";
+Write-Output "   ---";
 pyinstaller daemon_windows.spec
-Write-Output "checkpoint 4";
 
+Write-Output "   ---";
+Write-Output "Copy chia executables to electron-react/";
+Write-Output "   ---";
 cp -r dist/daemon ../electron-react/
 cd ../electron-react
 
-Write-Output "checkpoint 5";
-
+Write-Output "   ---";
+Write-Output "Prepare Electron package";
+Write-Output "   ---";
 npm install --save-dev electron-winstaller
 npm install -g electron-packager
 npm install
+
+Write-Output "   ---";
+Write-Output "Electron package Windows Installer";
+Write-Output "   ---";
 npm run build
 electron-packager . Chia --asar.unpack="**/daemon/**" --overwrite --icon=./src/assets/img/chia.ico
 node winstaller.js
+
+Write-Output "   ---";
+Write-Output "Windows Installer complete";
+Write-Output "   ---";
