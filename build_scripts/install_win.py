1465632a48304d01402e678e8434708244d9a536
Gene Hoffman <30377676+hoffmang9@users.noreply.github.com>
2020-05-27 01:10:48 -0700

    Simplify build_windows with pip install --no-index --find-links (#248)
    * only pip install chia-blockchain
    * pip install from local index

--- build_scripts/install_win.py
+++
@@ -1,6 +0,0 @@
-import os
-import subprocess
-import sys
-
-for filename in os.listdir('.\win_build'):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", f".\win_build\{filename}"])
