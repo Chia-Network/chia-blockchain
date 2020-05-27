c0a71692293326db2ffc632b8c24980dbef6685c
Gene Hoffman <hoffmang@hoffmang.com>
2020-05-27 16:35:28 -0700

    remove exteraneous --add-binary

--- build_scripts/build_windows.ps1
+++ build_scripts/build_windows.ps1
@@ -32,14 +32,19 @@ pip install pywin32 pyinstaller
 Write-Output "   ---";
 Write-Output "Install chia-blockchain wheels into venv with install_win.py";
 Write-Output "   ---";
-pip install --only-binary miniupnpc setproctitle
+
+Write-Output "pip install miniupnpc";
+pip install --no-index --find-links=.\win_build\ miniupnpc
+Write-Output "pip install setproctitle";
+pip install --no-index --find-links=.\win_build\ setproctitle
 cd build_scripts
+Write-Output "pip install chia-blockchain";
 pip install --no-index --find-links=.\win_build\ chia-blockchain
 
 Write-Output "   ---";
 Write-Output "Use pyinstaller to create chia .exe's";
 Write-Output "   ---";
-pyinstaller --add-binary daemon_windows.spec
+pyinstaller daemon_windows.spec
 
 Write-Output "   ---";
 Write-Output "Copy chia executables to electron-react/";
