5e81bac95e1c6ca7f891fd62f6c59154dac6f5aa
Yostra <straya@chia.net>
2020-05-30 22:05:25 -0700

    Code sign MacOS DMG, fix Mac icons, move to asar on mac install (#257)
    * osx code signing in ci
    * Install node
    * asar and icon

--- electron-react/src/electron-starter.js
+++ electron-react/src/electron-starter.js
@@ -28,7 +28,7 @@ global.sharedObj = { local_test: local_test };
  * py process
  *************************************************************/
 
-const PY_MAC_DIST_FOLDER = "../daemon";
+const PY_MAC_DIST_FOLDER = "../../app.asar.unpacked/daemon";
 const PY_WIN_DIST_FOLDER = "../../app.asar.unpacked/daemon";
 const PY_DIST_FILE = "daemon";
 const PY_FOLDER = "../src/daemon";
