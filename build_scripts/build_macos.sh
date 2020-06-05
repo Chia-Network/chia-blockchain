900a29051b932ee1d24b76dbc50a30cf14295afa
Gene Hoffman <hoffmang@hoffmang.com>
2020-06-05 15:51:27 -0700

    Move dmg to final_installer dir

--- build_scripts/build_macos.sh
+++ build_scripts/build_macos.sh
@@ -3,7 +3,7 @@ if [ ! $CHIA_INSTALLER_VERSION ]; then
   echo "No environment variable CHIA_INSTALLER_VERSION set. Using 0.0.0."
   CHIA_INSTALLER_VERSION="0.0.0"
 fi
-echo "Chia Installer Version is: $CHIA_INSTALLER_VERSION|"
+echo "Chia Installer Version is: $CHIA_INSTALLER_VERSION"
 
 echo "Installing npm and electron packagers"
 npm install electron-installer-dmg -g
@@ -30,6 +30,8 @@ mv Chia-darwin-x64 ../build_scripts/dist/
 cd ../build_scripts
 
 echo "Create .dmg"
-electron-installer-dmg dist/Chia-darwin-x64/Chia.app Chia-$CHIA_INSTALLER_VERSION --overwrite
+mkdir final_installer
+electron-installer-dmg dist/Chia-darwin-x64/Chia.app Chia-$CHIA_INSTALLER_VERSION --overwrite --out final_installer
 ls -l
-ls -l dist
+ls -l final_installer
+ls -l dist/Chia-darwin-x64/
