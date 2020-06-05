9cb06d71d0929de9bd72e05281dd2aeafd5b1046
Gene Hoffman <hoffmang@hoffmang.com>
2020-06-05 15:17:38 -0700

    return string from installer-version.py

--- build_scripts/installer-version.py
+++ build_scripts/installer-version.py
@@ -1,48 +1,51 @@
-from setuptools_scm import get_version
+from src import __version__
 
-full_version = get_version(root='..', relative_to=__file__)
 # example: 1.0b5.dev225
-left_version = full_version.split("+")
-version = left_version[0].split(".")
-print(version)
-scm_major_version = version[0]
-scm_minor_version = version[1]
-if len(version) > 2:
-    smc_patch_version = version[2]
-    patch_release_number = smc_patch_version
-else:
-    smc_patch_version = ""
+def main():
 
-major_release_number = scm_major_version
-minor_release_number = scm_minor_version
-dev_release_number = ""
+    version = __version__.split(".")
 
-# If this is a beta dev release - get which beta it is
-if "0b" in scm_minor_version:
-    orignial_minor_ver_list = scm_minor_version.split("0b")
-    major_release_number = str(1 - int(scm_major_version))  # decrement the major release for beta
-    minor_release_number = scm_major_version
-    patch_release_number = orignial_minor_ver_list[1]
-    if smc_patch_version and "dev" in smc_patch_version:
-        patch_release_number = str(int(patch_release_number) + 1)
-        dev_release_number = "." + smc_patch_version
-elif "0rc" in version[1]:
-    original_minor_ver_list = scm_minor_version.split("0rc")
-    major_release_number = str(1 - int(scm_major_version))  # decrement the major release for release candidate
-    minor_release_number = str(int(scm_major_version) + 1)  # RC is 0.2.1 for RC 1
-    patch_release_number = original_minor_ver_list[1]
-    if smc_patch_version and "dev" in smc_patch_version:
-        patch_release_number = str(int(patch_release_number) + 1)
-        dev_release_number = "." + smc_patch_version
-else:
+    scm_major_version = version[0]
+    scm_minor_version = version[1]
+    if len(version) > 2:
+        smc_patch_version = version[2]
+        patch_release_number = smc_patch_version
+    else:
+        smc_patch_version = ""
+
     major_release_number = scm_major_version
     minor_release_number = scm_minor_version
-    patch_release_number = smc_patch_version
     dev_release_number = ""
 
-install_release_number = major_release_number + "." + minor_release_number
-if len(patch_release_number) > 0:
-    install_release_number += "." + patch_release_number
-install_release_number += dev_release_number
+    # If this is a beta dev release - get which beta it is
+    if "0b" in scm_minor_version:
+        orignial_minor_ver_list = scm_minor_version.split("0b")
+        major_release_number = str(1 - int(scm_major_version))  # decrement the major release for beta
+        minor_release_number = scm_major_version
+        patch_release_number = orignial_minor_ver_list[1]
+        if smc_patch_version and "dev" in smc_patch_version:
+            patch_release_number = str(int(patch_release_number) + 1)
+            dev_release_number = "." + smc_patch_version
+    elif "0rc" in version[1]:
+        original_minor_ver_list = scm_minor_version.split("0rc")
+        major_release_number = str(1 - int(scm_major_version))  # decrement the major release for release candidate
+        minor_release_number = str(int(scm_major_version) + 1)  # RC is 0.2.1 for RC 1
+        patch_release_number = original_minor_ver_list[1]
+        if smc_patch_version and "dev" in smc_patch_version:
+            patch_release_number = str(int(patch_release_number) + 1)
+            dev_release_number = "." + smc_patch_version
+    else:
+        major_release_number = scm_major_version
+        minor_release_number = scm_minor_version
+        patch_release_number = smc_patch_version
+        dev_release_number = ""
 
-print(install_release_number)
+    install_release_number = major_release_number + "." + minor_release_number
+    if len(patch_release_number) > 0:
+        install_release_number += "." + patch_release_number
+    install_release_number += dev_release_number
+
+    print(str(install_release_number))
+
+if __name__ == "__main__":
+    main()
