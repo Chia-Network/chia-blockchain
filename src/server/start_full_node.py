917315876ff6c67a78ae3de2ab54ecac279d5659
Yostra <straya@chia.net>
2020-06-05 14:54:54 -0700

    freeze

--- src/server/start_full_node.py
+++ src/server/start_full_node.py
@@ -18,7 +18,7 @@ log = logging.getLogger(__name__)
 def service_kwargs_for_full_node(root_path):
     service_name = "full_node"
     config = load_config_cli(root_path, "config.yaml", service_name)
-
+    freeze_support()
     api = FullNode(config, root_path=root_path)
 
     introducer = config["introducer_peer"]
