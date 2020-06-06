27fb36e4d111ed5be655746b4318153dac3bbaf5
Gene Hoffman <hoffmang@hoffmang.com>
2020-06-06 10:06:16 -0700

    process.env.CHIA_INSTALLER_VERSION

--- electron-react/winstaller.js
+++ electron-react/winstaller.js
@@ -17,7 +17,7 @@ function getInstallerConfig () {
   return Promise.resolve({
     appDirectory: path.join(rootPath, 'Chia-win32-x64'),
     authors: 'Chia Network',
-    version: env.CHIA_INSTALLER_VERSION,
+    version: process.env.CHIA_INSTALLER_VERSION,
     noMsi: true,
     outputDirectory: path.join(outPath, 'windows-installer'),
     exe: 'Chia-' + process.env.CHIA_INSTALLER_VERSION + '.exe',
