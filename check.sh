a7e7f1eb932d94178d402aecb5ea9288d0dc15a0
Yostra <straya@chia.net>
2020-06-11 20:40:54 -0700

    tests lower puzzle count

---
+++ check.sh
@@ -0,0 +1,2 @@
+flake8 src --exclude src/electron-ui/node_modules
+mypy src tests
---
+++ format.sh
@@ -0,0 +1,1 @@
+black src tests scripts && flake8 src scripts --exclude src/electron-ui/node_modules && mypy src scripts tests
