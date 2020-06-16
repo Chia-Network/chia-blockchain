186e44dd943c977dd2da08dd74e3e91eb68d1d1b
Bill Blanke <wjb2002@flex.com>
2020-06-16 12:42:24 -0700

    update vdf and pos for non skylake and filehandle fixes

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ dependencies = [
     "aiosqlite==0.13.0",  # asyncio wrapper for sqlite, to store blocks
     "aiohttp==3.6.2",  # HTTP server for full node rpc
     "colorlog==4.1.0",  # Adds color to logs
-    "chiavdf==0.3b11",  # timelord and vdf verification
+    "chiavdf==0.12.17",  # timelord and vdf verification
     "chiabip158==0.14",  # bip158-style wallet filters
-    "chiapos==0.12.21",  # proof of space
+    "chiapos==0.12.22",  # proof of space
     "sortedcontainers==2.1.0",  # For maintaining sorted mempools
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
     "clvm-tools==0.1.1",  # clvm compiler tools
