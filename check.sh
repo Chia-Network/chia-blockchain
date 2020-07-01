f646b4cd287be107d635ab5d28c0828172bfed2f
Yostra <straya@chia.net>
2020-07-01 13:27:45 +0200

    delete accidental commit

--- check.sh
+++
@@ -1,2 +0,0 @@
-flake8 src --exclude src/electron-ui/node_modules
-mypy src tests
--- format.sh
+++
@@ -1,1 +0,0 @@
-black src tests scripts && flake8 src scripts --exclude src/electron-ui/node_modules && mypy src scripts tests
