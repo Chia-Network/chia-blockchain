741ce9127ee4dd4c7b1c47454a6982c700a72b22
Mariano Sorgente <sorgente711@gmail.com>
2020-07-06 13:28:25 +0900

    Attempt to fix tests

--- src/server/upnp.py
+++ src/server/upnp.py
@@ -8,11 +8,11 @@ def upnp_remap_port(port):
     log.info(f"Attempting to enable UPnP (open up port {port})")
     try:
         upnp = miniupnpc.UPnP()
-        upnp.discoverdelay = 5
+        upnp.discoverdelay = 30
         upnp.discover()
         upnp.selectigd()
         upnp.addportmapping(port, "TCP", upnp.lanaddr, port, "chia", "")
-        log.info(f"Port {port} opened with UPnP.")
+        log.info(f"Port {port} opened with UPnP. lanaddr {upnp.lanaddr} external: {upnp.externalipaddress()}")
     except Exception:
         log.warning(
             "UPnP failed. This is not required to run chia, but it allows incoming connections from other peers."
