4ba329bd3e0ea7f8f0e48a8145f34d35ecef9c5f
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2020-07-06 16:21:21 +0900

    Remove randomness from tests

--- tests/make_test_constants.py
+++ tests/make_test_constants.py
@@ -12,6 +12,7 @@ def make_test_constants_with_genesis(test_constants_overrides: Dict):
     bt = BlockTools()
 
     new_genesis_block = bt.create_genesis_block(test_constants, bytes([0] * 32), b"0")
+    print("genesis:", new_genesis_block)
 
     final_test_constants = test_constants.replace(
         GENESIS_BLOCK=bytes(new_genesis_block)
