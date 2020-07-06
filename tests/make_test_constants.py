6bb588a14ea56ba40274bc58cbb7554ce626d67f
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2020-07-06 16:23:18 +0900

    Remove logging

--- tests/make_test_constants.py
+++ tests/make_test_constants.py
@@ -5,14 +5,11 @@ from .block_tools import BlockTools
 
 
 def make_test_constants_with_genesis(test_constants_overrides: Dict):
-    print("AMKING TESTS CONSTANTS")
-
     test_constants = constants.replace(**test_constants_overrides)
 
     bt = BlockTools()
 
     new_genesis_block = bt.create_genesis_block(test_constants, bytes([0] * 32), b"0")
-    print("genesis:", new_genesis_block)
 
     final_test_constants = test_constants.replace(
         GENESIS_BLOCK=bytes(new_genesis_block)
