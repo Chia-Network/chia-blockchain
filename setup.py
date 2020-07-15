0187b60e5630d9929ae9592ee9d5e9edb1f67c1d
Gene Hoffman <hoffmang@hoffmang.com>
2020-07-15 02:13:46 -0700

    test aiosqlite 0.15.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependencies = [
     "clvm==0.4",  # contract language
     "clvm-tools==0.1.1",  # clvm compiler tools
     "aiohttp==3.6.2",  # HTTP server for full node rpc
-    "aiosqlite==0.13.0",  # asyncio wrapper for sqlite, to store blocks
+    "aiosqlite==0.15.0",  # asyncio wrapper for sqlite, to store blocks
     "bitstring==3.1.7",  # Binary data management library
     "cbor2==5.1.0",  # Used for network wire format
     "colorlog==4.1.0",  # Adds color to logs
