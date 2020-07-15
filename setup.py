a0a61fdd1892bb40f36f25a801c29ac9b30060e1
Gene Hoffman <hoffmang@hoffmang.com>
2020-07-15 12:16:31 -0700

    Downgrade to gmp 6.1.2 to deal w lzcnt on old cpus

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 dependencies = [
     "aiter==0.13.20191203",  # Used for async generator tools
-    "blspy==0.2c1",  # Signature library
-    "chiavdf==0.12.21",  # timelord and vdf verification
+    "blspy==0.1.30",  # Signature library
+    "chiavdf==0.12.22",  # timelord and vdf verification
     "chiabip158==0.15",  # bip158-style wallet filters
     "chiapos==0.12.23",  # proof of space
     "clvm==0.4",  # contract language
