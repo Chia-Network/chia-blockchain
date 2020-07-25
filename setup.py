ec2fa23278a638f3b2458701d587f7af67613f63
Gene Hoffman <hoffmang@hoffmang.com>
2020-07-24 18:39:48 -0700

    Update chiapos, chiavdf, chiabip158, and blspy

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 dependencies = [
     "aiter==0.13.20191203",  # Used for async generator tools
-    "blspy==0.2.0",  # Signature library
-    "chiavdf==0.12.22",  # timelord and vdf verification
-    "chiabip158==0.15",  # bip158-style wallet filters
-    "chiapos==0.12.23",  # proof of space
+    "blspy==0.2.1",  # Signature library
+    "chiavdf==0.12.23",  # timelord and vdf verification
+    "chiabip158==0.16",  # bip158-style wallet filters
+    "chiapos==0.12.24",  # proof of space
     "clvm==0.4",  # contract language
     "clvm-tools==0.1.1",  # clvm compiler tools
     "aiohttp==3.6.2",  # HTTP server for full node rpc
