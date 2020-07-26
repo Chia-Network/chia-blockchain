220441272c17e60d3186118f53051cbe04960a37
Yostra <straya@chia.net>
2020-07-26 22:44:04 +0200

    different file

---
+++ src/wallet/util/backup_utils.py
@@ -0,0 +1,29 @@
+import json
+
+from src.wallet.util.wallet_types import WalletType
+
+
+def get_backup_info(file_path):
+    info_dict = {}
+    wallets = []
+
+    backup_text = file_path.read_text
+    json_dict = json.loads(backup_text)
+
+    wallet_list_json = json_dict["wallet_list"]
+
+    for wallet_info in wallet_list_json:
+        wallet = {}
+        wallet["name"] = wallet_info["name"]
+        wallet["type"] = wallet_info["type"]
+        wallet["type_name"] = WalletType(wallet_info["type"]).name
+        wallet["id"] = wallet_info["id"]
+        wallet["data"] = wallet_info["data"]
+        wallets.append(wallet)
+
+    info_dict["version"] = json_dict["version"]
+    info_dict["fingerprint"] = json_dict["fingerprint"]
+    info_dict["timestamp"] = json_dict["timestamp"]
+    info_dict["wallets"] = wallets
+
+    return info_dict
--- src/wallet/wallet_state_manager.py
+++ src/wallet/wallet_state_manager.py
@@ -1349,31 +1349,6 @@ class WalletStateManager:
         await self.load_wallets()
         await self.user_settings.user_imported_backup()
 
-    async def get_backup_info(self, file_path):
-        info_dict = {}
-        wallets = []
-
-        backup_text = file_path.read_text
-        json_dict = json.loads(backup_text)
-
-        wallet_list_json = json_dict["wallet_list"]
-
-        for wallet_info in wallet_list_json:
-            wallet = {}
-            wallet["name"] = wallet_info["name"]
-            wallet["type"] = wallet_info["type"]
-            wallet["type_name"] = WalletType(wallet_info["type"]).name
-            wallet["id"] = wallet_info["id"]
-            wallet["data"] = wallet_info["data"]
-            wallets.append(wallet)
-
-        info_dict["version"] = json_dict["version"]
-        info_dict["fingerprint"] = json_dict["fingerprint"]
-        info_dict["timestamp"] = json_dict["timestamp"]
-        info_dict["wallets"] = wallets
-
-        return info_dict
-
     async def get_wallet_for_colour(self, colour):
         for wallet_id in self.wallets:
             wallet = self.wallets[wallet_id]
