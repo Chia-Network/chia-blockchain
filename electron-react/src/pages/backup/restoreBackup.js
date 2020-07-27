81db44a760d9a85a3eecc97d4293b81d9911f5e4
Yostra <straya@chia.net>
2020-07-27 15:34:03 +0200

    get backup info

--- electron-react/src/pages/backup/restoreBackup.js
+++ electron-react/src/pages/backup/restoreBackup.js
@@ -14,18 +14,23 @@ import {
   add_new_key_action,
   log_in_and_import_backup,
   add_and_restore_from_backup,
-  login_and_skip_action
+  login_and_skip_action,
+  get_backup_info_action
 } from "../../modules/message";
 import { Paper } from "@material-ui/core";
+import {
+  changeBackupView,
+  presentMain,
+  presentBackupInfo
+} from "../../modules/backup_state";
 
 const UIPart = props => {
+  const dispatch = useDispatch();
+  const classes = myStyle();
   var words = useSelector(state => state.mnemonic_state.mnemonic_input);
   var fingerprint = useSelector(
     state => state.wallet_state.selected_fingerprint
   );
-
-  const dispatch = useDispatch();
-  const classes = myStyle();
 
   for (let word of words) {
     if (word === "") {
@@ -62,11 +67,7 @@ const UIPart = props => {
     e.stopPropagation();
 
     const file_path = e.dataTransfer.files[0].path;
-    if (fingerprint !== null) {
-      dispatch(log_in_and_import_backup(fingerprint, file_path));
-    } else if (words !== null) {
-      dispatch(add_and_restore_from_backup(words, file_path));
-    }
+    dispatch(get_backup_info_action(file_path));
   };
 
   return (
@@ -114,6 +115,79 @@ const UIPart = props => {
   );
 };
 
+const BackupDetails = () => {
+  const classes = myStyle();
+  const dispatch = useDispatch();
+  const file_path = useSelector(state => state.backup_state.selected_file_path);
+  const backup_info = useSelector(state => state.backup_state.backup_info);
+  var words = useSelector(state => state.mnemonic_state.mnemonic_input);
+  var fingerprint = useSelector(
+    state => state.wallet_state.selected_fingerprint
+  );
+
+  for (let word of words) {
+    if (word === "") {
+      words = null;
+    }
+  }
+
+  function goBack() {
+    dispatch(changeBackupView(presentMain));
+    dispatch(changeEntranceMenu(presentSelectKeys));
+  }
+
+  function next() {
+    if (fingerprint !== null) {
+      dispatch(log_in_and_import_backup(fingerprint, file_path));
+    } else if (words !== null) {
+      dispatch(add_and_restore_from_backup(words, file_path));
+    }
+  }
+
+  return (
+    <div className={classes.root}>
+      <ArrowBackIosIcon onClick={goBack} className={classes.navigator}>
+        {" "}
+      </ArrowBackIosIcon>
+      <div className={classes.grid_wrap}>
+        <Container className={classes.grid} maxWidth="lg">
+          <Typography className={classes.title} component="h4" variant="h4">
+            Restore From Backup
+          </Typography>
+        </Container>
+      </div>
+      <div className={classes.dragContainer}>
+        <Paper
+          className={classes.drag}
+          style={{ position: "relative", width: "80%", margin: "auto" }}
+        >
+          <div className={classes.dragText}>Drag and drop your backup file</div>
+        </Paper>
+      </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Button
+            onClick={next}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Continue
+          </Button>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
 export const RestoreBackup = () => {
-  return <UIPart></UIPart>;
+  const view = useSelector(state => state.backup_state.view);
+  if (view === presentBackupInfo) {
+    return <BackupDetails></BackupDetails>;
+  } else {
+    return <UIPart></UIPart>;
+  }
 };
