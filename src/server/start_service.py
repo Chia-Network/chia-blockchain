e81c6901a78ea62d33671e94f1056f613f514de6
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2020-07-27 18:12:15 +0900

    Stop the service after closing connections

--- src/server/start_service.py
+++ src/server/start_service.py
@@ -198,9 +198,6 @@ class Service:
     def stop(self):
         if not self._is_stopping:
             self._is_stopping = True
-            self._log.info("Calling service stop callback")
-            if self._stop_callback:
-                self._stop_callback()
             self._log.info("Closing server sockets")
             for _ in self._server_sockets:
                 _.close()
@@ -221,6 +218,11 @@ class Service:
 
         self._log.info("Waiting for ChiaServer to be closed")
         await self._server.await_closed()
+
+        self._log.info("Calling service stop callback")
+        if self._stop_callback:
+            self._stop_callback()
+
         if self._rpc_task:
 
             self._log.info("Waiting for RPC server")
