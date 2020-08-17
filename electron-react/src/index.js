63c83dcdaf4e726a43e42afeddb3cdfbfe7fd7bb
Yostra <straya@chia.net>
2020-08-16 20:15:05 -0700

    only  from electron

--- electron-react/src/index.js
+++ electron-react/src/index.js
@@ -8,6 +8,7 @@ import store from "./modules/store";
 import WebSocketConnection from "./hocs/WebsocketConnection";
 import { daemon_rpc_ws } from "./util/config";
 import { exit_and_close } from "./modules/message";
+import isElectron from "is-electron";
 
 const Root = ({ store }) => (
   <Provider store={store}>
@@ -22,7 +23,9 @@ const Root = ({ store }) => (
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 
 window.onload = () => {
-  window.ipcRenderer.on("exit-daemon", (event, ...args) => {
-    store.dispatch(exit_and_close(event));
-  });
+  if (isElectron()) {
+    window.ipcRenderer.on("exit-daemon", (event, ...args) => {
+      store.dispatch(exit_and_close(event));
+    });
+  }
 };
