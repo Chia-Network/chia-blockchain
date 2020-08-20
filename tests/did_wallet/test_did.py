52a9c7c8d10fa87b97c3c5c9004c3943cca1c7b2
Matthew Howard <matt@chia.net>
2020-08-20 17:49:24 +0100

    fixed bug with bad signatures

--- tests/did_wallet/test_did.py
+++ tests/did_wallet/test_did.py
@@ -17,7 +17,7 @@ from src.types.coin_solution import CoinSolution
 from blspy import AugSchemeMPL
 from src.types.spend_bundle import SpendBundle
 from src.wallet.transaction_record import TransactionRecord
-from src.util.hash import std_hash
+from src.wallet.derive_keys import master_sk_to_wallet_sk
 
 
 @pytest.fixture(scope="module")
@@ -584,17 +584,11 @@ class TestWalletSimulator:
             )
         ]
         # sign for AGG_SIG_ME
-        message = std_hash(bytes(ph) + bytes(coin.name()))
+        message = bytes(coin.puzzle_hash) + bytes(coin.name())
         pubkey = did_wallet_puzzles.get_pubkey_from_innerpuz(innerpuz_str)
-        index = await did_wallet.wallet_state_manager.puzzle_store.index_for_pubkey(
-            pubkey
-        )
-        private = did_wallet.wallet_state_manager.private_key.private_child(
-            index
-        ).get_private_key()
-        # pk = BLSPrivateKey(private)
-        signature = private.sign(message)
-        assert signature.validate([signature.PkMessagePair(pubkey, message)])
+        index = await did_wallet.wallet_state_manager.puzzle_store.index_for_pubkey(pubkey)
+        private = master_sk_to_wallet_sk(did_wallet.wallet_state_manager.private_key, index)
+        signature = AugSchemeMPL.sign(private, message)
         sigs = [signature]
         aggsig = AugSchemeMPL.aggregate(sigs)
         spend_bundle = SpendBundle(list_of_solutions, aggsig)
@@ -613,6 +607,7 @@ class TestWalletSimulator:
             removals=spend_bundle.removals(),
             wallet_id=did_wallet.wallet_info.id,
             sent_to=[],
+            trade_id=None
         )
 
         await did_wallet.standard_wallet.push_transaction(did_record)
