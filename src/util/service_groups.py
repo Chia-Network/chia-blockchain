71e8e62a5e64a9c62842390700a4937ada37b2f4
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2020-08-24 22:28:59 +0900

    Allow running only farmer, wallet, timelord

--- src/util/service_groups.py
+++ src/util/service_groups.py
@@ -3,8 +3,12 @@ SERVICES_FOR_GROUP = {
     "node": "chia_full_node".split(),
     "harvester": "chia_harvester".split(),
     "farmer": "chia_harvester chia_farmer chia_full_node chia_wallet".split(),
+    "farmer-only": "chia_farmer".split(),
     "timelord": "chia_timelord_launcher chia_timelord chia_full_node".split(),
+    "timelord-only": "chia_timelord".split(),
+    "timelord-launcher-only": "chia_timelord_launcher".split(),
     "wallet": "chia_wallet chia_full_node".split(),
+    "wallet-only": "chia_wallet".split(),
     "introducer": "chia_introducer".split(),
     "simulator": "chia_full_node_simulator".split(),
 }
