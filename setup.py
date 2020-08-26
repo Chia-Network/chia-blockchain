845b1d047a1484f848ca76bbc804ddc7ecfc4eb4
Gene Hoffman <hoffmang@hoffmang.com>
2020-08-26 11:50:35 -0700

    Add setuptools_scm to setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ dependencies = [
     "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
     "PyYAML==5.3.1",  # Used for config file format
     "sortedcontainers==2.2.2",  # For maintaining sorted mempools
+    "setuptools_scm==4.1.2",  # Currrent version information
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
 ]
 
