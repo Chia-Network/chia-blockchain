b96582c3071213900ea5bd558de6d4b1e1a2b7bf
Matthew Howard <matt@chia.net>
2020-09-14 20:05:54 +0100

    removed (a)

--- src/wallet/did_wallet/did_wallet_puzzles.py
+++ src/wallet/did_wallet/did_wallet_puzzles.py
@@ -42,7 +42,9 @@ def create_innerpuz(pubkey: bytes, identities: List[bytes]) -> Program:
 
 
 def create_fullpuz(innerpuzhash, core) -> Program:
-    puzstring = f"(r (c (q 0x{innerpuzhash}) ((c (q {binutils.disassemble(core)}) (a)))))"
+    puzstring = (
+        f"(r (c (q 0x{innerpuzhash}) ((c (q {binutils.disassemble(core)}) 1))))"
+    )
     # return curry(DID_FULLPUZ_MOD, [innerpuzhash, core])
     return Program(binutils.assemble(puzstring))
 
