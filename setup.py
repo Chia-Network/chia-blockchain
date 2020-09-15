918af6b9e6d1e982b9146dba6be74d5b8c5bdbe9
Kyle Altendorf <sda@fstab.net>
2020-09-14 22:33:57 -0400

    Set long_description_content_type for nicer a PyPI page

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ kwargs = dict(
     },
     use_scm_version={"fallback_version": "unknown-no-.git-directory"},
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     zip_safe=False,
 )
 
