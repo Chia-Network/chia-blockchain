68abc7aeea5f2e0d4442782123275f993a88199a
Zlatko Fedor <zfedor@gmail.com>
2020-09-17 15:34:11 +0200

    Revert "rebase to dev | version 13"
    This reverts commit 5410bc33e5623b2dbb1c73ca48a95f1d94d764e1.

--- electron-react/src/types/Transaction.ts
+++ electron-react/src/types/Transaction.ts
@@ -4,7 +4,7 @@ import type SpendBundle from './SpendBundle';
 type Transaction = {
   confirmed_at_index: number,
   created_at_time: number,
-  to_address: string,
+  to_puzzle_hash: string,
   amount: number,
   fee_amount: number,
   incoming: boolean,
