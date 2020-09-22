fe8d3539f2ae444b824837767550d2f902cb4412
Richard Kiss <him@richardkiss.com>
2020-09-21 19:44:39 -0700

    Use `clvm` 0.5.1.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = [
     "chiavdf==0.12.24",  # timelord and vdf verification
     "chiabip158==0.16",  # bip158-style wallet filters
     "chiapos==0.12.28",  # proof of space
-    "clvm==0.5",
+    "clvm==0.5.1",
     "clvm_tools==0.1.5",
     "aiohttp==3.6.2",  # HTTP server for full node rpc
     "aiosqlite@git+https://github.com/mariano54/aiosqlite.git@28cb5754deec562ac931da8fca799fb82df97a12#egg=aiosqlite",
