d4d32c3ab1dc9f4c045f1e5092ed7bd53ed461b2
Richard Kiss <him@richardkiss.com>
2020-10-01 14:06:29 -0700

    Improve p2_delegated_puzzle_or_hidden_puzzle.

--- src/wallet/puzzles/p2_delegated_puzzle_or_hidden_puzzle.py
+++ src/wallet/puzzles/p2_delegated_puzzle_or_hidden_puzzle.py
@@ -28,8 +28,10 @@ from .load_clvm import load_clvm
 from .p2_conditions import puzzle_for_conditions
 
 
-DEFAULT_HIDDEN_PUZZLE = Program.from_bytes(
-    bytes.fromhex("ff0980")
+DEFAULT_HIDDEN_PUZZLE = Program.from_bytes(bytes.fromhex("ff0980"))
+
+DEFAULT_HIDDEN_PUZZLE_HASH = (
+    DEFAULT_HIDDEN_PUZZLE.get_tree_hash()
 )  # this puzzle `(x)` always fails
 
 MOD = load_clvm("p2_delegated_puzzle_or_hidden_puzzle.clvm")
@@ -38,18 +40,22 @@ SYNTHETIC_MOD = load_clvm("calculate_synthetic_public_key.clvm")
 
 PublicKeyProgram = Union[bytes, Program]
 
+GROUP_ORDER = 0x73EDA753299D7D483339D80809A1D80553BDA402FFFE5BFEFFFFFFFF00000001
+
 
 def calculate_synthetic_offset(
     public_key: G1Element, hidden_puzzle_hash: bytes32
 ) -> int:
     blob = hashlib.sha256(bytes(public_key) + hidden_puzzle_hash).digest()
-    return int_from_bytes(blob)
+    offset = int_from_bytes(blob)
+    offset %= GROUP_ORDER
+    return offset
 
 
 def calculate_synthetic_public_key(
-    public_key: G1Element, hidden_puzzle: Program
+    public_key: G1Element, hidden_puzzle_hash: bytes32
 ) -> G1Element:
-    r = SYNTHETIC_MOD.run([bytes(public_key), hidden_puzzle.get_tree_hash()])
+    r = SYNTHETIC_MOD.run([bytes(public_key), hidden_puzzle_hash])
     return G1Element.from_bytes(r.as_atom())
 
 
@@ -57,19 +63,31 @@ def puzzle_for_synthetic_public_key(synthetic_public_key: G1Element) -> Program:
     return MOD.curry(bytes(synthetic_public_key))
 
 
-def puzzle_for_public_key_and_hidden_puzzle(
-    public_key: G1Element, hidden_puzzle: Program
+def puzzle_for_public_key_and_hidden_puzzle_hash(
+    public_key: G1Element, hidden_puzzle_hash: bytes32
 ) -> Program:
-    synthetic_public_key = calculate_synthetic_public_key(public_key, hidden_puzzle)
+    synthetic_public_key = calculate_synthetic_public_key(
+        public_key, hidden_puzzle_hash
+    )
 
     return puzzle_for_synthetic_public_key(synthetic_public_key)
 
 
+def puzzle_for_public_key_and_hidden_puzzle(
+    public_key: G1Element, hidden_puzzle: Program
+) -> Program:
+    return puzzle_for_public_key_and_hidden_puzzle_hash(
+        public_key, hidden_puzzle.get_tree_hash()
+    )
+
+
 def puzzle_for_pk(public_key: G1Element) -> Program:
-    return puzzle_for_public_key_and_hidden_puzzle(public_key, DEFAULT_HIDDEN_PUZZLE)
+    return puzzle_for_public_key_and_hidden_puzzle_hash(
+        public_key, DEFAULT_HIDDEN_PUZZLE_HASH
+    )
 
 
-def solution_with_delegated_puzzle(
+def solution_for_delegated_puzzle(
     delegated_puzzle: Program, solution: Program
 ) -> Program:
     return Program.to([[], delegated_puzzle, solution])
@@ -91,4 +109,4 @@ def solution_with_hidden_puzzle(
 
 def solution_for_conditions(conditions: Program) -> Program:
     delegated_puzzle = puzzle_for_conditions(conditions)
-    return solution_with_delegated_puzzle(delegated_puzzle, Program.to(0))
+    return solution_for_delegated_puzzle(delegated_puzzle, Program.to(0))
