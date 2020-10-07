ee86a4a578613d4c09daa4fd632068e1cb8402fe
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2020-10-07 15:45:29 +0900

    Fix broken cryptfile version, default ram in UI, and fix redux dev tools brave (#450)

--- electron-react/src/modules/store.js
+++ electron-react/src/modules/store.js
@@ -13,9 +13,8 @@ const store =
     : createStore(
         rootReducer,
         compose(
-          applyMiddleware(...middleware),
-          window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
+          applyMiddleware(...middleware),  /* preloadedState, */
+          window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose
         )
       );
 
