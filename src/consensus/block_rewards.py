0200f0e924320da8db785e065aab74d7960869de
Mariano Sorgente <sorgente711@gmail.com>
2020-10-13 18:00:07 +0900

    Add pool and farmer rewards

--- src/consensus/block_rewards.py
+++ src/consensus/block_rewards.py
@@ -1,19 +1,33 @@
 from src.util.ints import uint32, uint64
 
 
-def calculate_block_reward(height: uint32) -> uint64:
+def calculate_pool_reward(height: uint32) -> uint64:
     """
     Returns the coinbase reward at a certain block height.
-    1 Chia coin = 16,000,000,000,000 = 16 trillion mojo.
+    1 Chia coin = 1,000,000,000,000 = 1 trillion mojo.
+    These are temporary testnet numbers to test halvings
     """
     if height == 0:
         return uint64(500000000000000000)
-    return uint64(14000000000000)
+    if height < 2000:
+        return uint64(875000000000)
+    if height < 4000:
+        return uint64(875000000000 // 2)
+    if height < 6000:
+        return uint64(875000000000 // 4)
+    return uint64(875000000000 // 8)
 
 
-def calculate_base_fee(height: uint32) -> uint64:
+def calculate_base_farmer_reward(height: uint32) -> uint64:
     """
-    Returns the base fee reward at a certain block height.
+    Returns the base farmer reward at a certain block height.
     1 base fee reward is 1/8 of total block reward
+    These are temporary testnet numbers to test halvings
     """
-    return uint64(2000000000000)
+    if height < 2000:
+        return uint64(125000000000)
+    if height < 4000:
+        return uint64(125000000000 // 2)
+    if height < 6000:
+        return uint64(125000000000 // 4)
+    return uint64(125000000000 // 8)
