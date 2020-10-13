28278a27b43c8fd90df582edb2cb7c23bee8e550
Zlatko Fedor <zfedor@gmail.com>
2020-10-13 15:27:39 +0200

    used material-ui SvgIcon and prepare sidebar icons for light/dark mode

--- electron-react/src/components/dashboard/DashboardSideBar.tsx
+++ electron-react/src/components/dashboard/DashboardSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Trans } from '@lingui/macro';
 import { useDispatch } from 'react-redux';
-import { List } from '@material-ui/core';
+import { List, SvgIcon } from '@material-ui/core';
 import { logOut } from '../../modules/message';
 import { ReactComponent as WalletsIcon } from './images/wallet.svg';
 import { ReactComponent as FarmIcon } from './images/farm.svg';
@@ -12,6 +12,14 @@ import { ReactComponent as PlotIcon } from './images/plot.svg';
 import { ReactComponent as TradeIcon } from './images/pool.svg';
 import SideBarItem from '../sideBar/SideBarItem';
 import Flex from '../flex/Flex';
+
+const StyledHomeIcon = styled(HomeIcon)`
+  path {
+    stroke: ${({ theme }) =>
+      theme.palette.type === 'dark' ? 'white' : '#757575'};;
+    stroke-width: 1;
+  }
+`;
 
 const StyledRoot = styled(Flex)`
   height: 100%;
@@ -34,33 +42,45 @@ export default function DashboardSideBar() {
       <StyledList disablePadding>
         <SideBarItem
           to="/dashboard"
-          icon={<HomeIcon />}
+          icon={(
+            <SvgIcon fontSize="large" component={StyledHomeIcon} viewBox="0 0 32 31" />
+          )}
           title={<Trans id="DashboardSideBar.home">Full Node</Trans>}
           exact
         />
         <SideBarItem
           to="/dashboard/wallets"
-          icon={<WalletsIcon />}
+          icon={(
+            <SvgIcon fontSize="large" component={WalletsIcon} viewBox="0 0 32 39" />
+          )}
           title={<Trans id="DashboardSideBar.wallets">Wallets</Trans>}
         />
         <SideBarItem
           to="/dashboard/plot"
-          icon={<PlotIcon />}
+          icon={(
+            <SvgIcon fontSize="large" component={PlotIcon} viewBox="0 0 40 32" />
+          )}
           title={<Trans id="DashboardSideBar.plot">Plot</Trans>}
         />
         <SideBarItem
           to="/dashboard/farm"
-          icon={<FarmIcon />}
+          icon={(
+            <SvgIcon fontSize="large" component={FarmIcon} viewBox="0 0 32 37" />
+          )}
           title={<Trans id="DashboardSideBar.farm">Farm</Trans>}
         />
         <SideBarItem
           to="/dashboard/trade"
-          icon={<TradeIcon />}
+          icon={(
+            <SvgIcon fontSize="large" component={TradeIcon} viewBox="0 0 34 34" />
+          )}
           title={<Trans id="DashboardSideBar.trade">Trade</Trans>}
         />
         <SideBarItem
           to="/"
-          icon={<KeysIcon />}
+          icon={(
+            <SvgIcon fontSize="large" component={KeysIcon} viewBox="0 0 32 33" />
+          )}
           onSelect={handleLogOut}
           title={<Trans id="DashboardSideBar.keys">Keys</Trans>}
           exact
