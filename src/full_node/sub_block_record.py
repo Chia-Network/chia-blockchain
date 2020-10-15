9d56183a24fe583f39ee66e081d2e127b9c85549
Mariano Sorgente <sorgente711@gmail.com>
2020-10-15 19:50:18 +0900

    Add more validation for foliage block

--- src/full_node/sub_block_record.py
+++ src/full_node/sub_block_record.py
@@ -17,15 +17,13 @@ class SubBlockRecord(Streamable):
 
     header_hash: bytes32
     prev_hash: bytes32  # Header hash of the previous sub-block
-    prev_block_hash: bytes32  # Header hash of the previous transaction block
+    prev_block_hash: Optional[bytes32]  # Header hash of the previous transaction block
     sub_block_height: uint32
     weight: uint128  # Total cumulative difficulty of all ancestor blocks since genesis
     total_iters: uint128  # Total number of VDF iterations since genesis, including this sub-block
     is_block: bool  # Whether or not this sub-block is also a block
-    slot_number: uint32  # Determines which PoSpace we are based off of
-    challenge_slot_number: uint32  # Determines how many infusions happened in challenge chain
-    overflows_slot: bool  # Determines whether infusion happens in next slot
-    slot_iterations: uint64  # How many iterations are necessary for this slot to finish
+    first_block_in_challenge_slot: bool
+    ips: uint64  # Current network iterations per second parameter
     pool_puzzle_hash: bytes32  # Need to keep track of these because Coins are created in a future block
     farmer_puzzle_hash: bytes32
     timestamp: Optional[uint64]
