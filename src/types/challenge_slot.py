d2579f223f45dd6f37abe3a444a8defbaeed23fd
Mariano Sorgente <sorgente711@gmail.com>
2020-10-16 22:13:32 +0900

    Start working on trunk validation

--- src/types/challenge_slot.py
+++ src/types/challenge_slot.py
@@ -4,18 +4,17 @@ from blspy import G2Element
 
 from src.types.sized_bytes import bytes32
 from src.util.streamable import Streamable, streamable
-from src.types.proof_of_time import ProofOfTimeOutput
 from src.types.proof_of_space import ProofOfSpace
+from src.types.classgroup import ClassgroupElement
 
 
 @dataclass(frozen=True)
 @streamable
 class ChallengeSlot(Streamable):
     prev_slot_hash: bytes32
-    prev_subepoch_summary_hash: Optional[bytes32]  # Only once per subepoch
-    prior_challenge_slot_hash: Optional[bytes32]  # If infused and from prior
+    subepoch_summary_hash: Optional[bytes32]  # Only once per subepoch
     proof_of_space: Optional[ProofOfSpace]
-    icp_proof_of_time_output: Optional[ProofOfTimeOutput]
+    icp_proof_of_time_output: Optional[ClassgroupElement]
     icp_signature: Optional[G2Element]
-    ip_proof_of_time_output: Optional[ProofOfTimeOutput]
-    end_of_slot_proof_of_time_output: ProofOfTimeOutput
+    ip_proof_of_time_output: Optional[ClassgroupElement]
+    end_of_slot_proof_of_time_output: ClassgroupElement
--- src/types/reward_chain_end_of_slot.py
+++ src/types/reward_chain_end_of_slot.py
@@ -3,14 +3,15 @@ from dataclasses import dataclass
 from src.types.sized_bytes import bytes32
 from src.util.ints import uint8
 from src.util.streamable import Streamable, streamable
-from src.types.proof_of_time import ProofOfTime, ProofOfTimeOutput
+from src.types.proof_of_time import ProofOfTime
+from src.types.classgroup import ClassgroupElement
 
 
 @dataclass(frozen=True)
 @streamable
 class RewardChainEndOfSlot(Streamable):
-    prior_point: bytes32
-    end_of_slot_output: ProofOfTimeOutput
+    prior_point: bytes32  # TODO: check what this is
+    end_of_slot_output: ClassgroupElement
     challenge_slot_hash: bytes32
     deficit: uint8  # 5 or less. usually zero
 
@@ -22,4 +23,3 @@ class EndOfSlotProofs(Streamable):
     challenge_chain_ip_proof: Optional[ProofOfTime]
     challenge_chain_slot_proof: ProofOfTime
     reward_chain_slot_proof: ProofOfTime
-
