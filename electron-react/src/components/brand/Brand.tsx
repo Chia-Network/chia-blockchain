b4c798a9f34cd41dcee6630c3bff93b63bc77b9a
Zlatko Fedor <zfedor@gmail.com>
2020-10-19 17:53:39 +0200

    removed unused rotation from the brand logo

--- electron-react/src/components/brand/Brand.tsx
+++ electron-react/src/components/brand/Brand.tsx
@@ -5,16 +5,6 @@ import brandSrc from './images/chia.svg';
 
 const StyledImage = styled('img')`
   max-width: 100%;
-  // animation: App-logo-spin infinite 20s linear;
-
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
 `;
 
 export default function Brand(props: BoxProps) {
