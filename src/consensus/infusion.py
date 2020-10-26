948ab5e2912e2cb881f90b12ee3e0bf0489c1acf
Mariano Sorgente <sorgente711@gmail.com>
2020-10-26 17:03:50 +0900

    change what is infused into challenge chain

--- src/consensus/infusion.py
+++ src/consensus/infusion.py
@@ -1,8 +1,10 @@
 from blspy import G2Element
 from src.types.classgroup import ClassgroupElement
 from src.types.sized_bytes import bytes32
-from src.util.hash import std_hash
+from src.types.vdf import VDFInfo
+from src.types.proof_of_space import ProofOfSpace
+from src.types.challenge_slot import ChallengeChainInfusionPoint
 
 
-def infuse_signature(output: ClassgroupElement, signature: G2Element) -> bytes32:
-    return std_hash(bytes(output) + bytes(signature))
+def infuse_challenge_chain(infusion_challenge_point: VDFInfo, pos: ProofOfSpace, signature: G2Element) -> bytes32:
+    return ChallengeChainInfusionPoint(infusion_challenge_point, pos, signature).get_hash()
