958d44b2afd9d46c02967f63f82dddbc968ae398
Mariano Sorgente <sorgente711@gmail.com>
2020-10-27 18:20:10 +0900

    change some test constants

--- src/util/block_tools.py
+++ src/util/block_tools.py
@@ -208,7 +208,7 @@ class BlockTools:
                 block_list[-1].proof_of_space,
                 curr_difficulty,
                 block_list[-1].proof_of_time.number_of_iterations,
-                test_constants.NUMBER_ZERO_BITS_CHALLENGE_SIG,
+                test_constants.NUMBER_ZERO_BITS_PLOT_FILTER,
             )
 
         starting_height = block_list[-1].height + 1
@@ -422,7 +422,7 @@ class BlockTools:
                 ccp = ProofOfSpace.can_create_proof(
                     plot_id,
                     challenge_hash,
-                    test_constants.NUMBER_ZERO_BITS_CHALLENGE_SIG,
+                    test_constants.NUMBER_ZERO_BITS_PLOT_FILTER,
                 )
                 if not ccp:
                     continue
@@ -439,7 +439,7 @@ class BlockTools:
                 ccp = ProofOfSpace.can_create_proof(
                     plot_id,
                     challenge_hash,
-                    test_constants.NUMBER_ZERO_BITS_CHALLENGE_SIG,
+                    test_constants.NUMBER_ZERO_BITS_PLOT_FILTER,
                 )
                 if not ccp:
                     continue
@@ -475,7 +475,7 @@ class BlockTools:
             proof_of_space,
             difficulty,
             min_iters,
-            test_constants.NUMBER_ZERO_BITS_CHALLENGE_SIG,
+            test_constants.NUMBER_ZERO_BITS_PLOT_FILTER,
         )
         if self.real_plots:
             print(f"Performing {number_iters} VDF iterations")
--- src/util/make_test_constants.py
+++ src/util/make_test_constants.py
@@ -1,25 +1,7 @@
 from typing import Dict
 
-from src.consensus.default_constants import DEFAULT_CONSTANTS
-from src.util.block_tools import BlockTools
+from src.consensus.constants import constants
 
 
-def make_test_constants_with_genesis(test_constants_overrides: Dict):
-    test_constants = make_test_constants_without_genesis(test_constants_overrides)
-
-    bt = BlockTools()
-
-    new_genesis_block = bt.create_genesis_block(test_constants, bytes([0] * 32), b"0")
-
-    final_test_constants = test_constants.replace(
-        GENESIS_BLOCK=bytes(new_genesis_block)
-    )
-
-    return final_test_constants, bt
-
-
-def make_test_constants_without_genesis(test_constants_overrides: Dict):
-
-    test_constants = DEFAULT_CONSTANTS.replace(**test_constants_overrides)
-
-    return test_constants
+def make_test_constants(test_constants_overrides: Dict):
+    return constants.replace(**test_constants_overrides)
