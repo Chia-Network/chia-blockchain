5cb0bbca51172c0376cba9e514b93172808b8c37
Mariano Sorgente <sorgente711@gmail.com>
2020-10-28 15:53:17 +0900

    New farmer protocol and harvester

--- tests/types/test_proof_of_space.py
+++ tests/types/test_proof_of_space.py
@@ -1,4 +1,5 @@
 from secrets import token_bytes
+from blspy import AugSchemeMPL
 from src.types.proof_of_space import ProofOfSpace  # pylint: disable=E0401
 from src.consensus.constants import constants
 from src.types.classgroup import ClassgroupElement
@@ -12,12 +13,14 @@ class TestProofOfSpace:
         num_trials = 40000
         success_count = 0
         target_filter = (2 ** constants.NUMBER_ZERO_BITS_PLOT_FILTER) * (2 ** constants.NUMBER_ZERO_BITS_ICP_FILTER)
-
+        sk = AugSchemeMPL.key_gen(bytes([0x44] * 32))
+        sig = AugSchemeMPL.sign(sk, b"")
         for _ in range(num_trials):
             challenge_hash = token_bytes(32)
             plot_id = token_bytes(32)
             icp_output = ClassgroupElement.get_default_element()
-            if ProofOfSpace.can_create_proof(constants, plot_id, challenge_hash, icp_output):
+
+            if ProofOfSpace.can_create_proof(constants, plot_id, challenge_hash, icp_output.get_hash(), sig):
                 success_count += 1
 
         assert abs((success_count * target_filter / num_trials) - 1) < 0.3
