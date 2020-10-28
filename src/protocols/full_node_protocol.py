5b4f6eec57691a9236976b7ddc3160c13eb89e35
Mariano Sorgente <sorgente711@gmail.com>
2020-10-28 18:58:25 +0900

    First pass at full node protocol messages

--- src/protocols/full_node_protocol.py
+++ src/protocols/full_node_protocol.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 from typing import List
 
+from src.types.challenge_slot import ChallengeSlot
 from src.types.full_block import FullBlock
+from src.types.reward_chain_end_of_slot import RewardChainEndOfSlot, EndOfSlotProofs
 from src.types.spend_bundle import SpendBundle
 from src.types.header_block import HeaderBlock
 from src.types.unfinished_block import UnfinishedBlock
-from src.types.vdf import ProofOfTime
 from src.types.sized_bytes import bytes32
+from src.types.vdf import VDFInfo, VDFProof
 from src.util.cbor_message import cbor_message
-from src.util.ints import uint8, uint32, uint64, uint128
+from src.util.ints import uint8, uint32, uint64, uint128, int32
 from src.types.peer_info import TimestampedPeerInfo
 
 """
@@ -18,16 +20,99 @@ Protocol between full nodes.
 
 @dataclass(frozen=True)
 @cbor_message
-class NewTip:
-    height: uint32
-    weight: uint128
+class NewPeak:
     header_hash: bytes32
+    sub_block_height: uint32
+    weight: uint128
+    shared_height_with_previous_peak: bytes32
 
 
 @dataclass(frozen=True)
 @cbor_message
-class RemovingTip:
-    header_hash: bytes32
+class RequestProofOfWeight:
+    # TODO(mariano/almog)
+    pass
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RespondProofOfWeight:
+    # TODO(mariano/almog)
+    pass
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RequestSubBlock:
+    height: uint32
+    header_hash: bytes32  # TODO(mariano): check with bram
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RespondSubBlock:
+    sub_block: FullBlock
+
+
+@dataclass(frozen=True)
+@cbor_message
+class NewChallengePoint:
+    index_from_challenge: uint8
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RequestChallengePoint:
+    index_from_challenge: int32
+
+
+@dataclass(frozen=True)
+@cbor_message
+class ChallengePoint:
+    challenge_hash: bytes32
+    index: uint8
+    challenge_chain_vdf: VDFInfo
+    challenge_chain_proof: VDFProof
+    reward_chain_vdf: VDFInfo
+    reward_chain_proof: VDFProof
+
+
+@dataclass(frozen=True)
+@cbor_message
+class NewUnincorporatedEndOfSlot:
+    challenge_height: uint32
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RequestUnincorporatedEndOfSlot:
+    challenge_height: uint32
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RespondUnincorporatedEndOfSlot:
+    challenge_slot: ChallengeSlot
+    reward_slot: RewardChainEndOfSlot
+    slot_proofs: EndOfSlotProofs
+
+
+@dataclass(frozen=True)
+@cbor_message
+class NewUnfinishedSubBlock:
+    unfinished_reward_hash: bytes32
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RequestUnfinishedSubBlock:
+    unfinished_reward_hash: bytes32
+
+
+@dataclass(frozen=True)
+@cbor_message
+class RespondUnfinishedSubBlock:
+    unfinished_sub_block: UnfinishedBlock
 
 
 @dataclass(frozen=True)
@@ -58,111 +143,23 @@ class RejectTransactionRequest:
 
 @dataclass(frozen=True)
 @cbor_message
-class NewProofOfTime:
-    height: uint32
-    challenge_hash: bytes32
-    number_of_iterations: uint64
-    witness_type: uint8
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RequestProofOfTime:
-    height: uint32
-    challenge_hash: bytes32
-    number_of_iterations: uint64
-    witness_type: uint8
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RespondProofOfTime:
-    proof: ProofOfTime
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RejectProofOfTimeRequest:
+class NewCompactVDFProof:
     challenge_hash: bytes32
     number_of_iterations: uint64
 
 
 @dataclass(frozen=True)
 @cbor_message
-class NewUnfinishedBlock:
-    previous_header_hash: bytes32
+class RequestCompactVDFProof:
+    challenge_hash: bytes32
     number_of_iterations: uint64
-    new_header_hash: bytes32
 
 
 @dataclass(frozen=True)
 @cbor_message
-class RequestUnfinishedBlock:
-    header_hash: bytes32
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RespondUnfinishedBlock:
-    block: UnfinishedBlock
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RejectUnfinishedBlockRequest:
-    header_hash: bytes32
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RequestBlock:
-    height: uint32
-    header_hash: bytes32
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RespondBlock:
-    block: FullBlock
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RejectBlockRequest:
-    height: uint32
-    header_hash: bytes32
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RequestAllHeaderHashes:
-    tip_header_hash: bytes32
-
-
-@dataclass(frozen=True)
-@cbor_message
-class AllHeaderHashes:
-    header_hashes: List[bytes32]
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RequestHeaderBlock:
-    height: uint32
-    header_hash: bytes32
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RespondHeaderBlock:
-    header_block: HeaderBlock
-
-
-@dataclass(frozen=True)
-@cbor_message
-class RejectHeaderBlockRequest:
-    height: uint32
-    header_hash: bytes32
+class RespondCompactVDFProof:
+    vdf_info: VDFInfo
+    proof: VDFProof
 
 
 @dataclass(frozen=True)
