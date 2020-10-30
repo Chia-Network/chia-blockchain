5bc7bffa6983c8c5cb039c7f8f9e181f5c5eb976
Will Bendick <wbendick@gmail.com>
2020-10-29 23:38:21 -0700

    rate limited chialisp update (#482)
    * adds chialisp generator
    
    * updates WHOLE_PUZZLE_WITH_CLAWBACK
    
    * updates CLAWBACK
    
    * updates WHOLE_PUZZLE
    
    * updates AGGSIG_ENTIRE_SOLUTION
    
    * fixes indentation
    
    * updates MODE_TWO
    
    * updates CREATE_LOCK
    
    * updates MODE_TWO_ME_STRING
    
    * updates CREATE_CONSOLIDATED
    
    * updates TEMPLATE_SINGLETON_RL_2
    
    * fixes args calculation bug
    
    * updates TEMPLATE_MY_PARENT_ID_2
    
    * updates RATE_LIMIT_PUZZLE
    
    * updates CREATE_NEW_COIN
    
    * updates CREATE_CHANGE
    
    * updates TEMPLATE_MY_ID
    
    * updates TEMPLATE_BLOCK_AGE
    
    * updates TEMPLATE_MY_PARENT_RL
    
    * updates TEMPLATE_MY_PARENT_ID
    
    * fixes lint
    
    * updates rl_make_aggregation_solution
    
    * updates make_clawback_solution
    
    * updates rl_make_solution_mode_2
    
    * updates solution_for_rl
    
    * updates rl_make_aggregation_puzzle
    
    * fixes typo
    
    * adds negative tests

---
+++ src/wallet/chialisp.py
@@ -0,0 +1,105 @@
+def sexp(*argv):
+    return f'({f" ".join([str(arg) for arg in argv])})'
+
+
+def cons(a, b):
+    return sexp("c", a, b)
+
+
+def first(obj):
+    return sexp("f", obj)
+
+
+def rest(obj):
+    return sexp("r", obj)
+
+
+def nth(obj, *path):
+    if not path:
+        return obj
+    if path[0] < 0:
+        raise ValueError
+    if path[0] == 0:
+        return nth(first(obj), *path[1:])
+    else:
+        return nth(rest(obj), *(path[0] - 1,) + path[1:])
+
+
+def args(*path, p=1):
+    if len(path) == 0:
+        return str(p)
+    if path[0] < 0:
+        raise ValueError
+    return args(*path[1:], p=(2 * p << path[0]) | (2 ** path[0] - 1))
+
+
+def eval(code, env=args()):
+    return sexp(cons(code, env))
+
+
+def apply(name, argv):
+    return sexp(*[name] + list(argv))
+
+
+def quote(obj):
+    return sexp("q", obj)
+
+
+nil = quote(sexp())
+
+
+def make_if(predicate, true_expression, false_expression):
+    return eval(
+        apply("i", [predicate, quote(true_expression), quote(false_expression)])
+    )
+
+
+def make_list(*argv, terminator=nil):
+    if len(argv) == 0:
+        return terminator
+    else:
+        return cons(argv[0], make_list(*argv[1:], terminator=terminator))
+
+
+def fail(*argv):
+    return apply("x", argv)
+
+
+def sha256(*argv):
+    return apply("sha256", argv)
+
+
+def sha256tree(*argv):
+    return apply("sha256tree", argv)
+
+
+def equal(*argv):
+    return apply("=", argv)
+
+
+def multiply(*argv):
+    return apply("*", argv)
+
+
+def add(*argv):
+    return apply("+", argv)
+
+
+def subtract(*argv):
+    return apply("-", argv)
+
+
+def is_zero(obj):
+    return equal(obj, quote("0"))
+
+
+def iff(*argv):
+    return apply("i", argv)
+
+
+def hexstr(str):
+    return quote(f"0x{str}")
+
+
+def greater(*argv):
+    return apply(">", argv)
