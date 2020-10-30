c3ef41f36149de7e32596382d7d29d89cb818695
Mariano Sorgente <sorgente711@gmail.com>
2020-10-30 15:56:05 +0900

    Support for reorging genesis block

--- src/consensus/find_fork_point.py
+++ src/consensus/find_fork_point.py
@@ -1,10 +1,12 @@
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 from src.util.ints import uint32
 
 
-def find_fork_point_in_chain(hash_to_block: Dict, block_1: Any, block_2: Any) -> uint32:
+def find_fork_point_in_chain(hash_to_block: Dict, block_1: Any, block_2: Any) -> int:
     """Tries to find height where new chain (block_2) diverged from block_1 (assuming prev blocks
-    are all included in chain)"""
+    are all included in chain)
+    Returns -1 if chains have no common ancestor
+    """
     while block_2.height > 0 or block_1.height > 0:
         if block_2.height > block_1.height:
             block_2 = hash_to_block[block_2.prev_header_hash]
@@ -15,5 +17,9 @@ def find_fork_point_in_chain(hash_to_block: Dict, block_1: Any, block_2: Any) ->
                 return block_2.height
             block_2 = hash_to_block[block_2.prev_header_hash]
             block_1 = hash_to_block[block_1.prev_header_hash]
-    assert block_2 == block_1  # Genesis block is the same, genesis fork
-    return uint32(0)
+    if block_2 != block_1:
+        # All blocks are different
+        return -1
+
+    # First block is the same
+    return 0
