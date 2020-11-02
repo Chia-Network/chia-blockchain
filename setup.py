499977329a69cfa5f1a54bf0228dacc9e0acd339
Richard Kiss <him@richardkiss.com>
2020-11-02 14:01:41 -0800

    Use updated `clvm` and `clvm_tools`.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ dependencies = [
     "chiavdf==0.12.26",  # timelord and vdf verification
     "chiabip158==0.17",  # bip158-style wallet filters
     "chiapos==0.12.36",  # proof of space
-    "clvm==0.5.3",
-    "clvm_tools==0.1.7",
+    "clvm==0.6",
+    "clvm_tools==0.1.8",
     "aiohttp==3.7.3",  # HTTP server for full node rpc
     "aiosqlite@git+https://github.com/mariano54/aiosqlite.git@28cb5754deec562ac931da8fca799fb82df97a12#egg=aiosqlite",
     # asyncio wrapper for sqlite, to store blocks
