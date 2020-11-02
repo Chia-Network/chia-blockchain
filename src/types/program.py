2f1e20034177c42e6fd757898d03a613e1f8cb92
Richard Kiss <him@richardkiss.com>
2020-11-02 14:01:41 -0800

    Use updated `clvm` and `clvm_tools`.

--- src/types/program.py
+++ src/types/program.py
@@ -37,11 +37,6 @@ class Program(SExp):
     A thin wrapper around s-expression data intended to be invoked with "eval".
     """
 
-    def __init__(self, v):
-        if isinstance(v, SExp):
-            v = v.v
-        super(Program, self).__init__(v)
-
     @classmethod
     def parse(cls, f):
         return sexp_from_stream(f, cls.to)
