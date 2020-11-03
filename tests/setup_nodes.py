6d8e0e0473f9361539f87868e2b65f1282d3305a
Mariano Sorgente <sorgente711@gmail.com>
2020-11-03 20:50:18 +0900

    More progress on validation

--- tests/setup_nodes.py
+++ tests/setup_nodes.py
@@ -28,7 +28,7 @@ from tests.time_out_assert import time_out_assert
 test_constants = constants.replace(
     **{
         "DIFFICULTY_STARTING": 1,
-        "DISCRIMINANT_SIZE_BITS": 8,
+        "DISCRIMINANT_SIZE_BITS": 16,
         "SUB_EPOCH_SUB_BLOCKS": 128,
         "EPOCH_SUB_BLOCKS": 512,
         "IPS_STARTING": 10 * 1,
