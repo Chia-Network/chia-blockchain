62c62ef58c355ca61a45e24106e73db391104866
Matthew Howard <matt@chia.net>
2020-11-03 14:35:54 +0000

    swapped standard puzzle to use aggsig_me
    fixed bug in sign_coin_solutions where aggsig_me would not be signed 
    correctly

--- src/wallet/sign_coin_solutions.py
+++ src/wallet/sign_coin_solutions.py
@@ -13,7 +13,8 @@ async def sign_coin_solutions(
     secret_key_for_public_key_f: Callable[[bytes], Optional[PrivateKey]],
 ) -> SpendBundle:
     signatures = []
-
+    pk_list = []
+    msg_list = []
     for coin_solution in coin_solutions:
         # Get AGGSIG conditions
         err, conditions_dict, cost = conditions_dict_for_solution(
@@ -25,16 +26,20 @@ async def sign_coin_solutions(
 
         # Create signature
         for _, msg in pkm_pairs_for_conditions_dict(
-            conditions_dict, bytes(coin_solution.coin)
+            conditions_dict, bytes(coin_solution.coin.name())
         ):
+            pk_list.append(_)
+            msg_list.append(msg)
             secret_key = secret_key_for_public_key_f(_)
             if secret_key is None:
                 e_msg = f"no secret key for {_}"
                 raise ValueError(e_msg)
             assert bytes(secret_key.get_g1()) == bytes(_)
             signature = AugSchemeMPL.sign(secret_key, msg)
+            assert AugSchemeMPL.verify(_, msg, signature)
             signatures.append(signature)
 
     # Aggregate signatures
     aggsig = AugSchemeMPL.aggregate(signatures)
+    assert AugSchemeMPL.aggregate_verify(pk_list, msg_list, aggsig)
     return SpendBundle(coin_solutions, aggsig)
