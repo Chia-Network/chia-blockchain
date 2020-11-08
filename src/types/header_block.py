3510a4d4435acd119c29203b20233dff126b7cff
almog <almogdepaz@gmail.com>
2020-11-08 17:47:32 +0200

    add logs to validation errors, need to add more logs

--- src/types/header_block.py
+++ src/types/header_block.py
@@ -42,3 +42,7 @@ class HeaderBlock(Streamable):
     @property
     def total_iters(self):
         return self.reward_chain_sub_block.total_iters
+
+    @property
+    def log_string(self):
+        return "block " + str(self.header_hash) + " height " + str(self.height) + " "
