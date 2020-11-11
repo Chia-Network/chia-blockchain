def37c426b791cd65f10e9224142d10da808c8f6
Will Bendick <wbendick@gmail.com>
2020-11-11 09:22:29 -0800

    uses taproot for standard transactions

--- tests/keys.py
+++ tests/keys.py
@@ -1,6 +1,6 @@
 from blspy import AugSchemeMPL
 
-from src.wallet.puzzles import p2_delegated_puzzle
+from src.wallet.puzzles.p2_delegated_puzzle_or_hidden_puzzle import puzzle_for_pk
 from src.util.ints import uint32
 from src.wallet.derive_keys import master_sk_to_wallet_sk
 
@@ -8,6 +8,4 @@ MASTER_KEY = AugSchemeMPL.key_gen(bytes([1] * 32))
 
 
 def puzzle_program_for_index(index: uint32):
-    return p2_delegated_puzzle.puzzle_for_pk(
-        bytes(master_sk_to_wallet_sk(MASTER_KEY, index).get_g1())
-    )
+    return puzzle_for_pk(bytes(master_sk_to_wallet_sk(MASTER_KEY, index).get_g1()))
