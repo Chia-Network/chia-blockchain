4cb7b2d14b46dfef5d594dfb8c8d975319f55d5f
almog <almogdepaz@gmail.com>
2020-11-12 11:51:21 +0200

    weight proof type

--- src/types/weight_proof.py
+++ src/types/weight_proof.py
@@ -10,42 +10,17 @@ from src.types.vdf import VDFProof
 from src.types.sized_bytes import bytes32
 
 from src.util import streamable
-from src.util.ints import uint64, uint32
+from src.util.ints import uint64, uint32, uint8
 from src.util.streamable import Streamable
 
 
 @dataclass(frozen=True)
 @streamable
-class InfusedChallengeChains(Streamable):
-
-    # VDF from beginning to end of subslot
-    start_to_end_sublot_vdf: Optional[VDFProof]  # if not infused
-
-    # Proof of space
-    proof_of_space: Optional[ProofOfSpace]  # if infused
-
-    # VDF to signage point
-    signage_point_vdf: Optional[VDFProof]  # if infused
-
-    # VDF to signage point
-    signage_point_vdf: Optional[VDFProof]  # if infused
-
-    # VDF from infusion point to end of subslot
-    infusion_to_slot_end_vdf: Optional[VDFProof]  # if infused
-
-
-@dataclass(frozen=True)
-@streamable
 class SubEpochData(Streamable):
-
-    infused_challenge_chains = List[InfusedChallengeChains]
-    # Hash of previous subepoch final reward chain slot end
-    final_reward_chain_hash: bytes32
+    reward_chain_hash: bytes32  # hash of reward chain at end of last segment
     # Number of subblocks overflow in previous subepoch
-    prev_epoch_overflow_count: uint32
-    # Hash of final challenge slot end
-    final_challenge_chain_hash: bytes32
-    # (at beginning of work difficulty reset) New work difficulty and iterations per subslot
+    previous_sub_epoch_overflows: uint8
+    # (at end of epoch) New work difficulty and iterations per subslot
     sub_slot_iters: Optional[uint64]
     new_difficulty: Optional[uint64]
 
@@ -53,7 +28,7 @@ class SubEpochData(Streamable):
 @dataclass(frozen=True)
 @streamable
 class SubepochChallengeSegment(Streamable):
-
+    sub_epoch_n: uint32
     # Proof of space
     proof_of_space: Optional[ProofOfSpace]  # if infused
     # VDF to signage point
@@ -72,11 +47,6 @@ class SubepochChallengeSegment(Streamable):
 @dataclass(frozen=True)
 @streamable
 class WeightProof(Streamable):
-
-    peak: bytes32
-
-    height: uint64
-
     sub_epoch_data: List[SubEpochData]
 
     sub_epoch_segments: List[SubepochChallengeSegment]
