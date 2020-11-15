b89959d736f2c40f1b99520f06f4b63c7acd7974
almog <almogdepaz@gmail.com>
2020-11-15 21:15:27 +0200

    refactor weight proof

--- tests/full_node/test_weight_proof.py
+++ tests/full_node/test_weight_proof.py
@@ -1,11 +1,13 @@
 import asyncio
-from typing import Dict
+from pathlib import Path
 
+import aiosqlite
 import pytest
-from src.full_node.sub_block_record import SubBlockRecord
 
+from src.full_node.block_store import BlockStore
+from src.full_node.blockchain import Blockchain
+from src.full_node.coin_store import CoinStore
 from src.full_node.make_proof_of_weight import create_sub_epoch_segments, get_sub_epoch_block_num
-from src.types.sized_bytes import bytes32
 from src.util.ints import uint32
 from tests.setup_nodes import test_constants, bt
 
@@ -16,14 +18,35 @@ def event_loop():
     yield loop
 
 
+# todo this is duplicated code from blockchain.py
+@pytest.fixture(scope="function")
+async def empty_blockchain():
+    """
+    Provides a list of 10 valid blocks, as well as a blockchain with 9 blocks added to it.
+    """
+    db_path = Path("blockchain_test.db")
+    if db_path.exists():
+        db_path.unlink()
+    connection = await aiosqlite.connect(db_path)
+    coin_store = await CoinStore.create(connection)
+    store = await BlockStore.create(connection)
+    bc1 = await Blockchain.create(coin_store, store, test_constants)
+    assert bc1.get_peak() is None
+
+    yield bc1
+
+    await connection.close()
+    bc1.shut_down()
+
+
 class TestWeightProof:
     @pytest.mark.asyncio
     async def test_create_sub_epoch_segments(self, empty_blockchain):
         assert empty_blockchain.get_peak() is None
         blocks = bt.get_consecutive_blocks(test_constants, 500)
 
-        for block in range(blocks):
-            empty_blockchain.receive_block(blocks)
+        for block in blocks:
+            empty_blockchain.receive_block(block)
         curr = blocks[-1]
         while True:
             # next sub block
