071e692ea53cf41aaf1a121ecee9f0dde01c0a99
Mariano Sorgente <sorgente711@gmail.com>
2020-11-16 17:06:40 +0900

    Update syncing code

--- src/full_node/sync_store.py
+++ src/full_node/sync_store.py
@@ -12,33 +12,30 @@ log = logging.getLogger(__name__)
 class SyncStore:
     # Whether or not we are syncing
     sync_mode: bool
-    # Whether we are waiting for tips (at the start of sync) or already syncing
-    waiting_for_tips: bool
-    # Potential new tips that we have received from others.
-    potential_tips: Dict[bytes32, FullBlock]
-    # List of all header hashes up to the tip, download up front
-    potential_hashes: List[bytes32]
+    # Whether we are waiting for peaks (at the start of sync) or already syncing
+    waiting_for_peaks: bool
+    # Potential new peaks that we have received from others.
+    potential_peaks: Dict[bytes32, FullBlock]
     # Blocks received from other peers during sync
     potential_blocks: Dict[uint32, FullBlock]
-    # Event to signal when header hashes are received
-    potential_hashes_received: Optional[asyncio.Event]
     # Event to signal when blocks are received at each height
     potential_blocks_received: Dict[uint32, asyncio.Event]
     # Blocks that we have finalized during sync, queue them up for adding after sync is done
     potential_future_blocks: List[FullBlock]
+    # A map from height to header hash of sub-blocks added to the chain
+    header_hashes_added: Dict[uint32, bytes32]
 
     @classmethod
     async def create(cls):
         self = cls()
 
         self.sync_mode = False
-        self.waiting_for_tips = True
-        self.potential_tips = {}
-        self.potential_hashes = []
+        self.waiting_for_peaks = True
+        self.potential_peaks = {}
         self.potential_blocks = {}
-        self.potential_hashes_received = None
         self.potential_blocks_received = {}
         self.potential_future_blocks = []
+        self.header_hashes_added = {}
         return self
 
     def set_sync_mode(self, sync_mode: bool) -> None:
@@ -47,42 +44,31 @@ class SyncStore:
     def get_sync_mode(self) -> bool:
         return self.sync_mode
 
-    def set_waiting_for_tips(self, waiting_for_tips: bool) -> None:
-        self.waiting_for_tips = waiting_for_tips
-
-    def get_waiting_for_tips(self) -> bool:
-        return self.waiting_for_tips
-
     async def clear_sync_info(self):
-        self.potential_tips.clear()
+        self.potential_peaks.clear()
         self.potential_blocks.clear()
         self.potential_blocks_received.clear()
         self.potential_future_blocks.clear()
-        self.waiting_for_tips = True
+        self.header_hashes_added.clear()
+        self.waiting_for_peaks = True
 
-    def get_potential_tips_tuples(self) -> List[Tuple[bytes32, FullBlock]]:
-        return list(self.potential_tips.items())
+    def get_potential_peaks_tuples(self) -> List[Tuple[bytes32, FullBlock]]:
+        return list(self.potential_peaks.items())
 
-    def add_potential_tip(self, block: FullBlock) -> None:
-        self.potential_tips[block.header_hash] = block
+    def add_potential_peak(self, block: FullBlock) -> None:
+        self.potential_peaks[block.header_hash] = block
 
-    def get_potential_tip(self, header_hash: bytes32) -> Optional[FullBlock]:
-        return self.potential_tips.get(header_hash, None)
-
-    def set_potential_hashes(self, potential_hashes: List[bytes32]) -> None:
-        self.potential_hashes = potential_hashes
-
-    def get_potential_hashes(self) -> List[bytes32]:
-        return self.potential_hashes
-
-    def set_potential_hashes_received(self, event: asyncio.Event):
-        self.potential_hashes_received = event
-
-    def get_potential_hashes_received(self) -> Optional[asyncio.Event]:
-        return self.potential_hashes_received
+    def get_potential_peak(self, header_hash: bytes32) -> Optional[FullBlock]:
+        return self.potential_peaks.get(header_hash, None)
 
     def add_potential_future_block(self, block: FullBlock):
         self.potential_future_blocks.append(block)
 
     def get_potential_future_blocks(self):
         return self.potential_future_blocks
+
+    def add_header_hashes_added(self, height: uint32, header_hash: bytes32):
+        self.header_hashes_added[height] = header_hash
+
+    def get_header_hashes_added(self, height: uint32) -> Optional[bytes32]:
+        return self.header_hashes_added.get(height, None)
