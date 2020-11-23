ccfc740db95f08027aba4c38c5590f5785421a50
Mariano Sorgente <sorgente711@gmail.com>
2020-11-23 17:07:39 +0900

    Fix difficulty adjustment bug

--- src/consensus/block_header_validation.py
+++ src/consensus/block_header_validation.py
@@ -7,7 +7,11 @@ from blspy import AugSchemeMPL
 
 from src.consensus.constants import ConsensusConstants
 from src.consensus.deficit import calculate_deficit
-from src.consensus.difficulty_adjustment import finishes_sub_epoch, get_ips_and_difficulty
+from src.consensus.difficulty_adjustment import (
+    finishes_sub_epoch,
+    get_ips_and_difficulty,
+)
+from src.consensus.get_block_challenge import get_block_challenge
 from src.consensus.make_sub_epoch_summary import make_sub_epoch_summary
 from src.consensus.pot_iterations import (
     is_overflow_sub_block,
@@ -67,6 +71,7 @@ async def validate_unfinished_header_block(
             constants, prev_sb.height, prev_sb.deficit, True, sub_blocks, prev_sb.prev_hash
         )
 
+        # Gets the difficulty and IPS for this sub-block
         ips, difficulty = get_ips_and_difficulty(constants, header_block, height_to_hash, prev_sb, sub_blocks)
 
     overflow = is_overflow_sub_block(constants, header_block.reward_chain_sub_block.signage_point_index)
@@ -592,45 +597,6 @@ async def validate_unfinished_header_block(
                 return None, ValidationError(Err.TIMESTAMP_TOO_FAR_IN_FUTURE)
 
     return required_iters, None  # Valid unfinished header block
-
-
-def get_block_challenge(
-    constants: ConsensusConstants,
-    genesis_block: bool,
-    header_block: UnfinishedHeaderBlock,
-    new_sub_slot: bool,
-    overflow: bool,
-    prev_sb: SubBlockRecord,
-    sub_blocks: Dict[bytes32, SubBlockRecord],
-):
-    if new_sub_slot:
-        if overflow:
-            # New slot with overflow block
-            challenge: bytes32 = header_block.finished_sub_slots[
-                -1
-            ].challenge_chain.challenge_chain_end_of_slot_vdf.challenge_hash
-        else:
-            # No overflow, new slot with a new challenge
-            challenge: bytes32 = header_block.finished_sub_slots[-1].challenge_chain.get_hash()
-    else:
-        if genesis_block:
-            challenge = constants.FIRST_CC_CHALLENGE
-        else:
-            if overflow:
-                # Overflow infusion, so get the second to last challenge
-                challenges_to_look_for = 2
-            else:
-                challenges_to_look_for = 1
-            reversed_challenge_hashes: List[bytes32] = []
-            curr: SubBlockRecord = prev_sb
-            while len(reversed_challenge_hashes) < challenges_to_look_for:
-                if curr.first_in_sub_slot:
-                    reversed_challenge_hashes += reversed(curr.finished_challenge_slot_hashes)
-                if curr.height == 0:
-                    break
-                curr = sub_blocks[curr.prev_hash]
-            challenge = reversed_challenge_hashes[challenges_to_look_for - 1]
-    return challenge
 
 
 async def validate_finished_header_block(
