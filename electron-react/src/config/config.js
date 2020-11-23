c75798922bdeaec8b53e76e0bacf7cb04db589a8
Zlatko Fedor <zfedor@gmail.com>
2020-11-23 18:14:19 +0100

    slovak translation correction

--- electron-react/src/config/config.js
+++ electron-react/src/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  local_test: process.env.NODE_ENV === 'development',
+  local_test: process.env.NODE_ENV === 'development' && process.env.TESTNET !== 'true',
   backup_host: 'https://backup.chia.net',
 };
