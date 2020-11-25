1023e38331de9eb1316da5c37f5d27315375edb5
almog <almogdepaz@gmail.com>
2020-11-25 14:49:44 +0200

    create persistent test database copy

--- tests/full_node/test_weight_proof.py
+++ tests/full_node/test_weight_proof.py
@@ -1,8 +1,10 @@
 import asyncio
-from typing import Dict
+import pickle
+from typing import Dict, List
 import pytest
 
 from src.consensus.blockchain import ReceiveBlockResult
+from src.types.full_block import FullBlock
 from tests.full_node.test_blockchain import empty_blockchain
 from src.full_node.weight_proof import (
     create_sub_epoch_segments,
@@ -13,6 +15,7 @@ from src.types.header_block import HeaderBlock
 from src.types.sized_bytes import bytes32
 from src.util.ints import uint32
 from tests.setup_nodes import test_constants, bt
+from os import path
 
 
 @pytest.fixture(scope="module")
@@ -21,9 +24,36 @@ def event_loop():
     yield loop
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 async def default_blocks():
-    yield bt.get_consecutive_blocks(200)
+    # try loading from disc, if not create new blocks.db file
+    db_name = "weight_proof_blocks.db"
+    num_of_blocks = 200
+    if path.exists(db_name):
+        file = open(db_name, "rb")
+        block_bytes_list: List[bytes] = pickle.load(file)
+        blocks: List[FullBlock] = []
+        for block_bytes in block_bytes_list:
+            blocks.append(FullBlock.from_bytes(block_bytes))
+        file.close()
+        print(f"loaded {db_name} with {len(blocks)} blocks")
+        if len(blocks) != num_of_blocks:
+            blocks = new_test_db(db_name, num_of_blocks)
+    else:
+        blocks = new_test_db(db_name, num_of_blocks)
+    yield blocks
+
+
+def new_test_db(db_name, num_of_blocks):
+    print(f"create {db_name} with {num_of_blocks} blocks")
+    file = open(db_name, "wb+")
+    blocks: List[FullBlock] = bt.get_consecutive_blocks(num_of_blocks)
+    block_bytes_list: List[bytes] = []
+    for block in blocks:
+        block_bytes_list.append(bytes(block))
+    pickle.dump(block_bytes_list, file)
+    file.close()
+    return blocks
 
 
 def count_sub_epochs(blockchain, last_hash) -> int:
@@ -55,6 +85,10 @@ def get_sub_epoch_start(blockchain, last_hash):
 
 
 class TestWeightProof:
+    @pytest.mark.asyncio
+    async def test_default_blocks(self, empty_blockchain, default_blocks):
+        assert len(default_blocks) == 200
+
     @pytest.mark.asyncio
     async def test_get_sub_epoch_block_num_basic(self, empty_blockchain, default_blocks):
         assert empty_blockchain.get_peak() is None
