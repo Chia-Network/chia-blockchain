7e82f3db8a25b294069050efbd8d405af7998092
Mariano Sorgente <sorgente711@gmail.com>
2020-11-25 23:03:39 +0900

    Fix long range reorg bug

--- src/consensus/default_constants.py
+++ src/consensus/default_constants.py
@@ -5,7 +5,7 @@ testnet_kwargs = {
     # TODO(mariano): write comments here
     "SLOT_SUB_BLOCKS_TARGET": 32,
     "MIN_SUB_BLOCKS_PER_CHALLENGE_BLOCK": 16,
-    "MAX_SLOT_SUB_BLOCKS": 128,
+    "MAX_SUB_SLOT_SUB_BLOCKS": 128,
     "NUM_SPS_SUB_SLOT": 64,
     "SUB_SLOT_ITERS_STARTING": 2 ** 25,
     # DIFFICULTY_STARTING is the starting difficulty for the first epoch, which is then further
@@ -18,7 +18,7 @@ testnet_kwargs = {
     "SIGNIFICANT_BITS": 12,  # The number of bits to look at in difficulty and min iters. The rest are zeroed
     "DISCRIMINANT_SIZE_BITS": 1024,  # Max is 1024 (based on ClassGroupElement int size)
     "NUMBER_ZERO_BITS_PLOT_FILTER": 9,  # H(plot signature of the challenge) must start with these many zeroes
-    "SLOT_TIME_TARGET": 600,  # The target number of seconds per slot
+    "SUB_SLOT_TIME_TARGET": 600,  # The target number of seconds per slot
     "NUM_SP_INTERVALS_EXTRA": 3,  # The number of sp intervals to add to the signage point
     "MAX_FUTURE_TIME": 7200,  # The next block can have a timestamp of at most these many seconds more
     "NUMBER_OF_TIMESTAMPS": 11,  # Than the average of the last NUMBER_OF_TIMESTAMPS blocks
