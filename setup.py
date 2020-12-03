8f29747bad24ee589402084aa4cb0a298783fc5e
Gene Hoffman <hoffmang@hoffmang.com>
2020-12-03 11:19:17 -0800

    Create bug fix release Beta 18

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 dependencies = [
     "aiter==0.13.20191203",  # Used for async generator tools
-    "blspy==0.2.4",  # Signature library
+    "blspy==0.2.9",  # Signature library
     "chiavdf==0.12.26",  # timelord and vdf verification
-    "chiabip158==0.16",  # bip158-style wallet filters
-    "chiapos==0.12.33",  # proof of space
+    "chiabip158==0.17",  # bip158-style wallet filters
+    "chiapos==0.12.37",  # proof of space
     "clvm==0.5.3",
     "clvm_tools==0.1.6",
     "aiohttp==3.6.3",  # HTTP server for full node rpc
