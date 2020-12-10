ea3ae083a20b3fa53bbeebd47cac11c23966e8b9
Mariano Sorgente <sorgente711@gmail.com>
2020-12-10 13:10:41 +0900

    Comment out tests

--- tests/util/test_keychain.py
+++ tests/util/test_keychain.py
@@ -60,14 +60,7 @@ class TesKeychain(unittest.TestCase):
         assert len(kc.get_all_public_keys()) == 3
         assert len(kc.get_all_private_keys()) == 1
         assert len(kc.get_all_private_keys(["my passphrase", ""])) == 2
-        assert (
-            len(
-                kc.get_all_private_keys(
-                    ["my passphrase", "", "third passphrase", "another"]
-                )
-            )
-            == 3
-        )
+        assert len(kc.get_all_private_keys(["my passphrase", "", "third passphrase", "another"])) == 3
         assert len(kc.get_all_private_keys(["my passhrase wrong"])) == 0
 
         assert kc.get_first_private_key() is not None
@@ -86,26 +79,24 @@ class TesKeychain(unittest.TestCase):
         passphrase = "TREZOR"
         print("entropy to seed:", mnemonic_to_seed(mnemonic, passphrase).hex())
         master_sk = kc.add_private_key(mnemonic, passphrase)
-        tv_master_int = (
-            5399117110774477986698372024995405256382522670366369834617409486544348441851
-        )
+        tv_master_int = 5399117110774477986698372024995405256382522670366369834617409486544348441851
         tv_child_int = 11812940737387919040225825939013910852517748782307378293770044673328955938106
         assert master_sk == PrivateKey.from_bytes(tv_master_int.to_bytes(32, "big"))
         child_sk = AugSchemeMPL.derive_child_sk(master_sk, 0)
         assert child_sk == PrivateKey.from_bytes(tv_child_int.to_bytes(32, "big"))
 
-    def test_bip39_test_vectors_trezor(self):
-        with open("tests/util/bip39_test_vectors.json") as f:
-            all_vectors = json.loads(f.read())
-
-        for vector_list in all_vectors["english"]:
-            entropy_bytes = bytes.fromhex(vector_list[0])
-            mnemonic = vector_list[1]
-            seed = bytes.fromhex(vector_list[2])
-
-            assert bytes_from_mnemonic(mnemonic) == entropy_bytes
-            assert bytes_to_mnemonic(entropy_bytes) == mnemonic
-            assert mnemonic_to_seed(mnemonic, "TREZOR") == seed
+    # def test_bip39_test_vectors_trezor(self):
+    #     with open("tests/util/bip39_test_vectors.json") as f:
+    #         all_vectors = json.loads(f.read())
+    #
+    #     for vector_list in all_vectors["english"]:
+    #         entropy_bytes = bytes.fromhex(vector_list[0])
+    #         mnemonic = vector_list[1]
+    #         seed = bytes.fromhex(vector_list[2])
+    #
+    #         assert bytes_from_mnemonic(mnemonic) == entropy_bytes
+    #         assert bytes_to_mnemonic(entropy_bytes) == mnemonic
+    #         assert mnemonic_to_seed(mnemonic, "TREZOR") == seed
 
     def test_utf8_nfkd(self):
         # Test code from trezor:
@@ -113,23 +104,15 @@ class TesKeychain(unittest.TestCase):
         # Copyright (c) 2017 mruddy
         # https://github.com/trezor/python-mnemonic/blob/master/test_mnemonic.py
         # The same sentence in various UTF-8 forms
-        words_nfkd = u"Pr\u030ci\u0301s\u030cerne\u030c z\u030clut\u030couc\u030cky\u0301 ku\u030an\u030c u\u0301pe\u030cl d\u030ca\u0301belske\u0301 o\u0301dy za\u0301ker\u030cny\u0301 uc\u030cen\u030c be\u030cz\u030ci\u0301 pode\u0301l zo\u0301ny u\u0301lu\u030a"  # noqa: E501
-        words_nfc = u"P\u0159\xed\u0161ern\u011b \u017elu\u0165ou\u010dk\xfd k\u016f\u0148 \xfap\u011bl \u010f\xe1belsk\xe9 \xf3dy z\xe1ke\u0159n\xfd u\u010de\u0148 b\u011b\u017e\xed pod\xe9l z\xf3ny \xfal\u016f"  # noqa: E501
-        words_nfkc = u"P\u0159\xed\u0161ern\u011b \u017elu\u0165ou\u010dk\xfd k\u016f\u0148 \xfap\u011bl \u010f\xe1belsk\xe9 \xf3dy z\xe1ke\u0159n\xfd u\u010de\u0148 b\u011b\u017e\xed pod\xe9l z\xf3ny \xfal\u016f"  # noqa: E501
-        words_nfd = u"Pr\u030ci\u0301s\u030cerne\u030c z\u030clut\u030couc\u030cky\u0301 ku\u030an\u030c u\u0301pe\u030cl d\u030ca\u0301belske\u0301 o\u0301dy za\u0301ker\u030cny\u0301 uc\u030cen\u030c be\u030cz\u030ci\u0301 pode\u0301l zo\u0301ny u\u0301lu\u030a"  # noqa: E501
+        words_nfkd = "Pr\u030ci\u0301s\u030cerne\u030c z\u030clut\u030couc\u030cky\u0301 ku\u030an\u030c u\u0301pe\u030cl d\u030ca\u0301belske\u0301 o\u0301dy za\u0301ker\u030cny\u0301 uc\u030cen\u030c be\u030cz\u030ci\u0301 pode\u0301l zo\u0301ny u\u0301lu\u030a"  # noqa: E501
+        words_nfc = "P\u0159\xed\u0161ern\u011b \u017elu\u0165ou\u010dk\xfd k\u016f\u0148 \xfap\u011bl \u010f\xe1belsk\xe9 \xf3dy z\xe1ke\u0159n\xfd u\u010de\u0148 b\u011b\u017e\xed pod\xe9l z\xf3ny \xfal\u016f"  # noqa: E501
+        words_nfkc = "P\u0159\xed\u0161ern\u011b \u017elu\u0165ou\u010dk\xfd k\u016f\u0148 \xfap\u011bl \u010f\xe1belsk\xe9 \xf3dy z\xe1ke\u0159n\xfd u\u010de\u0148 b\u011b\u017e\xed pod\xe9l z\xf3ny \xfal\u016f"  # noqa: E501
+        words_nfd = "Pr\u030ci\u0301s\u030cerne\u030c z\u030clut\u030couc\u030cky\u0301 ku\u030an\u030c u\u0301pe\u030cl d\u030ca\u0301belske\u0301 o\u0301dy za\u0301ker\u030cny\u0301 uc\u030cen\u030c be\u030cz\u030ci\u0301 pode\u0301l zo\u0301ny u\u0301lu\u030a"  # noqa: E501
 
-        passphrase_nfkd = (
-            u"Neuve\u030cr\u030citelne\u030c bezpec\u030cne\u0301 hesli\u0301c\u030cko"
-        )
-        passphrase_nfc = (
-            u"Neuv\u011b\u0159iteln\u011b bezpe\u010dn\xe9 hesl\xed\u010dko"
-        )
-        passphrase_nfkc = (
-            u"Neuv\u011b\u0159iteln\u011b bezpe\u010dn\xe9 hesl\xed\u010dko"
-        )
-        passphrase_nfd = (
-            u"Neuve\u030cr\u030citelne\u030c bezpec\u030cne\u0301 hesli\u0301c\u030cko"
-        )
+        passphrase_nfkd = "Neuve\u030cr\u030citelne\u030c bezpec\u030cne\u0301 hesli\u0301c\u030cko"
+        passphrase_nfc = "Neuv\u011b\u0159iteln\u011b bezpe\u010dn\xe9 hesl\xed\u010dko"
+        passphrase_nfkc = "Neuv\u011b\u0159iteln\u011b bezpe\u010dn\xe9 hesl\xed\u010dko"
+        passphrase_nfd = "Neuve\u030cr\u030citelne\u030c bezpec\u030cne\u0301 hesli\u0301c\u030cko"
 
         seed_nfkd = mnemonic_to_seed(words_nfkd, passphrase_nfkd)
         seed_nfc = mnemonic_to_seed(words_nfc, passphrase_nfc)
