5be507854eecc58dc13d4cbf98a0b7782dc6d394
Mariano Sorgente <sorgente711@gmail.com>
2020-12-11 15:35:08 +0900

    Fix weight issue, and restart timelord if necessary

--- src/timelord/timelord_api.py
+++ src/timelord/timelord_api.py
@@ -34,11 +34,10 @@ class TimelordAPI:
             ):
                 log.info("Skipping peak, already have.")
                 return
-            elif self.timelord.last_state.get_weight() == new_peak.reward_chain_sub_block.weight:
-                log.warning("Different sub-block at same weight, changing to it.")
-                self.timelord.new_peak = new_peak
             else:
-                log.info("Peak at smaller weight, skipping")
+                log.warning("Sub-block that we don't have, changing to it.")
+                self.timelord.new_peak = new_peak
+                self.timelord.new_subslot_end = None
 
     @api_request
     async def new_unfinished_sub_block(self, new_unfinished_subblock: timelord_protocol.NewUnfinishedSubBlock):
