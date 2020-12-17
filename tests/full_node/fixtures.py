328662bd2785d040d31b0328de98e8f630f4dccd
Gene Hoffman <hoffmang@hoffmang.com>
2020-12-17 03:05:59 -0800

    re-enable all the tests

--- tests/full_node/fixtures.py
+++ tests/full_node/fixtures.py
@@ -16,7 +16,6 @@ from os import path
 async def empty_blockchain():
     """
     Provides a list of 10 valid blocks, as well as a blockchain with 9 blocks added to it.
-    Break cache = 1
     """
     db_path = Path("blockchain_test.db")
     if db_path.exists():
