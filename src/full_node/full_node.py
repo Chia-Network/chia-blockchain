d6397c3f5493dfb138e118ab2cef0b995869f6cc
Yostra <straya@chia.net>
2020-12-21 14:08:06 -0500

    b size

--- src/full_node/full_node.py
+++ src/full_node/full_node.py
@@ -395,7 +395,7 @@ class FullNode:
 
         batch_size = self.constants.MAX_BLOCK_COUNT_PER_REQUESTS
         for i in range(fork_point_height, target_peak_sb_height, batch_size):
-            start_height = fork_point_height + (i * batch_size)
+            start_height = fork_point_height
             end_height = min(target_peak_sb_height, start_height + batch_size)
             request = RequestSubBlocks(uint32(start_height), uint32(end_height), True)
             peers_to_remove = []
