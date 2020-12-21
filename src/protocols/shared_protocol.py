c1caf8675a283120b1e52633f2565753dd5db0a1
Yostra <straya@chia.net>
2020-12-21 13:42:58 -0500

    protocol bump

--- src/protocols/shared_protocol.py
+++ src/protocols/shared_protocol.py
@@ -5,7 +5,7 @@ from src.types.sized_bytes import bytes32
 from src.util.cbor_message import cbor_message
 from src.util.ints import uint16
 
-protocol_version = "0.0.22"
+protocol_version = "0.0.23"
 
 """
 Handshake when establishing a connection between two servers.
