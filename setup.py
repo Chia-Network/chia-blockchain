558427f83bb6795ca2752676ebda2967e9e26dd2
Gene Hoffman <hoffmang@hoffmang.com>
2020-12-20 19:59:12 -0800

    chiavdf to 0.13.1b2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 dependencies = [
     "aiter==0.13.20191203",  # Used for async generator tools
     "blspy==0.3.0",  # Signature library
-    "chiavdf==0.13.0",  # timelord and vdf verification
+    "chiavdf==0.13.1b2",  # timelord and vdf verification
     "chiabip158==0.17",  # bip158-style wallet filters
     "chiapos==0.12.40",  # proof of space
     "clvm==0.7",
