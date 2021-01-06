04f66865aa9db9c19527f13c10db3767c66d8773
Gene Hoffman <hoffmang@hoffmang.com>
2021-01-05 18:37:56 -0800

    bump chiavdf, blspy for gmp-6.2.1

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 dependencies = [
     "aiter==0.13.20191203",  # Used for async generator tools
-    "blspy==0.3.0",  # Signature library
-    "chiavdf==0.13.1b2",  # timelord and vdf verification
+    "blspy==0.3.1",  # Signature library
+    "chiavdf==0.13.1",  # timelord and vdf verification
     "chiabip158==0.17",  # bip158-style wallet filters
     "chiapos==0.12.41",  # proof of space
     "clvm==0.7",
