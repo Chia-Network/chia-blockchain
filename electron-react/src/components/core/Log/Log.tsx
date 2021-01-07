2baa890974942681435000c14d63b34743c1bfb4
Zlatko <zlatko@chia.net>
2021-01-07 20:32:38 +0100

    added auto scroll to bottom

--- electron-react/src/components/core/Log/Log.tsx
+++ electron-react/src/components/core/Log/Log.tsx
@@ -1,18 +1,25 @@
 import React, { ReactNode } from 'react';
 import { Paper } from '@material-ui/core';
 import styled from 'styled-components';
+// @ts-ignore
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+const StyledScrollToBottom = styled(ScrollToBottom)`
+  width: 100%;
+  height: 100%;
+`;
 
 const StyledPaper = styled(Paper)`
   background-color: #272c34;
   color: white;
+  min-width: 50vw;
   width: 100%;
   height: 40vh;
-  padding: ${({ theme }) => `${theme.spacing(1)}px ${theme.spacing(2)}px`};
-  overflow: auto;
 
   pre {
     word-break: break-all;
     white-space: pre-wrap;
+    padding: ${({ theme }) => `${theme.spacing(1)}px ${theme.spacing(2)}px`};
   }
 `;
 
@@ -24,9 +31,12 @@ export default function Log(props: Props) {
   const { children } = props;
 
   return (
-    <StyledPaper>
-      <pre>{children}</pre>
-    </StyledPaper>
+    
+      <StyledPaper>
+        <StyledScrollToBottom>
+        <pre>{children}</pre>
+        </StyledScrollToBottom>
+      </StyledPaper>
   );
 }
 
