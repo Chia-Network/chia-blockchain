45990935413dab265e4f24a5a38e431b4ba407f2
Yostra <straya.mark@gmail.com>
2021-01-07 19:48:58 -0500

    farm sub block

--- src/simulator/full_node_simulator.py
+++ src/simulator/full_node_simulator.py
@@ -71,6 +71,32 @@ class FullNodeSimulator(FullNodeAPI):
             await self.full_node.respond_sub_block(rr)
 
     @api_request
+    async def farm_new_sub_block(self, request: FarmNewBlockProtocol):
+        async with self.lock:
+            self.log.info("Farming new block!")
+            current_blocks = await self.get_all_full_blocks()
+            if len(current_blocks) == 0:
+                genesis = self.bt.get_consecutive_blocks(uint8(1))[0]
+                await self.full_node.blockchain.receive_block(genesis)
+
+            peak = self.full_node.blockchain.get_peak()
+            assert peak is not None
+            bundle: Optional[SpendBundle] = await self.full_node.mempool_manager.create_bundle_from_mempool(
+                peak.header_hash
+            )
+            current_blocks = await self.get_all_full_blocks()
+            target = request.puzzle_hash
+            more = self.bt.get_consecutive_blocks(
+                1,
+                transaction_data=bundle,
+                farmer_reward_puzzle_hash=target,
+                pool_reward_puzzle_hash=target,
+                block_list_input=current_blocks,
+            )
+            rr = RespondSubBlock(more[-1])
+            await self.full_node.respond_sub_block(rr)
+
+    @api_request
     async def reorg_from_index_to_new_index(self, request: ReorgProtocol):
         new_index = request.new_index
         old_index = request.old_index
