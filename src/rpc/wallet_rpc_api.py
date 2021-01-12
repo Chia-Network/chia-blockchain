b4fe2a8b9c05976ea68d7f2abbbed34d4381c200
Yostra <straya.mark@gmail.com>
2021-01-11 20:23:10 -0500

    rpc return synced status

--- src/rpc/wallet_rpc_api.py
+++ src/rpc/wallet_rpc_api.py
@@ -252,8 +252,8 @@ class WalletRpcApi:
     async def get_sync_status(self, request: Dict):
         assert self.service.wallet_state_manager is not None
         syncing = self.service.wallet_state_manager.sync_mode
-
-        return {"syncing": syncing}
+        synced = await self.service.wallet_state_manager.synced()
+        return {"synced": synced, "syncing": syncing}
 
     async def get_height_info(self, request: Dict):
         assert self.service.wallet_state_manager is not None
