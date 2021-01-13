5441fd5078b6018b85b46f562eefcb4027c1c458
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2021-01-13 16:38:48 -0500

    Increase test timeout

--- tests/core/full_node/test_full_sync.py
+++ tests/core/full_node/test_full_sync.py
@@ -84,7 +84,7 @@ class TestFullSync:
 
         # The second node should eventually catch up to the first one, and have the
         # same tip at height num_blocks - 1 (or at least num_blocks - 3, in case we sync to below the tip)
-        await time_out_assert(60, node_height_at_least, True, full_node_2, num_blocks - 1)
+        await time_out_assert(120, node_height_at_least, True, full_node_2, num_blocks - 1)
 
     @pytest.mark.asyncio
     async def test_short_sync(self, two_nodes):
