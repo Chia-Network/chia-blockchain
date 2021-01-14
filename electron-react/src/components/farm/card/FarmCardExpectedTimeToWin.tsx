1d345ad949ca64d22d7952e07d98ecd8b573fc5b
Zlatko Fedor <zfedor@gmail.com>
2021-01-14 20:54:41 +0100

    show syncing on the farm page instead of bad expected time to win

--- electron-react/src/components/farm/card/FarmCardExpectedTimeToWin.tsx
+++ electron-react/src/components/farm/card/FarmCardExpectedTimeToWin.tsx
@@ -1,14 +1,30 @@
 import React, { useMemo } from 'react';
 import { Trans } from '@lingui/macro';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import { FiberManualRecord as FiberManualRecordIcon } from '@material-ui/icons';
 import moment from 'moment';
+import { Flex } from '@chia/core';
 import type { RootState } from '../../../modules/rootReducer';
 import FarmCard from './FarmCard';
 import type Plot from '../../../types/Plot';
+import StateColor from '../../../constants/StateColor';
+import FullNodeState from '../../../constants/FullNodeState';
+import useFullNodeState from '../../../hooks/useFullNodeState';
+
+const StyledFiberManualRecordIcon = styled(FiberManualRecordIcon)`
+  font-size: 1rem;
+`;
+
+const StyledFlexContainer = styled(({ color: Color, ...rest }) => <Flex {...rest} />)`
+  color: ${({ color }) => color};
+`;
 
 const MINUTES_PER_BLOCK = (24 * 60) / 4608; // 0.3125
 
 export default function FarmCardExpectedTimeToWin() {
+  const fullNodeState = useFullNodeState();
+
   const plots = useSelector(
     (state: RootState) => state.farming_state.harvester.plots,
   );
@@ -33,6 +49,22 @@ export default function FarmCardExpectedTimeToWin() {
     : 0;
 
   const expectedTimeToWin = moment.duration({ minutes }).humanize();
+
+  if (fullNodeState !== FullNodeState.SYNCED) {
+    return (
+      <FarmCard
+        title={
+          <Trans id="FarmCardExpectedTimeToWin.title">Expected Time to Win</Trans>
+        }
+        value={(
+          <StyledFlexContainer color={StateColor.WARNING} alignItems="center" gap={1}>
+            <Trans id="FarmCardExpectedTimeToWin.synching">Syncing</Trans>
+          </StyledFlexContainer>
+        )}
+      />
+    );
+
+  }
 
   return (
     <FarmCard
