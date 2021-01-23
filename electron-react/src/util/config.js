48187a390766379cee8a8c3e985bd4d721f4063c
Don Kackman <dkackman@gmail.com>
2021-01-23 11:24:51 -0600

    remove unsed exports

--- electron-react/src/util/config.js
+++ electron-react/src/util/config.js
@@ -1,13 +1,7 @@
 const self_hostname = 'farmer';
 const daemon_rpc_ws = `wss://${self_hostname}:55400`;
-const wallet_rpc_host_and_port = `ws://${self_hostname}:9256`;
-const full_node_rpc_host = self_hostname;
-const full_node_rpc_port = 8555;
 
 module.exports = {
   self_hostname,
-  daemon_rpc_ws,
-  wallet_rpc_host_and_port,
-  full_node_rpc_host,
-  full_node_rpc_port,
+  daemon_rpc_ws
 };
