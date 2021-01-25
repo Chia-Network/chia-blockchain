3a6e1001b91fa9b09996320c2d320982e95ae6d3
Gene Hoffman <hoffmang@hoffmang.com>
2021-01-25 14:24:11 -0800

    lint comment

--- src/cmds/init.py
+++ src/cmds/init.py
@@ -255,7 +255,7 @@ def chia_init(root_path: Path):
     MANIFEST: List[str] = [
         "config",
         "db/blockchain_v23.db",
-        #"wallet",
+        #  "wallet",
     ]
 
     for versionnumber in range(chiaMinorReleaseNumber() - 1, 8, -1):
