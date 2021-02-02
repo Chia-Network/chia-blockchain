c6c8c09e07f7f47d2507a7dd0ae36f554adefccc
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2021-02-02 16:32:01 +0900

    Fix double lock on end of slot (#798)
    * Fix double lock on end of slot
    
    * remove useless logging

--- src/consensus/blockchain.py
+++ src/consensus/blockchain.py
@@ -579,7 +579,6 @@ class Blockchain(BlockchainInterface):
             return
         blocks_to_remove = self.__sub_heights_in_cache.get(uint32(sub_height), None)
         while blocks_to_remove is not None and sub_height >= 0:
-            log.debug(f"delete sub height {sub_height} from sub blocks")
             for header_hash in blocks_to_remove:
                 del self.__sub_blocks[header_hash]  # remove from sub blocks
             del self.__sub_heights_in_cache[uint32(sub_height)]  # remove height from heights in cache
