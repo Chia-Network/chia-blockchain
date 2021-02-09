9c8bae1dc09ac8396ca07085a04095ca9e3b34ed
Adam Kelly <aqk@aqk.im>
2021-02-09 11:56:49 -0800

    lint

--- tests/clvm/test_serialized_program.py
+++ tests/clvm/test_serialized_program.py
@@ -1,15 +1,13 @@
-import pytest
 from unittest import TestCase
 
-from clvm import SExp
 from src.types.program import SerializedProgram, Program
 from src.wallet.puzzles.load_clvm import load_clvm
 
 
 SHA256TREE_MOD = load_clvm("sha256tree_module.clvm")
 
-# TODO: test multiple args
 
+# TODO: test multiple args
 class TestSerializedProgram(TestCase):
     def test_tree_hash(self):
         p = SHA256TREE_MOD
@@ -25,5 +23,6 @@ class TestSerializedProgram(TestCase):
     def test_serialization(self):
         s0 = SerializedProgram.from_bytes(b"\x00")
         p0 = Program.from_bytes(b"\x00")
+        print(s0, p0)
         # TODO: enable when clvm updated for minimal encoding of zero
         # self.assertEqual(bytes(p0), bytes(s0))
