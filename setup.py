92a479fea01055790fda96a1f2e6bb99e6c735d9
Rostislav <rostislav@users.noreply.github.com>
2021-02-09 21:21:11 +0200

    Change to compressed quadratic forms for VDFs
    Using compressed representation of quadratic forms reduces their
    serialized size from 130 to 100 bytes (for forms with 1024-bit
    discriminant). This shrinks the size of VDF outputs and VDF proofs, and
    it's a breaking change as compressed representation is not compatible
    with older uncompressed (a, b) representation.
    
    Compressed forms are also used in calls to chiavdf and in timelord's
    communication with VDF client.
    
    Update to version 0.14.0 of chiavdf and version 0.2.0 of test-cache.
    Older versions of these do not support compressed forms.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 dependencies = [
     "aiter==0.13.20191203",  # Used for async generator tools
     "blspy==0.3.5",  # Signature library
-    "chiavdf==0.13.4",  # timelord and vdf verification
+    "chiavdf==0.14.0",  # timelord and vdf verification
     "chiabip158==0.19",  # bip158-style wallet filters
     "chiapos==0.12.43",  # proof of space
     "clvm==0.8.0",
