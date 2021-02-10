e5c04e2c057a257d4274984562a50eb97acc1649
Rostislav <rostislav@users.noreply.github.com>
2021-02-10 22:58:01 +0200

    Use fixed-size bytes100 object for compressed quadratic forms

--- src/util/vdf_prover.py
+++ src/util/vdf_prover.py
@@ -23,6 +23,6 @@ def get_vdf_info_and_proof(
         number_iters,
     )
 
-    output = ClassgroupElement(result[:form_size])
+    output = ClassgroupElement.from_bytes(result[:form_size])
     proof_bytes = result[form_size : 2 * form_size]
     return VDFInfo(challenge_hash, number_iters, output), VDFProof(uint8(0), proof_bytes)
