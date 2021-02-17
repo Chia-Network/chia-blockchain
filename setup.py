84796c16813f65a1d99b4cca8ee8819c3c464b52
Adam Kelly <aqk@aqk.im>
2021-02-17 10:55:59 -0800

    Update clvm_rs version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = [
     "chiabip158==0.19",  # bip158-style wallet filters
     "chiapos==0.12.43",  # proof of space
     "clvm@git+https://github.com/Chia-Network/clvm.git@clvm_revamp#egg=clvm",  # noqa
-    "clvm_rs@git+https://github.com/Chia-Network/clvm_rs.git@clvm_revamp#egg=clvm-rs",  # noqa
+    "clvm_rs==0.1.3",  # noqa
     "clvm_tools@git+https://github.com/Chia-Network/clvm_tools.git@clvm_revamp#egg=clvm-tools",  # noqa
     "aiohttp==3.7.3",  # HTTP server for full node rpc
     "aiosqlite@git+https://github.com/mariano54/aiosqlite.git@47c7b21dd04adb1d41073ee9911a9d4b9c4b370f#egg=aiosqlite",
