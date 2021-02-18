4446a4c44cce65dd0c4b897e1f0f528c2b231208
Richard Kiss <him@richardkiss.com>
2021-02-17 17:07:45 -0800

    New version of `clvm` and `clvm_tools`.

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ dependencies = [
     "chiavdf==0.15.0",  # timelord and vdf verification
     "chiabip158==0.19",  # bip158-style wallet filters
     "chiapos==0.12.43",  # proof of space
-    "clvm@git+https://github.com/Chia-Network/clvm.git@clvm_revamp#egg=clvm",  # noqa
-    "clvm_rs==0.1.3",  # noqa
-    "clvm_tools@git+https://github.com/Chia-Network/clvm_tools.git@clvm_revamp#egg=clvm-tools",  # noqa
+    "clvm==0.9.0",
+    "clvm_rs==0.1.3",
+    "clvm_tools==0.4.0",
     "aiohttp==3.7.3",  # HTTP server for full node rpc
     "aiosqlite@git+https://github.com/mariano54/aiosqlite.git@47c7b21dd04adb1d41073ee9911a9d4b9c4b370f#egg=aiosqlite",
     # asyncio wrapper for sqlite, to store blocks
