857f9c2d451318d30ca1f485f1a23843c77c0ecf
Mariano Sorgente <sorgente711@gmail.com>
2021-02-19 02:16:48 +0900

    Fix typo in filename

--- src/cmds/init.py
+++ src/cmds/init.py
@@ -403,7 +403,7 @@ def chia_init(root_path: Path):
     for version_number in range(27, 18, -1):
         b27_windows_path = Path(os.path.expanduser("~/.chia/beta-0.1.27"))
         if b27_windows_path.is_dir():
-            b27_new_windows_path = Path(os.path.expanduser("~/.chia/beta-1.0.b27"))
+            b27_new_windows_path = Path(os.path.expanduser("~/.chia/beta-1.0b27"))
             b27_windows_path.symlink_to(b27_new_windows_path, target_is_directory=True)
 
         old_path = Path(os.path.expanduser("~/.chia/beta-1.0b%s" % version_number))
