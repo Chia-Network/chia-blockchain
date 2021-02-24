07a4b02806c2b63d59f5b4e2060acb90787f8c9e
Zlatko Fedor <zfedor@gmail.com>
2021-02-24 18:07:08 +0100

    showed Your Harvester Network when user has no plots

--- src/components/farm/Farm.tsx
+++ src/components/farm/Farm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Trans } from '@lingui/macro';
+import { useSelector } from 'react-redux';
 import { AdvancedOptions, Flex } from '@chia/core';
 import LayoutMain from '../layout/LayoutMain';
 import FarmOverview from './overview/FarmOverview';
@@ -8,9 +9,15 @@ import FarmFullNodeConnections from './FarmFullNodeConnections';
 import FarmYourHarvesterNetwork from './FarmYourHarvesterNetwork';
 import FarmLastAttemptedProof from './FarmLastAttemptedProof';
 import usePlots from '../../hooks/usePlots';
+import type { RootState } from '../../modules/rootReducer';
 
 export default function Farm() {
   const { hasPlots } = usePlots();
+  const hasHarvesterConnections = !!useSelector((state: RootState) =>
+    state.farming_state.farmer.connections.find(
+      (connection) => connection.type === 2,
+    ),
+  );
 
   return (
     <LayoutMain title={<Trans>Farming</Trans>}>
@@ -29,7 +36,16 @@ export default function Farm() {
             </AdvancedOptions>
           </>
         ) : (
-          <FarmLatestBlockChallenges />
+          <>
+            <FarmLatestBlockChallenges />
+            {hasHarvesterConnections && (
+              <AdvancedOptions>
+                <Flex flexDirection="column" gap={3}>
+                  <FarmYourHarvesterNetwork />
+                </Flex>
+              </AdvancedOptions>
+            )}
+          </>
         )}
       </Flex>
     </LayoutMain>
