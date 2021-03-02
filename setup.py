c62589f3e7c90552566fcda2b976f1914cf934eb
Richard Kiss <him@richardkiss.com>
2021-03-01 16:57:15 -0800

    Bump clvm versions and replace `((c P A))` with `(a P A)`. Again.

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ dependencies = [
     "chiavdf==0.15.0",  # timelord and vdf verification
     "chiabip158==0.19",  # bip158-style wallet filters
     "chiapos==0.12.45",  # proof of space
-    "clvm==0.9.0",
-    "clvm_rs==0.1.3",  # noqa
-    "clvm_tools==0.4.1",  # noqa
+    "clvm==0.9.3",
+    "clvm_rs==0.1.4",
+    "clvm_tools==0.4.3",
     "aiohttp==3.7.4",  # HTTP server for full node rpc
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
     "bitstring==3.1.7",  # Binary data management library
