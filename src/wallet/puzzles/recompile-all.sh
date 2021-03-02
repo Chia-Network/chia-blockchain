a8a7af9ebc6a698014235213adbc4e732d7304e2
Richard Kiss <him@richardkiss.com>
2021-03-01 16:50:40 -0800

    Revert "Bump clvm versions and replace `((c P A))` with `(a P A)`."
    This reverts commit d0a08d2a9334ebcf28742f76c47817dcda2fd45b.

--- src/wallet/puzzles/recompile-all.sh
+++ src/wallet/puzzles/recompile-all.sh
@@ -9,16 +9,9 @@ echo "$FILES"
 
 INCLUDE_DIR=$(pwd)
 
+
 for FILE in $FILES
 do
   echo "run -d -i $INCLUDE_DIR $FILE > $FILE.hex"
   # run -d -i $INCLUDE_DIR $FILE > $FILE.hex
 done
-
-for FILE in $FILES
-do
-  echo "opd -H $FILE.hex | head -1  > $FILE.hex.sha256tree"
-done
-
-echo
-echo "Copy & paste the above to the shell to recompile"
