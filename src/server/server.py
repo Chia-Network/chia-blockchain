9879bdbfadf1932a03606beb344f5e2e73dcad66
Mariano <sorgente711@gmail.com>
2021-03-03 19:06:34 +0900

    Set ban to warning

--- src/server/server.py
+++ src/server/server.py
@@ -411,7 +411,7 @@ class ChiaServer:
         self.log.info(f"Connection closed: {connection.peer_host}, node id: {connection.peer_node_id}")
         if ban_time > 0:
             ban_until: float = time.time() + ban_time
-            self.log.info(f"Banning {connection.peer_host} until {ban_until}")
+            self.log.warning(f"Banning {connection.peer_host} until {ban_until}")
             if connection.peer_host in self.banned_peers:
                 if ban_until > self.banned_peers[connection.peer_host]:
                     self.banned_peers[connection.peer_host] = ban_until
