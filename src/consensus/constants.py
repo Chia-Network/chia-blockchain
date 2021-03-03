b69a7b3cd7cfe83f43151fab86e64f914c68c606
Yostra <straya@chia.net>
2021-03-02 22:47:40 -0500

    rebase and change network type

--- src/consensus/constants.py
+++ src/consensus/constants.py
@@ -60,7 +60,7 @@ class ConsensusConstants:
     MAX_BLOCK_COUNT_PER_REQUESTS: uint32
     INITIAL_FREEZE_PERIOD: uint32
     BLOCKS_CACHE_SIZE: uint32
-    NETWORK: int
+    NETWORK_TYPE: int
     MAX_GENERATOR_SIZE: uint32
 
     def replace(self, **changes):
