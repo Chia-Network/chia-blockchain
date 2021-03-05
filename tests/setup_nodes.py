049fc1adf5a3f2deeb31a3cdfd1eda80d5b5f538
Yostra <straya@chia.net>
2021-03-04 21:46:12 -0500

    daemon't tests expects genesis to be None

--- tests/setup_nodes.py
+++ tests/setup_nodes.py
@@ -59,9 +59,11 @@ async def setup_daemon(port, alert_url, pubkey):
     key_path = root_path / config["daemon_ssl"]["private_key"]
     ca_crt_path = root_path / config["private_ssl_ca"]["crt"]
     ca_key_path = root_path / config["private_ssl_ca"]["key"]
+    # TODO: make overrides configurable
     config["selected_network"] = "testnet5"
     config["ALERTS_URL"] = alert_url
     config["CHIA_ALERTS_PUBKEY"] = pubkey
+    config["network_overrides"]["constants"]["testnet5"]["GENESIS_CHALLENGE"] = None
     btools._config = config
     save_config(root_path, "config.yaml", btools._config)
     assert lockfile is not None
