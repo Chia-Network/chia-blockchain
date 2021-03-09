8ccd3436fb4590f706a153f46b792b0f0da2c09f
Gene Hoffman <hoffmang@hoffmang.com>
2021-03-09 12:26:39 -0800

    cryptography and keyring to current version - keyrings.cryptfile issue

--- setup.py
+++ setup.py
@@ -18,7 +18,9 @@ dependencies = [
     "cryptography==3.4.1",  # Python cryptography library for TLS - keyring conflict
     #  "cryptography==3.3.2",  # Python cryptography library for TLS
     "keyring==22.3.0",  # Store keys in MacOS Keychain, Windows Credential Locker
-    "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
+    "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
+    #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
+    #  See https://github.com/frispete/keyrings.cryptfile/issues/15#issuecomment-794349242
     "PyYAML==5.4.1",  # Used for config file format
     "setproctitle==1.2.2",  # Gives the chia processes readable names
     "sortedcontainers==2.3.0",  # For maintaining sorted mempools
