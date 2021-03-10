62df8f8884ba3c632fcb9c91c772f52e20d577c0
Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
2021-03-11 03:02:59 +0900

    Revert changes

--- src/consensus/default_constants.py
+++ src/consensus/default_constants.py
@@ -54,7 +54,7 @@ testnet_kwargs = {
     # Max block cost in clvm cost units (MAX_BLOCK_COST * CLVM_COST_RATIO_CONSTANT)
     "MAX_BLOCK_COST_CLVM": 23077872,
     "WEIGHT_PROOF_THRESHOLD": 2,
-    "BLOCKS_CACHE_SIZE": 32256 + (128 * 4),
+    "BLOCKS_CACHE_SIZE": (384 * 2) + (128 * 4),
     "WEIGHT_PROOF_RECENT_BLOCKS": 1000,
     "MAX_BLOCK_COUNT_PER_REQUESTS": 32,  # Allow up to 32 blocks per request
     "INITIAL_FREEZE_PERIOD": 5000,  # Transaction are disabled first 5000 blocks
