58a1b06ef6f6dcc7be3c7df8d39ba65b0f6e683d
Mariano <sorgente711@gmail.com>
2021-03-16 04:30:51 +0900

    Remove comment

--- src/consensus/default_constants.py
+++ src/consensus/default_constants.py
@@ -59,7 +59,7 @@ testnet_kwargs = {
     "BLOCKS_CACHE_SIZE": 4608 + (128 * 4),
     "WEIGHT_PROOF_RECENT_BLOCKS": 1000,
     "MAX_BLOCK_COUNT_PER_REQUESTS": 32,  # Allow up to 32 blocks per request
-    "INITIAL_FREEZE_PERIOD": 42 * 4608,  # Transaction are disabled first 5000 blocks
+    "INITIAL_FREEZE_PERIOD": 42 * 4608,
     "NETWORK_TYPE": 0,
     "MAX_GENERATOR_SIZE": 1000000,
     "MAX_GENERATOR_REF_LIST_SIZE": 10000,  # Number of references allowed in the block generator ref list
