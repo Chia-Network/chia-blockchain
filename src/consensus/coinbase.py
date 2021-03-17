7a668321b83bb024d222acd83154f698a120563a
Yostra <straya@chia.net>
2021-03-17 04:00:55 -0400

    Mariano's suggestion

--- src/consensus/coinbase.py
+++ src/consensus/coinbase.py
@@ -22,15 +22,11 @@ def sign_coinbase_coin(coin: Coin, private_key: PrivateKey):
 
 
 def pool_parent_id(block_height: uint32, genesis_challenge: bytes32) -> uint32:
-    network_offset = int.from_bytes(genesis_challenge[:16], "big") << 128
-    virtual_height = (block_height + network_offset) & ((1 << 256) - 1)
-    return bytes32(virtual_height.to_bytes(32, "big"))
+    return bytes32(genesis_challenge[:16] + block_height.to_bytes(16, "big"))
 
 
 def farmer_parent_id(block_height: uint32, genesis_challenge: bytes32) -> uint32:
-    network_offset = int.from_bytes(genesis_challenge[16:], "big") << 128
-    virtual_height = (block_height + network_offset) & ((1 << 256) - 1)
-    return bytes32(virtual_height.to_bytes(32, "big"))
+    return bytes32(genesis_challenge[16:] + block_height.to_bytes(16, "big"))
 
 
 def create_pool_coin(block_height: uint32, puzzle_hash: bytes32, reward: uint64, genesis_challenge: bytes32):
