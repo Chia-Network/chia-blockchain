805e29e69cdd4ea2753f59115f1c6563ead62b71
Richard Kiss <him@richardkiss.com>
2021-03-18 14:25:33 -0700

    Monkey-patch click instead of forking.

--- src/cmds/chia.py
+++ src/cmds/chia.py
@@ -20,6 +20,22 @@ from src.util.default_root import DEFAULT_ROOT_PATH
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
+def monkey_patch_click():
+    # this hacks around what seems to be an incompatibility between the python from `pyinstaller`
+    # and `click`
+    #
+    # Not 100% sure on the details, but it seems that `click` performs a check on start-up
+    # that `codecs.lookup(locale.getpreferredencoding()).name != 'ascii'`, and refuses to start
+    # if it's not. The python that comes with `pyinstaller` fails this check.
+    #
+    # This will probably cause problems with the command-line tools that use parameters that
+    # are not strict ascii. The real fix is likely with the `pyinstaller` python.
+
+    import click.core
+
+    click.core._verify_python3_env = lambda *args, **kwargs: 0
+
+
 @click.group(
     help=f"\n  Manage chia blockchain infrastructure ({__version__})\n",
     epilog="Try 'chia start node', 'chia netspace -d 192', or 'chia show -s'",
@@ -56,6 +72,7 @@ cli.add_command(farm_cmd)
 
 
 def main() -> None:
+    monkey_patch_click()
     cli()  # pylint: disable=no-value-for-parameter
 
 
