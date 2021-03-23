4780fabc2b9bef6c1140f657545d6703974355fe
Yostra <straya@chia.net>
2021-03-23 02:02:57 -0400

    add pytz

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ dependencies = [
     "setproctitle==1.2.2",  # Gives the chia processes readable names
     "sortedcontainers==2.3.0",  # For maintaining sorted mempools
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
-    "click==7.1.2",  # For the CLI
+    "click@https://github.com/Chia-Network/click/tarball/master#egg=package-1.0",  # For the CLI
+    "pytz==2021.1"
 ]
 
 upnp_dependencies = [
