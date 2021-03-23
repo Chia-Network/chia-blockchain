e67e333d7f2c7747ac407affb07c3b92e4613dfc
Zlatko Fedor <zfedor@gmail.com>
2021-03-23 17:19:45 +0100

    typo

--- src/components/wallet/WalletStatusCard.tsx
+++ src/components/wallet/WalletStatusCard.tsx
@@ -26,10 +26,9 @@ export default function WalletStatusCard() {
           </Box>
           <Box>
             {syncing ? (
-                <Trans>syncing</Trans>
-              ) : (
-                <Trans>synced</Trans>
-              )
+              <Trans>syncing</Trans>
+            ) : (
+              <Trans>synced</Trans>
             )}
           </Box>
         </Box>
