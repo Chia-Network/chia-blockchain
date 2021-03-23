cd151fd116ccf67646a5a8cf6fb67e6cb9d611ec
Yostra <straya@chia.net>
2021-03-23 13:49:37 -0400

    fix wallet status (#1453)

--- src/rpc/wallet_rpc_api.py
+++ src/rpc/wallet_rpc_api.py
@@ -257,7 +257,10 @@ class WalletRpcApi:
     ##########################################################################################
 
     async def get_sync_status(self, request: Dict):
-        return {"synced": False, "syncing": False, "genesis_initialized": True}
+        assert self.service.wallet_state_manager is not None
+        syncing = self.service.wallet_state_manager.sync_mode
+        synced = await self.service.wallet_state_manager.synced()
+        return {"synced": synced, "syncing": syncing, "genesis_initialized": True}
 
     async def get_height_info(self, request: Dict):
         assert self.service.wallet_state_manager is not None
