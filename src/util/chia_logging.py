35528e92684ed5e7f1191e40d042f746e82aa855
Alfonso Perez <alfperata@gmail.com>
2021-03-25 12:23:20 -0700

    ISO8601

--- src/util/chia_logging.py
+++ src/util/chia_logging.py
@@ -10,7 +10,7 @@ from src.util.path import mkdir, path_from_root
 
 def initialize_logging(service_name: str, logging_config: Dict, root_path: Path):
     log_path = path_from_root(root_path, logging_config.get("log_filename", "log/debug.log"))
-    log_date_format = "%Y-%m-%d %H:%M:%S"
+    log_date_format = "%Y-%m-%dT%H:%M:%S"
 
     mkdir(str(log_path.parent))
     file_name_length = 33 - len(service_name)
