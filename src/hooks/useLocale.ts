b2c3bc1cc5b8166902aa6307206777048a785951
Zlatko Fedor <zlatko@chia.net>
2021-03-30 21:28:07 +0200

    fixed old locale codes (#222)
    Co-authored-by: Gene Hoffman <30377676+hoffmang9@users.noreply.github.com>

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -3,7 +3,11 @@ import { useLocalStorage, writeStorage } from '@rehooks/local-storage';
 export default function useLocale(
   defaultLocale: string,
 ): [string, (locale: string) => void] {
-  const [locale] = useLocalStorage('locale');
+  let [locale] = useLocalStorage('locale');
+
+  if (locale && locale.length === 2) {
+    locale = null;
+  }
 
   function handleSetLocale(locale: string) {
     writeStorage('locale', locale);
