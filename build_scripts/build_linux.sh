6cae9625f69bb25c80842bb19c260b640d6b45d2
Richard Kiss <him@richardkiss.com>
2021-04-04 19:30:34 -0700

    Fix linux build.

--- build_scripts/build_linux.sh
+++ build_scripts/build_linux.sh
@@ -35,7 +35,7 @@ mkdir dist
 
 echo "Create executables with pyinstaller"
 pip install pyinstaller==4.2
-SPEC_FILE=$(python -c 'import src; print(src.PYINSTALLER_SPEC_PATH)')
+SPEC_FILE=$(python -c 'import chia; print(chia.PYINSTALLER_SPEC_PATH)')
 pyinstaller --log-level=INFO "$SPEC_FILE"
 LAST_EXIT_CODE=$?
 if [ "$LAST_EXIT_CODE" -ne 0 ]; then
