a3b2c0ba9eca5796c1da5d7dc28fd550a4e32b95
Mariano <sorgente711@gmail.com>
2021-04-15 22:38:55 +0900

    Combine adam's and straya's PRs

--- chia/full_node/bundle_tools.py
+++ chia/full_node/bundle_tools.py
@@ -2,14 +2,14 @@ from clvm import SExp
 from clvm_tools import binutils
 
 from chia.types.blockchain_format.program import SerializedProgram
+from chia.types.generator_types import BlockGenerator, GeneratorArg
 from chia.types.spend_bundle import SpendBundle
 
 
-def best_solution_program(bundle: SpendBundle) -> SerializedProgram:
+def simple_solution_program(bundle: SpendBundle) -> BlockGenerator:
     """
     This could potentially do a lot of clever and complicated compression
     optimizations in conjunction with choosing the set of SpendBundles to include.
-
     For now, we just quote the solutions we know.
     """
     r = []
@@ -19,4 +19,24 @@ def best_solution_program(bundle: SpendBundle) -> SerializedProgram:
             [coin_solution.puzzle_reveal, coin_solution.solution],
         ]
         r.append(entry)
-    return SerializedProgram.from_bytes(SExp.to((binutils.assemble("#q"), r)).as_bin())
+
+    block_program = SerializedProgram.from_bytes(SExp.to((binutils.assemble("#q"), r)).as_bin())
+    g = BlockGenerator(block_program, [])
+    return g
+
+
+def best_solution_generator_from_template(bundle: SpendBundle, previous_generator: GeneratorArg) -> BlockGenerator:
+    """
+    Creates a compressed block generator, taking in a block that passes the checks below
+    """
+    # TODO: (adam): Implement this with actual compression.
+    return simple_solution_program(bundle)
+
+
+def detect_potential_template_generator(generator: SerializedProgram) -> bool:
+    """
+    If returns True, that means that generator has a standard transaction that is not compressed that we can use
+    as a template for future blocks. This block will be compressed with the above code.
+    """
+    # TODO: (adam): Implement this with actual compression
+    return False
--- chia/util/generator_tools.py
+++ chia/util/generator_tools.py
@@ -5,6 +5,7 @@ from chia.full_node.mempool_check_conditions import get_name_puzzle_conditions
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.full_block import FullBlock
+from chia.types.generator_types import BlockGenerator
 from chia.types.header_block import HeaderBlock
 from chia.types.name_puzzle_condition import NPC
 from chia.util.condition_tools import created_outputs_for_conditions_dict
@@ -96,7 +97,7 @@ def run_and_get_removals_and_additions(block: FullBlock, safe_mode=False) -> Tup
         return [], []
 
     if block.transactions_generator is not None:
-        npc_result = get_name_puzzle_conditions(block.transactions_generator, safe_mode)
+        npc_result = get_name_puzzle_conditions(BlockGenerator(block.transactions_generator, []), safe_mode)
         # build removals list
         for npc in npc_result.npc_list:
             removals.append(npc.coin_name)
