a114077629a3b409a52e40688fc139feb1e333e4
Adam Kelly <aqk@aqk.im>
2021-04-18 23:41:07 -0700

    Simplify block compressor scanning

--- chia/full_node/bundle_tools.py
+++ chia/full_node/bundle_tools.py
@@ -38,6 +38,8 @@ def simple_solution_generator(bundle: SpendBundle) -> BlockGenerator:
 
 STANDARD_TRANSACTION_PUZZLE_PREFIX = r"""ff02ffff01ff02ffff01ff02ffff03ff0bffff01ff02ffff03ffff09ff05ffff1dff0bffff1effff0bff0bffff02ff06ffff04ff02ffff04ff17ff8080808080808080ffff01ff02ff17ff2f80ffff01ff088080ff0180ffff01ff04ffff04ff04ffff04ff05ffff04ffff02ff06ffff04ff02ffff04ff17ff80808080ff80808080ffff02ff17ff2f808080ff0180ffff04ffff01ff32ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff06ffff04ff02ffff04ff09ff80808080ffff02ff06ffff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080ffff04ffff01"""  # noqa
 
+STANDARD_TRANSACTION_PUZZLE_PREFIX_PATTERN = re.compile(STANDARD_TRANSACTION_PUZZLE_PREFIX)
+
 STANDARD_TRANSACTION_PUZZLE_PATTERN = re.compile(STANDARD_TRANSACTION_PUZZLE_PREFIX + r"(b0[a-f0-9]{96})ff018080")
 
 
@@ -45,14 +47,11 @@ STANDARD_TRANSACTION_PUZZLE_PATTERN = re.compile(STANDARD_TRANSACTION_PUZZLE_PRE
 def match_standard_transaction_at_any_index(generator_body: bytes) -> Optional[Tuple[int, int]]:
     """Return (start, end) of match, or None if pattern could not be found"""
 
-    # We intentionally match the entire puzzle, not just the prefix that we will use,
-    # in case we later want to convert the template generator into a tree of CLVM
-    # Objects before operating on it
-    m = STANDARD_TRANSACTION_PUZZLE_PATTERN.search(generator_body.hex())
+    m = STANDARD_TRANSACTION_PUZZLE_PREFIX_PATTERN.search(generator_body.hex())
     if m:
         assert m.start() % 2 == 0 and m.end() % 2 == 0
         start = m.start() // 2
-        end = (m.end() - 98 - len("ff018080")) // 2
+        end = m.end() // 2
         assert generator_body[start:end] == bytes.fromhex(STANDARD_TRANSACTION_PUZZLE_PREFIX)
         return start, end
     else:
