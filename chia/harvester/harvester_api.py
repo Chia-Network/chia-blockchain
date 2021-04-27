d78933c36f117c9f52010f8216323d63b28eed4d
Will Bendick <wbendick@gmail.com>
2021-04-26 17:26:33 -0700

    line too long

--- chia/harvester/harvester_api.py
+++ chia/harvester/harvester_api.py
@@ -91,7 +91,8 @@ class HarvesterAPI:
                 except Exception as e:
                     self.harvester.log.error(f"Error using prover object {e}")
                     self.harvester.log.error(
-                        f"File: {filename} Plot ID: {plot_id.hex()}, challenge: {sp_challenge_hash}, plot_info: {plot_info}"
+                        f"File: {filename} Plot ID: {plot_id.hex()}, "
+                        f"challenge: {sp_challenge_hash}, plot_info: {plot_info}"
                     )
                     return []
 
