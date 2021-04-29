88b0a5155d72e903955c9fd7f3e1399e0d0101ed
sashacmc <Alexander@Bushnev.ru>
2021-04-30 01:00:18 +0200

    Add harvester count and correct remote plots calculation
    Add connected harvester count to farm summary.
    And correct remote plots calculation
    (it requires change self_hostname to '0.0.0.0' on remote harvester hosts)

--- chia/cmds/farm_funcs.py
+++ chia/cmds/farm_funcs.py
@@ -12,30 +12,64 @@ from chia.util.config import load_config
 from chia.util.default_root import DEFAULT_ROOT_PATH
 from chia.util.ints import uint16
 from chia.util.misc import format_minutes
+from chia.server.outbound_message import NodeType
 
 SECONDS_PER_BLOCK = (24 * 3600) / 4608
 
 
-async def get_plots(harvester_rpc_port: int) -> Optional[Dict[str, Any]]:
+async def get_plots(harvester_rpc_port: int, harvester_rpc_hostname: str) -> Optional[Dict[str, Any]]:
     plots = None
     try:
         config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
-        self_hostname = config["self_hostname"]
+        if harvester_rpc_hostname is None:
+            harvester_rpc_hostname = config["self_hostname"]
         if harvester_rpc_port is None:
             harvester_rpc_port = config["harvester"]["rpc_port"]
         harvester_client = await HarvesterRpcClient.create(
-            self_hostname, uint16(harvester_rpc_port), DEFAULT_ROOT_PATH, config
+            harvester_rpc_hostname, uint16(harvester_rpc_port), DEFAULT_ROOT_PATH, config
         )
         plots = await harvester_client.get_plots()
     except Exception as e:
         if isinstance(e, aiohttp.ClientConnectorError):
-            print(f"Connection error. Check if harvester is running at {harvester_rpc_port}")
+            print(f"Connection error. Check if harvester is running at {harvester_rpc_hostname}:{harvester_rpc_port}")
         else:
             print(f"Exception from 'harvester' {e}")
 
     harvester_client.close()
     await harvester_client.await_closed()
     return plots
+
+
+async def get_harvesters(farmer_rpc_port: int) -> Optional[List[str]]:
+    harvester_hosts = []
+    try:
+        config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
+        self_hostname = config["self_hostname"]
+        if farmer_rpc_port is None:
+            farmer_rpc_port = config["farmer"]["rpc_port"]
+        farmer_client = await FarmerRpcClient.create(self_hostname, uint16(farmer_rpc_port), DEFAULT_ROOT_PATH, config)
+        connections = await farmer_client.get_connections()
+        for c in connections:
+            if c['type'] == NodeType.HARVESTER:
+                harvester_hosts.append(c['peer_host'])
+    except Exception as e:
+        print(f"Exception from 'farmer' {e}")
+
+    farmer_client.close()
+    await farmer_client.await_closed()
+    return harvester_hosts
+
+
+async def get_all_plots(harvester_rpc_port: int, harvester_hosts: List[str]) -> Optional[Dict[str, Any]]:
+    all_plots = None
+    for host in harvester_hosts:
+        plots = await get_plots(harvester_rpc_port, host)
+        if plots is not None and plots['success']:
+            if all_plots is None:
+                all_plots = plots
+            else:
+                all_plots['plots'] += plots['plots']
+    return all_plots
 
 
 async def get_blockchain_state(rpc_port: int) -> Optional[Dict[str, Any]]:
@@ -183,7 +217,8 @@ async def challenges(farmer_rpc_port: int, limit: int) -> None:
 
 async def summary(rpc_port: int, wallet_rpc_port: int, harvester_rpc_port: int, farmer_rpc_port: int) -> None:
     amounts = await get_wallets_stats(wallet_rpc_port)
-    plots = await get_plots(harvester_rpc_port)
+    harvesters = await get_harvesters(farmer_rpc_port)
+    plots = await get_all_plots(harvester_rpc_port, harvesters)
     blockchain_state = await get_blockchain_state(rpc_port)
     farmer_running = await is_farmer_running(farmer_rpc_port)
 
@@ -209,6 +244,8 @@ async def summary(rpc_port: int, wallet_rpc_port: int, harvester_rpc_port: int, 
         print("User transaction fees: Unknown")
         print("Block rewards: Unknown")
         print("Last height farmed: Unknown")
+
+    print(f"Harvesters count: {len(harvesters)}")
 
     total_plot_size = 0
     if plots is not None:
