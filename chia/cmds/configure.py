9191cda89dc92a3aebcdaca423c898777599ba56
justinengland <justin@chia.net>
2021-05-18 15:27:41 -0600

    adding false setting to reset back to mainnet

--- chia/cmds/configure.py
+++ chia/cmds/configure.py
@@ -73,26 +73,48 @@ def configure(
             print("uPnP disabled")
         change_made = True
     if testnet is not None:
-        testnet_port = "58444"
-        testnet_introducer = "beta1_introducer.chia.net"
-        testnet = "testnet7"
-        config["full_node"]["port"] = int(testnet_port)
-        config["full_node"]["introducer_peer"]["port"] = int(testnet_port)
-        config["farmer"]["full_node_peer"]["port"] = int(testnet_port)
-        config["timelord"]["full_node_peer"]["port"] = int(testnet_port)
-        config["wallet"]["full_node_peer"]["port"] = int(testnet_port)
-        config["wallet"]["introducer_peer"]["port"] = int(testnet_port)
-        config["introducer"]["port"] = int(testnet_port)
-        config["full_node"]["introducer_peer"]["host"] = testnet_introducer
-        config["selected_network"] = testnet
-        config["harvester"]["selected_network"] = testnet
-        config["pool"]["selected_network"] = testnet
-        config["farmer"]["selected_network"] = testnet
-        config["timelord"]["selected_network"] = testnet
-        config["full_node"]["selected_network"] = testnet
-        config["ui"]["selected_network"] = testnet
-        config["introducer"]["selected_network"] = testnet
-        config["wallet"]["selected_network"] = testnet
+        if testnet is 'true' or 't' or 'True'
+            testnet_port = "58444"
+            testnet_introducer = "beta1_introducer.chia.net"
+            testnet = "testnet7"
+            config["full_node"]["port"] = int(testnet_port)
+            config["full_node"]["introducer_peer"]["port"] = int(testnet_port)
+            config["farmer"]["full_node_peer"]["port"] = int(testnet_port)
+            config["timelord"]["full_node_peer"]["port"] = int(testnet_port)
+            config["wallet"]["full_node_peer"]["port"] = int(testnet_port)
+            config["wallet"]["introducer_peer"]["port"] = int(testnet_port)
+            config["introducer"]["port"] = int(testnet_port)
+            config["full_node"]["introducer_peer"]["host"] = testnet_introducer
+            config["selected_network"] = testnet
+            config["harvester"]["selected_network"] = testnet
+            config["pool"]["selected_network"] = testnet
+            config["farmer"]["selected_network"] = testnet
+            config["timelord"]["selected_network"] = testnet
+            config["full_node"]["selected_network"] = testnet
+            config["ui"]["selected_network"] = testnet
+            config["introducer"]["selected_network"] = testnet
+            config["wallet"]["selected_network"] = testnet
+        elif testnet is 'false' or 'f' or 'False'
+            mainnet_port = "8444"
+            mainnet_introducer = "introducer.chia.net"
+            net = "mainnet"
+            config["full_node"]["port"] = int(mainnet_port)
+            config["full_node"]["introducer_peer"]["port"] = int(mainnet_port)
+            config["farmer"]["full_node_peer"]["port"] = int(mainnet_port)
+            config["timelord"]["full_node_peer"]["port"] = int(mainnet_port)
+            config["wallet"]["full_node_peer"]["port"] = int(mainnet_port)
+            config["wallet"]["introducer_peer"]["port"] = int(mainnet_port)
+            config["introducer"]["port"] = int(mainnet_port)
+            config["full_node"]["introducer_peer"]["host"] = mainnet_introducer
+            config["selected_network"] = net
+            config["harvester"]["selected_network"] = net
+            config["pool"]["selected_network"] = net
+            config["farmer"]["selected_network"] = net
+            config["timelord"]["selected_network"] = net
+            config["full_node"]["selected_network"] = net
+            config["ui"]["selected_network"] = net
+            config["introducer"]["selected_network"] = net
+            config["wallet"]["selected_network"] = net
         print("Default full node port, introducer and network setting updated")
         change_made = True
     if change_made:
