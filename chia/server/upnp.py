37cb31a98bac17af3ef1b037f526efa7f4866685
Mariano <sorgente711@gmail.com>
2021-05-20 02:53:14 +0900

    Revert "release upnp ports on shutdown (#4024)"
    This reverts commit 39ef69643b0bcaf4309866df3802850903275b92.

--- chia/server/upnp.py
+++ chia/server/upnp.py
@@ -1,6 +1,4 @@
 import logging
-import threading
-from queue import Queue
 
 try:
     import miniupnpc
@@ -11,55 +9,14 @@ except ImportError:
 log = logging.getLogger(__name__)
 
 
-class UPnP:
-    def __init__(self):
-        self.queue = Queue()
-
-        def run():
-            try:
-                self.upnp = miniupnpc.UPnP()
-                self.upnp.discoverdelay = 30
-                self.upnp.discover()
-                self.upnp.selectigd()
-                keep_going = True
-                while keep_going:
-                    msg = self.queue.get()
-                    if msg[0] == "remap":
-                        port = msg[1]
-                        log.info(f"Attempting to enable UPnP (open up port {port})")
-                        self.upnp.deleteportmapping(port, "TCP")
-                        self.upnp.addportmapping(port, "TCP", self.upnp.lanaddr, port, "chia", "")
-                        log.info(
-                            f"Port {port} opened with UPnP. lanaddr {self.upnp.lanaddr} "
-                            f"external: {self.upnp.externalipaddress()}"
-                        )
-                    elif msg[0] == "release":
-                        port = msg[1]
-                        self.upnp.deleteportmapping(port, "TCP")
-                        log.info(f"Port {port} closed with UPnP")
-                    elif msg[0] == "shutdown":
-                        keep_going = False
-            except Exception as e:
-                log.info(
-                    "UPnP failed. This is not required to run chia, it allows incoming connections from other peers."
-                )
-                log.info(e)
-
-        self.thread = threading.Thread(target=run)
-        self.thread.start()
-
-    def remap(self, port):
-        self.queue.put(("remap", port))
-
-    def release(self, port):
-        self.queue.put(("release", port))
-
-    def shutdown(self):
-        self.queue.put(("shutdown",))
-        self.thread.join()
-
-    def __del__(self):
-        self.shutdown()
-
-
-upnp: UPnP = UPnP()
+def upnp_remap_port(port) -> None:
+    log.info(f"Attempting to enable UPnP (open up port {port})")
+    try:
+        upnp = miniupnpc.UPnP()
+        upnp.discoverdelay = 30
+        upnp.discover()
+        upnp.selectigd()
+        upnp.addportmapping(port, "TCP", upnp.lanaddr, port, "chia", "")
+        log.info(f"Port {port} opened with UPnP. lanaddr {upnp.lanaddr} external: {upnp.externalipaddress()}")
+    except Exception:
+        log.info("UPnP failed. This is not required to run chia, but it allows incoming connections from other peers.")
