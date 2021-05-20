938f4c768f77324b63ff35007a3b3537a5bc62c3
Zlatko <zlatko@chia.net>
2021-05-20 17:56:18 +0200

    more types for group info

--- src/types/Group.ts
+++ src/types/Group.ts
@@ -9,6 +9,27 @@ type Group = {
   targetState?: 'FREE' | 'POOLING' | 'ESCAPING';
   balance: number;
   address: string;
+
+  p2_singleton_puzzle_hash: string;
+  points_found_since_start: number;
+  points_found_24h: number[];
+  points_acknowledged_since_start: number;
+  points_acknowledged_24h: number[];
+  current_points_balance: number;
+  current_difficulty: number;
+  pool_errors_24h: string[];
+  pool_info: {
+    pool_name: string;
+    pool_description: string;
+  };
+  pool_config: {
+    owner_public_key: string;
+    pool_puzzle_hash: string;
+    pool_url: string;
+    singleton_genesis: string;
+    target: string;
+    target_signature: string;
+  };
 };
 
 export default Group;
