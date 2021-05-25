fd5956c80f1028219b89cf9b013960f3c9c59b54
Nikolaj-K <treegraph@gmail.com>
2021-05-25 22:20:24 +0200

    init

--- chia/util/misc.py
+++ chia/util/misc.py
@@ -1,18 +1,14 @@
 def format_bytes(bytes: int) -> str:
-
     if not isinstance(bytes, int) or bytes < 0:
         return "Invalid"
 
-    labels = {0: "MiB", 1: "GiB", 2: "TiB", 3: "PiB", 4: "EiB", 5: "ZiB", 6: "YiB"}
-
-    base = 1024
-    value = bytes / base ** 2
-    key = 0
-    while value >= base and key < 6:
-        value /= base
-        key += 1
-
-    return f"{value:.3f} {labels[key]}"
+    LABELS = ("MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+    BASE = 1024
+    value = bytes / BASE
+    for label in LABELS:
+        value /= BASE
+        if value < BASE or label==LABELS[-1]:
+            return f"{value:.3f} {label}"
 
 
 def format_minutes(minutes: int) -> str:
