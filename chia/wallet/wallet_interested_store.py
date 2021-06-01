123b22bbcf1ed73772f9ca233d4deae6f00b1fe0
Mariano Sorgente <sorgente711@gmail.com>
2021-06-01 13:19:16 -0400

    Pool fixes

--- chia/wallet/wallet_interested_store.py
+++ chia/wallet/wallet_interested_store.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import aiosqlite
 
@@ -25,10 +25,16 @@ class WalletInterestedStore:
 
         await self.db_connection.execute("CREATE TABLE IF NOT EXISTS interested_coins(coin_name text PRIMARY KEY)")
 
+        await self.db_connection.execute(
+            "CREATE TABLE IF NOT EXISTS interested_puzzle_hashes(puzzle_hash text, wallet_id integer, "
+            f"PRIMARY KEY (puzzle_hash, wallet_id))"
+        )
         await self.db_connection.commit()
         return self
 
     async def _clear_database(self):
+        cursor = await self.db_connection.execute("DELETE FROM puzzle_hashes")
+        await cursor.close()
         cursor = await self.db_connection.execute("DELETE FROM interested_coins")
         await cursor.close()
         await self.db_connection.commit()
@@ -45,6 +51,27 @@ class WalletInterestedStore:
         try:
             cursor = await self.db_connection.execute(
                 "INSERT OR REPLACE INTO interested_coins VALUES (?)", (coin_id.hex(),)
+            )
+            await cursor.close()
+        finally:
+            if not in_transaction:
+                await self.db_connection.commit()
+                self.db_wrapper.lock.release()
+
+    async def get_interested_puzzle_hashes(self) -> List[Tuple[bytes32, int]]:
+        cursor = await self.db_connection.execute("SELECT puzzle_hash, wallet_id FROM interested_puzzle_hashes")
+        rows_hex = await cursor.fetchall()
+        return [(bytes32(bytes.fromhex(row[0])), row[1]) for row in rows_hex]
+
+    async def add_interested_puzzle_hash(
+        self, puzzle_hash: bytes32, wallet_id: int, in_transaction: bool = False
+    ) -> None:
+
+        if not in_transaction:
+            await self.db_wrapper.lock.acquire()
+        try:
+            cursor = await self.db_connection.execute(
+                "INSERT OR REPLACE INTO interested_puzzle_hashes VALUES (?, ?)", (puzzle_hash.hex(), wallet_id)
             )
             await cursor.close()
         finally:
