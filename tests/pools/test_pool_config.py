4833cd6ee6a4ca65f167b4ed1d79ea8c47463eb3
matt <matt@chia.net>
2021-06-14 13:32:19 +0100

    fix test_pool_config test

--- tests/pools/test_pool_config.py
+++ tests/pools/test_pool_config.py
@@ -24,6 +24,7 @@ def test_pool_config():
         "authentication_public_key": bytes(auth_sk.get_g1()).hex(),
         "authentication_public_key_timestamp": 1621854388,
         "owner_public_key": "84c3fcf9d5581c1ddc702cb0f3b4a06043303b334dd993ab42b2c320ebfa98e5ce558448615b3f69638ba92cf7f43da5",
+        "p2_singleton_puzzle_hash": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
         "pool_payout_instructions": "c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8",
         "pool_url": "localhost",
         "launcher_id": "ae4ef3b9bfe68949691281a015a9c16630fc8f66d48c19ca548fb80768791afa",
