0ffbe1339cb2b44b7ce4fa4238b8d9c0b3dbfdec
Gian Luca Decurtins <35364122+gldecurtins@users.noreply.github.com>
2021-06-15 04:11:35 +0200

    Log syslog (#6490)
    * Keep mnemnic as secret as possible
    
    * Add logging support for remote syslog
    
    * Fix lint (W293 and E251)
    
    * syslog Formatter in one line
    
    * Set log_syslog to False in case not specified

--- chia/util/chia_logging.py
+++ chia/util/chia_logging.py
@@ -4,6 +4,7 @@ from typing import Dict
 
 import colorlog
 from concurrent_log_handler import ConcurrentRotatingFileHandler
+from logging.handlers import SysLogHandler
 
 from chia.util.path import mkdir, path_from_root
 
@@ -38,6 +39,14 @@ def initialize_logging(service_name: str, logging_config: Dict, root_path: Path)
             )
         )
         logger.addHandler(handler)
+
+    if logging_config.get("log_syslog", False):
+        log_syslog_host = logging_config.get("log_syslog_host", "localhost")
+        log_syslog_port = logging_config.get("log_syslog_port", 514)
+        log_syslog_handler = SysLogHandler(address=(log_syslog_host, log_syslog_port))
+        log_syslog_handler.setFormatter(logging.Formatter(fmt=f"{service_name} %(message)s", datefmt=log_date_format))
+        logger = logging.getLogger()
+        logger.addHandler(log_syslog_handler)
 
     if "log_level" in logging_config:
         if logging_config["log_level"] == "CRITICAL":
