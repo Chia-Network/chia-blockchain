2c96610e9506b06de608bb4cfe182fbf594e33a7
arvidn <arvid@libtorrent.org>
2021-06-16 10:03:47 +0200

    move chia.util.block_tools and chia.util.wallet_tools into tests

--- chia/server/start_wallet.py
+++ chia/server/start_wallet.py
@@ -8,7 +8,7 @@ from chia.rpc.wallet_rpc_api import WalletRpcApi
 from chia.server.outbound_message import NodeType
 from chia.server.start_service import run_service
 from chia.types.peer_info import PeerInfo
-from chia.util.block_tools import test_constants
+from tests.block_tools import test_constants
 from chia.util.config import load_config_cli, load_config
 from chia.util.default_root import DEFAULT_ROOT_PATH
 from chia.util.keychain import Keychain
