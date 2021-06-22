c6c6268952dc087d4dbb13dff009ea3e31753888
huangxufu <huangxufu2017@gmail.com>
2021-06-22 16:36:49 +0800

    update default config

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -188,7 +188,8 @@ class FullNode:
             dns_servers = self.config["dns_servers"]
         elif self.config["port"] == 8444:
             # If `dns_servers` misses from the `config`, hardcode it if we're running mainnet.
-            dns_servers.append("dns-introducer.chia.net")
+            # dns_servers.append("dns-introducer.chia.net")
+            pass
         try:
             self.full_node_peers = FullNodePeers(
                 self.server,
