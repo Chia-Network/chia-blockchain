7548374f2f8e91abd71fefbf1f54fd74916415b5
HiveProject2021 <jwang123@nyit.edu>
2021-06-22 16:18:59 -0700

    www.chivescoin.org sha256

--- chia/consensus/default_constants.py
+++ chia/consensus/default_constants.py
@@ -30,7 +30,7 @@ testnet_kwargs = {
     # Default used for tests is std_hash(b'')
     "GENESIS_CHALLENGE": bytes.fromhex("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
     # Forks of chia should change this value to provide replay attack protection. This is set to mainnet genesis chall
-    "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
+    "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("1171692f4b57dcaf0c86a2ec665d53c14d4335e37984bf843fc803607683ac6a"),
     "GENESIS_PRE_FARM_POOL_PUZZLE_HASH": bytes.fromhex(
         "d23da14695a188ae5708dd152263c4db883eb27edeb936178d4d988b8f3ce5fc"
     ),
