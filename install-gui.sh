4aadcaf4942bbc0f62a31d73925b3fe556641d56
HiveProject2021 <jwang123@nyit.edu>
2021-06-23 15:14:54 -0700

    Update install-gui.sh

--- install-gui.sh
+++ install-gui.sh
@@ -80,22 +80,16 @@ if [ ! "$CI" ]; then
 	git submodule update
 	cd chia-blockchain-gui
 
-	if [ "$SUBMODULE_BRANCH" ];	then
-		git fetch
-			git checkout "$SUBMODULE_BRANCH"
-		git pull
-			echo "**********************************************"
-			echo "Building the GUI with branch $SUBMODULE_BRANCH"
-			echo "**********************************************"
-	else
-		git fetch
-			git checkout "$SUBMODULE_BRANCH"
-		git pull origin main
-			echo "----------------------------------------------"
-			echo "Building the GUI with branch $SUBMODULE_BRANCH"
-			echo "----------------------------------------------"
+	if [ "$SUBMODULE_BRANCH" ];
+	then
+    git fetch
+		git checkout "$SUBMODULE_BRANCH"
+    git pull
+		echo ""
+		echo "Building the GUI with branch $SUBMODULE_BRANCH"
+		echo ""
 	fi
-	
+
 	npm install
 	npm audit fix || true
 	npm run build
