945225d359e4207ac2b5125529d6a04b52349c65
HiveProject2021 <jwang123@nyit.edu>
2021-06-22 21:23:50 -0700

    Update timelord.py

--- chia/timelord/timelord.py
+++ chia/timelord/timelord.py
@@ -783,7 +783,7 @@ class Timelord:
         else:
             # If there were no failures recently trigger a reset after 60 seconds of no activity.
             # Signage points should be every 9 seconds
-            active_time_threshold = 60
+            active_time_threshold = 600
         if time.time() - self.last_active_time > active_time_threshold:
             log.error(f"Not active for {active_time_threshold} seconds, restarting all chains")
             await self._reset_chains()
