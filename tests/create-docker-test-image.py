55f1d09b7d33c69e5ee92e18645b4fc0a4335329
arty <art.yerkes@gmail.com>
2021-06-24 12:56:42 -0700

    replace single with double quote because the linter likes it

--- tests/create-docker-test-image.py
+++ tests/create-docker-test-image.py
@@ -8,52 +8,52 @@ PYTHON_VERSIONS = ["3.8", "3.9"]
 
 
 def pyver_string(v):
-    return v.replace('.', '_')
+    return v.replace(".", "_")
 
 
 def writePythonEnv(targetDir, target):
     flat_ver = pyver_string(target)
     name = "python%s" % flat_ver
-    f = open(os.path.join(targetDir, 'environment%s.yml' % flat_ver), 'w')
-    f.write('name: %s\ndependencies:\n - python=%s' % (name, target))
+    f = open(os.path.join(targetDir, "environment%s.yml" % flat_ver), "w")
+    f.write("name: %s\ndependencies:\n - python=%s" % (name, target))
     f.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        shutil.rmtree('../docker-stage')
+        shutil.rmtree("../docker-stage")
     except Exception as _:  # noqa: F841
         pass
 
     try:
-        shutil.rmtree('./docker-compose')
+        shutil.rmtree("./docker-compose")
     except Exception as _:  # noqa: F841
         pass
 
     have_test_cache = False
     try:
-        mode = os.stat('test-cache').st_mode
+        mode = os.stat("test-cache").st_mode
         have_test_cache = S_ISDIR(mode)
     except Exception as _:  # noqa: F841
         pass
 
     if not have_test_cache:
-        subprocess.check_call(['git', 'clone', '--depth', '1', 'https://github.com/Chia-Network/test-cache'])
+        subprocess.check_call(["git", "clone", "--depth", "1", "https://github.com/Chia-Network/test-cache"])
 
-    os.makedirs('../docker-stage')
-    os.makedirs('./docker-compose')
-    shutil.copytree('..', '../docker-stage/app', ignore=shutil.ignore_patterns('docker-stage'))
-    shutil.copy('docker/Dockerfile', '../docker-stage/Dockerfile')
-    shutil.copy('docker/create-container.sh', '../docker-stage/app/create-container.sh')
-    shutil.copy('docker/run-in-container.sh', '../docker-stage/app/run-in-container.sh')
+    os.makedirs("../docker-stage")
+    os.makedirs("./docker-compose")
+    shutil.copytree("..", "../docker-stage/app", ignore=shutil.ignore_patterns("docker-stage"))
+    shutil.copy("docker/Dockerfile", "../docker-stage/Dockerfile")
+    shutil.copy("docker/create-container.sh", "../docker-stage/app/create-container.sh")
+    shutil.copy("docker/run-in-container.sh", "../docker-stage/app/run-in-container.sh")
     for pv in PYTHON_VERSIONS:
-        writePythonEnv('../docker-stage/app', pv)
+        writePythonEnv("../docker-stage/app", pv)
 
     versions_env = " ".join(map(pyver_string, PYTHON_VERSIONS))
     cmd = [
-        'sh',
-        '-c',
-        'cd ../docker-stage && docker build --build-arg PYTHON_VERSIONS="%s" -t chia-test .' % versions_env
+        "sh",
+        "-c",
+        "cd ../docker-stage && docker build --build-arg PYTHON_VERSIONS=\"%s\" -t chia-test ." % versions_env
     ]
     subprocess.check_call(cmd)
-    subprocess.check_call(['python3', 'build-workflows.py', '-t', './docker-templates', '-d', 'docker-compose'])
+    subprocess.check_call(["python3", "build-workflows.py", "-t", "./docker-templates", "-d", "docker-compose"])
