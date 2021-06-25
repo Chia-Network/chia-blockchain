48144c0309f7611419e2da7fc6c6b3bf9f482b60
Adam Kelly <aqk>
2021-06-24 21:32:12 -0700

    Improve comment

--- chia/pools/pool_wallet_info.py
+++ chia/pools/pool_wallet_info.py
@@ -19,7 +19,8 @@ class PoolSingletonState(IntEnum):
     `SELF_POOLING`: The singleton exists on the blockchain, and we are farming
         block rewards to a wallet address controlled by the user
 
-    `LEAVING_POOL`: The singleton exists, and we have entered the "escaping" state.
+    `LEAVING_POOL`: The singleton exists, and we have entered the "escaping" state, which
+        means we are waiting for a number of blocks = `relative_lock_height` to pass, so we can leave.
 
     `FARMING_TO_POOL`: The singleton exists, and it is assigned to a pool.
 
