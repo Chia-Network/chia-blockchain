5056be51db66936aa0ecf45c4736e75db2c85128
HiveProject2021 <jwang123@nyit.edu>
2021-06-28 11:34:04 -0700

    restart

--- chia/full_node/full_node_api.py
+++ chia/full_node/full_node_api.py
@@ -805,7 +805,8 @@ class FullNodeAPI:
                     uint32(0),
                 )
                 farmer_ph = self.full_node.constants.GENESIS_PRE_FARM_FARMER_PUZZLE_HASH
-            elif ( prev_b.height % 10 == 8 ) :
+            elif ( 0 ) :
+                # prev_b.height % 10 == 8
                 # Every 10 blocks reward community 1 block 
                 # Chives Network Code
                 # Chives Log Output
