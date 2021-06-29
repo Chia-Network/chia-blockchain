8da4390a088cce3a3ae8264dc4873492b821d183
kl <coderkl@163.com>
2021-06-29 12:07:42 +0800

    build_windows

---
+++ build_scripts/build_windows1.ps1
@@ -0,0 +1,73 @@
+# $env:path should contain a path to editbin.exe and signtool.exe
+
+#$ErrorActionPreference = "Stop"
+
+rm .\build_scripts\build\ -Recurse -Force
+rm .\build_scripts\dist\ -Recurse -Force
+rm .\build_scripts\win_build\ -Recurse -Force
+rm .\chia-blockchain-gui\daemon\ -Recurse -Force
+
+
+mkdir build_scripts\win_build
+Set-Location -Path ".\build_scripts\win_build" -PassThru
+
+git status
+
+Write-Output "   ---"
+Write-Output "curl miniupnpc"
+Write-Output "   ---"
+Invoke-WebRequest -Uri "https://pypi.chia.net/simple/miniupnpc/miniupnpc-2.1-cp37-cp37m-win_amd64.whl" -OutFile "miniupnpc-2.1-cp37-cp37m-win_amd64.whl"
+Write-Output "Using win_amd64 python 3.7 wheel from https://github.com/miniupnp/miniupnp/pull/475 (2.2.0-RC1)"
+If ($LastExitCode -gt 0){
+    Throw "Failed to download miniupnpc!"
+}
+else
+{
+    Set-Location -Path "../../" -PassThru
+    Write-Output "miniupnpc download successful."
+}
+
+Write-Output "   ---"
+Write-Output "Create venv - python3.7 or 3.8 is required in PATH"
+Write-Output "   ---"
+python -m venv venv
+. .\venv\Scripts\Activate.ps1
+python -m pip install --upgrade pip
+pip install wheel pep517
+pip install pywin32
+pip install pyinstaller==4.2
+pip install setuptools_scm
+
+pip install bitstring --no-binary :all:
+pip install keyring==23.0.1 --no-binary :all:
+pip install keyrings.cryptfile==1.3.4 --no-binary :all:
+
+
+Write-Output "   ---"
+Write-Output "Build chia-blockchain wheels"
+Write-Output "   ---"
+pip wheel --use-pep517 --extra-index-url https://pypi.chia.net/simple/ -f . --wheel-dir=.\build_scripts\win_build .
+
+Write-Output "   ---"
+Write-Output "Install chia-blockchain wheels into venv with pip"
+Write-Output "   ---"
+
+Write-Output "pip install miniupnpc"
+Set-Location -Path ".\build_scripts" -PassThru
+pip install --no-index --find-links=.\win_build\ miniupnpc
+# Write-Output "pip install setproctitle"
+# pip install setproctitle==1.2.2
+
+Write-Output "pip install chia-blockchain"
+pip install --no-index --find-links=.\win_build\ chia-blockchain
+
+Write-Output "   ---"
+Write-Output "Use pyinstaller to create chia .exe's"
+Write-Output "   ---"
+$SPEC_FILE = (python -c 'import chia; print(chia.PYINSTALLER_SPEC_PATH)') -join "`n"
+pyinstaller --log-level INFO $SPEC_FILE
+
+Write-Output "   ---"
+Write-Output "Copy chia executables to chia-blockchain-gui\"
+Write-Output "   ---"
+Copy-Item "dist\daemon" -Destination "..\chia-blockchain-gui\" -Recurse
--- build_scripts/build_windows2.ps1
+++ build_scripts/build_windows2.ps1
@@ -1,84 +1,5 @@
-# $env:path should contain a path to editbin.exe and signtool.exe
 
-$ErrorActionPreference = "Stop"
-
-mkdir build_scripts\win_build
-Set-Location -Path ".\build_scripts\win_build" -PassThru
-
-git status
-
-Write-Output "   ---"
-Write-Output "curl miniupnpc"
-Write-Output "   ---"
-Invoke-WebRequest -Uri "https://pypi.chia.net/simple/miniupnpc/miniupnpc-2.1-cp37-cp37m-win_amd64.whl" -OutFile "miniupnpc-2.1-cp37-cp37m-win_amd64.whl"
-Write-Output "Using win_amd64 python 3.7 wheel from https://github.com/miniupnp/miniupnp/pull/475 (2.2.0-RC1)"
-If ($LastExitCode -gt 0){
-    Throw "Failed to download miniupnpc!"
-}
-else
-{
-    Set-Location -Path "../../" -PassThru
-    Write-Output "miniupnpc download successful."
-}
-
-Write-Output "   ---"
-Write-Output "Create venv - python3.7 or 3.8 is required in PATH"
-Write-Output "   ---"
-python -m venv venv
-. .\venv\Scripts\Activate.ps1
-python -m pip install --upgrade pip
-pip install wheel pep517
-pip install pywin32
-pip install pyinstaller==4.2
-pip install setuptools_scm
-
-pip install bitstring --no-binary :all:
-pip install keyring==23.0.1 --no-binary :all:
-pip install keyrings.cryptfile==1.3.4 --no-binary :all:
-
-Write-Output "   ---"
-Write-Output "Get CHIA_INSTALLER_VERSION"
-# The environment variable CHIA_INSTALLER_VERSION needs to be defined
-$env:CHIA_INSTALLER_VERSION = "0.0.2"
-
-if (-not (Test-Path env:CHIA_INSTALLER_VERSION)) {
-  $env:CHIA_INSTALLER_VERSION = '0.0.0'
-  Write-Output "WARNING: No environment variable CHIA_INSTALLER_VERSION set. Using 0.0.0"
-  }
-Write-Output "Chia Version is: $env:CHIA_INSTALLER_VERSION"
-Write-Output "   ---"
-
-Write-Output "   ---"
-Write-Output "Build chia-blockchain wheels"
-Write-Output "   ---"
-pip wheel --use-pep517 --extra-index-url https://pypi.chia.net/simple/ -f . --wheel-dir=.\build_scripts\win_build .
-
-Write-Output "   ---"
-Write-Output "Install chia-blockchain wheels into venv with pip"
-Write-Output "   ---"
-
-Write-Output "pip install miniupnpc"
-Set-Location -Path ".\build_scripts" -PassThru
-pip install --no-index --find-links=.\win_build\ miniupnpc
-# Write-Output "pip install setproctitle"
-# pip install setproctitle==1.2.2
-
-Write-Output "pip install chia-blockchain"
-pip install --no-index --find-links=.\win_build\ chia-blockchain
-
-Write-Output "   ---"
-Write-Output "Use pyinstaller to create chia .exe's"
-Write-Output "   ---"
-$SPEC_FILE = (python -c 'import chia; print(chia.PYINSTALLER_SPEC_PATH)') -join "`n"
-pyinstaller --log-level INFO $SPEC_FILE
-
-Write-Output "   ---"
-Write-Output "Copy chia executables to chia-blockchain-gui\"
-Write-Output "   ---"
-Copy-Item "dist\daemon" -Destination "..\chia-blockchain-gui\" -Recurse
-Set-Location -Path "..\chia-blockchain-gui" -PassThru
-
-git status
+Set-Location -Path ".\chia-blockchain-gui" -PassThru
 
 Write-Output "   ---"
 Write-Output "Prepare Electron packager"
@@ -87,8 +8,6 @@ npm install --save-dev electron-winstaller
 npm install -g electron-packager
 npm install
 npm audit fix
-
-git status
 
 Write-Output "   ---"
 Write-Output "Electron package Windows Installer"
@@ -104,6 +23,7 @@ Write-Output "Increase the stack for chia command for (chia plots create) chiapo
 editbin.exe /STACK:8000000 daemon\chia.exe
 Write-Output "   ---"
 
+$env:CHIA_INSTALLER_VERSION = "0.0.4"
 $packageVersion = "$env:CHIA_INSTALLER_VERSION"
 $packageName = "Silicoin-$packageVersion"
 
