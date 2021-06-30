04eef77d52c23f50a67ef7b7b4489bfafcd37bb0
HiveProject2021 <jwang123@nyit.edu>
2021-06-30 08:40:50 -0700

    Revert "XCH TO XCC"
    This reverts commit 30cc3b4bb282018667c21c43c80dfe1d67b5b969.

--- chives/wallet/puzzles/prefarm/make_prefarm_ph.py
+++ chives/wallet/puzzles/prefarm/make_prefarm_ph.py
@@ -8,8 +8,8 @@ from chives.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
 from chives.util.condition_tools import parse_sexp_to_conditions
 from chives.util.ints import uint32
 
-address1 = "txcc15gx26ndmacfaqlq8m0yajeggzceu7cvmaz4df0hahkukes695rss6lej7h"  # Gene wallet (m/12381/9444/2/42):
-address2 = "txcc1c2cguswhvmdyz9hr3q6hak2h6p9dw4rz82g4707k2xy2sarv705qcce4pn"  # Mariano address (m/12381/9444/2/0)
+address1 = "txch15gx26ndmacfaqlq8m0yajeggzceu7cvmaz4df0hahkukes695rss6lej7h"  # Gene wallet (m/12381/9444/2/42):
+address2 = "txch1c2cguswhvmdyz9hr3q6hak2h6p9dw4rz82g4707k2xy2sarv705qcce4pn"  # Mariano address (m/12381/9444/2/0)
 
 ph1 = decode_puzzle_hash(address1)
 ph2 = decode_puzzle_hash(address2)
