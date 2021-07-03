b810eae6f49ba0d48098bf38b1d6544a3c0efeb7
HiveProject2021 <jwang123@nyit.edu>
2021-07-03 15:55:32 -0700

    1.1.750

--- build_scripts/build_windows.ps1
+++ build_scripts/build_windows.ps1
@@ -100,7 +100,7 @@ Write-Output "Get CHIVES_INSTALLER_VERSION"
 # The environment variable CHIVES_INSTALLER_VERSION needs to be defined
 $env:CHIVES_INSTALLER_VERSION = python .\build_scripts\installer-version.py -win
 
-$env:CHIVES_INSTALLER_VERSION = "1.1.740"
+$env:CHIVES_INSTALLER_VERSION = "1.1.750"
 
 if (-not (Test-Path env:CHIVES_INSTALLER_VERSION)) {
   $env:CHIVES_INSTALLER_VERSION = '0.0.0'
