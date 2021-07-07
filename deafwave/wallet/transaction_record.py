6562ba67a162c5b3e03b6757b40bf78c038119ae
SparXalt <SparXalt@Gmail.com>
2021-07-06 23:20:01 -0400

    Update transaction_record.py
    goji fix for "> not supported between instance of NoneType and uint32"

--- deafwave/wallet/transaction_record.py
+++ deafwave/wallet/transaction_record.py
@@ -64,4 +64,4 @@ class TransactionRecord(Streamable):
                     return uint32(block_index)
                 if postfarm_parent == self.additions[0].parent_coin_info:
                     return uint32(block_index)
-        return None
+        return uint32(block_index)
