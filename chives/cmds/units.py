2df951775deef511462b9ecacdf2ed1c727831c4
HiveProject2021 <jwang123@nyit.edu>
2021-07-07 10:34:56 -0700

    CHANGE UNIT TO 8 FROM 12

--- chives/cmds/units.py
+++ chives/cmds/units.py
@@ -3,7 +3,7 @@ from typing import Dict
 # The rest of the codebase uses mojos everywhere.
 # Only use these units for user facing interfaces.
 units: Dict[str, int] = {
-    "chives": 10 ** 12,  # 1 chives (XCC) is 1,000,000,000,000 mojo (1 trillion)
+    "chives": 10 ** 8,  # 1 chives (XCC) is 100,000,000 mojo
     "mojo:": 1,
     "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin mojos
 }
