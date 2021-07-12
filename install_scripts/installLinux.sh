ef31456c76521092ca1024671144edd089a26fb4
albertxuzeng <86463074+albertxuzeng@users.noreply.github.com>
2021-07-12 21:06:09 +0800

    Update installLinux.sh

--- install_scripts/installLinux.sh
+++ install_scripts/installLinux.sh
@@ -9,7 +9,7 @@ fi
 
 pip install --upgrade pip
 pip install -i https://hosted.chia.net/simple/ miniupnpc==2.1 setproctitle==1.1.10
-pip install git+https://github.com/silicoin-network/silicoin-blockchain.git@v0.0.7
+pip install git+https://github.com/silicoin-network/silicoin-blockchain.git@v0.0.8
 
 
 echo -e "\n===================================================="
