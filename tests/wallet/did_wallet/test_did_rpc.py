9fc64bbe8b1f1c5bc25793bfb7782f0692c5184d
matt <matt@chia.net>
2021-07-21 14:57:57 +0100

    black tests fix

--- tests/wallet/did_wallet/test_did_rpc.py
+++ tests/wallet/did_wallet/test_did_rpc.py
@@ -4,14 +4,17 @@ import pytest
 
 from chia.rpc.wallet_rpc_api import WalletRpcApi
 from chia.simulator.simulator_protocol import FarmNewBlockProtocol
+
 # from chia.types.blockchain_format.coin import Coin
 # from chia.types.blockchain_format.sized_bytes import bytes32
 # from chia.types.mempool_inclusion_status import MempoolInclusionStatus
 from chia.types.peer_info import PeerInfo
+
 # from chia.util.bech32m import encode_puzzle_hash
 from chia.util.ints import uint16
 from chia.wallet.util.wallet_types import WalletType
 from tests.setup_nodes import self_hostname, setup_simulators_and_wallets
+
 # from tests.time_out_assert import time_out_assert
 # from tests.wallet.sync.test_wallet_sync import wallet_height_at_least
 
