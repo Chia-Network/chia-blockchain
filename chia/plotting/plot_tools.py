8a8abc41a8c37a920d84ee90c34ece4d6d0254c5
xdustinface <xdustinfacex@gmail.com>
2021-07-27 20:32:51 +0200

    plotting: Fix `failed_to_open_filenames` re-try interval
    With `< 1200` it just tries it on the next refresh.

--- chia/plotting/plot_tools.py
+++ chia/plotting/plot_tools.py
@@ -225,7 +225,7 @@ class PlotManager:
             if filename.exists():
                 if (
                     filename in self.failed_to_open_filenames
-                    and (time.time() - self.failed_to_open_filenames[filename]) < 1200
+                    and (time.time() - self.failed_to_open_filenames[filename]) > 1200
                 ):
                     # Try once every 20 minutes to open the file
                     return 0, new_provers
