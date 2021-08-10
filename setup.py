7f8416364d4ab213af760d99ebe5ec4ca56d07a0
Jesse Chang <lcf.zhangxing@hotmail.com>
2021-08-10 10:10:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,14 @@ dependencies = [
     "bitstring==3.1.7",  # Binary data management library
     "colorlog==5.0.1",  # Adds color to logs
     "concurrent-log-handler==0.9.19",  # Concurrently log and rotate logs
-    "cryptography==3.4.7",  # Python cryptography library for TLS - keyring conflict
-    "keyring==23.0.1",  # Store keys in MacOS Keychain, Windows Credential Locker
-    "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
-    #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
+    # Python cryptography library for TLS - keyring conflict
+    "cryptography==3.4.7",
+    # Store keys in MacOS Keychain, Windows Credential Locker
+    "keyring==23.0.1",
+    # Secure storage for keys on Linux (Will be replaced)
+    "keyrings.cryptfile==1.3.4",
+    #  "keyrings.cryptfile==1.3.8"
+    # Secure storage for keys on Linux (Will be replaced)
     #  See https://github.com/frispete/keyrings.cryptfile/issues/15
     "PyYAML==5.4.1",  # Used for config file format
     "setproctitle==1.2.2",  # Gives the chia processes readable names
@@ -44,6 +48,7 @@ dev_dependencies = [
 def long_description():
     with open("README.md", "r", encoding="utf-8") as f:
         return f.read()
+
 
 kwargs = dict(
     name="chia-blockchain",
