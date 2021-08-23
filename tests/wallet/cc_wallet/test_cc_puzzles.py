b70f5f399704d196f2e11dd1b0fc893f24912573
matt <matt@chia.net>
2021-08-23 14:44:42 +0100

    commit finished test

--- tests/wallet/cc_wallet/test_cc_puzzles.py
+++ tests/wallet/cc_wallet/test_cc_puzzles.py
@@ -13,15 +13,16 @@ from chia.wallet.cc_wallet.cc_utils import cc_puzzle_for_inner_puzzle
 def test_signature_genesis():
     pubkey = Program.to(0x00fadeddab)
     genesis_checker = create_genesis_sig_checker(pubkey)
-    innerpuz = Program.to([[51, 0x00cafef00d, 200]])
+    innerpuz = Program.to([1, [51, 0x00cafef00d, 200]])
     puzzle = cc_puzzle_for_inner_puzzle(CC_MOD, genesis_checker, innerpuz)
 
     coin = Coin(puzzle.get_tree_hash(), puzzle.get_tree_hash(), uint64(200))
     # lineage proof is  (0 . some_opaque_proof_passed_to_GENESIS_COIN_CHECKER)
     lineage_proof = (0, 0)
 
-    my_coin_bundle = [coin.as_list(), lineage_proof]
-    # breakpoint()
+    my_coin_bundle = (coin.as_list(), lineage_proof)
     solution = Program.to([0, my_coin_bundle, my_coin_bundle, my_coin_bundle, 0])
+
     result = puzzle.run_with_cost(INFINITE_COST, solution)
-    # breakpoint()
+    assert result[1].first().first().as_int() == 50
+    assert result[1].first().rest().first().as_int() == 0x00fadeddab
