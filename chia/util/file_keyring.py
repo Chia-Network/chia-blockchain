ff1770562c2cf4a7f9f0fd203afef87af1735a19
Jeff Cruikshank <jeff@chia.net>
2021-08-31 19:37:22 -0700

    Use with_name instead of with_stem (which is new to Python 3.9)

--- chia/util/file_keyring.py
+++ chia/util/file_keyring.py
@@ -134,7 +134,7 @@ class FileKeyring(FileSystemEventHandler):
 
     @staticmethod
     def lockfile_path_for_file_path(file_path: Path) -> Path:
-        return file_path.with_stem("." + file_path.stem).with_suffix(".lock")
+        return file_path.with_name(f".{file_path.name}.lock")
 
     def __init__(self, keys_root_path: Path = DEFAULT_KEYS_ROOT_PATH):
         """
