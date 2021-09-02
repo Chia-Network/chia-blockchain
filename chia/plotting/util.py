2db9944687aca17904e72e2f65934a945071c892
dustinface <35775977+xdustinface@users.noreply.github.com>
2021-09-02 17:23:52 +0200

    plotting|util: Adjust plot refresh parameter (#8275)

--- chia/plotting/util.py
+++ chia/plotting/util.py
@@ -17,8 +17,8 @@ log = logging.getLogger(__name__)
 class PlotsRefreshParameter:
     interval_seconds: int = 120
     retry_invalid_seconds: int = 1200
-    batch_size: int = 30
-    batch_sleep_milliseconds: int = 10
+    batch_size: int = 300
+    batch_sleep_milliseconds: int = 1
 
 
 @dataclass
