27f9b68f5fa969321ba9b569b05e0bf505b40e80
Matt Hauff <quexington@gmail.com>
2021-09-09 15:03:11 -0700

    Fixed failing CLVM tests

--- tests/clvm/test_clvm_compilation.py
+++ tests/clvm/test_clvm_compilation.py
@@ -8,12 +8,13 @@ from chia.types.blockchain_format.program import Program, SerializedProgram
 wallet_program_files = set(
     [
         "chia/wallet/puzzles/calculate_synthetic_public_key.clvm",
-        "chia/wallet/puzzles/cc.clvm",
+        "chia/wallet/puzzles/cat.clvm",
         "chia/wallet/puzzles/chialisp_deserialisation.clvm",
         "chia/wallet/puzzles/rom_bootstrap_generator.clvm",
         "chia/wallet/puzzles/generator_for_single_coin.clvm",
         "chia/wallet/puzzles/genesis-by-coin-id-with-0.clvm",
         "chia/wallet/puzzles/genesis-by-puzzle-hash-with-0.clvm",
+        "chia/wallet/puzzles/everything_with_signature.clvm",
         "chia/wallet/puzzles/lock.inner.puzzle.clvm",
         "chia/wallet/puzzles/p2_conditions.clvm",
         "chia/wallet/puzzles/p2_delegated_conditions.clvm",
