1feb3704bc83272f65080026af50ec527c6884ed
matt <matt@chia.net>
2021-09-10 17:02:06 +0100

    fix everything with sig test and inconsistent names

--- tests/wallet/cc_wallet/test_cc_puzzles.py
+++ tests/wallet/cc_wallet/test_cc_puzzles.py
@@ -2,7 +2,7 @@ from chia.types.blockchain_format.program import Program, INFINITE_COST
 from chia.util.ints import uint64
 from chia.wallet.puzzles.cc_loader import CC_MOD
 from chia.types.blockchain_format.coin import Coin
-from chia.wallet.puzzles.genesis_with_signature import create_genesis_sig_checker
+from chia.wallet.puzzles.everything_with_signature import create_genesis_sig_checker
 from chia.wallet.cc_wallet.cc_utils import cc_puzzle_for_inner_puzzle
 
 
@@ -16,9 +16,9 @@ def test_signature_genesis():
     # lineage proof is  (0 . some_opaque_proof_passed_to_GENESIS_COIN_CHECKER)
     lineage_proof = (0, 0)
 
-    my_coin_bundle = (coin.as_list(), lineage_proof)
-    solution = Program.to([0, my_coin_bundle, my_coin_bundle, my_coin_bundle, 0])
-
+    my_coin_bundle = ([puzzle.get_tree_hash(), puzzle.get_tree_hash(), uint64(200), innerpuz.get_tree_hash()], lineage_proof)
+    solution = Program.to([0, my_coin_bundle, my_coin_bundle, my_coin_bundle, 0, 0])
     result = puzzle.run_with_cost(INFINITE_COST, solution)
-    assert result[1].first().first().as_int() == 50
-    assert result[1].first().rest().first().as_int() == 0x00FADEDDAB
+
+    assert result[1].rest().first().first().as_int() == 50
+    assert result[1].rest().first().rest().first().as_int() == 0x00FADEDDAB
