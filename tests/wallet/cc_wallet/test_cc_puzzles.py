e42c97bc2d02527ba7c697914db2fa03900b52f4
matt <matt@chia.net>
2021-09-10 17:02:47 +0100

    remove unused import

--- tests/wallet/cc_wallet/test_cc_puzzles.py
+++ tests/wallet/cc_wallet/test_cc_puzzles.py
@@ -1,7 +1,6 @@
 from chia.types.blockchain_format.program import Program, INFINITE_COST
 from chia.util.ints import uint64
 from chia.wallet.puzzles.cc_loader import CC_MOD
-from chia.types.blockchain_format.coin import Coin
 from chia.wallet.puzzles.everything_with_signature import create_genesis_sig_checker
 from chia.wallet.cc_wallet.cc_utils import cc_puzzle_for_inner_puzzle
 
@@ -12,7 +11,8 @@ def test_signature_genesis():
     innerpuz = Program.to([1, [51, 0x00CAFEF00D, 200]])
     puzzle = cc_puzzle_for_inner_puzzle(CC_MOD, genesis_checker, innerpuz)
 
-    coin = Coin(puzzle.get_tree_hash(), puzzle.get_tree_hash(), uint64(200))
+    # coin = Coin(puzzle.get_tree_hash(), puzzle.get_tree_hash(), uint64(200))
+
     # lineage proof is  (0 . some_opaque_proof_passed_to_GENESIS_COIN_CHECKER)
     lineage_proof = (0, 0)
 
