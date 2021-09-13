0b8bcf127e606f4b71146136ea4cd23a62ff5d43
Jack Nelson <jack@jacknelson.xyz>
2021-09-12 18:35:37 -0700

    didn't mean to change that

--- src/dev_config.js
+++ src/dev_config.js
@@ -1,8 +1,8 @@
 const dev_config = {
-  //redux_tool: null,
-  //react_tool: null,
-  react_tool: "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\fmkadmapgofadopljbjfkapdkoienihi\\4.18.0_0",
-  redux_tool: "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\lmhkpmbekcpmknklioeibfkpmmfibljd\\2.17.2_0"
+  redux_tool: null,
+  react_tool: null,
+  //react_tool: "/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.6.0_0",
+  //redux_tool: "/Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.17.0_0"
 };
 
 module.exports = dev_config;
