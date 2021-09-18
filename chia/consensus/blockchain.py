1ad862bbbee36cd88fbc5eca77c2cf5d304162bf
Yostra <straya@chia.net>
2021-09-18 01:23:20 -0400

    remove unused tests, lint

--- chia/consensus/blockchain.py
+++ chia/consensus/blockchain.py
@@ -317,7 +317,7 @@ class Blockchain(BlockchainInterface):
                     tx_removals, tx_additions = [], []
                 if block.is_transaction_block():
                     assert block.foliage_transaction_block is not None
-                    added, removed = await self.coin_store.new_block(
+                    added, _ = await self.coin_store.new_block(
                         block.height,
                         block.foliage_transaction_block.timestamp,
                         block.get_included_reward_coins(),
@@ -325,7 +325,7 @@ class Blockchain(BlockchainInterface):
                         tx_removals,
                     )
                 else:
-                    added, removed = [], []
+                    added, _ = [], []
                 await self.block_store.set_peak(block_record.header_hash)
                 return uint32(0), uint32(0), [block_record], added
             return None, None, [], []
