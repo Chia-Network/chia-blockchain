0cc1e0446173fad1b1148887f3d1ffc6df52d1bc
almog <almogdepaz@gmail.com>
2021-09-20 13:52:57 +0300

    fix naming

--- chia/data_layer/data_layer_rpc_api.py
+++ chia/data_layer/data_layer_rpc_api.py
@@ -9,8 +9,8 @@ from chia.util.hash import std_hash
 
 
 class DataLayerRpcApi:
-    def __init__(self, service: DataLayer, wallet: DataLayerWallet):
-        self.data_layer_service: DataLayer = service
+    def __init__(self, data_layer: DataLayer, wallet: DataLayerWallet):
+        self.service: DataLayer = data_layer
         self.data_layer_wallet = wallet
         self.service_name = "chia_data_layer"
 
@@ -20,9 +20,9 @@ class DataLayerRpcApi:
     async def get_row(self, request: Dict) -> bytes:
         row = b""
         if "hash" in request:
-            row = await self.data_layer_service.data_store.get_row_by_hash(request["hash"])
+            row = await self.service.data_store.get_row_by_hash(request["hash"])
         elif "index" in request:
-            row = await self.data_layer_service.data_store.get_row_by_index(request["index"])
+            row = await self.service.data_store.get_row_by_index(request["index"])
         return row
 
     async def update(self, request: Dict):
@@ -37,13 +37,13 @@ class DataLayerRpcApi:
             if change["action"] == "insert":
                 row = change["row"]
                 operation = OperationType.INSERT
-                index = await self.data_layer_service.data_store.insert_row(table, row)
+                index = await self.service.data_store.insert_row(table, row)
             else:
                 assert change["action"] == "delete"
-                row, index = await self.data_layer_service.data_store.delete_row_by_hash(table, change["row"])
+                row, index = await self.service.data_store.delete_row_by_hash(table, change["row"])
                 operation = OperationType.DELETE
             action_list.append(Action(op=operation, row=row, row_index=index))
-        state = await self.data_layer_service.data_store.get_table_state(table)
+        state = await self.service.data_store.get_table_state(table)
 
         # todo get changelist hash, order changelist before committing hash
         await self.data_layer_wallet.uptate_table_state(table, state, std_hash(action_list))
