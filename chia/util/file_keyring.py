1254d4decdadc141bed0b4eb9f6b454e857e0210
Jeff Cruikshank <paninaro@gmail.com>
2021-09-24 13:24:03 -0700

    In rare cases where os.replace fails, retry using shutil.move.  (#8589)
    * In rare cases where os.replace() fails, retry using shutil.move. Seen on some Windows installs.
    
    * Catch the PermissionError specifically

--- chia/util/file_keyring.py
+++ chia/util/file_keyring.py
@@ -1,6 +1,7 @@
 import base64
 import fasteners
 import os
+import shutil
 import sys
 import threading
 import yaml
@@ -432,7 +433,10 @@ class FileKeyring(FileSystemEventHandler):
         temp_path: Path = self.keyring_path.with_suffix("." + str(os.getpid()))
         with open(os.open(str(temp_path), os.O_CREAT | os.O_EXCL | os.O_WRONLY, 0o600), "w") as f:
             _ = yaml.safe_dump(data, f)
-        os.replace(str(temp_path), self.keyring_path)
+        try:
+            os.replace(str(temp_path), self.keyring_path)
+        except PermissionError:
+            shutil.move(str(temp_path), str(self.keyring_path))
 
     def prepare_for_migration(self):
         if not self.payload_cache:
