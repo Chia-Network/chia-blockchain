8406558a6f5b87c44e8ef39b3dd64f618587afe2
Flora-Network <github@floracoin.farm>
2021-09-26 12:53:36 +0200

    Rewrite chia to flora

--- chia/__init__.py
+++ chia/__init__.py
@@ -1,7 +1,7 @@
 from pkg_resources import DistributionNotFound, get_distribution, resource_filename
 
 try:
-    __version__ = get_distribution("chia-blockchain").version
+    __version__ = get_distribution("flora-blockchain").version
 except DistributionNotFound:
     # package is not installed
     __version__ = "unknown"
