37c77b32b452d9445a9725abc989ff922e1680a7
Arvid Norberg <arvid.norberg@gmail.com>
2021-09-29 00:46:20 +0200

    update create_coin hint test to use a list (#8628)
    * update create_coin hint test to use a list
    
    * the hint/memo is always included in the CREATE_COIN conditions now

--- tests/core/full_node/test_mempool.py
+++ tests/core/full_node/test_mempool.py
@@ -1818,7 +1818,7 @@ class TestGeneratorConditions:
             assert c.conditions == [
                 (
                     opcode.value,
-                    [ConditionWithArgs(opcode, [puzzle_hash.encode("ascii"), bytes([10])])],
+                    [ConditionWithArgs(opcode, [puzzle_hash.encode("ascii"), bytes([10]), b""])],
                 )
             ]
 
@@ -1832,11 +1832,11 @@ class TestGeneratorConditions:
         assert len(npc_result.npc_list) == 1
         opcode = ConditionOpcode.CREATE_COIN
         assert (
-            ConditionWithArgs(opcode, [puzzle_hash_1.encode("ascii"), bytes([5])])
+            ConditionWithArgs(opcode, [puzzle_hash_1.encode("ascii"), bytes([5]), b""])
             in npc_result.npc_list[0].conditions[0][1]
         )
         assert (
-            ConditionWithArgs(opcode, [puzzle_hash_2.encode("ascii"), bytes([5])])
+            ConditionWithArgs(opcode, [puzzle_hash_2.encode("ascii"), bytes([5]), b""])
             in npc_result.npc_list[0].conditions[0][1]
         )
 
@@ -1849,11 +1849,11 @@ class TestGeneratorConditions:
         assert len(npc_result.npc_list) == 1
         opcode = ConditionOpcode.CREATE_COIN
         assert (
-            ConditionWithArgs(opcode, [puzzle_hash.encode("ascii"), bytes([5])])
+            ConditionWithArgs(opcode, [puzzle_hash.encode("ascii"), bytes([5]), b""])
             in npc_result.npc_list[0].conditions[0][1]
         )
         assert (
-            ConditionWithArgs(opcode, [puzzle_hash.encode("ascii"), bytes([4])])
+            ConditionWithArgs(opcode, [puzzle_hash.encode("ascii"), bytes([4]), b""])
             in npc_result.npc_list[0].conditions[0][1]
         )
 
@@ -1861,7 +1861,7 @@ class TestGeneratorConditions:
         # CREATE_COIN
         puzzle_hash_1 = "abababababababababababababababab"
         hint = "12341234123412341234213421341234"
-        npc_result = generator_condition_tester(f'(51 "{puzzle_hash_1}" 5 "{hint}")')
+        npc_result = generator_condition_tester(f'(51 "{puzzle_hash_1}" 5 ("{hint}"))')
         assert npc_result.error is None
         assert len(npc_result.npc_list) == 1
         opcode = ConditionOpcode.CREATE_COIN
