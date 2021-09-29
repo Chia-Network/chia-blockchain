4fddb5cdf11faf5b26984c66bfd0bf90b17c6373
Yostra <straya@chia.net>
2021-09-29 18:04:29 -0400

    Full node changes required for CAT / Standalone (#8616)
    * full node changes
    
    * hint store
    
    * finish coin state tracking, mypy, flake8
    
    * type hints
    
    * bump protocol version
    
    * change wallet tool for testing hint list
    
    * mypy
    
    * add check for state, future proof hint db for multiple hints per coin
    
    * get hint rename
    
    * clean
    
    * Update chia/consensus/blockchain.py
    
    Co-authored-by: Arvid Norberg <arvid.norberg@gmail.com>
    
    Co-authored-by: Mariano Sorgente <3069354+mariano54@users.noreply.github.com>
    Co-authored-by: Arvid Norberg <arvid.norberg@gmail.com>

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -24,6 +24,7 @@ from chia.full_node.block_store import BlockStore
 from chia.full_node.bundle_tools import detect_potential_template_generator
 from chia.full_node.coin_store import CoinStore
 from chia.full_node.full_node_store import FullNodeStore
+from chia.full_node.hint_store import HintStore
 from chia.full_node.mempool_manager import MempoolManager
 from chia.full_node.signage_point import SignagePoint
 from chia.full_node.sync_store import SyncStore
@@ -145,10 +146,11 @@ class FullNode:
         self.db_wrapper = DBWrapper(self.connection)
         self.block_store = await BlockStore.create(self.db_wrapper)
         self.sync_store = await SyncStore.create()
+        self.hint_store = await HintStore.create(self.db_wrapper)
         self.coin_store = await CoinStore.create(self.db_wrapper)
         self.log.info("Initializing blockchain from disk")
         start_time = time.time()
-        self.blockchain = await Blockchain.create(self.coin_store, self.block_store, self.constants)
+        self.blockchain = await Blockchain.create(self.coin_store, self.block_store, self.constants, self.hint_store)
         self.mempool_manager = MempoolManager(self.coin_store, self.constants)
         self.weight_proof_handler = None
         self._init_weight_proof = asyncio.create_task(self.initialize_weight_proof())
@@ -859,11 +861,17 @@ class FullNode:
         return peers_with_peak
 
     async def update_wallets(
-        self, height: uint32, fork_height: uint32, peak_hash: bytes32, state_update: List[CoinRecord]
+        self,
+        height: uint32,
+        fork_height: uint32,
+        peak_hash: bytes32,
+        state_update: Tuple[List[CoinRecord], Dict[bytes, Dict[bytes32, CoinRecord]]],
     ):
         changes_for_peer: Dict[bytes32, Set[CoinState]] = {}
 
-        for coin_record in state_update:
+        states, hint_state = state_update
+
+        for coin_record in states:
             if coin_record.name in self.coin_subscriptions:
                 subscribed_peers = self.coin_subscriptions[coin_record.name]
                 for peer in subscribed_peers:
@@ -877,6 +885,15 @@ class FullNode:
                     if peer not in changes_for_peer:
                         changes_for_peer[peer] = set()
                     changes_for_peer[peer].add(coin_record.coin_state)
+
+        for hint, records in hint_state.items():
+            if hint in self.ph_subscriptions:
+                subscribed_peers = self.ph_subscriptions[hint]
+                for peer in subscribed_peers:
+                    if peer not in changes_for_peer:
+                        changes_for_peer[peer] = set()
+                    for record in records.values():
+                        changes_for_peer[peer].add(record.coin_state)
 
         for peer, changes in changes_for_peer.items():
             if peer not in self.server.all_connections:
@@ -892,7 +909,7 @@ class FullNode:
         peer: ws.WSChiaConnection,
         fork_point: Optional[uint32],
         wp_summaries: Optional[List[SubEpochSummary]] = None,
-    ) -> Tuple[bool, bool, Optional[uint32], List[CoinRecord]]:
+    ) -> Tuple[bool, bool, Optional[uint32], Tuple[List[CoinRecord], Dict[bytes, Dict[bytes, CoinRecord]]]]:
         advanced_peak = False
         fork_height: Optional[uint32] = uint32(0)
 
@@ -902,7 +919,7 @@ class FullNode:
                 blocks_to_validate = all_blocks[i:]
                 break
         if len(blocks_to_validate) == 0:
-            return True, False, fork_height, []
+            return True, False, fork_height, ([], {})
 
         pre_validate_start = time.time()
         pre_validation_results: Optional[
@@ -914,25 +931,40 @@ class FullNode:
         else:
             self.log.debug(f"Block pre-validation time: {pre_validate_end - pre_validate_start:0.2f} seconds")
         if pre_validation_results is None:
-            return False, False, None, []
+            return False, False, None, ([], {})
         for i, block in enumerate(blocks_to_validate):
             if pre_validation_results[i].error is not None:
                 self.log.error(
                     f"Invalid block from peer: {peer.get_peer_logging()} {Err(pre_validation_results[i].error)}"
                 )
-                return False, advanced_peak, fork_height, []
+                return False, advanced_peak, fork_height, ([], {})
+
+        # Dicts because deduping
+        all_coin_changes: Dict[bytes32, CoinRecord] = {}
+        all_hint_changes: Dict[bytes, Dict[bytes32, CoinRecord]] = {}
 
         for i, block in enumerate(blocks_to_validate):
             assert pre_validation_results[i].required_iters is not None
             result, error, fork_height, coin_changes = await self.blockchain.receive_block(
                 block, pre_validation_results[i], None if advanced_peak else fork_point
             )
+            coin_record_list, hint_records = coin_changes
+
+            # Update all changes
+            for record in coin_record_list:
+                all_coin_changes[record.name] = record
+            for hint, list_of_records in hint_records.items():
+                if hint not in all_hint_changes:
+                    all_hint_changes[hint] = {}
+                for record in list_of_records:
+                    all_hint_changes[hint][record.name] = record
+
             if result == ReceiveBlockResult.NEW_PEAK:
                 advanced_peak = True
             elif result == ReceiveBlockResult.INVALID_BLOCK or result == ReceiveBlockResult.DISCONNECTED_BLOCK:
                 if error is not None:
                     self.log.error(f"Error: {error}, Invalid block from peer: {peer.get_peer_logging()} ")
-                return False, advanced_peak, fork_height, []
+                return False, advanced_peak, fork_height, ([], {})
             block_record = self.blockchain.block_record(block.header_hash)
             if block_record.sub_epoch_summary_included is not None:
                 if self.weight_proof_handler is not None:
@@ -943,7 +975,7 @@ class FullNode:
                 f"Total time for {len(blocks_to_validate)} blocks: {time.time() - pre_validate_start}, "
                 f"advanced: {advanced_peak}"
             )
-        return True, advanced_peak, fork_height, coin_changes
+        return True, advanced_peak, fork_height, (list(all_coin_changes.values()), all_hint_changes)
 
     async def _finish_sync(self):
         """
@@ -1046,7 +1078,7 @@ class FullNode:
         record: BlockRecord,
         fork_height: uint32,
         peer: Optional[ws.WSChiaConnection],
-        coin_changes: List[CoinRecord],
+        coin_changes: Tuple[List[CoinRecord], Dict[bytes, Dict[bytes32, CoinRecord]]],
     ):
         """
         Must be called under self.blockchain.lock. This updates the internal state of the full node with the
@@ -1259,7 +1291,7 @@ class FullNode:
                 )
                 # This recursion ends here, we cannot recurse again because transactions_generator is not None
                 return await self.respond_block(block_response, peer)
-        coin_changes: List[CoinRecord] = []
+        coin_changes: Tuple[List[CoinRecord], Dict[bytes, Dict[bytes32, CoinRecord]]] = ([], {})
         async with self.blockchain.lock:
             # After acquiring the lock, check again, because another asyncio thread might have added it
             if self.blockchain.contains_block(header_hash):
--- chia/full_node/full_node_api.py
+++ chia/full_node/full_node_api.py
@@ -18,12 +18,19 @@ from chia.full_node.signage_point import SignagePoint
 from chia.protocols import farmer_protocol, full_node_protocol, introducer_protocol, timelord_protocol, wallet_protocol
 from chia.protocols.full_node_protocol import RejectBlock, RejectBlocks
 from chia.protocols.protocol_message_types import ProtocolMessageTypes
-from chia.protocols.wallet_protocol import PuzzleSolutionResponse, RejectHeaderBlocks, RejectHeaderRequest, CoinState
+from chia.protocols.wallet_protocol import (
+    PuzzleSolutionResponse,
+    RejectHeaderBlocks,
+    RejectHeaderRequest,
+    CoinState,
+    RespondSESInfo,
+)
 from chia.server.outbound_message import Message, make_msg
 from chia.types.blockchain_format.coin import Coin, hash_coin_list
 from chia.types.blockchain_format.pool_target import PoolTarget
 from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.types.blockchain_format.sub_epoch_summary import SubEpochSummary
 from chia.types.coin_record import CoinRecord
 from chia.types.end_of_slot_bundle import EndOfSubSlotBundle
 from chia.types.full_block import FullBlock
@@ -1057,7 +1064,7 @@ class FullNodeAPI:
             return msg
         block: Optional[FullBlock] = await self.full_node.block_store.get_full_block(header_hash)
         if block is not None:
-            tx_removals, tx_additions = await self.full_node.blockchain.get_tx_removals_and_additions(block)
+            tx_removals, tx_additions, _ = await self.full_node.blockchain.get_tx_removals_and_additions(block)
             header_block = get_block_header(block, tx_additions, tx_removals)
             msg = make_msg(
                 ProtocolMessageTypes.respond_block_header,
@@ -1331,8 +1338,11 @@ class FullNodeAPI:
         if peer.peer_node_id not in self.full_node.peer_sub_counter:
             self.full_node.peer_sub_counter[peer.peer_node_id] = 0
 
+        hint_coin_ids = []
         # Add peer to the "Subscribed" dictionary
         for puzzle_hash in request.puzzle_hashes:
+            ph_hint_coins = await self.full_node.hint_store.get_coin_ids(puzzle_hash)
+            hint_coin_ids.extend(ph_hint_coins)
             if puzzle_hash not in self.full_node.ph_subscriptions:
                 self.full_node.ph_subscriptions[puzzle_hash] = set()
             if (
@@ -1347,6 +1357,12 @@ class FullNodeAPI:
         states: List[CoinState] = await self.full_node.coin_store.get_coin_states_by_puzzle_hashes(
             include_spent_coins=True, puzzle_hashes=request.puzzle_hashes, start_height=request.min_height
         )
+
+        if len(hint_coin_ids) > 0:
+            hint_states = await self.full_node.coin_store.get_coin_state_by_ids(
+                include_spent_coins=True, coin_ids=hint_coin_ids, start_height=request.min_height
+            )
+            states.extend(hint_states)
 
         response = wallet_protocol.RespondToPhUpdates(request.puzzle_hashes, request.min_height, states)
         msg = make_msg(ProtocolMessageTypes.respond_to_ph_update, response)
@@ -1390,4 +1406,40 @@ class FullNodeAPI:
         states = [record.coin_state for record in coin_records]
         response = wallet_protocol.RespondChildren(states)
         msg = make_msg(ProtocolMessageTypes.respond_children, response)
+        return msg
+
+    @api_request
+    async def request_ses_hashes(self, request: wallet_protocol.RequestSESInfo):
+        """Returns the start and end height of a sub-epoch for the height specified in request"""
+
+        ses_height = self.full_node.blockchain.get_ses_heights()
+        start_height = request.start_height
+        end_height = request.end_height
+        ses_hash_heights = []
+        ses_reward_hashes = []
+
+        for idx, ses_start_height in enumerate(ses_height):
+            if idx == len(ses_height) - 1:
+                break
+
+            next_ses_height = ses_height[idx + 1]
+            # start_ses_hash
+            if ses_start_height <= start_height < next_ses_height:
+                ses_hash_heights.append([ses_start_height, next_ses_height])
+                ses: SubEpochSummary = self.full_node.blockchain.get_ses(ses_start_height)
+                ses_reward_hashes.append(ses.reward_chain_hash)
+                if ses_start_height < end_height < next_ses_height:
+                    break
+                else:
+                    if idx == len(ses_height) - 2:
+                        break
+                    # else add extra ses as request start <-> end spans two ses
+                    next_next_height = ses_height[idx + 2]
+                    ses_hash_heights.append([next_ses_height, next_next_height])
+                    nex_ses: SubEpochSummary = self.full_node.blockchain.get_ses(next_ses_height)
+                    ses_reward_hashes.append(nex_ses.reward_chain_hash)
+                    break
+
+        response = RespondSESInfo(ses_reward_hashes, ses_hash_heights)
+        msg = make_msg(ProtocolMessageTypes.respond_ses_hashes, response)
         return msg
