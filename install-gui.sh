9865827a1128cd53608a6ebe4aa1fd45b12fed47
xdustinface <xdustinfacex@gmail.com>
2021-10-05 16:25:13 +0200

    install-gui: Drop trailing whitespaces

--- install-gui.sh
+++ install-gui.sh
@@ -24,7 +24,7 @@ if [ "$(uname)" = "Linux" ]; then
 	if type apt-get; then
 		# Debian/Ubuntu
 		UBUNTU=true
-		
+
 		# Check if we are running a Raspberry PI 4
 		if [ "$(uname -m)" = "aarch64" ] \
 		&& [ "$(uname -n)" = "raspberrypi" ]; then
