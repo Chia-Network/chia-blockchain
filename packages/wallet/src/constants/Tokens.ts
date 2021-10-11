f6d202d3dda9e7b25b1f4c5c78eb898ea5cd1ad9
Yostra <straya@chia.net>
2021-10-11 16:48:41 -0400

    tree hash instead of full tail

--- packages/wallet/src/constants/Tokens.ts
+++ packages/wallet/src/constants/Tokens.ts
@@ -5,19 +5,19 @@ const Tokens: CATToken = [{
   icon: AsteroidIcon,
   symbol: 'AST',
   name: 'Asteroid',
-  tail: 'ff02ffff01ff02ffff03ff2fffff01ff0880ffff01ff02ffff03ffff21ff0bffff09ff2dff028080ffff015fffff01ff088080ff018080ff0180ffff04ffff01a0716525483ce568e51dd1a2708838eccc2347efcc0268a6d007ec0a84308e3fcdff018080',
+  tail: '8cbc44d2230d42405df3846f5f124c38a824f96419cecea8ef7f49787e1d05c5',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 }, {
   icon: WaterIcon,
   symbol: 'WAT',
   name: 'Water',
-  tail: 'ff02ffff01ff02ffff03ff2fffff01ff0880ffff01ff02ffff03ffff21ff0bffff09ff2dff028080ffff015fffff01ff088080ff018080ff0180ffff04ffff01a0a557cbf053d3efb62ce8d4acf9ed82cdca260593466c62fcdb19902d47518e1cff018080',
+  tail: 'c227d12fbcf14ed3cb66730971117bf7c25f0a14f3c4916a2f9c91dc452fab8e',
   description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 }, {
   icon: EnergyIcon,
   symbol: 'ENG',
   name: 'Energy',
-  tail: 'ff02ffff01ff02ffff03ff2fffff01ff0880ffff01ff02ffff03ffff21ff0bffff09ff2dff028080ffff015fffff01ff088080ff018080ff0180ffff04ffff01a0ded7745e3502d18a08fdfcd5fce275ea985803fd2a24ca06d9a17aaa09a6d957ff018080',
+  tail: '916d3b662263a8afa6fd7b5c0d5c79d6652677a6279217fd667f60a8ee8c83a8',
   description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
 }];
 
