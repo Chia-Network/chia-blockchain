c1a1d81620e6c07f2e1f44235775096ca10eced2
Jeff Cruikshank <jeff@chia.net>
2021-10-12 11:48:26 -0700

    Renamed chia.util.ssl to avoid conflicting with aiohttp's ssl import. Removed hacky sys.path reordering in dump_keyring.py

--- chia/cmds/init_funcs.py
+++ chia/cmds/init_funcs.py
@@ -25,7 +25,7 @@ from chia.util.config import (
 from chia.util.ints import uint32
 from chia.util.keychain import Keychain
 from chia.util.path import mkdir
-from chia.util.ssl import (
+from chia.util.ssl_check import (
     DEFAULT_PERMISSIONS_CERT_FILE,
     DEFAULT_PERMISSIONS_KEY_FILE,
     RESTRICT_MASK_CERT_FILE,
