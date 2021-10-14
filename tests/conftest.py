5a4b2030abb2ee59fa45348430f05e8525cace1b
Kyle Altendorf <sda@fstab.net>
2021-10-13 22:47:12 -0400

    black

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,18 +23,21 @@ block_format_version = "rc4"
 @pytest.fixture(scope="session")
 async def default_400_blocks():
     from tests.util.blockchain import persistent_blocks
+
     return persistent_blocks(400, f"test_blocks_400_{block_format_version}.db", seed=b"alternate2")
 
 
 @pytest.fixture(scope="session")
 async def default_1000_blocks():
     from tests.util.blockchain import persistent_blocks
+
     return persistent_blocks(1000, f"test_blocks_1000_{block_format_version}.db")
 
 
 @pytest.fixture(scope="session")
 async def pre_genesis_empty_slots_1000_blocks():
     from tests.util.blockchain import persistent_blocks
+
     return persistent_blocks(
         1000, f"pre_genesis_empty_slots_1000_blocks{block_format_version}.db", seed=b"alternate2", empty_sub_slots=1
     )
@@ -43,18 +46,21 @@ async def pre_genesis_empty_slots_1000_blocks():
 @pytest.fixture(scope="session")
 async def default_10000_blocks():
     from tests.util.blockchain import persistent_blocks
+
     return persistent_blocks(10000, f"test_blocks_10000_{block_format_version}.db")
 
 
 @pytest.fixture(scope="session")
 async def default_20000_blocks():
     from tests.util.blockchain import persistent_blocks
+
     return persistent_blocks(20000, f"test_blocks_20000_{block_format_version}.db")
 
 
 @pytest.fixture(scope="session")
 async def default_10000_blocks_compact():
     from tests.util.blockchain import persistent_blocks
+
     return persistent_blocks(
         10000,
         f"test_blocks_10000_compact_{block_format_version}.db",
