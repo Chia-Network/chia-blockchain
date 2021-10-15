e8b5ba067cbdeb0ac3be1711fc7204a938ad79f4
Yostra <straya@chia.net>
2021-10-15 13:39:23 -0400

    restore cat with hardcoded name

---
+++ chia/wallet/cc_wallet/cat_constants.py
@@ -0,0 +1,23 @@
+ASTEROID = {
+    "asset_id": "783cd84333cacd7e3f6292963b59dd5855ce6a609b9992a60392725a3ed719de",
+    "name": "Asteroid",
+    "symbol": "AST",
+}
+
+WATER = {
+    "asset_id": "28307353c18ef004294d266fbd4a753eef3b60e2c98ba648b271ac277d33073f",
+    "name": "Water",
+    "symbol": "WAT",
+}
+
+ENERGY = {
+    "asset_id": "1c0304430aa61c3448c3effed0e5a13f017ddd53557f84029f047b82097d2ccc",
+    "name": "Energy",
+    "symbol": "ENG",
+}
+
+DEFAULT_CATS = {
+    ASTEROID["asset_id"]: ASTEROID,
+    WATER["asset_id"]: WATER,
+    ENERGY["asset_id"]: ENERGY,
+}
