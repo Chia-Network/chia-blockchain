56dbd80772be7a5b172f7471c4db00c8681c4196
Don Kackman <dkackman@gmail.com>
2021-10-16 10:28:50 -0500

    use fixture for get_daemon

--- tests/core/test_daemon_rpc.py
+++ tests/core/test_daemon_rpc.py
@@ -5,15 +5,18 @@ from chia.daemon.client import connect_to_daemon
 from tests.setup_nodes import bt
 from chia import __version__
 
-class TestDaemonRpc:
-    @pytest.mark.asyncio
-    async def test_get_version_rpc(self):
-        config = bt.config
 
-        async for _ in setup_daemon(bt):
-            connection = await connect_to_daemon(config["self_hostname"], config["daemon_port"], bt.get_daemon_ssl_context())
-            response = await connection.get_version()
-            assert response is not None
-            assert response["data"]["success"] == True
-            assert response["data"]["version"] == __version__
-            
+class TestDaemonRpc:
+    @pytest.fixture(scope="function")
+    async def get_daemon(self):
+        async for _ in setup_daemon(btools=bt):
+            yield _
+
+    @pytest.mark.asyncio
+    async def test_get_version_rpc(self, get_daemon):
+        config = bt.config
+        client = await connect_to_daemon(config["self_hostname"], config["daemon_port"], bt.get_daemon_ssl_context())
+        response = await client.get_version()
+
+        assert response["data"]["success"] == True
+        assert response["data"]["version"] == __version__
