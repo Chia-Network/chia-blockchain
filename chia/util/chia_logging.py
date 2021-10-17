dcdac1e42ae42805b8f168ac5d1f66dace59df5c
Kyle Altendorf <sda@fstab.net>
2021-10-16 22:52:37 -0400

    Use ISO8601 extended format with time zone for log timestamps
    https://en.wikipedia.org/wiki/ISO_8601
    
    ```
    Existing: 2021-10-16T22:50:30.453 full_node full_node_server        : INFO
    New     : 2021-10-16T22:49:25.210-04:00 full_node full_node_server        : INFO
    ```
    
    https://github.com/Chia-Network/chia-blockchain/issues/8802

--- chia/util/chia_logging.py
+++ chia/util/chia_logging.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from pathlib import Path
 from typing import Dict
@@ -9,19 +10,31 @@ from logging.handlers import SysLogHandler
 from chia.util.path import mkdir, path_from_root
 
 
+def iso8601_format_time(self, record, datefmt=None):
+    as_utc = datetime.datetime.fromtimestamp(record.created, datetime.timezone.utc)
+    as_local = as_utc.astimezone()
+    return as_local.isoformat(timespec="milliseconds")
+
+
+class ISO8601Formatter(logging.Formatter):
+    formatTime = iso8601_format_time
+
+
+class ISO8601ColoredFormatter(colorlog.ColoredFormatter):
+    formatTime = iso8601_format_time
+
+
 def initialize_logging(service_name: str, logging_config: Dict, root_path: Path):
     log_path = path_from_root(root_path, logging_config.get("log_filename", "log/debug.log"))
-    log_date_format = "%Y-%m-%dT%H:%M:%S"
 
     mkdir(str(log_path.parent))
     file_name_length = 33 - len(service_name)
     if logging_config["log_stdout"]:
         handler = colorlog.StreamHandler()
         handler.setFormatter(
-            colorlog.ColoredFormatter(
-                f"%(asctime)s.%(msecs)03d {service_name} %(name)-{file_name_length}s: "
+            ISO8601ColoredFormatter(
+                f"%(asctime)s {service_name} %(name)-{file_name_length}s: "
                 f"%(log_color)s%(levelname)-8s%(reset)s %(message)s",
-                datefmt=log_date_format,
                 reset=True,
             )
         )
@@ -33,9 +46,8 @@ def initialize_logging(service_name: str, logging_config: Dict, root_path: Path)
         maxrotation = logging_config.get("log_maxfilesrotation", 7)
         handler = ConcurrentRotatingFileHandler(log_path, "a", maxBytes=20 * 1024 * 1024, backupCount=maxrotation)
         handler.setFormatter(
-            logging.Formatter(
-                fmt=f"%(asctime)s.%(msecs)03d {service_name} %(name)-{file_name_length}s: %(levelname)-8s %(message)s",
-                datefmt=log_date_format,
+            ISO8601Formatter(
+                fmt=f"%(asctime)s {service_name} %(name)-{file_name_length}s: %(levelname)-8s %(message)s",
             )
         )
         logger.addHandler(handler)
@@ -44,7 +56,7 @@ def initialize_logging(service_name: str, logging_config: Dict, root_path: Path)
         log_syslog_host = logging_config.get("log_syslog_host", "localhost")
         log_syslog_port = logging_config.get("log_syslog_port", 514)
         log_syslog_handler = SysLogHandler(address=(log_syslog_host, log_syslog_port))
-        log_syslog_handler.setFormatter(logging.Formatter(fmt=f"{service_name} %(message)s", datefmt=log_date_format))
+        log_syslog_handler.setFormatter(ISO8601Formatter(fmt=f"%(asctime)s {service_name} %(message)s"))
         logger = logging.getLogger()
         logger.addHandler(log_syslog_handler)
 
