5f77d9d4570b4f1d1d16e3b4612e185160275126
Yostra <straya@chia.net>
2021-10-20 16:13:00 -0400

    new cats

--- packages/wallet/src/constants/Tokens.ts
+++ packages/wallet/src/constants/Tokens.ts
@@ -5,19 +5,19 @@ const Tokens: CATToken = [{
   icon: AsteroidIcon,
   symbol: 'AST',
   name: 'Asteroid',
-  tail: '783cd84333cacd7e3f6292963b59dd5855ce6a609b9992a60392725a3ed719de',
+  tail: 'e656d039dc76dbf5354a4932aafe1b25fc10119e4d747499938d6f552564d1e5',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 }, {
   icon: WaterIcon,
   symbol: 'WAT',
   name: 'Water',
-  tail: '28307353c18ef004294d266fbd4a753eef3b60e2c98ba648b271ac277d33073f',
+  tail: '5dd58cab46eb4dccf5dbed297945881631e70727b1800d605cbab7b531039179',
   description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 }, {
   icon: EnergyIcon,
   symbol: 'ENG',
   name: 'Energy',
-  tail: '1c0304430aa61c3448c3effed0e5a13f017ddd53557f84029f047b82097d2ccc',
+  tail: '2b3927c42df28492b38ae16ee956302cd27718e6c71dea13efd523ef1e3250ed',
   description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
 }];
 
