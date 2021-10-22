29252629676f09974e36f52927bd7bfec27ac777
Yostra <straya@chia.net>
2021-10-22 13:34:58 -0400

    packaging

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ dependencies = [
     "click==7.1.2",  # For the CLI
     "dnspython==2.1.0",  # Query DNS seeds
     "watchdog==2.1.3",  # Filesystem event watching - watches keyring.yaml
+    "packaging==21.0"
 ]
 
 upnp_dependencies = [
