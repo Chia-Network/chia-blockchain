9af52881f973865e41abae385ca8a8fe860801f7
Yostra <straya@chia.net>
2021-10-27 14:40:32 -0400

    new cats

--- chia/wallet/cc_wallet/cat_constants.py
+++ chia/wallet/cc_wallet/cat_constants.py
@@ -1,17 +1,17 @@
 ASTEROID = {
-    "asset_id": "e656d039dc76dbf5354a4932aafe1b25fc10119e4d747499938d6f552564d1e5",
+    "asset_id": "4cc7939c2e7ba16c7092dc21a5ef3850d8c6b7d5eb47f86ee4fb912f40f85c80",
     "name": "Asteroid",
     "symbol": "AST",
 }
 
 WATER = {
-    "asset_id": "5dd58cab46eb4dccf5dbed297945881631e70727b1800d605cbab7b531039179",
+    "asset_id": "c4d661e951226142113bd0803b278652096b82b3181e3e37343513b047b127c8",
     "name": "Water",
     "symbol": "WAT",
 }
 
 ENERGY = {
-    "asset_id": "2b3927c42df28492b38ae16ee956302cd27718e6c71dea13efd523ef1e3250ed",
+    "asset_id": "b00565f79240d1716e8a6b02694dee5e5b657e6415145049d0b75e8668cc8b8e",
     "name": "Energy",
     "symbol": "ENG",
 }
