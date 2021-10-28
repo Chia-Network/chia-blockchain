b7ae2e27cc2bea2225fefba7f2c98c19187c01df
Yostra <straya@chia.net>
2021-10-28 15:40:15 -0400

    copy daemon to correct location

--- build_scripts/build_macos.sh
+++ build_scripts/build_macos.sh
@@ -34,7 +34,7 @@ if [ "$LAST_EXIT_CODE" -ne 0 ]; then
 	echo >&2 "pyinstaller failed!"
 	exit $LAST_EXIT_CODE
 fi
-cp -r dist/daemon ../chia-blockchain-gui
+cp -r dist/daemon ../chia-blockchain-gui/packages/wallet
 cd .. || exit
 cd chia-blockchain-gui || exit
 
