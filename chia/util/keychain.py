8e67c028b5d1c4a0a8d716290118c53bc38c0f1f
Mariano Sorgente <sorgente711@gmail.com>
2021-10-28 11:33:34 -0400

    Disable passphrase by default

--- chia/util/keychain.py
+++ chia/util/keychain.py
@@ -44,8 +44,7 @@ class KeyringMaxUnlockAttempts(Exception):
 
 
 def supports_keyring_passphrase() -> bool:
-    # Support can be disabled by setting CHIA_PASSPHRASE_SUPPORT to 0/false
-    return os.environ.get("CHIA_PASSPHRASE_SUPPORT", "true").lower() in ["1", "true"]
+    return False or os.environ.get("CHIA_PASSPHRASE_SUPPORT", "").lower() in ["1", "true"]
 
 
 def supports_os_passphrase_storage() -> bool:
