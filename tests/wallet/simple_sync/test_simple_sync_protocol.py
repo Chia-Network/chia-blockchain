ba47e939e58eaf937e06a67bacf4d6dcf1f8d8f2
Matt Hauff <quexington@gmail.com>
2021-10-28 09:17:10 -0700

    Lint

--- tests/wallet/simple_sync/test_simple_sync_protocol.py
+++ tests/wallet/simple_sync/test_simple_sync_protocol.py
@@ -203,6 +203,7 @@ class TestSimpleSyncProtocol:
 
         # Let's make sure the wallet can handle a non ephemeral launcher
         from chia.wallet.puzzles.singleton_top_layer import SINGLETON_LAUNCHER_HASH
+
         tx_record = await wallet.generate_signed_transaction(uint64(10), SINGLETON_LAUNCHER_HASH, uint64(0))
         await wallet.push_transaction(tx_record)
 
