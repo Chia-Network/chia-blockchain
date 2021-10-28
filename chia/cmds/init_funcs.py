1e691efe7c3031700229a9dae5475d8dcb8a220b
Mariano Sorgente <sorgente711@gmail.com>
2021-10-28 11:45:06 -0400

    Don't check keys

--- chia/cmds/init_funcs.py
+++ chia/cmds/init_funcs.py
@@ -365,7 +365,7 @@ def chia_full_version_str() -> str:
     return f"{major}.{minor}.{patch}{dev}"
 
 
-def chia_init(root_path: Path, *, should_check_keys: bool = True, fix_ssl_permissions: bool = False):
+def chia_init(root_path: Path, *, should_check_keys: bool = False, fix_ssl_permissions: bool = False):
     """
     Standard first run initialization or migration steps. Handles config creation,
     generation of SSL certs, and setting target addresses (via check_keys).
