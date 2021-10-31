f23680d6464fb6d41dca8167c37a10d5b576165c
Gregory <93157836+greg-008@users.noreply.github.com>
2021-10-30 18:47:49 -0700

    Crawler API check

--- chia/seeder/crawler_api.py
+++ chia/seeder/crawler_api.py
@@ -1,10 +1,10 @@
 from typing import Callable, Optional
 
 import chia.server.ws_connection as ws
+from chia.full_node.full_node import full_node_protocol, wallet_protocol
 from chia.seeder.crawler import Crawler
 from chia.server.outbound_message import Message
 from chia.util.api_decorators import api_request, peer_required
-from chia.full_node.full_node import full_node_protocol, wallet_protocol
 
 
 class CrawlerAPI:
