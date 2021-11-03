c1796c3b638a8ea3f36082ac0f952ea0c7c16566
wjblanke <wjb98672@gmail.com>
2021-11-03 16:45:16 -0700

    switch to forked dnspython v2.2.0 (#9107)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ dependencies = [
     "sortedcontainers==2.4.0",  # For maintaining sorted mempools
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
     "click==7.1.2",  # For the CLI
-    "dnspython==2.1.0",  # Query DNS seeds
+    "dnspythonchia==2.2.0",  # Query DNS seeds
     "watchdog==2.1.6",  # Filesystem event watching - watches keyring.yaml
 ]
 
