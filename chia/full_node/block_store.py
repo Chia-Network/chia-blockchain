48c8930562b0bc10bfbb49a18c7c5509cc03037a
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-11-04 22:02:17 +0100

    changed to chia-style comment

--- chia/full_node/block_store.py
+++ chia/full_node/block_store.py
@@ -60,8 +60,8 @@ class BlockStore:
         # earlier versions of chia created this index despite no lookups needing
         # it. For now, just don't create it for new installs. In the future we
         # may remove the index from existing installations as well
-        # Do not create named index on column header_hash, because it already is auto indexed as of Primary Key (Index Name sqlite_autoindex_block_records_1)
-        #await self.db.execute("CREATE INDEX IF NOT EXISTS hh on block_records(header_hash)")
+        # await self.coin_record_db.execute("DROP INDEX IF EXISTS header_hash")
+        
         await self.db.execute("CREATE INDEX IF NOT EXISTS peak on block_records(is_peak)")
 
         # this index is not used by any queries, don't create it for new
