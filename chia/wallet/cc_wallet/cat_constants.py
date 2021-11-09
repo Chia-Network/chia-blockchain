468063d227c692c76d0fde2450e8f1cb24ee9353
Matt Hauff <quexington@gmail.com>
2021-11-09 14:08:50 -0700

    Update verified list

--- chia/wallet/cc_wallet/cat_constants.py
+++ chia/wallet/cc_wallet/cat_constants.py
@@ -1,29 +1,29 @@
 ASTEROID = {
-    "asset_id": "4cc7939c2e7ba16c7092dc21a5ef3850d8c6b7d5eb47f86ee4fb912f40f85c80",
+    "asset_id": "86bf9abe0600edf96b2e0fa928d19435b5aa756a9c9151c4b53c2c3da258502f",
     "name": "Asteroid",
     "symbol": "AST",
 }
 
 WATER = {
-    "asset_id": "c4d661e951226142113bd0803b278652096b82b3181e3e37343513b047b127c8",
+    "asset_id": "8829a36776a15477a7f41f8fb6397752922374b60be7d3b2d7881c54b86b32a1",
     "name": "Water",
     "symbol": "WAT",
 }
 
 ENERGY = {
-    "asset_id": "67e294b2cf6be4c6e3485008c67c5ed44101408c108f55d6f62628ab5ee390a7",
+    "asset_id": "6528b0cf7227e966254d51945d8589cdb9be17017d70145461ad867678c86fb1",
     "name": "Energy",
     "symbol": "ENG",
 }
 
 MARMOTCOIN = {
-    "asset_id": "42dc1c8c740edef7da4e686da1e8975a7d706342ac56fbfbdc981b0baf384db4",
+    "asset_id": "a41077f9cc7fccbec1937d1c23c0bd55b4bccfe3bb90af502a9f98ff650122a1",
     "name": "Marmotcoin",
     "symbol": "MRT",
 }
 
 HIGH_TECH_KAT = {
-    "asset_id": "a8b30b328a778f663ed46676cd52c4b2dde3801b0c2e39f1d64122c9de7723ec",
+    "asset_id": "e0cbb58878e463f6c6bd224e5b33a95c5511617964425d534b6f1c585d4fbc99",
     "name": "High-Tech Kat",
     "symbol": "HTK",
 }
