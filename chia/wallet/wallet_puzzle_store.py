2b70877359c100294bdd3de5f29f8532f043519e
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2021-11-17 00:24:00 +0100

    renamed indexes for wallet_type/wallet_id because of multiple uses in this db. also drop index used because it was created on wrong column and create it new

--- chia/wallet/wallet_puzzle_store.py
+++ chia/wallet/wallet_puzzle_store.py
@@ -54,11 +54,13 @@ class WalletPuzzleStore:
 
         await self.db_connection.execute("CREATE INDEX IF NOT EXISTS pubkey on derivation_paths(pubkey)")
 
-        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS wallet_type on derivation_paths(wallet_type)")
+        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS dp_wallet_type on derivation_paths(wallet_type)")
 
-        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS wallet_id on derivation_paths(wallet_id)")
-
-        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS used on derivation_paths(wallet_type)")
+        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS dp_wallet_id on derivation_paths(wallet_id)")
+        # the index named used was created on column wallet_type
+        # drop this version of the index and create an index on column used
+        await self.db.execute("DROP INDEX IF EXISTS used on derivation_paths(wallet_type)")
+        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS dp_used on derivation_paths(used)")
 
         await self.db_connection.commit()
         # Lock
