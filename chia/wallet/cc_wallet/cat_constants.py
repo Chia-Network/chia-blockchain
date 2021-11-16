412b1fb1a43fb57509be4cd992a3024274411da6
Earle Lowe <30607889+emlowe@users.noreply.github.com>
2021-11-15 18:05:08 -0800

    Update with SBX actual

--- chia/wallet/cc_wallet/cat_constants.py
+++ chia/wallet/cc_wallet/cat_constants.py
@@ -1,37 +1,9 @@
-ASTEROID = {
-    "asset_id": "86bf9abe0600edf96b2e0fa928d19435b5aa756a9c9151c4b53c2c3da258502f",
-    "name": "Asteroid",
-    "symbol": "AST",
-}
-
-WATER = {
-    "asset_id": "8829a36776a15477a7f41f8fb6397752922374b60be7d3b2d7881c54b86b32a1",
-    "name": "Water",
-    "symbol": "WAT",
-}
-
-ENERGY = {
-    "asset_id": "6528b0cf7227e966254d51945d8589cdb9be17017d70145461ad867678c86fb1",
-    "name": "Energy",
-    "symbol": "ENG",
-}
-
-MARMOTCOIN = {
-    "asset_id": "a41077f9cc7fccbec1937d1c23c0bd55b4bccfe3bb90af502a9f98ff650122a1",
-    "name": "Marmotcoin",
-    "symbol": "MRT",
-}
-
-HIGH_TECH_KAT = {
-    "asset_id": "e0cbb58878e463f6c6bd224e5b33a95c5511617964425d534b6f1c585d4fbc99",
-    "name": "High-Tech Kat",
-    "symbol": "HTK",
+SPACEBUCKS = {
+    "asset_id": "78ad32a8c9ea70f27d73e9306fc467bab2a6b15b30289791e37ab6e8612212b1",
+    "name": "Spacebucks",
+    "symbol": "SBX",
 }
 
 DEFAULT_CATS = {
-    ASTEROID["asset_id"]: ASTEROID,
-    WATER["asset_id"]: WATER,
-    ENERGY["asset_id"]: ENERGY,
-    MARMOTCOIN["asset_id"]: MARMOTCOIN,
-    HIGH_TECH_KAT["asset_id"]: HIGH_TECH_KAT,
+    SPACEBUCKS["asset_id"]: SPACEBUCKS,
 }
