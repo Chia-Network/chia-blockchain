e887c8a4410c7f53c3b4a3150838460316a2d7aa
Kyle Altendorf <sda@fstab.net>
2021-11-18 23:12:50 -0500

    ignore a few entirely commented test files

--- tests/build-job-matrix.py
+++ tests/build-job-matrix.py
@@ -14,6 +14,7 @@ test_paths = sorted(test_paths)
 configuration = [{'path': os.fspath(path), 'name': '.'.join(path.with_suffix('').parts)} for path in test_paths]
 # TODO: remove this.  filtering just to avoid the hanging tests while
 # configuration = [c for c in configuration if c['name'] in ['plotting', 'generator', 'core.full_node']]
+configuration = [c for c in configuration if c['name'] not in ['wallet.test_backup', 'wallet.test_wallet_store']]
 configuration_json = json.dumps(configuration)
 
 print(json.dumps(configuration, indent=4))
