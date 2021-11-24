e28633888fb614f0adc72e479cb69df510968f10
xdustinface <xdustinfacex@gmail.com>
2021-11-24 10:49:26 +0100

    tests: Test list sizes from 1-10 in `test_valid`

--- tests/generator/test_list_to_batches.py
+++ tests/generator/test_list_to_batches.py
@@ -9,15 +9,16 @@ def test_empty_lists():
 
 
 def test_valid():
-    test_list = [x for x in range(0, 10)]
-    for i in range(1, len(test_list) + 1):  # Test batch_size 1 to 11 (length + 1)
-        checked = 0
-        for remaining, batch in list_to_batches(test_list, i):
-            assert remaining == max(len(test_list) - checked - i, 0)
-            assert len(batch) <= i
-            assert batch == test_list[checked : min(checked + i, len(test_list))]
-            checked += len(batch)
-        assert checked == len(test_list)
+    for k in range(1, 10):
+        test_list = [x for x in range(0, k)]
+        for i in range(1, len(test_list) + 1):  # Test batch_size 1 to 11 (length + 1)
+            checked = 0
+            for remaining, batch in list_to_batches(test_list, i):
+                assert remaining == max(len(test_list) - checked - i, 0)
+                assert len(batch) <= i
+                assert batch == test_list[checked : min(checked + i, len(test_list))]
+                checked += len(batch)
+            assert checked == len(test_list)
 
 
 def test_invalid_batch_sizes():
