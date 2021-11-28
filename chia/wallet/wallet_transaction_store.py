d3af2d74e42e471a5d0c864965823a6a30f85bc2
Kyle Altendorf <sda@fstab.net>
2021-11-28 12:17:24 -0500

    debug

--- chia/wallet/wallet_transaction_store.py
+++ chia/wallet/wallet_transaction_store.py
@@ -302,6 +302,9 @@ class WalletTransactionStore:
 
             await asyncio.sleep(0.050)
 
+        # TODO: this is just random...  let's see if it helps then explore
+        await asyncio.sleep(1)
+
     async def get_farming_rewards(self) -> List[TransactionRecord]:
         """
         Returns the list of all farming rewards.
