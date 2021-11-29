8803594f055f418cd1ae0c45e831c1a0a7cfd803
Florin Chirica <fchirica96@gmail.com>
2021-11-29 20:23:31 +0200

    Fix sanity check.

--- chia/timelord/timelord.py
+++ chia/timelord/timelord.py
@@ -1133,7 +1133,7 @@ class Timelord:
                         return
                     vdf_proof = VDFProof(uint8(0), proof_part, True)
                     initial_form = ClassgroupElement.get_default_element()
-                    if not vdf_proof.is_valid(self.constants, initial_form, picked_info):
+                    if not vdf_proof.is_valid(self.constants, initial_form, picked_info.new_proof_of_time):
                         log.error("Invalid compact proof of time!")
                         return
                     response = timelord_protocol.RespondCompactProofOfTime(
