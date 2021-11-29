ba04a8ec6791ef9c503c35f8fa966d4ea84d4cb7
Matt Hauff <quexington@gmail.com>
2021-11-29 09:03:54 -0700

    Add convenience methods to TradeRecord

--- chia/wallet/trade_record.py
+++ chia/wallet/trade_record.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Dict, Any
 
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.ints import uint8, uint32, uint64
 from chia.util.streamable import Streamable, streamable
 from chia.wallet.trading.offer import Offer
+from chia.wallet.trading.trade_status import TradeStatus
 
 
 @dataclass(frozen=True)
@@ -25,3 +26,23 @@ class TradeRecord(Streamable):
     trade_id: bytes32
     status: uint32  # TradeStatus, enum not streamable
     sent_to: List[Tuple[str, uint8, Optional[str]]]
+
+    def to_json_dict_convenience(self) -> Dict[str, Any]:
+        formatted = self.to_json_dict()
+        formatted["status"] = TradeStatus(self.status).name
+        offered, requested = Offer.from_bytes(self.offer).summary()
+        formatted["summary"] = {
+            "offered": offered,
+            "requested": requested,
+        }
+        del formatted["offer"]
+        return formatted
+
+    @classmethod
+    def from_json_dict_convenience(cls, record: Dict[str, Any], offer: str) -> "TradeRecord":
+        new_record = record.copy()
+        new_record["status"] = TradeStatus[record["status"]].value
+        del new_record["summary"]
+        new_record["offer"] = offer
+        return cls.from_json_dict(new_record)
+
