ca317418c5a66d26d8f0f8b802f9e05dce957015
Matt Hauff <quexington@gmail.com>
2021-12-01 09:49:04 -0700

    Added to client too

--- chia/rpc/wallet_rpc_client.py
+++ chia/rpc/wallet_rpc_client.py
@@ -53,6 +53,9 @@ class WalletRpcClient(RpcClient):
         except ValueError as e:
             return e.args[0]
 
+    async def get_fingerprint(self) -> int:
+        return (await self.fetch("get_fingerprint", {}))
+
     async def get_public_keys(self) -> List[int]:
         return (await self.fetch("get_public_keys", {}))["public_key_fingerprints"]
 
