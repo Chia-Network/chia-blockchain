138f09b3c3bd58fc311cae70ec1141be9c677c07
xdustinface <xdustinfacex@gmail.com>
2021-12-29 14:42:27 +0100

    protocols|server: Define new harvester plot refreshing protocol messages

--- chia/protocols/harvester_protocol.py
+++ chia/protocols/harvester_protocol.py
@@ -5,7 +5,7 @@ from blspy import G1Element, G2Element
 
 from chia.types.blockchain_format.proof_of_space import ProofOfSpace
 from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.util.ints import uint8, uint64
+from chia.util.ints import int16, uint8, uint32, uint64
 from chia.util.streamable import Streamable, streamable
 
 """
@@ -95,3 +95,80 @@ class RespondPlots(Streamable):
     plots: List[Plot]
     failed_to_open_filenames: List[str]
     no_key_filenames: List[str]
+
+
+@dataclass(frozen=True)
+@streamable
+class PlotSyncIdentifier(Streamable):
+    timestamp: uint64
+    sync_id: uint64
+    message_id: uint64
+
+
+@dataclass(frozen=True)
+@streamable
+class PlotSyncStart(Streamable):
+    identifier: PlotSyncIdentifier
+    initial: bool
+    last_sync_id: uint64
+    plot_file_count: uint32
+
+    def __str__(self):
+        return (
+            f"PlotSyncStart: identifier {self.identifier}, initial {self.initial}, "
+            f"last_sync_id {self.last_sync_id}, plot_file_count {self.plot_file_count}"
+        )
+
+
+@dataclass(frozen=True)
+@streamable
+class PlotSyncPathList(Streamable):
+    identifier: PlotSyncIdentifier
+    data: List[str]
+    final: bool
+
+    def __str__(self):
+        return f"PlotSyncPathList: identifier {self.identifier}, count {len(self.data)}, final {self.final}"
+
+
+@dataclass(frozen=True)
+@streamable
+class PlotSyncPlotList(Streamable):
+    identifier: PlotSyncIdentifier
+    data: List[Plot]
+    final: bool
+
+    def __str__(self):
+        return f"PlotSyncPlotList: identifier {self.identifier}, count {len(self.data)}, final {self.final}"
+
+
+@dataclass(frozen=True)
+@streamable
+class PlotSyncDone(Streamable):
+    identifier: PlotSyncIdentifier
+    duration: uint64
+
+    def __str__(self):
+        return f"PlotSyncDone: identifier {self.identifier}, duration {self.duration}"
+
+
+@dataclass(frozen=True)
+@streamable
+class PlotSyncError(Streamable):
+    code: int16
+    message: str
+    expected_identifier: Optional[PlotSyncIdentifier]
+
+    def __str__(self):
+        return f"PlotSyncError: code {self.code}, count {self.message}, expected_identifier {self.expected_identifier}"
+
+
+@dataclass(frozen=True)
+@streamable
+class PlotSyncResponse(Streamable):
+    identifier: PlotSyncIdentifier
+    message_type: int16
+    error: Optional[PlotSyncError]
+
+    def __str__(self):
+        return f"PlotSyncResponse: identifier {self.identifier}, message_type {self.message_type}, error {self.error}"
