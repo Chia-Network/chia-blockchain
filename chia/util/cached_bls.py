e32c197a1db24c2ccb5fcc121cc97259c6d92347
Matt Hauff <quexington@gmail.com>
2022-01-10 14:18:19 -0700

    Use main cached_bls.py

--- chia/util/cached_bls.py
+++ chia/util/cached_bls.py
@@ -1,5 +1,5 @@
 import functools
-from typing import List, Optional
+from typing import List, Optional, Sequence
 
 from blspy import AugSchemeMPL, G1Element, G2Element, GTElement
 
@@ -8,7 +8,7 @@ from chia.util.hash import std_hash
 from chia.util.lru_cache import LRUCache
 
 
-def get_pairings(cache: LRUCache, pks: List[bytes48], msgs: List[bytes], force_cache: bool) -> List[GTElement]:
+def get_pairings(cache: LRUCache, pks: List[bytes48], msgs: Sequence[bytes], force_cache: bool) -> List[GTElement]:
     pairings: List[Optional[GTElement]] = []
     missing_count: int = 0
     for pk, msg in zip(pks, msgs):
@@ -26,22 +26,22 @@ def get_pairings(cache: LRUCache, pks: List[bytes48], msgs: List[bytes], force_c
 
     for i, pairing in enumerate(pairings):
         if pairing is None:
-            aug_msg = bytes(pks[i]) + msgs[i]
+            aug_msg = pks[i] + msgs[i]
             aug_hash: G2Element = AugSchemeMPL.g2_from_message(aug_msg)
             pairing = G1Element.from_bytes(pks[i]).pair(aug_hash)
 
             h = bytes(std_hash(aug_msg))
             cache.put(h, pairing)
             pairings[i] = pairing
-
     return pairings
 
 
+# Increasing this number will increase RAM usage, but decrease BLS validation time for blocks and unfinished blocks.
 LOCAL_CACHE: LRUCache = LRUCache(50000)
 
 
 def aggregate_verify(
-    pks: List[bytes48], msgs: List[bytes], sig: G2Element, force_cache: bool = False, cache: LRUCache = LOCAL_CACHE
+    pks: List[bytes48], msgs: Sequence[bytes], sig: G2Element, force_cache: bool = False, cache: LRUCache = LOCAL_CACHE
 ):
     pairings: List[GTElement] = get_pairings(cache, pks, msgs, force_cache)
     if len(pairings) == 0:
