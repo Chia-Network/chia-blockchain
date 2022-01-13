581bcadac475d04768e71ee1336e826775b40b22
Jack Nelson <jack@jacknelson.xyz>
2022-01-13 17:13:29 -0500

    final changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dependencies = [
     "aiofiles==0.8.0",  # Async IO for files
     "blspy==1.0.9",  # Signature library
     "chiavdf==1.0.4",  # timelord and vdf verification
-    "chiabip158==1.0",  # bip158-style wallet filters
+    "chiabip158==1.1",  # bip158-style wallet filters
     "chiapos==1.0.8",  # proof of space
     "clvm==0.9.7",
     "clvm_rs==0.1.17",
@@ -14,7 +14,7 @@ dependencies = [
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
     "bitstring==3.1.9",  # Binary data management library
     "colorama==0.4.4",  # Colorizes terminal output
-    "colorlog==5.0.1",  # Adds color to logs
+    "colorlog==6.6.0",  # Adds color to logs
     "concurrent-log-handler==0.9.19",  # Concurrently log and rotate logs
     "cryptography==3.4.7",  # Python cryptography library for TLS - keyring conflict
     "fasteners==0.16.3",  # For interprocess file locking
@@ -22,11 +22,11 @@ dependencies = [
     "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
     #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
     #  See https://github.com/frispete/keyrings.cryptfile/issues/15
-    "PyYAML==5.4.1",  # Used for config file format
+    "PyYAML==6.0",  # Used for config file format
     "setproctitle==1.2.2",  # Gives the chia processes readable names
     "sortedcontainers==2.4.0",  # For maintaining sorted mempools
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
-    "click==7.1.2",  # For the CLI
+    "click==8.0.3",  # For the CLI
     "dnspythonchia==2.2.0",  # Query DNS seeds
     "watchdog==2.1.6",  # Filesystem event watching - watches keyring.yaml
     "dnslib==0.9.18",  # dns lib
@@ -36,7 +36,7 @@ dependencies = [
 ]
 
 upnp_dependencies = [
-    "miniupnpc==2.2.3",  # Allows users to open ports on their router
+    "miniupnpc==2.2.2",  # Allows users to open ports on their router
 ]
 
 dev_dependencies = [
