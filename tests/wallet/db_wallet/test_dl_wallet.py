deb19f7f2bee213ef0f19aff0271837e8e4e981a
Matt Hauff <quexington@gmail.com>
2022-01-19 07:32:40 -0700

    flake 8

--- tests/wallet/db_wallet/test_dl_wallet.py
+++ tests/wallet/db_wallet/test_dl_wallet.py
@@ -1,20 +1,16 @@
 import asyncio
 import pytest
-import time
 from typing import AsyncIterator, Iterator
 
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.peer_info import PeerInfo
-from chia.util.ints import uint16, uint32, uint64
+from chia.util.ints import uint16, uint64
 from tests.setup_nodes import setup_simulators_and_wallets
 from chia.data_layer.data_layer_wallet import DataLayerWallet
-from chia.wallet.dlo_wallet.dlo_wallet import DLOWallet
+# from chia.wallet.dlo_wallet.dlo_wallet import DLOWallet
 from chia.types.blockchain_format.program import Program
-from chia.types.spend_bundle import SpendBundle
 from tests.time_out_assert import time_out_assert
 from chia.wallet.util.merkle_tree import MerkleTree
-from chia.wallet.transaction_record import TransactionRecord
-from chia.wallet.util.transaction_type import TransactionType
 
 from tests.setup_nodes import SimulatorsAndWallets
 
