a98dcac54a99623ed5f298f726c875a7fe26f301
Jack Nelson <jack@jacknelson.xyz>
2022-01-20 15:27:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ dependencies = [
     "clvm==0.9.7",
     "clvm_rs==0.1.17",
     "clvm_tools==0.4.3",
-    "aiohttp==3.8.1",  # HTTP server for full node rpc
+    "aiohttp==3.7.4",  # HTTP server for full node rpc
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
     "bitstring==3.1.9",  # Binary data management library
     "colorama==0.4.4",  # Colorizes terminal output
-    "colorlog==6.6.0",  # Adds color to logs
+    "colorlog==5.0.1",  # Adds color to logs
     "concurrent-log-handler==0.9.19",  # Concurrently log and rotate logs
     "cryptography==3.4.7",  # Python cryptography library for TLS - keyring conflict
     "fasteners==0.16.3",  # For interprocess file locking
@@ -22,17 +22,17 @@ dependencies = [
     "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
     #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
     #  See https://github.com/frispete/keyrings.cryptfile/issues/15
-    "PyYAML==6.0",  # Used for config file format
+    "PyYAML==5.4.1",  # Used for config file format
     "setproctitle==1.2.2",  # Gives the chia processes readable names
     "sortedcontainers==2.4.0",  # For maintaining sorted mempools
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
     "click==7.1.2",  # For the CLI
     "dnspythonchia==2.2.0",  # Query DNS seeds
     "watchdog==2.1.6",  # Filesystem event watching - watches keyring.yaml
-    "dnslib==0.9.18",  # dns lib
+    "dnslib==0.9.17",  # dns lib
     "typing-extensions==4.0.1",  # typing backports like Protocol and TypedDict
-    "zstd==1.5.1.0",
-    "packaging==21.3",
+    "zstd==1.5.0.4",
+    "packaging==21.0",
 ]
 
 upnp_dependencies = [
