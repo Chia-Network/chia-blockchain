c1a8dac07c611bff452cc8ed3e258cf832a60d19
Kyle Altendorf <sda@fstab.net>
2022-01-21 21:53:48 -0500

    Set default wallet: dns_servers in code (#9880)

--- chia/wallet/wallet_node.py
+++ chia/wallet/wallet_node.py
@@ -352,7 +352,7 @@ class WalletNode:
                     default_peers_file_path="wallet/db/wallet_peers.dat",
                 ),
                 self.config["introducer_peer"],
-                self.config["dns_servers"],
+                self.config.get("dns_servers", ["dns-introducer.chia.net"]),
                 self.config["peer_connect_interval"],
                 network_name,
                 None,
