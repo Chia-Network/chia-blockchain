fa0167402e6cf91abbe28587cafec6dc0d4fc5aa
Florin Chirica <fchirica96@gmail.com>
2022-01-23 20:19:48 +0200

    Fix function name post rebase.

--- chia/data_layer/data_store.py
+++ chia/data_layer/data_store.py
@@ -370,7 +370,7 @@ class DataStore:
 
         return ancestors
 
-    async def get_pairs(self, tree_id: bytes32, *, lock: bool = True) -> List[TerminalNode]:
+    async def get_keys_values(self, tree_id: bytes32, *, lock: bool = True) -> List[TerminalNode]:
         async with self.db_wrapper.locked_transaction(lock=lock):
             root = await self.get_tree_root(tree_id=tree_id, lock=False)
 
