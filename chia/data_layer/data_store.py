f31ebe88b35ee8c52179d2b18af5f8fab91bccc5
Almog De Paz <almogdepaz@gmail.com>
2022-01-25 18:57:46 +0200

    Update chia/data_layer/data_store.py
    Co-authored-by: Kyle Altendorf <sda@fstab.net>

--- chia/data_layer/data_store.py
+++ chia/data_layer/data_store.py
@@ -318,7 +318,7 @@ class DataStore:
                 {"tree_id": tree_id.hex(), "generation": generation},
             )
             if cursor.rowcount < 1:
-                raise Exception("tree id not in store")
+                raise Exception(f"tree id not in store: {tree_id.hex()}")
             [root_dict] = [row async for row in cursor]
 
         return Root.from_row(row=root_dict)
