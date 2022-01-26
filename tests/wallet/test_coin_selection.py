71d7b49a53f37578d2f05767849b97b845d7d948
Jack Nelson <jack@jacknelson.xyz>
2022-01-26 13:39:46 -0500

    add extra test cases

--- tests/wallet/test_coin_selection.py
+++ tests/wallet/test_coin_selection.py
@@ -19,6 +19,8 @@ class TestCoinSelection:
         ]
         assert check_for_exact_match(coin_list, uint64(220000)) == coin_list[0]
         assert check_for_exact_match(coin_list, uint64(22)) == coin_list[2]
+        # check for no match.
+        assert check_for_exact_match(coin_list, uint64(20)) is None
 
     def test_smallest_individual_coin_selection(self, a_hash):
         coin_list = [
@@ -32,16 +34,16 @@ class TestCoinSelection:
         ]
         assert find_smallest_coin(coin_list, uint64(100000), DEFAULT_CONSTANTS.MAX_COIN_AMOUNT) == coin_list[5]
         assert find_smallest_coin(coin_list, uint64(320000), DEFAULT_CONSTANTS.MAX_COIN_AMOUNT) == coin_list[0]
+        # test for failure where target is greater than any available coin.
+        assert find_smallest_coin(coin_list, uint64(360000), DEFAULT_CONSTANTS.MAX_COIN_AMOUNT) is None
 
     def test_knapsack_coin_selection(self, a_hash):
-        coin_list = {
-            Coin(a_hash, a_hash, uint64(200000)),
-            Coin(a_hash, a_hash, uint64(123331)),
-            Coin(a_hash, a_hash, uint64(120000)),
-            Coin(a_hash, a_hash, uint64(110000)),
-            Coin(a_hash, a_hash, uint64(300)),
-        }
-        knapsack = knapsack_coin_algorithm(coin_list, uint64(310000), DEFAULT_CONSTANTS.MAX_COIN_AMOUNT)
-        assert knapsack is not None
-        assert sum([coin.amount for coin in knapsack]) == 310000
-        assert Coin(a_hash, a_hash, uint64(200000)) in knapsack and Coin(a_hash, a_hash, uint64(110000)) in knapsack
+        tries = 100
+        coins_to_append = 1000
+        coin_list = set()
+        for i in range(coins_to_append):
+            coin_list.add(Coin(a_hash, a_hash, uint64(200000 * i)))
+        for i in range(tries):
+            knapsack = knapsack_coin_algorithm(coin_list, uint64(310000000), DEFAULT_CONSTANTS.MAX_COIN_AMOUNT)
+            assert knapsack is not None
+            assert sum([coin.amount for coin in knapsack]) >= 310000000
