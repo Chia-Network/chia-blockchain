99eb7d077467606bfb6116c86c95cf9ce6daf172
Arvid Norberg <arvid.norberg@gmail.com>
2022-01-26 14:22:32 +0100

    use context manager for get_client() (#9956)

--- chia/cmds/data_funcs.py
+++ chia/cmds/data_funcs.py
@@ -1,4 +1,5 @@
-from typing import Any, Optional, Tuple, Dict
+from typing import Any, Optional, Tuple, Dict, Type
+from types import TracebackType
 
 import aiohttp
 
@@ -13,52 +14,56 @@ from chia.util.ints import uint16
 # TODO: there seems to be a large amount of repetition in these to dedupe
 
 
-async def get_client(rpc_port: Optional[int]) -> Tuple[DataLayerRpcClient, int]:
-    config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
-    self_hostname = config["self_hostname"]
-    if rpc_port is None:
-        rpc_port = config["data_layer"]["rpc_port"]
-    # TODO: context manager for this and closing etc?
-    client = await DataLayerRpcClient.create(self_hostname, uint16(rpc_port), DEFAULT_ROOT_PATH, config)
-    return client, rpc_port
+class get_client:
+    _port: Optional[int]
+    _client: Optional[DataLayerRpcClient] = None
+
+    def __init__(self, rpc_port: Optional[int]):
+        self._port = rpc_port
+
+    async def __aenter__(self) -> Tuple[DataLayerRpcClient, int]:
+        config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
+        self_hostname = config["self_hostname"]
+        if self._port is None:
+            self._port = config["data_layer"]["rpc_port"]
+        self._client = await DataLayerRpcClient.create(self_hostname, uint16(self._port), DEFAULT_ROOT_PATH, config)
+        assert self._client is not None
+        return self._client, int(self._port)
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        if self._client is None:
+            return
+        self._client.close()
+        await self._client.await_closed()
 
 
 async def create_data_store_cmd(rpc_port: Optional[int], table_string: str) -> Optional[Dict[str, Any]]:
-    # TODO: nice cli error handling
-    client = None
-    response = None
     try:
-        client, rpc_port = await get_client(rpc_port)
-        response = await client.create_data_store()
+        async with get_client(rpc_port) as (client, rpc_port):
+            return await client.create_data_store()
     except aiohttp.ClientConnectorError:
         print(f"Connection error. Check if data is running at {rpc_port}")
     except Exception as e:
         print(f"Exception from 'data': {e}")
-    finally:
-        if client is not None:
-            client.close()
-            await client.await_closed()
-    return response
+    return None
 
 
 async def get_value_cmd(rpc_port: Optional[int], store_id: str, key: str) -> Optional[Dict[str, Any]]:
-    # TODO: nice cli error handling
-    client = None
-    response = None
     store_id_bytes = bytes32.from_hexstr(store_id)
     key_bytes = hexstr_to_bytes(key)
     try:
-        client, rpc_port = await get_client(rpc_port)
-        response = await client.get_value(store_id=store_id_bytes, key=key_bytes)
+        async with get_client(rpc_port) as (client, rpc_port):
+            return await client.get_value(store_id=store_id_bytes, key=key_bytes)
     except aiohttp.ClientConnectorError:
         print(f"Connection error. Check if data is running at {rpc_port}")
     except Exception as e:
         print(f"Exception from 'data': {e}")
-    finally:
-        if client is not None:
-            client.close()
-            await client.await_closed()
-    return response
+    return None
 
 
 async def update_data_store_cmd(
@@ -66,63 +71,42 @@ async def update_data_store_cmd(
     store_id: str,
     changelist: Dict[str, str],
 ) -> Optional[Dict[str, Any]]:
-    # TODO: nice cli error handling
-    client = None
-    response = None
     store_id_bytes = bytes32.from_hexstr(store_id)
     try:
-        client, rpc_port = await get_client(rpc_port)
-        response = await client.update_data_store(store_id=store_id_bytes, changelist=changelist)
+        async with get_client(rpc_port) as (client, rpc_port):
+            return await client.update_data_store(store_id=store_id_bytes, changelist=changelist)
     except aiohttp.ClientConnectorError:
         print(f"Connection error. Check if data is running at {rpc_port}")
     except Exception as e:
         print(f"Exception from 'data': {e}")
-    finally:
-        if client is not None:
-            client.close()
-            await client.await_closed()
-    return response
+    return None
 
 
 async def get_keys_values_cmd(
     rpc_port: Optional[int],
     store_id: str,
 ) -> Optional[Dict[str, Any]]:
-    # TODO: nice cli error handling
-    client = None
-    response = None
     store_id_bytes = bytes32.from_hexstr(store_id)
     try:
-        client, rpc_port = await get_client(rpc_port)
-        response = await client.get_keys_values(store_id=store_id_bytes)
+        async with get_client(rpc_port) as (client, rpc_port):
+            return await client.get_keys_values(store_id=store_id_bytes)
     except aiohttp.ClientConnectorError:
         print(f"Connection error. Check if data is running at {rpc_port}")
     except Exception as e:
         print(f"Exception from 'data': {e}")
-    finally:
-        if client is not None:
-            client.close()
-            await client.await_closed()
-    return response
+    return None
 
 
 async def get_root_cmd(
     rpc_port: Optional[int],
     store_id: str,
 ) -> Optional[Dict[str, Any]]:
-    # TODO: nice cli error handling
-    client = None
-    response = None
     store_id_bytes = bytes32.from_hexstr(store_id)
     try:
-        client, rpc_port = await get_client(rpc_port)
-        response = await client.get_root(store_id=store_id_bytes)
+        async with get_client(rpc_port) as (client, rpc_port):
+            return await client.get_root(store_id=store_id_bytes)
     except aiohttp.ClientConnectorError:
         print(f"Connection error. Check if data is running at {rpc_port}")
     except Exception as e:
         print(f"Exception from 'data': {e}")
-    finally:
-        if client is not None:
-            client.close()
-            await client.await_closed()
-    return response
+    return None
