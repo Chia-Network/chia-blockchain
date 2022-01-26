ba3a894f19fb56253f1dcd46a31060a8d7f38978
ChiaMineJP <admin@chiamine.jp>
2022-01-27 03:02:57 +0900

    Updated start-gui.sh

--- start-gui.sh
+++ start-gui.sh
@@ -2,8 +2,25 @@
 set -e
 export NODE_OPTIONS="--max-old-space-size=3000"
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export N_PREFIX="${SCRIPT_DIR}/.n"
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+if [ -d  "${SCRIPT_DIR}/.n" ]; then
+  export N_PREFIX="${SCRIPT_DIR}/.n"
+  export PATH="${N_PREFIX}/bin:${PATH}"
+fi
+
+if ! npm version >/dev/null 2>&1; then
+  echo "Please install GUI dependencies by:"
+  echo "  sh install-gui.sh"
+  echo "on ${SCRIPT_DIR}"
+  exit 1
+fi
+
+NPM_VERSION="$(npm -v | cut -d'.' -f 1)"
+if [ "$NPM_VERSION" -lt "7" ]; then
+  echo "Current npm version($(npm -v)) is less than 7. GUI app requires npm>=7."
+  exit 1
+fi
 
 cd "${SCRIPT_DIR}/chia-blockchain-gui/"
 npm run electron
