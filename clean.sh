e6abf8f73905a1e94eb99f376ce796e384c19f60
Edward Teach <edward@edwardteach.co>
2022-01-25 19:51:54 -0800

    updated for v2 gui

--- clean.sh
+++ clean.sh
@@ -7,5 +7,19 @@ rm -rf venv
 rm -rf activate
 rm -rf chinilla-blockchain-gui/build
 rm -rf chinilla-blockchain-gui/node_modules
+rm -rf chinilla-blockchain-gui/packages/api/node_modules
+rm -rf chinilla-blockchain-gui/packages/api/dist
+rm -rf chinilla-blockchain-gui/packages/api-react/node_modules
+rm -rf chinilla-blockchain-gui/packages/api-react/dist
+rm -rf chinilla-blockchain-gui/packages/core/node_modules
+rm -rf chinilla-blockchain-gui/packages/core/dist
+rm -rf chinilla-blockchain-gui/packages/gui/node_modules
+rm -rf chinilla-blockchain-gui/packages/gui/build
+rm -rf chinilla-blockchain-gui/packages/icons/node_modules
+rm -rf chinilla-blockchain-gui/packages/icons/dist
+rm -rf chinilla-blockchain-gui/packages/wallet/node_modules
+rm -rf chinilla-blockchain-gui/packages/wallet/build
+rm -rf chinilla-blockchain-gui/packages/wallets/node_modules
+rm -rf chinilla-blockchain-gui/packages/wallets/dist
 
 echo "Virtual environment has been scrubbed."
