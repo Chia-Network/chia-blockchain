2713b725f819326265a49f0ed153e617e8a43e9e
Zlatko Fedor <zfedor@gmail.com>
2022-01-28 19:58:56 +0100

    used number for catToMojo

--- packages/core/src/utils/catToMojo.ts
+++ packages/core/src/utils/catToMojo.ts
@@ -2,8 +2,8 @@ import Big from 'big.js';
 import Unit from '../constants/Unit';
 import chiaFormatter from './chiaFormatter';
 
-export default function catToMojo(cat: string | number | Big): string {
+export default function catToMojo(cat: string | number | Big): number {
   return chiaFormatter(cat, Unit.CAT)
     .to(Unit.MOJO)
-    .toString();
+    .toNumber();
 }
