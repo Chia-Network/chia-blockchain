40d130fda950f81fd2cb5232b070131b450290b0
almog <almogdepaz@gmail.com>
2022-02-01 12:17:26 +0200

    uae .get default value

--- chia/rpc/data_layer_rpc_api.py
+++ chia/rpc/data_layer_rpc_api.py
@@ -40,10 +40,8 @@ def process_change(change: Dict[str, Any]) -> Dict[str, Any]:
 def get_fee(config: Dict[str, Any], request: Dict[str, Any]) -> uint64:
     fee = request.get("fee")
     if fee is None:
-        config_fee = config.get("fee")
-        if config_fee is not None:
-            return uint64(config_fee)
-        return uint64(0)
+        config_fee = config.get("fee", 0)
+        return uint64(config_fee)
     return uint64(fee)
 
 
