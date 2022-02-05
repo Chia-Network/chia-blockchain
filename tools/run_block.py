92f5b26c10a5699f1a8569076f2b9207d6c58f07
neurosis69 <83925572+neurosis69@users.noreply.github.com>
2022-02-05 21:29:43 +0100

    Revert "Revert "Merge branch 'dbv2_ugprade' of https://github.com/neurosis69/chia-blockchain into dbv2_ugprade""
    This reverts commit e86e3be4c1c399f4c69cf6c449fdfb2f76b34b1b.

--- tools/run_block.py
+++ tools/run_block.py
@@ -37,7 +37,8 @@ and in this way they control whether a spend is valid or not.
 """
 import json
 from dataclasses import dataclass
-from typing import List, TextIO, Tuple, Dict
+from pathlib import Path
+from typing import List, Tuple, Dict
 
 import click
 
@@ -49,7 +50,6 @@ from chia.types.blockchain_format.program import SerializedProgram
 from chia.types.blockchain_format.coin import Coin
 from chia.types.condition_opcodes import ConditionOpcode
 from chia.types.condition_with_args import ConditionWithArgs
-from chia.types.full_block import FullBlock
 from chia.types.generator_types import BlockGenerator
 from chia.types.name_puzzle_condition import NPC
 from chia.util.config import load_config
@@ -61,12 +61,12 @@ from clvm.casts import int_from_bytes
 
 @dataclass
 class CAT:
-    tail_hash: str
+    asset_id: str
     memo: str
     npc: NPC
 
     def cat_to_dict(self):
-        return {"tail_hash": self.tail_hash, "memo": self.memo, "npc": npc_to_dict(self.npc)}
+        return {"asset_id": self.asset_id, "memo": self.memo, "npc": npc_to_dict(self.npc)}
 
 
 def condition_with_args_to_dict(condition_with_args: ConditionWithArgs):
@@ -115,7 +115,7 @@ def run_generator(
         if not matched:
             continue
 
-        _, tail_hash, _ = curried_args
+        _, asset_id, _ = curried_args
         memo = ""
 
         result = puzzle.run(solution)
@@ -158,7 +158,7 @@ def run_generator(
         coin = Coin(parent.atom, puzzle_hash, int_from_bytes(amount.atom))
         cat_list.append(
             CAT(
-                tail_hash=bytes(tail_hash).hex()[2:],
+                asset_id=bytes(asset_id).hex()[2:],
                 memo=memo,
                 npc=NPC(coin.name(), puzzle_hash, [(op, cond) for op, cond in conds.items()]),
             )
@@ -167,23 +167,13 @@ def run_generator(
     return cat_list
 
 
-def ref_list_to_args(ref_list: List[uint32]) -> List[SerializedProgram]:
+def ref_list_to_args(ref_list: List[uint32], root_path: Path) -> List[SerializedProgram]:
     args = []
     for height in ref_list:
-        with open(f"{height}.json", "r") as f:
+        with open(root_path / f"{height}.json", "rb") as f:
             program_str = json.load(f)["block"]["transactions_generator"]
             args.append(SerializedProgram.fromhex(program_str))
     return args
-
-
-def run_full_block(block: FullBlock, constants: ConsensusConstants) -> List[CAT]:
-    generator_args = ref_list_to_args(block.transactions_generator_ref_list)
-    if block.transactions_generator is None or block.transactions_info is None:
-        raise RuntimeError("transactions_generator of FullBlock is null")
-    block_generator = BlockGenerator(block.transactions_generator, generator_args, [])
-    return run_generator(
-        block_generator, constants, min(constants.MAX_BLOCK_COST_CLVM, block.transactions_info.cost), block.height
-    )
 
 
 def run_generator_with_args(
@@ -201,13 +191,13 @@ def run_generator_with_args(
 
 
 @click.command()
-@click.argument("file", type=click.File("rb"))
-def cmd_run_json_block_file(file):
+@click.argument("filename", type=click.Path(exists=True), default="testnet10.396963.json")
+def cmd_run_json_block_file(filename):
     """`file` is a file containing a FullBlock in JSON format"""
-    return run_json_block_file(file)
+    return run_json_block_file(Path(filename))
 
 
-def run_json_block(full_block, constants: ConsensusConstants) -> List[CAT]:
+def run_json_block(full_block, parent: Path, constants: ConsensusConstants) -> List[CAT]:
     ref_list = full_block["block"]["transactions_generator_ref_list"]
     tx_info: dict = full_block["block"]["transactions_info"]
     generator_program_hex: str = full_block["block"]["transactions_generator"]
@@ -215,18 +205,18 @@ def run_json_block(full_block, constants: ConsensusConstants) -> List[CAT]:
     cat_list: List[CAT] = []
     if tx_info and generator_program_hex:
         cost = tx_info["cost"]
-        args = ref_list_to_args(ref_list)
+        args = ref_list_to_args(ref_list, parent)
         cat_list = run_generator_with_args(generator_program_hex, args, constants, cost, height)
 
     return cat_list
 
 
-def run_json_block_file(file: TextIO):
-    full_block = json.load(file)
+def run_json_block_file(filename: Path):
+    full_block = json.load(filename.open("rb"))
     # pull in current constants from config.yaml
     _, constants = get_config_and_constants()
 
-    cat_list = run_json_block(full_block, constants)
+    cat_list = run_json_block(full_block, filename.parent.absolute(), constants)
 
     cat_list_json = json.dumps([cat.cat_to_dict() for cat in cat_list])
     print(cat_list_json)
