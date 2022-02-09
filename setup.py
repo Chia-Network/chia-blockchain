ce4f9aa888b15e3dac377ea8a282c2a50eef05af
Kyle Altendorf <sda@fstab.net>
2022-02-08 22:05:56 -0500

    just get *.sql from anywhere for package data

--- setup.py
+++ setup.py
@@ -130,8 +130,7 @@ kwargs = dict(
     },
     package_data={
         "chia": ["pyinstaller.spec"],
-        "": ["*.clvm", "*.clvm.hex", "*.clib", "*.clinc", "*.clsp", "py.typed"],
-        "chia.full_node.sql": ["*.sql"],
+        "": ["*.clvm", "*.clvm.hex", "*.clib", "*.clinc", "*.clsp", "*.sql", "py.typed"],
         "chia.util": ["initial-*.yaml", "english.txt"],
         "chia.ssl": ["chia_ca.crt", "chia_ca.key", "dst_root_ca.pem"],
         "mozilla-ca": ["cacert.pem"],
