220e845d51fb9dc143867cf53d8cb11529121ee3
Kyle Altendorf <sda@fstab.net>
2022-02-15 15:22:23 -0500

    shutdown the weight proof process pool (#10163)
    * shutdown the weight proof process pool
    
    * use a context manager for the weight proof process pool executor
    
    * record of the debug code
    
    * mostly cleaned up
    
    * suppress sync task cancellation propagation when awaited while closing
    
    * breakup multi-second WeightProofHandler.validate_weight_proof with async sleeps
    
    * move awaiting of sync task until after existing cancellation
    
    * properly handle shutdown file with a new instance each time and a context manager
    
    * cleanup

--- chia/wallet/wallet_weight_proof_handler.py
+++ chia/wallet/wallet_weight_proof_handler.py
@@ -29,7 +29,7 @@ log = logging.getLogger(__name__)
 
 
 def _create_shutdown_file() -> IO:
-    return tempfile.NamedTemporaryFile(prefix="chia_executor_shutdown_trigger")
+    return tempfile.NamedTemporaryFile(prefix="chia_wallet_weight_proof_handler_executor_shutdown_trigger")
 
 
 class WalletWeightProofHandler:
