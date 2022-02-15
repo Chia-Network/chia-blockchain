c8f2cdefd8d0e1d757180839523dbaab1d0b0504
William Blanke <wjb98672@gmail.com>
2022-02-14 19:06:13 -0800

    lint

--- tests/pools/test_pool_rpc.py
+++ tests/pools/test_pool_rpc.py
@@ -458,7 +458,6 @@ class TestPoolWalletRpc:
     @pytest.mark.asyncio
     @pytest.mark.parametrize("trusted", [True, False])
     @pytest.mark.parametrize("fee", [0, FEE_AMOUNT])
-
     async def test_absorb_self(self, one_wallet_node_and_rpc, fee, trusted):
         client, wallet_node_0, full_node_api = one_wallet_node_and_rpc
         if trusted:
