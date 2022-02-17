fad3f88b13bd334b7be23ea5ce3b760b865ae96b
Matt Hauff <quexington@gmail.com>
2022-02-17 12:44:28 -0700

    Bundle CATs with announcements (#10260)
    * Bundle CATs with announcements
    
    * Make the announcement deterministic to the coins being spent

--- chia/wallet/cat_wallet/cat_wallet.py
+++ chia/wallet/cat_wallet/cat_wallet.py
@@ -22,6 +22,7 @@ from chia.types.spend_bundle import SpendBundle
 from chia.types.condition_opcodes import ConditionOpcode
 from chia.util.byte_types import hexstr_to_bytes
 from chia.util.condition_tools import conditions_dict_for_solution, pkm_pairs_for_conditions_dict
+from chia.util.hash import std_hash
 from chia.util.ints import uint8, uint32, uint64, uint128
 from chia.wallet.cat_wallet.cat_constants import DEFAULT_CATS
 from chia.wallet.cat_wallet.cat_info import CATInfo, LegacyCATInfo
@@ -609,12 +610,13 @@ class CATWallet:
         spendable_cc_list = []
         chia_tx = None
         first = True
+        announcement: Announcement
         for coin in cat_coins:
             if first:
                 first = False
+                announcement = Announcement(coin.name(), std_hash(b"".join([c.name() for c in cat_coins])), b"\xca")
                 if need_chia_transaction:
                     if fee > regular_chia_to_claim:
-                        announcement = Announcement(coin.name(), b"$", b"\xca")
                         chia_tx, _ = await self.create_tandem_xch_tx(
                             fee, uint64(regular_chia_to_claim), announcement_to_assert=announcement
                         )
@@ -627,16 +629,22 @@ class CATWallet:
                     elif regular_chia_to_claim > fee:
                         chia_tx, _ = await self.create_tandem_xch_tx(fee, uint64(regular_chia_to_claim))
                         innersol = self.standard_wallet.make_solution(
-                            primaries=primaries, coin_announcements_to_assert={announcement.name()}
+                            primaries=primaries,
+                            coin_announcements={announcement.message},
+                            coin_announcements_to_assert={announcement.name()},
                         )
                 else:
                     innersol = self.standard_wallet.make_solution(
                         primaries=primaries,
+                        coin_announcements={announcement.message},
                         coin_announcements_to_assert=coin_announcements_bytes,
                         puzzle_announcements_to_assert=puzzle_announcements_bytes,
                     )
             else:
-                innersol = self.standard_wallet.make_solution(primaries=[])
+                innersol = self.standard_wallet.make_solution(
+                    primaries=[],
+                    coin_announcements_to_assert={announcement.name()},
+                )
             inner_puzzle = await self.inner_puzzle_for_cc_puzhash(coin.puzzle_hash)
             lineage_proof = await self.get_lineage_proof_for_coin(coin)
             assert lineage_proof is not None
