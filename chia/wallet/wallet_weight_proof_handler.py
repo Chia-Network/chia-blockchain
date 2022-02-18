b5ca8c409e00244fe0586bb002427cf8d38448a0
Mariano Sorgente <sorgente711@gmail.com>
2022-02-18 10:14:21 -0500

    Spawn for memory

--- chia/wallet/wallet_weight_proof_handler.py
+++ chia/wallet/wallet_weight_proof_handler.py
@@ -24,6 +24,7 @@ from chia.types.weight_proof import (
 )
 
 from chia.util.ints import uint32
+from chia.util.setproctitle import setproctitle
 
 log = logging.getLogger(__name__)
 
@@ -45,7 +46,9 @@ class WalletWeightProofHandler:
         self._constants = constants
         self._num_processes = 4
         self._executor_shutdown_tempfile: IO = _create_shutdown_file()
-        self._executor: ProcessPoolExecutor = ProcessPoolExecutor(self._num_processes)
+        self._executor: ProcessPoolExecutor = ProcessPoolExecutor(
+            self._num_processes, initializer=setproctitle, initargs=("chia_wallet",)
+        )
         self._weight_proof_tasks: List[asyncio.Task] = []
 
     def cancel_weight_proof_tasks(self):
