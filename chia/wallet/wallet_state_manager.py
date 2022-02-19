9e9c75a0e3db45f269cec5d7d5bb59d46a583d65
William Blanke <wjb98672@gmail.com>
2022-02-19 11:21:03 -0800

    unused locks

--- chia/wallet/wallet_state_manager.py
+++ chia/wallet/wallet_state_manager.py
@@ -201,9 +201,6 @@ class WalletStateManager:
                 )
             if wallet is not None:
                 self.wallets[wallet_info.id] = wallet
-
-        # Lock
-        self.lock = asyncio.Lock()  # external
         return self
 
     def get_derivation_index(self, pubkey: G1Element, max_depth: int = 1000) -> int:
