d74d37722bab47f1768811d73583cd25b51f213d
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-02-21 14:34:00 -0800

    Fix test_json (inheritance from unittest.TestCase)

--- tests/core/util/test_streamable.py
+++ tests/core/util/test_streamable.py
@@ -11,7 +11,7 @@ from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.full_block import FullBlock
 from chia.types.weight_proof import SubEpochChallengeSegment
-from chia.util.ints import uint8, uint32
+from chia.util.ints import uint8, uint32, uint64
 from chia.util.streamable import (
     Streamable,
     streamable,
