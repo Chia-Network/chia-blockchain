6d835483e6a472fcc4b4fdf0243c98f108c1b89e
Chris Marslender <chrismarslender@gmail.com>
2022-02-21 18:02:26 -0600

    Update param to `to_address` and use timeout assert in the test

--- chia/rpc/wallet_rpc_api.py
+++ chia/rpc/wallet_rpc_api.py
@@ -680,10 +680,10 @@ class WalletRpcApi:
         sort_key = request.get("sort_key", None)
         reverse = request.get("reverse", False)
 
-        address = request.get("address", None)
+        to_address = request.get("to_address", None)
         to_puzzle_hash: Optional[bytes32] = None
-        if address is not None:
-            to_puzzle_hash = decode_puzzle_hash(address)
+        if to_address is not None:
+            to_puzzle_hash = decode_puzzle_hash(to_address)
 
         transactions = await self.service.wallet_state_manager.tx_store.get_transactions_between(
             wallet_id, start, end, sort_key=sort_key, reverse=reverse, to_puzzle_hash=to_puzzle_hash
--- tests/wallet/rpc/test_wallet_rpc.py
+++ tests/wallet/rpc/test_wallet_rpc.py
@@ -386,9 +386,9 @@ class TestWalletRpc:
             ph_by_addr = await wallet.get_new_puzzlehash()
             await client.send_transaction("1", 1, encode_puzzle_hash(ph_by_addr, "xch"))
             await client.farm_block(encode_puzzle_hash(ph_by_addr, "xch"))
-            await asyncio.sleep(0.5)
+            await time_out_assert(10, wallet_is_synced, True, wallet_node, full_node_api)
             tx_for_address = await wallet_rpc_api.get_transactions(
-                {"wallet_id": "1", "address": encode_puzzle_hash(ph_by_addr, "xch")}
+                {"wallet_id": "1", "to_address": encode_puzzle_hash(ph_by_addr, "xch")}
             )
             assert len(tx_for_address["transactions"]) == 1
             assert decode_puzzle_hash(tx_for_address["transactions"][0]["to_address"]) == ph_by_addr
