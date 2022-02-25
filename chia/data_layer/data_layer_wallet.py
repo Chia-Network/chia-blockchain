6fe0be524bc78c3c6f95d85b2b62c2a11aefb48c
Matt Hauff <quexington@gmail.com>
2022-02-25 11:02:20 -0700

    Stop running singleton puzzle in python

--- chia/data_layer/data_layer_wallet.py
+++ chia/data_layer/data_layer_wallet.py
@@ -668,8 +668,8 @@ class DataLayerWallet:
 
     async def singleton_removed(self, parent_spend: CoinSpend, height: uint32) -> None:
         parent_name = parent_spend.coin.name()
-        puzzle: Program = parent_spend.puzzle_reveal.to_program()
-        solution: Program = parent_spend.solution.to_program()
+        puzzle = parent_spend.puzzle_reveal
+        solution = parent_spend.solution
 
         matched, curried_args = match_dl_singleton(puzzle)
         if matched:
@@ -694,7 +694,7 @@ class DataLayerWallet:
             root: bytes32
             inner_puzzle_hash: bytes32
 
-            conditions = puzzle.run(solution).as_python()
+            conditions = puzzle.run_with_cost(solution)[0].as_python()
             found_singleton: bool = False
             for condition in conditions:
                 if condition[0] == ConditionOpcode.CREATE_COIN and int.from_bytes(condition[2], "big") % 2 == 1:
