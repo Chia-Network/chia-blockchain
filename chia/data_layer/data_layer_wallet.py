c39e42cc7565fda2a7d136c097a60aa7d39292d3
Matt Hauff <quexington@gmail.com>
2022-02-25 11:18:53 -0700

    Whoops

--- chia/data_layer/data_layer_wallet.py
+++ chia/data_layer/data_layer_wallet.py
@@ -671,7 +671,7 @@ class DataLayerWallet:
         puzzle = parent_spend.puzzle_reveal
         solution = parent_spend.solution
 
-        matched, curried_args = match_dl_singleton(puzzle)
+        matched, curried_args = match_dl_singleton(puzzle.to_program())
         if matched:
             self.log.info(f"DL singleton removed: {parent_spend.coin}")
             singleton_record: Optional[SingletonRecord] = await self.wallet_state_manager.dl_store.get_singleton_record(
@@ -694,7 +694,9 @@ class DataLayerWallet:
             root: bytes32
             inner_puzzle_hash: bytes32
 
-            conditions = puzzle.run_with_cost(solution)[0].as_python()
+            conditions = puzzle.run_with_cost(self.wallet_state_manager.constants.MAX_BLOCK_COST_CLVM, solution)[
+                1
+            ].as_python()
             found_singleton: bool = False
             for condition in conditions:
                 if condition[0] == ConditionOpcode.CREATE_COIN and int.from_bytes(condition[2], "big") % 2 == 1:
