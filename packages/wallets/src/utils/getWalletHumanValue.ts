bb236baefe1e0d44199553ba964aba55299a6e90
Zlatko Fedor <zfedor@gmail.com>
2022-03-06 22:08:29 +0100

    removed unused code

--- packages/wallets/src/utils/getWalletHumanValue.ts
+++
@@ -1,9 +0,0 @@
-import type { Wallet } from '@chia/api';
-import { WalletType } from '@chia/api';
-import { mojoToCATLocaleString, mojoToChiaLocaleString } from '@chia/core';
-
-export default function getWalletHumanValue(wallet: Wallet, value: number): string {
-  return wallet.type === WalletType.CAT
-    ? mojoToCATLocaleString(value)
-    : mojoToChiaLocaleString(value);
-}
--- packages/wallets/src/utils/index.ts
+++ packages/wallets/src/utils/index.ts
@@ -1,3 +1,2 @@
-export { default as getWalletHumanValue } from './getWalletHumanValue';
 export { default as getWalletSyncingStatus } from './getWalletSyncingStatus';
 export { default as isCATWalletPresent } from './isCATWalletPresent';
