616c2077fc1bde368cfc0ae1d014cc8d1894da9d
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-03-07 17:31:36 -0800

    Remove code that set "daemon_port" before calling `setup_daemon`

--- tests/core/test_daemon_rpc.py
+++ tests/core/test_daemon_rpc.py
@@ -4,25 +4,21 @@ import pytest_asyncio
 from tests.setup_nodes import setup_daemon
 from chia.daemon.client import connect_to_daemon
 from chia import __version__
-from tests.util.socket import find_available_listen_port
-from chia.util.config import save_config
 
 
 class TestDaemonRpc:
     @pytest_asyncio.fixture(scope="function")
     async def get_daemon(self, bt):
-        bt._config["daemon_port"] = find_available_listen_port()
-        # unfortunately, the daemon's WebSocketServer loads the configs from
-        # disk, so the only way to configure its port is to write it to disk
-        save_config(bt.root_path, "config.yaml", bt._config)
         async for _ in setup_daemon(btools=bt):
             yield _
 
     @pytest.mark.asyncio
     async def test_get_version_rpc(self, get_daemon, bt):
+        ws_server = get_daemon
         config = bt.config
         client = await connect_to_daemon(config["self_hostname"], config["daemon_port"], bt.get_daemon_ssl_context())
         response = await client.get_version()
 
         assert response["data"]["success"]
         assert response["data"]["version"] == __version__
+        ws_server.stop()
