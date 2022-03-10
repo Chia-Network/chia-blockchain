301f3f40bd4a88861a36211528661b3fceff7910
arty <art.yerkes@gmail.com>
2022-03-09 20:50:35 -0800

    Properly balance blank lines

--- chia/util/lock.py
+++ chia/util/lock.py
@@ -2,7 +2,9 @@ import os
 import time
 from typing import Optional, TextIO, TypeVar, Callable
 
+
 T = TypeVar("T")
+
 
 # Cribbed mostly from chia/daemon/server.py
 def create_exclusive_lock(lockfile: str) -> Optional[TextIO]:
