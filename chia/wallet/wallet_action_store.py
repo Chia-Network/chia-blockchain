d08b9c63403142e406c4f3528291c1e9fb705cde
arvidn <arvid@libtorrent.org>
2022-03-14 16:55:29 +0100

    deduplicate wallet_type index

--- chia/wallet/wallet_action_store.py
+++ chia/wallet/wallet_action_store.py
@@ -41,7 +41,7 @@ class WalletActionStore:
 
         await self.db_connection.execute("CREATE INDEX IF NOT EXISTS wallet_id on action_queue(wallet_id)")
 
-        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS wallet_type on action_queue(wallet_type)")
+        await self.db_connection.execute("CREATE INDEX IF NOT EXISTS action_queue_wallet_type on action_queue(wallet_type)")
 
         await self.db_connection.commit()
         return self
