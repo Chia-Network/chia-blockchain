f1ffd13c606996b7ce8536e2ff4f119d570e4a14
arty <art.yerkes@gmail.com>
2022-03-17 14:04:32 -0700

    Bump clvm_tools_rs to fix a problem running as daemon caused by old log message that is now eliminated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependencies = [
     "clvm==0.9.7",
     "clvm_tools==0.4.3",  # Currying, Program.to, other conveniences
     "clvm_rs==0.1.19",
-    "clvm-tools-rs==0.1.6",  # Rust implementation of clvm_tools
+    "clvm-tools-rs==0.1.7",  # Rust implementation of clvm_tools
     "aiohttp==3.7.4",  # HTTP server for full node rpc
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
     "bitstring==3.1.9",  # Binary data management library
