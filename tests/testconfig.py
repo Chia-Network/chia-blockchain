bd2194524e9031145a9cf3bcb1a9f35efd46ae8a
Kyle Altendorf <sda@fstab.net>
2022-03-22 22:02:35 -0400

    hint testconfig.custom_vars

--- tests/testconfig.py
+++ tests/testconfig.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # Github actions template config.
 oses = ["ubuntu", "macos"]
 
@@ -7,4 +9,4 @@ checkout_blocks_and_plots = True
 install_timelord = False
 check_resource_usage = False
 job_timeout = 30
-custom_vars = []
+custom_vars: List[str] = []
