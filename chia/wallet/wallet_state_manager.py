a0afbe9c26dce4fb531a3b7a5b0e5be89ed63b63
ytx1991 <ytx1991@gmail.com>
2022-03-23 11:20:43 -0700

    Add state change notification

--- chia/wallet/wallet_state_manager.py
+++ chia/wallet/wallet_state_manager.py
@@ -612,6 +612,7 @@ class WalletStateManager:
                         parent_coin_state.spent_height,
                         parent_coin_state.coin.puzzle_hash,
                     )
+                    self.state_changed("added_unacknowledged_token")
 
         return wallet_id, wallet_type
 
