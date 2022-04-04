f86f9eaae09765f2d1c10931547902fd0db9cdaf
Kyle Altendorf <sda@fstab.net>
2022-04-03 21:12:10 -0400

    less

--- setup.py
+++ setup.py
@@ -4,49 +4,18 @@ dependencies = [
     "pywin32",
 ]
 
-upnp_dependencies = [
-]
-
 dev_dependencies = [
     "pytest",
 ]
 
 kwargs = dict(
     name="chia-blockchain",
-    author="Mariano Sorgente",
-    author_email="mariano@chia.net",
-    description="Chia blockchain full node, farmer, timelord, and wallet.",
-    url="https://chia.net/",
     license="Apache License",
     python_requires=">=3.7, <4",
-    keywords="chia blockchain node",
     install_requires=dependencies,
-    extras_require=dict(
-        uvloop=["uvloop"],
-        dev=dev_dependencies,
-        upnp=upnp_dependencies,
-    ),
     packages=[
         "chia",
     ],
-    entry_points={
-        "console_scripts": [
-            "chia = chia.cmds.chia:main",
-            "chia_wallet = chia.server.start_wallet:main",
-            "chia_full_node = chia.server.start_full_node:main",
-            "chia_harvester = chia.server.start_harvester:main",
-            "chia_farmer = chia.server.start_farmer:main",
-            "chia_introducer = chia.server.start_introducer:main",
-            "chia_crawler = chia.seeder.start_crawler:main",
-            "chia_seeder = chia.seeder.dns_server:main",
-            "chia_timelord = chia.server.start_timelord:main",
-            "chia_timelord_launcher = chia.timelord.timelord_launcher:main",
-            "chia_full_node_simulator = chia.simulator.start_simulator:main",
-        ]
-    },
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    zip_safe=False,
 )
 
 
