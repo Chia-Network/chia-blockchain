f6e638400512b0d1da27c185088c1d77680c8cd1
Kyle Altendorf <sda@fstab.net>
2022-04-05 08:42:24 -0400

    preliminary setup with poetry

--- install.sh
+++ install.sh
@@ -259,7 +259,12 @@ if [ -e venv/bin/python ]; then
   fi
 fi
 
-$INSTALL_PYTHON_PATH -m venv venv $VENV_CLEAR
+rm -rf .penv .venv venv
+$INSTALL_PYTHON_PATH -m venv .penv
+.penv/bin/python -m pip install --upgrade pip setuptools wheel
+.penv/bin/python -m pip install --requirement requirements.penv.txt
+.penv/bin/poetry install
+ln -s .venv venv
 if [ ! -f "activate" ]; then
   ln -s venv/bin/activate .
 fi
@@ -272,12 +277,12 @@ fi
 # shellcheck disable=SC1091
 . ./activate
 # pip 20.x+ supports Linux binary wheels
-python -m pip install --upgrade pip
-python -m pip install wheel
+#python -m pip install --upgrade pip
+#python -m pip install wheel
 #if [ "$INSTALL_PYTHON_VERSION" = "3.8" ]; then
 # This remains in case there is a diversion of binary wheels
-python -m pip install --extra-index-url https://pypi.chia.net/simple/ miniupnpc==2.2.2
-python -m pip install -e ."${EXTRAS}" --extra-index-url https://pypi.chia.net/simple/
+#python -m pip install --extra-index-url https://pypi.chia.net/simple/ miniupnpc==2.2.2
+#python -m pip install -e ."${EXTRAS}" --extra-index-url https://pypi.chia.net/simple/
 
 echo ""
 echo "Chia blockchain install.sh complete."
