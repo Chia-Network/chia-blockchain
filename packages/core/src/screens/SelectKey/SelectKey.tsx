d7f341912b07465cce8b11812bc3adf72c30a47a
Zlatko Fedor <zlatko@chia.net>
2022-04-05 20:47:02 +0100

    show tokens after login

--- packages/core/src/screens/SelectKey/SelectKey.tsx
+++ packages/core/src/screens/SelectKey/SelectKey.tsx
@@ -57,7 +57,7 @@ export default function SelectKey() {
         fingerprint,
       }).unwrap();
 
-      navigate('/dashboard');
+      navigate('/dashboard/wallets');
     } catch (error) {
       showError(error)
     } finally {
