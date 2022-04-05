261f5baa6ffecb62130e4118f01dd0329378c076
wjblanke <wjb98672@gmail.com>
2022-04-04 23:35:28 -0700

    bump up to 2.1.7 to fix inotify issue resolved by 848 (#11042)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ dependencies = [
     # TODO: when moving to click 8 remove the pinning of black noted below
     "click==7.1.2",  # For the CLI
     "dnspythonchia==2.2.0",  # Query DNS seeds
-    "watchdog==2.1.6",  # Filesystem event watching - watches keyring.yaml
+    "watchdog==2.1.7",  # Filesystem event watching - watches keyring.yaml
     "dnslib==0.9.17",  # dns lib
     "typing-extensions==4.0.1",  # typing backports like Protocol and TypedDict
     "zstd==1.5.0.4",
