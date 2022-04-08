fccbd3e10d27673e39c01f0f89e47b5455b8331a
Kyle Altendorf <sda@fstab.net>
2022-04-08 17:56:12 -0400

    correct case to ./images/settings.svg

--- packages/icons/src/Settings.tsx
+++ packages/icons/src/Settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SvgIcon, SvgIconProps } from '@mui/material';
-import SettingsIcon from './images/Settings.svg';
+import SettingsIcon from './images/settings.svg';
 
 export default function Settings(props: SvgIconProps) {
   return <SvgIcon component={SettingsIcon} viewBox="0 0 30 30" {...props} />;
