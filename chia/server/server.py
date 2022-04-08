214d782e69c1a14261195e8267541a91e22ec206
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-04-08 11:59:36 -0700

    try ci without int coercion

--- chia/server/server.py
+++ chia/server/server.py
@@ -265,7 +265,7 @@ class ChiaServer:
 
         self.site = web.TCPSite(
             self.runner,
-            port=int(self._port),
+            port=self._port,
             shutdown_timeout=3,
             ssl_context=ssl_context,
         )
