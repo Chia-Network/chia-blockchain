ca49193115f43b60061f8dc4301798f470d9bcec
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-04-07 20:40:41 -0700

    Increase max timeout while waiting to see tx in mempool

--- tests/pools/test_pool_rpc.py
+++ tests/pools/test_pool_rpc.py
@@ -795,8 +795,8 @@ class TestPoolWalletRpc:
                 fetched: Optional[TransactionRecord] = await client.get_transaction(wid, tx.name)
                 return fetched is not None and fetched.is_in_mempool()
 
-            await time_out_assert(10, tx_is_in_mempool, True, wallet_id, join_pool_tx)
-            await time_out_assert(10, tx_is_in_mempool, True, wallet_id_2, join_pool_tx_2)
+            await time_out_assert(20, tx_is_in_mempool, True, wallet_id, join_pool_tx)
+            await time_out_assert(20, tx_is_in_mempool, True, wallet_id_2, join_pool_tx_2)
 
             assert status.current.state == PoolSingletonState.SELF_POOLING.value
             assert status.target is not None
