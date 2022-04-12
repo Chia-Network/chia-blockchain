00d0c39ed96bb21bf424ca0a54a1c87bbdd37b1e
Zlatko Fedor <zlatko@chia.net>
2022-04-12 23:35:06 +0100

    lgtm

--- packages/gui/src/components/pool/PoolOverview.tsx
+++ packages/gui/src/components/pool/PoolOverview.tsx
@@ -1,125 +1,20 @@
 import React from 'react';
 import { Trans } from '@lingui/macro';
-import { useToggle } from 'react-use';
-import { Button, Flex, UnitFormat, More, Table, mojoToChiaLocaleString, Suspender } from '@chia/core';
+import { Button, Flex, Suspender } from '@chia/core';
 import { useNavigate } from 'react-router';
 import {
-  Payment as PaymentIcon,
-  Power as PowerIcon,
-} from '@mui/icons-material';
-import {
-  Box,
-  ListItemIcon,
-  MenuItem,
   Grid,
   Typography,
 } from '@mui/material';
 import PlotNFTCard from '../plotNFT/PlotNFTCard';
 import PlotExternalNFTCard from '../plotNFT/PlotExternalNFTCard';
-import PlotNFTName from '../plotNFT/PlotNFTName';
-import PoolAbsorbRewards from './PoolAbsorbRewards';
-import PoolJoin from './PoolJoin';
 import PoolHero from './PoolHero';
-import type PlotNFT from '../../types/PlotNFT';
 import usePlotNFTs from '../../hooks/usePlotNFTs';
-import PlotNFTStateEnum from '../../constants/PlotNFTState';
 import PlotNFTUnconfirmedCard from '../plotNFT/PlotNFTUnconfirmedCard';
-import PlotNFTState from '../plotNFT/PlotNFTState';
 import useUnconfirmedPlotNFTs from '../../hooks/useUnconfirmedPlotNFTs';
-
-const groupsCols = [
-  {
-    field: (nft: PlotNFT) => <PlotNFTName nft={nft} />,
-    title: <Trans>Plot NFT</Trans>,
-  },
-  {
-    field: (nft: PlotNFT) => <PlotNFTState nft={nft} />,
-    title: <Trans>Status</Trans>,
-  },
-  {
-    field: (nft: PlotNFT) => {
-      const {
-        poolWalletStatus: {
-          current: { state },
-        },
-      } = nft;
-
-      if (state === PlotNFTStateEnum.SELF_POOLING) {
-        return (
-          <UnitFormat
-            value={mojoToChiaLocaleString(nft.walletBalance.confirmedWalletBalance ?? 0)}
-          />
-        );
-      }
-
-      return null;
-    },
-    title: <Trans>Unclaimed Rewards</Trans>,
-  },
-  {
-    title: <Trans>Actions</Trans>,
-    field(nft: PlotNFT) {
-      const isSelfPooling =
-        nft.poolWalletStatus.current.state === PlotNFTStateEnum.SELF_POOLING;
-
-      return (
-        <More>
-          {({ onClose }) => (
-            <Box>
-              {isSelfPooling && (
-                <PoolAbsorbRewards nft={nft}>
-                  {({ absorb, disabled }) => (
-                    <MenuItem
-                      onClick={() => {
-                        onClose();
-                        absorb();
-                      }}
-                      disabled={disabled}
-                    >
-                      <ListItemIcon>
-                        <PaymentIcon fontSize="small" />
-                      </ListItemIcon>
-                      <Typography variant="inherit" noWrap>
-                        <Trans>Claim Rewards</Trans>
-                      </Typography>
-                    </MenuItem>
-                  )}
-                </PoolAbsorbRewards>
-              )}
-
-              <PoolJoin nft={nft}>
-                {({ join, disabled }) => (
-                  <MenuItem
-                    onClick={() => {
-                      onClose();
-                      join();
-                    }}
-                    disabled={disabled}
-                  >
-                    <ListItemIcon>
-                      <PowerIcon fontSize="small" />
-                    </ListItemIcon>
-                    <Typography variant="inherit" noWrap>
-                      {isSelfPooling ? (
-                        <Trans>Join Pool</Trans>
-                      ) : (
-                        <Trans>Change Pool</Trans>
-                      )}
-                    </Typography>
-                  </MenuItem>
-                )}
-              </PoolJoin>
-            </Box>
-          )}
-        </More>
-      );
-    },
-  },
-];
 
 export default function PoolOverview() {
   const navigate = useNavigate();
-  const [showTable, toggleShowTable] = useToggle(false);
   const { nfts, external, loading } = usePlotNFTs();
   const { unconfirmed } = useUnconfirmedPlotNFTs();
 
@@ -157,43 +52,35 @@ export default function PoolOverview() {
         </Flex>
       </Flex>
       <Flex flexDirection="column" gap={1}>
-        {showTable ? (
-          <Table
-            uniqueField="poolState.p2SingletonPuzzleHash"
-            rows={nfts}
-            cols={groupsCols}
-          />
-        ) : (
-          <Grid spacing={3} alignItems="stretch" container>
-            {unconfirmed.map((unconfirmedPlotNFT) => (
-              <Grid key={unconfirmedPlotNFT.transactionId} xs={12} md={6} item>
-                <PlotNFTUnconfirmedCard
-                  unconfirmedPlotNFT={unconfirmedPlotNFT}
-                />
-              </Grid>
-            ))}
-            {nfts?.map((item) => (
-              <Grid
-                key={item.poolState.p2SingletonPuzzleHash}
-                xs={12}
-                md={6}
-                item
-              >
-                <PlotNFTCard nft={item} />
-              </Grid>
-            ))}
-            {external?.map((item) => (
-              <Grid
-                key={item.poolState.p2SingletonPuzzleHash}
-                xs={12}
-                md={6}
-                item
-              >
-                <PlotExternalNFTCard nft={item} />
-              </Grid>
-            ))}
-          </Grid>
-        )}
+        <Grid spacing={3} alignItems="stretch" container>
+          {unconfirmed.map((unconfirmedPlotNFT) => (
+            <Grid key={unconfirmedPlotNFT.transactionId} xs={12} md={6} item>
+              <PlotNFTUnconfirmedCard
+                unconfirmedPlotNFT={unconfirmedPlotNFT}
+              />
+            </Grid>
+          ))}
+          {nfts?.map((item) => (
+            <Grid
+              key={item.poolState.p2SingletonPuzzleHash}
+              xs={12}
+              md={6}
+              item
+            >
+              <PlotNFTCard nft={item} />
+            </Grid>
+          ))}
+          {external?.map((item) => (
+            <Grid
+              key={item.poolState.p2SingletonPuzzleHash}
+              xs={12}
+              md={6}
+              item
+            >
+              <PlotExternalNFTCard nft={item} />
+            </Grid>
+          ))}
+        </Grid>
       </Flex>
     </Flex>
   );
