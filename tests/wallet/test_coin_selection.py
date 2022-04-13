adbf7f4f94867aa5bfbded17ae1e568ab802759c
Jack Nelson <jack@jacknelson.xyz>
2022-04-12 20:27:05 -0400

    linty lint

--- tests/wallet/test_coin_selection.py
+++ tests/wallet/test_coin_selection.py
@@ -124,7 +124,7 @@ class TestCoinSelection:
         # test for failure
         with pytest.raises(ValueError):
             for target_amount in [10000, 9999]:
-                result: Set[Coin] = await select_coins(
+                await select_coins(
                     spendable_amount,
                     DEFAULT_CONSTANTS.MAX_COIN_AMOUNT,
                     coin_list,
@@ -135,7 +135,7 @@ class TestCoinSelection:
         # test not enough coin failure.
         with pytest.raises(ValueError):
             for target_amount in [10001, 20000]:
-                result: Set[Coin] = await select_coins(
+                await select_coins(
                     spendable_amount,
                     DEFAULT_CONSTANTS.MAX_COIN_AMOUNT,
                     coin_list,
