f7c13ab8530ea18682e47e7f4d315b63eb0370d4
Sebastjan <trepca@gmail.com>
2022-04-15 13:50:24 +0200

    perform_handshake call fix

--- tests/core/ssl/test_ssl.py
+++ tests/core/ssl/test_ssl.py
@@ -37,7 +37,9 @@ async def establish_connection(server: ChiaServer, self_hostname: str, ssl_conte
             100,
             30,
         )
-        handshake = await wsc.perform_handshake(server._network_id, protocol_version, dummy_port, NodeType.FULL_NODE)
+        handshake = await wsc.perform_handshake(
+            server._network_id, protocol_version, dummy_port, NodeType.FULL_NODE, server._local_capabilities
+        )
         await session.close()
         return handshake
     except Exception:
