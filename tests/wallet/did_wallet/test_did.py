ea7c29bcbd9e9a4f35308ec1ef5141f765d5a05c
ytx1991 <ytx1991@gmail.com>
2022-04-15 15:58:33 -0700

    Add test config for DID/NFT

--- tests/wallet/did_wallet/test_did.py
+++ tests/wallet/did_wallet/test_did.py
@@ -1,4 +1,3 @@
-import asyncio
 
 import pytest
 from blspy import AugSchemeMPL
--- tests/wallet/did_wallet/test_nft_wallet.py
+++ tests/wallet/did_wallet/test_nft_wallet.py
@@ -15,7 +15,6 @@ from chia.wallet.did_wallet.did_wallet import DIDWallet
 from chia.wallet.nft_wallet.nft_puzzles import get_uri_list_from_puzzle
 from chia.wallet.nft_wallet.nft_wallet import NFTWallet
 from chia.wallet.transaction_record import TransactionRecord
-
 from tests.time_out_assert import time_out_assert, time_out_assert_not_none
 
 # pytestmark = pytest.mark.skip("TODO: Fix tests")
