f8fbbbc6164e0ab9d23f9e37f0b9801794f8c059
Zlatko Fedor <zlatko@chia.net>
2022-04-20 10:36:59 +0100

    use GUI repo for issues

--- packages/core/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ packages/core/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -58,7 +58,7 @@ export default class ErrorBoundary extends Component {
   render() {
     if (this.state.hasError) {
       const { stacktrace, error } = this.state;
-      const issueLink = `https://github.com/Chia-Network/chia-blockchain/issues/new?${qs.stringify({
+      const issueLink = `https://github.com/Chia-Network/chia-blockchain-gui/issues/new?${qs.stringify({
         labels: 'bug',
         template: 'bug_report.yaml',
         title: `[BUG] ${error.message}`,
