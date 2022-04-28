9be6b8b863a10dc9bd40217bc4e23d30a488e4ba
xdustinface <xdustinfacex@gmail.com>
2022-04-29 00:55:40 +0200

    More tests

--- tests/util/test_paginator.py
+++ tests/util/test_paginator.py
@@ -6,14 +6,16 @@ import pytest
 from chia.util.paginator import InvalidPageSizeError, InvalidPageSizeLimit, PageOutOfBoundsError, Paginator
 
 
-@pytest.mark.parametrize("page_size, page_size_limit", [(1, 1), (1, 2), (2, 2), (10, 100), (1000, 1000)])
-def test_constructor_valid_inputs(page_size: int, page_size_limit: int) -> None:
-    default_paginator: Paginator = Paginator([], page_size, page_size_limit)
-    for i in range(1, default_paginator.page_size_limit()):
-        paginator: Paginator = Paginator([], i, page_size_limit)
-        assert paginator.page_size() == i
-        assert paginator.page_count() == 1
-        assert paginator.get_page(0) == []
+@pytest.mark.parametrize(
+    "source, page_size, page_size_limit",
+    [([], 1, 1), ([1], 1, 2), ([1, 2], 2, 2), ([], 10, 100), ([1, 2, 10], 1000, 1000)],
+)
+def test_constructor_valid_inputs(source: List[int], page_size: int, page_size_limit: int) -> None:
+    paginator: Paginator = Paginator(source, page_size, page_size_limit)
+    assert paginator.page_size() == page_size
+    assert paginator.page_size_limit() == page_size_limit
+    assert paginator.page_count() == 1
+    assert paginator.get_page(0) == source
 
 
 @pytest.mark.parametrize(
@@ -42,10 +44,29 @@ def test_empty_source() -> None:
 
 
 @pytest.mark.parametrize(
-    "page, expected_data", [(0, [0, 1, 2, 3, 4]), (1, [5, 6, 7, 8, 9]), (2, [10, 11, 12, 13, 14]), (3, [15, 16])]
+    "length, page_size, page, expected_data",
+    [
+        (17, 5, 0, [0, 1, 2, 3, 4]),
+        (17, 5, 1, [5, 6, 7, 8, 9]),
+        (17, 5, 2, [10, 11, 12, 13, 14]),
+        (17, 5, 3, [15, 16]),
+        (3, 4, 0, [0, 1, 2]),
+        (3, 3, 0, [0, 1, 2]),
+        (3, 2, 0, [0, 1]),
+        (3, 2, 1, [2]),
+        (3, 1, 0, [0]),
+        (3, 1, 1, [1]),
+        (3, 1, 2, [2]),
+        (2, 2, 0, [0, 1]),
+        (2, 1, 0, [0]),
+        (2, 1, 1, [1]),
+        (1, 2, 0, [0]),
+        (0, 2, 0, []),
+        (0, 10, 0, []),
+    ],
 )
-def test_get_page_valid(page: int, expected_data: List[int]) -> None:
-    assert Paginator(list(range(0, 17)), 5).get_page(page) == expected_data
+def test_get_page_valid(length: int, page: int, page_size: int, expected_data: List[int]) -> None:
+    assert Paginator(list(range(0, length)), page_size).get_page(page) == expected_data
 
 
 @pytest.mark.parametrize("page", [-1000, -10, -1, 5, 10, 1000])
