2212e99cc437907d4bc3c7d94e1d3d946470c9a2
Jeff <jeff@chia.net>
2022-04-29 11:45:22 -0700

    Move offer sources into packages/gui (#708)
    * First stab at moving Offers sources into packages/gui
    
    * Update package-lock.json
    
    * More fixes after moving offers to packages/gui

--- packages/gui/src/components/app/AppRouter.tsx
+++ packages/gui/src/components/app/AppRouter.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { SelectKey, LayoutHero, LayoutDashboard, Mode, useMode } from '@chia/core';
-import { WalletAdd, WalletImport, Wallets, Offers, WalletStatusHeader } from '@chia/wallets';
+import {
+  SelectKey,
+  LayoutHero,
+  LayoutDashboard,
+  Mode,
+  useMode,
+} from '@chia/core';
+import {
+  WalletAdd,
+  WalletImport,
+  Wallets,
+  WalletStatusHeader,
+} from '@chia/wallets';
 import AppProviders from './AppProviders';
 import FullNode from '../fullNode/FullNode';
 import Block from '../block/Block';
@@ -10,6 +21,7 @@ import Plot from '../plot/Plot';
 import Farm from '../farm/Farm';
 import Pool from '../pool/Pool';
 import NFTs from '../nfts/NFTs';
+import { CreateOffer } from '../offers/OfferManager';
 import DashboardSideBar from '../dashboard/DashboardSideBar';
 import SettingsPanel from '../settings/SettingsPanel';
 
@@ -23,43 +35,45 @@ export default function AppRouter() {
           <Route element={<LayoutHero settings={<SettingsPanel />} outlet />}>
             <Route index element={<SelectKey />} />
           </Route>
-          <Route element={<LayoutHero settings={<SettingsPanel />} back outlet />}>
+          <Route
+            element={<LayoutHero settings={<SettingsPanel />} back outlet />}
+          >
             <Route path="wallet/add" element={<WalletAdd />} />
             <Route path="wallet/import" element={<WalletImport />} />
           </Route>
           {mode === Mode.WALLET ? (
             <Route
-              element={(
+              element={
                 <LayoutDashboard
                   settings={<SettingsPanel />}
                   sidebar={<DashboardSideBar simple />}
                   actions={<WalletStatusHeader />}
                   outlet
                 />
-              )}
+              }
             >
               <Route path="dashboard" element={<Navigate to="wallets" />} />
               <Route path="dashboard/wallets/*" element={<Wallets />} />
-              <Route path="dashboard/offers/*" element={<Offers />} />
+              <Route path="dashboard/offers/*" element={<CreateOffer />} />
               <Route path="dashboard/nfts/*" element={<NFTs />} />
               <Route path="dashboard/*" element={<Navigate to="wallets" />} />
               <Route path="dashboard/settings/*" element={<Settings />} />
             </Route>
           ) : (
             <Route
-              element={(
+              element={
                 <LayoutDashboard
                   settings={<SettingsPanel />}
                   sidebar={<DashboardSideBar />}
                   actions={<WalletStatusHeader />}
                   outlet
                 />
-              )}
+              }
             >
               <Route path="dashboard" element={<FullNode />} />
               <Route path="dashboard/block/:headerHash" element={<Block />} />
               <Route path="dashboard/wallets/*" element={<Wallets />} />
-              <Route path="dashboard/offers/*" element={<Offers />} />
+              <Route path="dashboard/offers/*" element={<CreateOffer />} />
               <Route path="dashboard/nfts/*" element={<NFTs />} />
               <Route path="dashboard/settings/*" element={<Settings />} />
               <Route path="dashboard/plot/*" element={<Plot />} />
--- packages/wallets/src/components/index.ts
+++ packages/wallets/src/components/index.ts
@@ -6,4 +6,3 @@ export { default as WalletImport } from './WalletImport';
 export { default as Wallets } from './Wallets';
 export { default as WalletStatus } from './WalletStatus';
 export { default as WalletStatusHeader } from './WalletStatusHeader';
-export { CreateOffer as Offers } from './offers/OfferManager';
