32abaa4a58bb3e58f67a563613607fab8ca708c9
arty <art.yerkes@gmail.com>
2022-05-03 08:15:35 -0700

    flake8

--- tests/clvm/test_clvm_step.py
+++ tests/clvm/test_clvm_step.py
@@ -1,16 +1,17 @@
 from unittest import TestCase
-
-import json
 from clvm_tools_rs import start_clvm_program
 
 factorial = "ff02ffff01ff02ff02ffff04ff02ffff04ff05ff80808080ffff04ffff01ff02ffff03ffff09ff05ffff010180ffff01ff0101ffff01ff12ff05ffff02ff02ffff04ff02ffff04ffff11ff05ffff010180ff808080808080ff0180ff018080"
 
 
+factorial_sym = {
+    "de3687023fa0a095d65396f59415a859dd46fc84ed00504bf4c9724fca08c9de": "factorial"
+}
+
+
 class TestRunProgram(TestCase):
     def test_simple_program_run(self):
-        p = start_clvm_program(
-            factorial, "ff0580", {"de3687023fa0a095d65396f59415a859dd46fc84ed00504bf4c9724fca08c9de":"factorial"}
-        )
+        p = start_clvm_program(factorial, "ff0580", factorial_sym)
 
         last = None
         location = None
