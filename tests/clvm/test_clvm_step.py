df09b9b96b30d41603d4bc0e3c7f5ea36e9589fc
arty <art.yerkes@gmail.com>
2022-05-03 08:30:38 -0700

    lint

--- tests/clvm/test_clvm_step.py
+++ tests/clvm/test_clvm_step.py
@@ -4,9 +4,7 @@ from clvm_tools_rs import start_clvm_program
 factorial = "ff02ffff01ff02ff02ffff04ff02ffff04ff05ff80808080ffff04ffff01ff02ffff03ffff09ff05ffff010180ffff01ff0101ffff01ff12ff05ffff02ff02ffff04ff02ffff04ffff11ff05ffff010180ff808080808080ff0180ff018080"
 
 
-factorial_sym = {
-    "de3687023fa0a095d65396f59415a859dd46fc84ed00504bf4c9724fca08c9de": "factorial"
-}
+factorial_sym = {"de3687023fa0a095d65396f59415a859dd46fc84ed00504bf4c9724fca08c9de": "factorial"}
 
 
 class TestRunProgram(TestCase):
