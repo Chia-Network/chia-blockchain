42a71277fe95e15345a995dee98c1e4bb50a4f3c
matt-o-how <matt@chia.net>
2022-05-09 16:32:15 +0100

    initial commit for new state layer and added clvm tests

--- tests/wallet/nft_wallet/test_nft_clvm.py
+++ tests/wallet/nft_wallet/test_nft_clvm.py
@@ -28,14 +28,17 @@ SINGLETON_MOD = load_clvm("singleton_top_layer.clvm")
 LAUNCHER_PUZZLE = load_clvm("singleton_launcher.clvm")
 DID_MOD = load_clvm("did_innerpuz.clvm")
 NFT_STATE_LAYER_MOD = load_clvm("nft_state_layer.clvm")
+# NFT_OWNERSHIP_LAYER = load_clvm("nft_ownership_layer.clvm")
+# NFT_TRANSFER_PROGRAM_DEFAULT = load_clvm("nft_ownership_transfer_program_one_way_claim.clvm")
+NFT_INNER_INNERPUZ = load_clvm("nft_v1_innerpuz.clvm")
 STANDARD_PUZZLE_MOD = load_clvm("p2_delegated_puzzle_or_hidden_puzzle.clvm")
 LAUNCHER_PUZZLE_HASH = LAUNCHER_PUZZLE.get_tree_hash()
 NFT_STATE_LAYER_MOD_HASH = NFT_STATE_LAYER_MOD.get_tree_hash()
 SINGLETON_MOD_HASH = SINGLETON_MOD.get_tree_hash()
 
 LAUNCHER_ID = Program.to(b"launcher-id").get_tree_hash()
-NFT_METADATA_UPDATER = load_clvm("nft_metadata_updater.clvm")
-
+NFT_METADATA_UPDATER_DEFAULT = load_clvm("nft_metadata_updater_default.clvm")
+NFT_METADATA_UPDATER_UPDATEABLE = load_clvm("nft_metadata_updater_updateable.clvm")
 
 def test_new_nft_ownership_layer() -> None:
     pubkey = int_to_public_key(1)
@@ -51,7 +54,7 @@ def test_new_nft_ownership_layer() -> None:
         [
             NFT_STATE_LAYER_MOD_HASH,
             metadata,
-            NFT_METADATA_UPDATER.get_tree_hash(),
+            NFT_METADATA_UPDATER_DEFAULT.get_tree_hash(),
             innerpuz,
             # below here is the solution
             solution_for_conditions(condition_list),
@@ -64,7 +67,7 @@ def test_new_nft_ownership_layer() -> None:
     assert res.first().first().as_int() == 73
     assert res.first().rest().first().as_int() == 1
     assert res.rest().rest().first().first().as_int() == 51
-    assert res.rest().rest().first().rest().first().as_atom() == NFT_STATE_LAYER_MOD.curry(NFT_STATE_LAYER_MOD_HASH, metadata, NFT_METADATA_UPDATER.get_tree_hash(), destination).get_tree_hash()
+    assert res.rest().rest().first().rest().first().as_atom() == NFT_STATE_LAYER_MOD.curry(NFT_STATE_LAYER_MOD_HASH, metadata, NFT_METADATA_UPDATER_DEFAULT.get_tree_hash(), destination).get_tree_hash()
 
 
 def test_update_metadata() -> None:
@@ -73,7 +76,8 @@ def test_update_metadata() -> None:
     my_amount = 1
     destination: bytes32 = puzzle_for_pk(int_to_public_key(2))
     condition_list = [make_create_coin_condition(destination.get_tree_hash(), my_amount, [])]
-    condition_list.append([-24, NFT_METADATA_UPDATER, "https://www.chia.net/img/branding/chia-logo-2.svg"])
+    condition_list.append([-24, NFT_METADATA_UPDATER_DEFAULT, "https://www.chia.net/img/branding/chia-logo-2.svg"])
+    condition_list.append([-70, NFT_METADATA_UPDATER_DEFAULT, "https://www.chia.net/img/branding/chia-logo-2.svg"])
     metadata = [
         ("u", ["https://www.chia.net/img/branding/chia-logo.svg"]),
         ("h", 0xD4584AD463139FA8C0D9F68F4B59F185),
@@ -82,7 +86,7 @@ def test_update_metadata() -> None:
         [
             NFT_STATE_LAYER_MOD_HASH,
             metadata,
-            NFT_METADATA_UPDATER.get_tree_hash(),
+            NFT_METADATA_UPDATER_DEFAULT.get_tree_hash(),
             innerpuz,
             # below here is the solution
             solution_for_conditions(condition_list),
@@ -97,7 +101,45 @@ def test_update_metadata() -> None:
     ]
 
     cost, res = NFT_STATE_LAYER_MOD.run_with_cost(INFINITE_COST, solution)
+    assert len(res.as_python()) == 3  # check that the negative conditions have been filtered out
     assert res.first().first().as_int() == 73
     assert res.first().rest().first().as_int() == 1
     assert res.rest().rest().first().first().as_int() == 51
-    assert res.rest().rest().first().rest().first().as_atom() == NFT_STATE_LAYER_MOD.curry(NFT_STATE_LAYER_MOD_HASH, metadata, NFT_METADATA_UPDATER.get_tree_hash(), destination).get_tree_hash()
+    assert res.rest().rest().first().rest().first().as_atom() == NFT_STATE_LAYER_MOD.curry(NFT_STATE_LAYER_MOD_HASH, metadata, NFT_METADATA_UPDATER_DEFAULT.get_tree_hash(), destination).get_tree_hash()
+
+
+def test_update_metadata_updater() -> None:
+    pubkey = int_to_public_key(1)
+    innerpuz = puzzle_for_pk(pubkey)
+    my_amount = 1
+    destination: bytes32 = puzzle_for_pk(int_to_public_key(2))
+    condition_list = [make_create_coin_condition(destination.get_tree_hash(), my_amount, [])]
+    condition_list.append([-24, NFT_METADATA_UPDATER_UPDATEABLE, ["test", NFT_METADATA_UPDATER_DEFAULT.get_tree_hash()]])
+    metadata = [
+        ("u", ["https://www.chia.net/img/branding/chia-logo.svg"]),
+        ("h", 0xD4584AD463139FA8C0D9F68F4B59F185),
+    ]
+    solution = Program.to(
+        [
+            NFT_STATE_LAYER_MOD_HASH,
+            metadata,
+            NFT_METADATA_UPDATER_UPDATEABLE.get_tree_hash(),
+            innerpuz,
+            # below here is the solution
+            solution_for_conditions(condition_list),
+            my_amount,
+            0,
+        ]
+    )
+
+    metadata = [
+        ("u", ["test", "https://www.chia.net/img/branding/chia-logo.svg"]),
+        ("h", 0xD4584AD463139FA8C0D9F68F4B59F185),
+    ]
+
+    cost, res = NFT_STATE_LAYER_MOD.run_with_cost(INFINITE_COST, solution)
+    assert len(res.as_python()) == 3  # check that the negative conditions have been filtered out
+    assert res.first().first().as_int() == 73
+    assert res.first().rest().first().as_int() == 1
+    assert res.rest().rest().first().first().as_int() == 51
+    assert res.rest().rest().first().rest().first().as_atom() == NFT_STATE_LAYER_MOD.curry(NFT_STATE_LAYER_MOD_HASH, metadata, NFT_METADATA_UPDATER_DEFAULT.get_tree_hash(), destination).get_tree_hash()
