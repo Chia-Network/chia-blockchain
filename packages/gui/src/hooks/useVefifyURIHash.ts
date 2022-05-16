e9767f52e202284e0c9130212f9938771207b047
Zlatko Fedor <zlatko@chia.net>
2022-05-16 22:12:39 +0100

    added cache for file hash

--- packages/gui/src/hooks/useVefifyURIHash.ts
+++ packages/gui/src/hooks/useVefifyURIHash.ts
@@ -3,7 +3,10 @@ import isURL from 'validator/lib/isURL';
 import isContentHashValid from '../util/isContentHashValid';
 import getRemoteFileContent from '../util/getRemoteFileContent';
 
+const CACHE_SIZE = 1000;
 const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10 MB
+
+const cache = new Map<string, boolean>();
 
 export default function useVerifyURIHash(uri: string, hash: string): {
   isValid: boolean;
@@ -20,18 +23,32 @@ export default function useVerifyURIHash(uri: string, hash: string): {
       setIsLoading(true);
       setIsValid(false);
 
-      if (!isURL(uri)) {
-        throw new Error('Invalid URI');
-      }
-
-      if (uri) {
-        const content = await getRemoteFileContent(uri, MAX_FILE_SIZE);
-        const isHashValid = isContentHashValid(content, hash);
-        if (!isHashValid) {
-          throw new Error(`Hash mismatch`);
+      // get cached value
+      const cacheKey = `${hash}#${uri}`;
+      const isValid = cache.get(cacheKey);
+      if (isValid) {
+        setIsValid(true);
+      } else {
+        if (!isURL(uri)) {
+          throw new Error('Invalid URI');
         }
 
-        setIsValid(true);
+        if (uri) {
+          const content = await getRemoteFileContent(uri, MAX_FILE_SIZE);
+          const isHashValid = isContentHashValid(content, hash);
+          if (!isHashValid) {
+            throw new Error(`Hash mismatch`);
+          }
+
+          setIsValid(true);
+          cache.set(cacheKey, true);
+
+          // remove oldest cache entry
+          if (cache.size > CACHE_SIZE) {
+            const [key] = cache.keys();
+            cache.delete(key);
+          }
+        }
       }
     } catch (e: any) {
       setError(e);
