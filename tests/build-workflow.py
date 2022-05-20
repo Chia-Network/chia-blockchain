713059976fa5d00f367a664e88cff0904572092c
Kyle Altendorf <sda@fstab.net>
2022-05-19 23:11:08 -0400

    maybe

--- tests/build-workflow.py
+++ tests/build-workflow.py
@@ -1,10 +1,70 @@
+from collections import OrderedDict
 import copy
 from pathlib import Path
 
 import yaml
+from yaml.composer import Composer
+from yaml.constructor import BaseConstructor, Constructor, FullConstructor, SafeConstructor
+from yaml.parser import Parser
+from yaml.reader import Reader
+from yaml.resolver import BaseResolver, Resolver
+from yaml.scanner import Scanner
+
+
+# remove resolver entries for On/Off/Yes/No
+for ch in "OoYyNn":
+    if len(Resolver.yaml_implicit_resolvers[ch]) == 1:
+        del Resolver.yaml_implicit_resolvers[ch]
+    else:
+        Resolver.yaml_implicit_resolvers[ch] = [x for x in
+                Resolver.yaml_implicit_resolvers[ch] if x[0] != 'tag:yaml.org,2002:bool']
 
 
 here = Path(__file__).parent
+
+
+def bool_constructor(self, node):
+    print(node)
+    return self.construct_scalar(node)
+
+Constructor.add_constructor(u'tag:yaml.org,2002:bool', bool_constructor)
+
+# class OurConstructor(yaml.constructor.Constructor):
+#     def __init__(self, *args, **kwargs):
+#         # super().__init__(*args, **kwargs)
+#
+#         self.add_constructor(u'tag:yaml.org,2002:bool', bool_constructor)
+
+
+# class Loader(Reader, Scanner, Parser, Composer, OurConstructor, Resolver):
+#     def __init__(self, stream) -> None: ...
+#     # def __init__(self, stream: yaml._ReadStream) -> None: ...
+
+
+def ordered_dict_representer(dumper, data):
+    return dumper.represent_mapping('tag:yaml.org,2002:map', data.items())
+
+
+def str_representer(dumper, data):
+    if '\n' in data:
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style="")
+
+
+class TidyOrderedDictDumper(yaml.Dumper):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.add_representer(
+            OrderedDict,
+            ordered_dict_representer,
+        )
+
+        self.add_representer(
+            str,
+            str_representer,
+        )
 
 
 def main():
@@ -13,6 +73,7 @@ def main():
 
     with source_path.open() as file:
         source = yaml.safe_load(stream=file)
+        # source = yaml.load(stream=file, Loader=Loader)
 
     output = copy.deepcopy(source)
     del output["jobs"]["test"]
@@ -25,7 +86,12 @@ def main():
         output["jobs"][f"test_{os_entry['matrix']}"] = d
 
     with output_path.open("w") as file:
-        yaml.safe_dump(data=output, stream=file)
+        yaml.dump(
+            data=output,
+            stream=file,
+            Dumper=TidyOrderedDictDumper,
+            sort_keys=False,
+        )
 
 
 main()
