c4d84de55ea25414ad78cb9eb84676a29571b7a8
Jack Nelson <jack@jacknelson.xyz>
2022-05-21 17:26:48 -0400

    remove exit_node command
    Also do a lot of cleanup

--- chia/cmds/show.py
+++ chia/cmds/show.py
@@ -1,15 +1,16 @@
-from typing import Any, Callable, Dict, Optional, Union
+from typing import Any, Callable, Dict, Optional, Union, List
+import time
 
 import click
 
-from chia.rpc.full_node_rpc_client import FullNodeRpcClient
 from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.util.bech32m import encode_puzzle_hash
-from chia.util.ints import uint64
-from chia.util.network import is_trusted_inner
+from chia.consensus.block_record import BlockRecord
+from chia.server.outbound_message import NodeType
 
 
-async def print_connections(node_client, time, NodeType, trusted_peers: Dict):
+async def print_connections(node_client, trusted_peers: Dict):
+    from chia.util.network import is_trusted_inner
+
     connections = await node_client.get_connections()
     print("Connections:")
     print("Type      IP                                     Ports       NodeID      Last Connect" + "      MiB Up|Dwn")
@@ -60,11 +61,95 @@ async def print_connections(node_client, time, NodeType, trusted_peers: Dict):
         print(con_str)
 
 
+async def print_blockchain_state(node_client, config: Dict):
+    # node_client is FullNodeRpcClient
+    from chia.util.misc import format_bytes
+    from chia.util.ints import uint64
+
+    blockchain_state = await node_client.get_blockchain_state()
+    if blockchain_state is None:
+        print("There is no blockchain found yet. Try again shortly")
+        return None
+    peak: Optional[BlockRecord] = blockchain_state["peak"]
+    node_id = blockchain_state["node_id"]
+    difficulty = blockchain_state["difficulty"]
+    sub_slot_iters = blockchain_state["sub_slot_iters"]
+    synced = blockchain_state["sync"]["synced"]
+    sync_mode = blockchain_state["sync"]["sync_mode"]
+    total_iters = peak.total_iters if peak is not None else 0
+    num_blocks: int = 10
+    network_name = config["selected_network"]
+    genesis_challenge = config["farmer"]["network_overrides"]["constants"][network_name]["GENESIS_CHALLENGE"]
+    full_node_port = config["full_node"]["port"]
+    full_node_rpc_port = config["full_node"]["rpc_port"]
+
+    print(f"Network: {network_name}    Port: {full_node_port}   RPC Port: {full_node_rpc_port}")
+    print(f"Node ID: {node_id}")
+    print(f"Genesis Challenge: {genesis_challenge}")
+
+    if synced:
+        print("Current Blockchain Status: Full Node Synced")
+        print("\nPeak: Hash:", peak.header_hash if peak is not None else "")
+    elif peak is not None and sync_mode:
+        sync_max_block = blockchain_state["sync"]["sync_tip_height"]
+        sync_current_block = blockchain_state["sync"]["sync_progress_height"]
+        print(
+            f"Current Blockchain Status: Syncing {sync_current_block}/{sync_max_block} "
+            f"({sync_max_block - sync_current_block} behind)."
+        )
+        print("Peak: Hash:", peak.header_hash if peak is not None else "")
+    elif peak is not None:
+        print(f"Current Blockchain Status: Not Synced. Peak height: {peak.height}")
+    else:
+        print("\nSearching for an initial chain\n")
+        print("You may be able to expedite with 'chia show -a host:port' using a known node.\n")
+
+    if peak is not None:
+        if peak.is_transaction_block:
+            peak_time = peak.timestamp
+        else:
+            peak_hash = peak.header_hash
+            curr = await node_client.get_block_record(peak_hash)
+            while curr is not None and not curr.is_transaction_block:
+                curr = await node_client.get_block_record(curr.prev_hash)
+            if curr is not None:
+                peak_time = curr.timestamp
+            else:
+                peak_time = uint64(0)
+        peak_time_struct = time.struct_time(time.localtime(peak_time))
+
+        print(
+            "      Time:",
+            f"{time.strftime('%a %b %d %Y %T %Z', peak_time_struct)}",
+            f"                 Height: {peak.height:>10}\n",
+        )
+
+        print("Estimated network space: ", end="")
+        print(format_bytes(blockchain_state["space"]))
+        print(f"Current difficulty: {difficulty}")
+        print(f"Current VDF sub_slot_iters: {sub_slot_iters}")
+        print("Total iterations since the start of the blockchain:", total_iters)
+        print("")
+        print("  Height: |   Hash:")
+
+        added_blocks: List[BlockRecord] = []
+        curr = await node_client.get_block_record(peak.header_hash)
+        while curr is not None and len(added_blocks) < num_blocks and curr.height > 0:
+            added_blocks.append(curr)
+            curr = await node_client.get_block_record(curr.prev_hash)
+
+        for b in added_blocks:
+            print(f"{b.height:>9} | {b.header_hash}")
+    else:
+        print("Blockchain has no blocks yet")
+
+
 async def execute_with_node(rpc_port: Optional[int], function: Callable, *args):
     import traceback
 
-    import aiohttp
+    from aiohttp import ClientConnectorError
 
+    from chia.rpc.full_node_rpc_client import FullNodeRpcClient
     from chia.util.config import load_config
     from chia.util.default_root import DEFAULT_ROOT_PATH
     from chia.util.ints import uint16
@@ -80,7 +165,7 @@ async def execute_with_node(rpc_port: Optional[int], function: Callable, *args):
         await function(node_client, config, *args)
 
     except Exception as e:
-        if isinstance(e, aiohttp.ClientConnectorError):
+        if isinstance(e, ClientConnectorError):
             print(f"Connection error. Check if full node rpc is running at {rpc_port}")
             print("This is normal if full node is still starting up")
         else:
@@ -92,116 +177,31 @@ async def execute_with_node(rpc_port: Optional[int], function: Callable, *args):
 
 
 async def show_async(
-    node_client: FullNodeRpcClient,
+    node_client,
     config: Dict,
     state: bool,
     show_connections: bool,
-    exit_node: bool,
     add_connection: str,
     remove_connection: str,
     block_header_hash_by_height: str,
     block_by_header_hash: str,
 ) -> None:
-    import time
-    from typing import List, Optional
 
-    from chia.consensus.block_record import BlockRecord
-    from chia.server.outbound_message import NodeType
-    from chia.types.full_block import FullBlock
-    from chia.util.byte_types import hexstr_to_bytes
-    from chia.util.misc import format_bytes
-
+    # Check State
     if state:
-        blockchain_state = await node_client.get_blockchain_state()
-        if blockchain_state is None:
-            print("There is no blockchain found yet. Try again shortly")
+        if await print_blockchain_state(node_client, config) is None:
             return None
-        peak: Optional[BlockRecord] = blockchain_state["peak"]
-        node_id = blockchain_state["node_id"]
-        difficulty = blockchain_state["difficulty"]
-        sub_slot_iters = blockchain_state["sub_slot_iters"]
-        synced = blockchain_state["sync"]["synced"]
-        sync_mode = blockchain_state["sync"]["sync_mode"]
-        total_iters = peak.total_iters if peak is not None else 0
-        num_blocks: int = 10
-        network_name = config["selected_network"]
-        genesis_challenge = config["farmer"]["network_overrides"]["constants"][network_name]["GENESIS_CHALLENGE"]
-        full_node_port = config["full_node"]["port"]
-        full_node_rpc_port = config["full_node"]["rpc_port"]
-
-        print(f"Network: {network_name}    Port: {full_node_port}   RPC Port: {full_node_rpc_port}")
-        print(f"Node ID: {node_id}")
-
-        print(f"Genesis Challenge: {genesis_challenge}")
-
-        if synced:
-            print("Current Blockchain Status: Full Node Synced")
-            print("\nPeak: Hash:", peak.header_hash if peak is not None else "")
-        elif peak is not None and sync_mode:
-            sync_max_block = blockchain_state["sync"]["sync_tip_height"]
-            sync_current_block = blockchain_state["sync"]["sync_progress_height"]
-            print(
-                f"Current Blockchain Status: Syncing {sync_current_block}/{sync_max_block} "
-                f"({sync_max_block - sync_current_block} behind)."
-            )
-            print("Peak: Hash:", peak.header_hash if peak is not None else "")
-        elif peak is not None:
-            print(f"Current Blockchain Status: Not Synced. Peak height: {peak.height}")
-        else:
-            print("\nSearching for an initial chain\n")
-            print("You may be able to expedite with 'chia show -a host:port' using a known node.\n")
-
-        if peak is not None:
-            if peak.is_transaction_block:
-                peak_time = peak.timestamp
-            else:
-                peak_hash = peak.header_hash
-                curr = await node_client.get_block_record(peak_hash)
-                while curr is not None and not curr.is_transaction_block:
-                    curr = await node_client.get_block_record(curr.prev_hash)
-                if curr is not None:
-                    peak_time = curr.timestamp
-                else:
-                    peak_time = uint64(0)
-            peak_time_struct = time.struct_time(time.localtime(peak_time))
-
-            print(
-                "      Time:",
-                f"{time.strftime('%a %b %d %Y %T %Z', peak_time_struct)}",
-                f"                 Height: {peak.height:>10}\n",
-            )
-
-            print("Estimated network space: ", end="")
-            print(format_bytes(blockchain_state["space"]))
-            print(f"Current difficulty: {difficulty}")
-            print(f"Current VDF sub_slot_iters: {sub_slot_iters}")
-            print("Total iterations since the start of the blockchain:", total_iters)
-            print("")
-            print("  Height: |   Hash:")
-
-            added_blocks: List[BlockRecord] = []
-            curr = await node_client.get_block_record(peak.header_hash)
-            while curr is not None and len(added_blocks) < num_blocks and curr.height > 0:
-                added_blocks.append(curr)
-                curr = await node_client.get_block_record(curr.prev_hash)
-
-            for b in added_blocks:
-                print(f"{b.height:>9} | {b.header_hash}")
-        else:
-            print("Blockchain has no blocks yet")
-
         # if called together with show_connections, leave a blank line
         if show_connections:
             print("")
+
+    # Check or edit node connections
     if show_connections:
         trusted_peers: Dict = config["full_node"].get("trusted_peers", {})
-        await print_connections(node_client, time, NodeType, trusted_peers)
+        await print_connections(node_client, trusted_peers)
         # if called together with state, leave a blank line
         if state:
             print("")
-    if exit_node:
-        node_stop = await node_client.stop_node()
-        print(node_stop, "Node stopped")
     if add_connection:
         if ":" not in add_connection:
             print("Enter a valid IP and port in the following format: 10.5.4.3:8000")
@@ -234,6 +234,8 @@ async def show_async(
                 elif result_txt == "":
                     result_txt = f"NodeID {remove_connection}... not found"
         print(result_txt)
+
+        # Get Block Information
     if block_header_hash_by_height != "":
         block_header = await node_client.get_block_record_by_height(block_header_hash_by_height)
         if block_header is not None:
@@ -241,6 +243,10 @@ async def show_async(
         else:
             print("Block height", block_header_hash_by_height, "not found")
     if block_by_header_hash != "":
+        from chia.types.full_block import FullBlock
+        from chia.util.byte_types import hexstr_to_bytes
+        from chia.util.bech32m import encode_puzzle_hash
+
         block: Optional[BlockRecord] = await node_client.get_block_record(hexstr_to_bytes(block_by_header_hash))
         full_block: Optional[FullBlock] = await node_client.get_block(hexstr_to_bytes(block_by_header_hash))
         # Would like to have a verbose flag for this
@@ -323,7 +329,6 @@ async def show_async(
 @click.option(
     "-c", "--connections", help="List nodes connected to this Full Node", is_flag=True, type=bool, default=False
 )
-@click.option("-e", "--exit-node", help="Shut down the running Full Node", is_flag=True, default=False)
 @click.option("-a", "--add-connection", help="Connect to another Full Node by ip:port", type=str, default="")
 @click.option(
     "-r", "--remove-connection", help="Remove a Node by the first 8 characters of NodeID", type=str, default=""
@@ -337,7 +342,6 @@ def show_cmd(
     wallet_rpc_port: Optional[int],
     state: bool,
     connections: bool,
-    exit_node: bool,
     add_connection: str,
     remove_connection: str,
     block_header_hash_by_height: str,
@@ -351,7 +355,6 @@ def show_cmd(
             show_async,
             state,
             connections,
-            exit_node,
             add_connection,
             remove_connection,
             block_header_hash_by_height,
