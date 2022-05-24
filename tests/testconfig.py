2d1aa8b1338da2581f1f53a09bfb0c704cd560c6
Kyle Altendorf <sda@fstab.net>
2022-05-24 13:21:06 -0400

    Update testconfig.py

--- tests/testconfig.py
+++ tests/testconfig.py
@@ -5,8 +5,9 @@ from typing import TYPE_CHECKING, List, Union
 if TYPE_CHECKING:
     from typing_extensions import Literal
 
+Oses = Literal["macos", "ubuntu", "windows"]
 # Github actions template config.
-oses = ["ubuntu", "macos", "windows"]
+oses: List[Oses] = ["ubuntu", "macos", "windows"]
 
 # Defaults are conservative.
 parallel: Union[bool, int, Literal["auto"]] = False
@@ -15,3 +16,4 @@ install_timelord = False
 check_resource_usage = False
 job_timeout = 30
 custom_vars: List[str] = []
+os_skip: List[Oses] = []
