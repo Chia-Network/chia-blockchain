21d6cd40542f7d9c10101a600dcbeeb1c5ede492
Kyle Altendorf <sda@fstab.net>
2022-05-25 21:33:16 -0400

    Merge pull request #11640 from Chia-Network/atari-hint_row_more

--- chia/data_layer/data_layer_types.py
+++ chia/data_layer/data_layer_types.py
@@ -53,8 +53,10 @@ class TerminalNode:
     def pair(self) -> Tuple[bytes32, bytes32]:
         return Program.to(self.key), Program.to(self.value)
 
+    # It is unclear how to properly satisfy the generic Row normally, let alone for
+    # dict-like rows.
     @classmethod
-    def from_row(cls, row: aiosqlite.Row) -> "TerminalNode":
+    def from_row(cls, row: aiosqlite.Row) -> "TerminalNode":  # type: ignore[type-arg]
         return cls(
             hash=bytes32.fromhex(row["hash"]),
             # generation=row["generation"],
@@ -114,8 +116,10 @@ class InternalNode:
     pair: Optional[Tuple[Node, Node]] = None
     atom: None = None
 
+    # It is unclear how to properly satisfy the generic Row normally, let alone for
+    # dict-like rows.
     @classmethod
-    def from_row(cls, row: aiosqlite.Row) -> "InternalNode":
+    def from_row(cls, row: aiosqlite.Row) -> "InternalNode":  # type: ignore[type-arg]
         return cls(
             hash=bytes32(hexstr_to_bytes(row["hash"])),
             # generation=row["generation"],
@@ -149,8 +153,10 @@ class Root:
     generation: int
     status: Status
 
+    # It is unclear how to properly satisfy the generic Row normally, let alone for
+    # dict-like rows.
     @classmethod
-    def from_row(cls, row: aiosqlite.Row) -> "Root":
+    def from_row(cls, row: aiosqlite.Row) -> "Root":  # type: ignore[type-arg]
         raw_node_hash = row["node_hash"]
         if raw_node_hash is None:
             node_hash = None
