2e03a608c204412add3f38b0fc5078aaf04b9c8f
Kyle Altendorf <sda@fstab.net>
2022-05-29 09:31:07 -0400

    zstd==1.5.2.5 for wheels on PyPI

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ dependencies = [
     "watchdog==2.1.7",  # Filesystem event watching - watches keyring.yaml
     "dnslib==0.9.17",  # dns lib
     "typing-extensions==4.0.1",  # typing backports like Protocol and TypedDict
-    "zstd==1.5.0.4",
+    "zstd==1.5.2.5",
     "packaging==21.0",
 ]
 
