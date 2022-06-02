2ec5c767be87dab47604a94ef8c19cc0aa4637d1
Matt Hauff <quexington@gmail.com>
2022-06-02 15:09:57 -0700

    Create ownership outer puzzle

--- chia/wallet/outer_puzzles.py
+++ chia/wallet/outer_puzzles.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, Optional
 from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.wallet.cat_wallet.cat_outer_puzzle import CATOuterPuzzle
+from chia.wallet.nft_wallet.ownership_outer_puzzle import OwnershipOuterPuzzle
 from chia.wallet.nft_wallet.metadata_outer_puzzle import MetadataOuterPuzzle
 from chia.wallet.nft_wallet.singleton_outer_puzzle import SingletonOuterPuzzle
 from chia.wallet.puzzle_drivers import PuzzleInfo, Solver
@@ -32,6 +33,7 @@ class AssetType(Enum):
     CAT = "CAT"
     SINGLETON = "singleton"
     METADATA = "metadata"
+    OWNERSHIP = "ownership"
 
 
 def match_puzzle(puzzle: Program) -> Optional[PuzzleInfo]:
@@ -66,4 +68,5 @@ driver_lookup: Dict[AssetType, Any] = {
     AssetType.CAT: CATOuterPuzzle(*function_args),
     AssetType.SINGLETON: SingletonOuterPuzzle(*function_args),
     AssetType.METADATA: MetadataOuterPuzzle(*function_args),
+    AssetType.OWNERSHIP: OwnershipOuterPuzzle(*function_args),
 }
