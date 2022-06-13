e0d8ce3aa83a104a4ec83ebfa196d82f6e9e89f7
Kyle Altendorf <sda@fstab.net>
2022-06-13 12:26:28 -0400

    make example runnable via pytest

--- maybe404.py
+++ maybe404.py
@@ -1,3 +1,5 @@
+import string
+
 import asyncio
 import aiohttp
 import aiohttp.web
@@ -38,4 +40,86 @@ def main():
         asyncio.run(async_main())
 
 
-main()
+if __name__ == "__main__":
+    main()
+else:
+    import pytest
+
+    import pytest_asyncio
+
+
+    @pytest_asyncio.fixture()
+    async def port():
+        print(" ==== running")
+        app = aiohttp.web.Application()
+        app.add_routes(
+            [
+                aiohttp.web.post('/', handle),
+                *[
+                    aiohttp.web.post(f"/{c}", handle)
+                    for c in string.ascii_letters
+                ],
+            ],
+        )
+
+        runner = aiohttp.web.AppRunner(app)
+        await runner.setup()
+        try:
+            host = "0.0.0.0"
+
+            site = aiohttp.web.TCPSite(runner=runner, host=host, port=0)
+            await site.start()
+            [[interface, port]] = runner.addresses
+            print(f" ==== listening on port {port}")
+            await request_and_assert(port=port)
+            try:
+                yield port
+            finally:
+                await site.stop()
+        finally:
+            await runner.cleanup()
+
+
+    @pytest_asyncio.fixture()
+    async def another():
+        print(" ==== running")
+        app = aiohttp.web.Application()
+        app.add_routes(
+            [
+                # aiohttp.web.post('/', handle),
+                *[
+                    aiohttp.web.post(f"/{c}", handle)
+                    for c in string.ascii_letters
+                ],
+            ],
+        )
+
+        runner = aiohttp.web.AppRunner(app)
+        await runner.setup()
+        try:
+            host = "0.0.0.0"
+
+            site = aiohttp.web.TCPSite(runner=runner, host=host, port=0)
+            await site.start()
+            [[interface, port]] = runner.addresses
+            print(f" ==== listening on port {port}")
+            await request_and_assert(port=port, route="/a")
+            try:
+                yield port
+            finally:
+                await site.stop()
+        finally:
+            await runner.cleanup()
+
+
+    async def request_and_assert(port, route="/"):
+        async with aiohttp.ClientSession(raise_for_status=True) as session:
+            async with session.post(f"http://localhost:{port}{route}", json={}) as response:
+                text = await response.text()
+                assert text == "Heya", repr(text)
+
+    @pytest.mark.parametrize(argnames="index", argvalues=range(11))
+    @pytest.mark.asyncio()
+    async def test_it(port, index, another):
+        print(f" ==== running {port=} {another=}")
+        await request_and_assert(port=port)
