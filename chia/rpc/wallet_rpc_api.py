8b4c4abcb538332217f32321893b93e4999e60c6
William Blanke <wjb98672@gmail.com>
2022-06-13 16:15:08 -0700

    added try/except for cannot start a transaction within a transaction

--- chia/rpc/wallet_rpc_api.py
+++ chia/rpc/wallet_rpc_api.py
@@ -819,11 +819,17 @@ class WalletRpcApi:
 
         async with self.service.wallet_state_manager.lock:
             async with self.service.wallet_state_manager.tx_store.db_wrapper.lock:
-                await self.service.wallet_state_manager.tx_store.db_wrapper.begin_transaction()
-                await self.service.wallet_state_manager.tx_store.delete_unconfirmed_transactions(wallet_id)
-                if self.service.wallet_state_manager.wallets[wallet_id].type() == WalletType.POOLING_WALLET.value:
-                    self.service.wallet_state_manager.wallets[wallet_id].target_state = None
-                await self.service.wallet_state_manager.tx_store.db_wrapper.commit_transaction()
+                try:
+                    await self.service.wallet_state_manager.tx_store.db_wrapper.begin_transaction()
+                    await self.service.wallet_state_manager.tx_store.delete_unconfirmed_transactions(wallet_id)
+                    if self.service.wallet_state_manager.wallets[wallet_id].type() == WalletType.POOLING_WALLET.value:
+                        self.service.wallet_state_manager.wallets[wallet_id].target_state = None
+                    await self.service.wallet_state_manager.tx_store.db_wrapper.commit_transaction()
+                except Exception:
+                    self.log.exception("Exception while delete_unconfirmed_transactions")
+                    await self.wallet_state_manager.db_wrapper.rollback_transaction()
+                    raise
+
                 # Update the cache
                 await self.service.wallet_state_manager.tx_store.rebuild_tx_cache()
                 return {}
