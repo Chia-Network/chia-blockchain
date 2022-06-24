370b9b32de5c80c73a4e17802f6b34fee744a0c0
William Blanke <wjb98672@gmail.com>
2022-06-23 21:34:40 -0700

    cc to cat

--- chia/wallet/cat_wallet/cat_utils_test.py
+++ chia/wallet/cat_wallet/cat_utils_test.py
@@ -1,5 +1,5 @@
 from chia.rpc.cat_utils import convert_to_coin_spend
-from chia.wallet.cc_wallet.cc_utils import get_parent_cat_coin_spend_lineage_proof
+from chia.wallet.cat_wallet.cat_utils import get_parent_cat_coin_spend_lineage_proof
 
 
 class Test_get_parent_cat_coin_spend_lineage_proof:
