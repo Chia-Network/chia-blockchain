27d8061d6375781b5dfc7ddbd76f83ce04a85223
Jack Nelson <jack@jacknelson.xyz>
2022-06-24 14:10:54 -0400

    fix lint

--- tests/core/test_full_node_rpc.py
+++ tests/core/test_full_node_rpc.py
@@ -197,12 +197,14 @@ class TestRpc:
                 )
                 await client.push_tx(spend_bundle)
                 coin_spends = coin_spends + spend_bundle.coin_spends
-                await time_out_assert(5, full_node_api_1.full_node.mempool_manager.get_spendbundle, spend_bundle, spend_bundle.name())
+                await time_out_assert(
+                    5, full_node_api_1.full_node.mempool_manager.get_spendbundle, spend_bundle, spend_bundle.name()
+                )
 
             await full_node_api_1.farm_new_transaction_block(FarmNewBlockProtocol(ph_2))
             block: FullBlock = (await full_node_api_1.get_all_full_blocks())[-1]
 
-            assert (len(block.transactions_generator_ref_list) > 0)  # compression has occurred
+            assert len(block.transactions_generator_ref_list) > 0  # compression has occurred
 
             block_spends = await client.get_block_spends(block.header_hash)
 
