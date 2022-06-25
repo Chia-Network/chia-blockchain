db3840197fcd580addefc22b74602499aeddf44a
Kyle Altendorf <sda@fstab.net>
2022-06-25 10:31:53 -0400

    Update activated.ps1

--- activated.ps1
+++ activated.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop"
 
 $script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-& "$script_directory/venv/Scripts/activate"
+& "$script_directory/venv/Scripts/Activate.ps1"
 & "$args"
