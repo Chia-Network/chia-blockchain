408017c036b1a2b36d6c3189917bb267cee5de32
Kyle Altendorf <sda@fstab.net>
2022-06-25 10:22:54 -0400

    & not Invoke-Expression

--- activated.ps1
+++ activated.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop"
 
 $script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-Invoke-Expression "$script_directory/venv/Scripts/activate"
-Invoke-Expression "$args"
+& "$script_directory/venv/Scripts/activate"
+& "$args"
