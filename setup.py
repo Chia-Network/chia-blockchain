5bdecea3c542ea40cc7095b0768d546a4f5edbb7
Kyle Altendorf <sda@fstab.net>
2022-06-25 09:33:41 -0400

    move py3createtorrent to development dependencies

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ dev_dependencies = [
     "build",
     "coverage",
     "pre-commit",
+    "py3createtorrent",
     "pytest",
     "pytest-asyncio>=0.18.1",  # require attribute 'fixture'
     "pytest-monitor; sys_platform == 'linux'",
