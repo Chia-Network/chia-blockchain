280809f9442c521150b3896127f15e8e4685bd84
Jack Nelson <jack@jacknelson.xyz>
2022-06-27 16:43:54 -0400

    modify api for greater block customization
    Added block type option & amount of block option

--- chia/simulator/SimulatorFullNodeRpcApi.py
+++ chia/simulator/SimulatorFullNodeRpcApi.py
@@ -9,15 +9,24 @@ from chia.util.bech32m import decode_puzzle_hash
 class SimulatorFullNodeRpcApi(FullNodeRpcApi):
     def get_routes(self) -> Dict[str, Endpoint]:
         routes = super().get_routes()
-        routes["/farm_tx_block"] = self.farm_tx_block
+        routes["/farm_block"] = self.farm_block
         routes["/auto_farm"] = self.auto_farm
         return routes
 
-    async def farm_tx_block(self, _request: Dict[str, object]) -> Dict[str, object]:
+    async def farm_block(self, _request: Dict[str, object]) -> Dict[str, object]:
         request_address = str(_request["address"])
+        block_type = str(_request.get("block_type", "tx_block"))  # tx_block or full_block
+        blocks = int(str(_request.get("blocks", 1)))  # mypy made me do this
         ph = decode_puzzle_hash(request_address)
         req = FarmNewBlockProtocol(ph)
-        await self.service.server.api.farm_new_transaction_block(req)
+        if block_type == "tx_block":
+            for i in range(blocks):
+                await self.service.server.api.farm_new_transaction_block(req)  # these can only be tx blocks
+        elif block_type == "full_block":
+            for i in range(blocks):
+                await self.service.server.api.farm_new_block(req)  # these can either be full blocks or tx blocks
+        else:
+            raise ValueError(f"Unknown block type: {block_type}")
         return {}
 
     async def auto_farm(self, _request: Dict[str, object]) -> Dict[str, object]:
