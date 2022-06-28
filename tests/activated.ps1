706964114fd0f9023b7f89632251aae789e6ba37
Kyle Altendorf <sda@fstab.net>
2022-06-28 13:58:04 -0400

    tidy

--- tests/activated.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
-
-Invoke-Expression "$script_directory/../venv/Scripts/activate"
-Invoke-Expression "$args"
--- tests/activated.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-SCRIPT_DIRECTORY=$(cd -- "$(dirname -- "$0")"; pwd)
-source "${SCRIPT_DIRECTORY}/../venv/bin/activate"
-
-"$@"
