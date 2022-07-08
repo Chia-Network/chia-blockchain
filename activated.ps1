c7e8cf752a6bf476ee6aa4c17cf02e7e2f40d730
Kyle Altendorf <sda@fstab.net>
2022-07-08 10:05:34 -0400

    Update activated.ps1

--- activated.ps1
+++ activated.ps1
@@ -3,9 +3,6 @@ $ErrorActionPreference = "Stop"
 $script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 & $script_directory/venv/Scripts/Activate.ps1
-
-python --version --version
-
 & @args
 
 exit $LASTEXITCODE
