1205418f224db9f48830d1c6ca7a8cad89fb213b
Kyle Altendorf <sda@fstab.net>
2022-07-07 20:57:08 -0400

    Simplify service start configuration loading (#12204)
    * Simplify service start configuration loading
    
    * oops
    
    * more oops
    
    * rename `config` to `service_config` as appropriate
    
    * rename `full_config` to `config`

--- chia/server/start_harvester.py
+++ chia/server/start_harvester.py
@@ -9,7 +9,7 @@ from chia.rpc.harvester_rpc_api import HarvesterRpcApi
 from chia.server.outbound_message import NodeType
 from chia.server.start_service import run_service
 from chia.types.peer_info import PeerInfo
-from chia.util.config import load_config_cli
+from chia.util.config import load_config, load_config_cli
 from chia.util.default_root import DEFAULT_ROOT_PATH
 
 # See: https://bugs.python.org/issue29288
@@ -23,32 +23,38 @@ def service_kwargs_for_harvester(
     config: Dict,
     consensus_constants: ConsensusConstants,
 ) -> Dict:
-    connect_peers = [PeerInfo(config["farmer_peer"]["host"], config["farmer_peer"]["port"])]
-    overrides = config["network_overrides"]["constants"][config["selected_network"]]
+    service_config = config[SERVICE_NAME]
+
+    connect_peers = [PeerInfo(service_config["farmer_peer"]["host"], service_config["farmer_peer"]["port"])]
+    overrides = service_config["network_overrides"]["constants"][service_config["selected_network"]]
     updated_constants = consensus_constants.replace_str_to_bytes(**overrides)
 
-    harvester = Harvester(root_path, config, updated_constants)
+    harvester = Harvester(root_path, service_config, updated_constants)
     peer_api = HarvesterAPI(harvester)
-    network_id = config["selected_network"]
+    network_id = service_config["selected_network"]
     kwargs = dict(
         root_path=root_path,
+        config=config,
         node=harvester,
         peer_api=peer_api,
         node_type=NodeType.HARVESTER,
-        advertised_port=config["port"],
+        advertised_port=service_config["port"],
         service_name=SERVICE_NAME,
-        server_listen_ports=[config["port"]],
+        server_listen_ports=[service_config["port"]],
         connect_peers=connect_peers,
         auth_connect_peers=True,
         network_id=network_id,
     )
-    if config["start_rpc_server"]:
-        kwargs["rpc_info"] = (HarvesterRpcApi, config["rpc_port"])
+    if service_config["start_rpc_server"]:
+        kwargs["rpc_info"] = (HarvesterRpcApi, service_config["rpc_port"])
     return kwargs
 
 
 def main() -> None:
-    config = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", SERVICE_NAME)
+    # TODO: refactor to avoid the double load
+    config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
+    service_config = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", SERVICE_NAME)
+    config[SERVICE_NAME] = service_config
     kwargs = service_kwargs_for_harvester(DEFAULT_ROOT_PATH, config, DEFAULT_CONSTANTS)
     return run_service(**kwargs)
 
