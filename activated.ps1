736135cf17082712dd93244dbf8addaf11a5bdbd
Kyle Altendorf <sda@fstab.net>
2022-07-07 20:06:34 -0400

    adjust activated.ps1

--- activated.ps1
+++ activated.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = "Stop"
 
 $script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-& "$script_directory/venv/Scripts/Activate.ps1"
+& $script_directory/venv/Scripts/Activate.ps1
 
 python --version --version
 
-& "$args"
+& @args
