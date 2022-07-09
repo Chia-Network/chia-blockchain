cc04e0b3798fb1e799e3f14435e9886df0f4a81f
Kyle Altendorf <sda@fstab.net>
2022-07-09 16:39:44 -0400

    tidy the tests

--- tests/core/data_layer/test_data_layer_types.py
+++ tests/core/data_layer/test_data_layer_types.py
@@ -3,10 +3,11 @@ from typing import List
 
 import pytest
 
+# TODO: update after resolution in https://github.com/pytest-dev/pytest/issues/7469
+from _pytest.fixtures import SubRequest
+
 from chia.data_layer.data_layer_types import ProofOfInclusion, ProofOfInclusionLayer, Side
 from chia.types.blockchain_format.sized_bytes import bytes32
-
-# TODO: the names and form of the fixtures/tests don't strike me as super clear...
 
 
 @pytest.fixture(name="valid_proof_of_inclusion")
@@ -29,28 +30,17 @@ def valid_proof_of_inclusion_fixture() -> ProofOfInclusion:
     return ProofOfInclusion(node_hash=node_hash, layers=layers)
 
 
-@pytest.fixture(name="invalid_proof_of_inclusion_bad_root")
-def invalid_proof_of_inclusion_bad_root_fixture(valid_proof_of_inclusion: ProofOfInclusion) -> ProofOfInclusion:
-    layers = list(valid_proof_of_inclusion.layers)
-    layers[-1] = dataclasses.replace(layers[-1], combined_hash=bytes32(b"f" * 32))
-    return dataclasses.replace(valid_proof_of_inclusion, layers=layers)
-
-
-@pytest.fixture(name="invalid_proof_of_inclusion_bad_layer_other_hash")
-def invalid_proof_of_inclusion_bad_layer_other_hash_fixture(
-    valid_proof_of_inclusion: ProofOfInclusion,
+@pytest.fixture(name="invalid_proof_of_inclusion", params=[0, 1, 2])
+def invalid_proof_of_inclusion_fixture(
+    request: SubRequest, valid_proof_of_inclusion: ProofOfInclusion
 ) -> ProofOfInclusion:
     layers = list(valid_proof_of_inclusion.layers)
-    layers[1] = dataclasses.replace(layers[1], other_hash=bytes32(b"f" * 32))
-    return dataclasses.replace(valid_proof_of_inclusion, layers=layers)
-
-
-@pytest.fixture(name="invalid_proof_of_inclusion_bad_layer_other_hash_side")
-def invalid_proof_of_inclusion_bad_layer_other_hash_side_fixture(
-    valid_proof_of_inclusion: ProofOfInclusion,
-) -> ProofOfInclusion:
-    layers = list(valid_proof_of_inclusion.layers)
-    layers[1] = dataclasses.replace(layers[1], other_hash_side=layers[1].other_hash_side.other())
+    if request.param == 0:
+        layers[-1] = dataclasses.replace(layers[-1], combined_hash=bytes32(b"f" * 32))
+    elif request.param == 1:
+        layers[1] = dataclasses.replace(layers[1], other_hash=bytes32(b"f" * 32))
+    elif request.param == 2:
+        layers[1] = dataclasses.replace(layers[1], other_hash_side=layers[1].other_hash_side.other())
     return dataclasses.replace(valid_proof_of_inclusion, layers=layers)
 
 
@@ -58,17 +48,5 @@ def test_proof_of_inclusion_is_valid(valid_proof_of_inclusion: ProofOfInclusion)
     assert valid_proof_of_inclusion.valid()
 
 
-def test_proof_of_inclusion_is_invalid_bad_root(invalid_proof_of_inclusion_bad_root: ProofOfInclusion) -> None:
-    assert not invalid_proof_of_inclusion_bad_root.valid()
-
-
-def test_proof_of_inclusion_is_invalid_bad_layer_other_hash(
-    invalid_proof_of_inclusion_bad_layer_other_hash: ProofOfInclusion,
-) -> None:
-    assert not invalid_proof_of_inclusion_bad_layer_other_hash.valid()
-
-
-def test_proof_of_inclusion_is_invalid_bad_layer_other_hash_side(
-    invalid_proof_of_inclusion_bad_layer_other_hash_side: ProofOfInclusion,
-) -> None:
-    assert not invalid_proof_of_inclusion_bad_layer_other_hash_side.valid()
+def test_proof_of_inclusion_is_invalid(invalid_proof_of_inclusion: ProofOfInclusion) -> None:
+    assert not invalid_proof_of_inclusion.valid()
