1ca6001785277c3d6a7c456e004bee8b32b5a1ae
Earle Lowe <e.lowe@chia.net>
2022-07-11 14:32:49 -0700

    Add check to verify wallet is tracking DL in batch_update

--- chia/data_layer/data_layer.py
+++ chia/data_layer/data_layer.py
@@ -112,6 +112,12 @@ class DataLayer:
         changelist: List[Dict[str, Any]],
         fee: uint64,
     ) -> TransactionRecord:
+
+        # check before any DL changes that this singleton is currently owned by this wallet
+        singleton_record = await self.wallet_rpc.dl_latest_singleton(tree_id, True)
+        if singleton_record is None:
+            raise ValueError(f"Singleton with launcher ID {tree_id} is not tracked by DL Wallet")
+
         t1 = time.monotonic()
         await self.data_store.insert_batch(tree_id, changelist, lock=True)
         t2 = time.monotonic()
