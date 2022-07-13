34f6adf7a2977a965f6a3f089cf8a910715f18f0
Jack Nelson <jack@jacknelson.xyz>
2022-07-13 18:45:47 -0400

    happiness is a thing

--- chia/cmds/show.py
+++ chia/cmds/show.py
@@ -218,34 +218,45 @@ async def show_async(
 
 @click.command("show", short_help="Show node information")
 @click.option(
-    "-p",
-    "--rpc-port",
-    help=(
-        "Set the port where the Full Node is hosting the RPC interface. "
-        "See the rpc_port under full_node in config.yaml"
-    ),
+    "-wp",
+    "--wallet-rpc-port",
+    help="Set the port where the Wallet is hosting the RPC interface. See the rpc_port under wallet in config.yaml",
     type=int,
     default=None,
+    deprecated=True,
 )
 @click.option("-s", "--state", help="Show the current state of the blockchain", is_flag=True, type=bool, default=False)
+@click.option(
+    "-c", "--connections", help="List nodes connected to this Full Node", is_flag=True, type=bool, default=False
+)
+@click.option("-a", "--add-connection", help="Connect to another Full Node by ip:port", type=str, default="")
+@click.option(
+    "-r", "--remove-connection", help="Remove a Node by the first 8 characters of NodeID", type=str, default=""
+)
 @click.option(
     "-bh", "--block-header-hash-by-height", help="Look up a block header hash by block height", type=str, default=""
 )
 @click.option("-b", "--block-by-header-hash", help="Look up a block by block header hash", type=str, default="")
-@click.option("-c", "--connections", help="Use `chia peer -c`", is_flag=True, type=bool, default=False)
 @click.pass_context
 def show_cmd(
     ctx: click.Context,
     rpc_port: Optional[int],
+    wallet_rpc_port: Optional[int],
     state: bool,
+    connections: bool,
+    add_connection: str,
+    remove_connection: str,
     block_header_hash_by_height: str,
     block_by_header_hash: str,
-    connections: bool,
 ) -> None:
     import asyncio
 
     if connections:
         print("'chia show -c' has been renamed to 'chia peer -c' ")
+    if add_connection != "":
+        print("'chia show -a' has been renamed to 'chia peer -a' ")
+    if remove_connection != "":
+        print("'chia show -r' has been renamed to 'chia peer -r' ")
     asyncio.run(
         execute_with_node(
             rpc_port,
