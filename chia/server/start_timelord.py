f2061d86d62dd8ed4a09ea4f63404784e1726adc
Kyle Altendorf <sda@fstab.net>
2022-07-13 12:43:48 -0400

    rework service creation to be hint checkable (#12153)
    * rework service creation to be hint checkable
    
    * fixup config vs. service_config
    
    * more fixup
    
    * service start cleanup
    
    * Update nft_metadata_updater_default.clvm.hex
    
    * reorder advertised_port= parameter
    
    * undo unrelated tweaks
    
    * Optional[RpcInfo]
    
    * undo unrelated whitespace change

--- chia/server/start_timelord.py
+++ chia/server/start_timelord.py
@@ -1,15 +1,17 @@
 import logging
 import pathlib
-from typing import Dict
+import sys
+from typing import Dict, Optional
 
 from chia.consensus.constants import ConsensusConstants
 from chia.consensus.default_constants import DEFAULT_CONSTANTS
 from chia.rpc.timelord_rpc_api import TimelordRpcApi
 from chia.server.outbound_message import NodeType
-from chia.server.start_service import run_service
+from chia.server.start_service import RpcInfo, Service, async_run
 from chia.timelord.timelord import Timelord
 from chia.timelord.timelord_api import TimelordAPI
 from chia.types.peer_info import PeerInfo
+from chia.util.chia_logging import initialize_logging
 from chia.util.config import load_config, load_config_cli
 from chia.util.default_root import DEFAULT_ROOT_PATH
 
@@ -22,11 +24,12 @@ SERVICE_NAME = "timelord"
 log = logging.getLogger(__name__)
 
 
-def service_kwargs_for_timelord(
+def create_timelord_service(
     root_path: pathlib.Path,
     config: Dict,
     constants: ConsensusConstants,
-) -> Dict:
+    connect_to_daemon: bool = True,
+) -> Service:
     service_config = config[SERVICE_NAME]
 
     connect_peers = [PeerInfo(service_config["full_node_peer"]["host"], service_config["full_node_peer"]["port"])]
@@ -36,7 +39,12 @@ def service_kwargs_for_timelord(
     node = Timelord(root_path, service_config, updated_constants)
     peer_api = TimelordAPI(node)
     network_id = service_config["selected_network"]
-    kwargs = dict(
+
+    rpc_info: Optional[RpcInfo] = None
+    if service_config.get("start_rpc_server", True):
+        rpc_info = (TimelordRpcApi, service_config.get("rpc_port", 8557))
+
+    return Service(
         root_path=root_path,
         config=config,
         peer_api=peer_api,
@@ -48,22 +56,31 @@ def service_kwargs_for_timelord(
         connect_peers=connect_peers,
         auth_connect_peers=False,
         network_id=network_id,
+        rpc_info=rpc_info,
+        connect_to_daemon=connect_to_daemon,
     )
 
-    if service_config.get("start_rpc_server", True):
-        kwargs["rpc_info"] = (TimelordRpcApi, service_config.get("rpc_port", 8557))
 
-    return kwargs
-
-
-def main() -> None:
+async def async_main() -> int:
     # TODO: refactor to avoid the double load
     config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
     service_config = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", SERVICE_NAME)
     config[SERVICE_NAME] = service_config
-    kwargs = service_kwargs_for_timelord(DEFAULT_ROOT_PATH, config, DEFAULT_CONSTANTS)
-    return run_service(**kwargs)
+    initialize_logging(
+        service_name=SERVICE_NAME,
+        logging_config=service_config["logging"],
+        root_path=DEFAULT_ROOT_PATH,
+    )
+    service = create_timelord_service(DEFAULT_ROOT_PATH, config, DEFAULT_CONSTANTS)
+    await service.setup_process_global_state()
+    await service.run()
+
+    return 0
+
+
+def main() -> int:
+    return async_run(async_main())
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
