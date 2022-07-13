0f5d81cc652fd51de7634ca4b09585f45feab313
Juraj Oršulić <juraj.orsulic@fer.hr>
2022-07-13 14:28:34 +0200

    Revert change in server.py

--- chia/server/server.py
+++ chia/server/server.py
@@ -806,7 +806,7 @@ class ChiaServer:
             return False
         if not self.config["testing"] and peer.peer_host == "127.0.0.1":
             return True
-        if peer.peer_node_id.hex() not in trusted_peers.values():
+        if peer.peer_node_id.hex() not in trusted_peers:
             return False
 
         return True
