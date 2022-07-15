18730962f62afd50b5bf1445f1ed928931e4ae48
Jack Nelson <jack@jacknelson.xyz>
2022-07-14 21:41:56 -0400

    Revert "curry and make better"
    The tests do not like it

--- chia/wallet/puzzles/decompress_block_spends.py
+++ chia/wallet/puzzles/decompress_block_spends.py
@@ -1,7 +1,3 @@
-from chia.types.blockchain_format.program import SerializedProgram
-from chia.wallet.puzzles.load_clvm import load_clvm
+from chia.wallet.puzzles.load_clvm import load_serialized_clvm
 
-uncurried_puzzle = load_clvm("decompress_block_spends.clvm", package_or_requirement=__name__)
-deserialization_puzzle = load_clvm("chialisp_deserialisation.clvm", package_or_requirement=__name__)
-
-DECOMPRESS_BLOCK_SPENDS = SerializedProgram.from_program(uncurried_puzzle.curry(deserialization_puzzle))
+DECOMPRESS_BLOCK_SPENDS = load_serialized_clvm("decompress_block_spends.clvm", package_or_requirement=__name__)
