bda20cb085b7352032a4b1a0fd0d7be673dc97bf
Jack Nelson <jack@jacknelson.xyz>
2022-07-14 21:16:42 -0400

    re add rpc, oops

--- chia/cmds/show.py
+++ chia/cmds/show.py
@@ -218,6 +218,16 @@ async def show_async(
 
 @click.command("show", short_help="Show node information")
 @click.option(
+    "-p",
+    "--rpc-port",
+    help=(
+        "Set the port where the Full Node is hosting the RPC interface. "
+        "See the rpc_port under full_node in config.yaml"
+    ),
+    type=int,
+    default=None,
+)
+@click.option(
     "-wp",
     "--wallet-rpc-port",
     help="Set the port where the Wallet is hosting the RPC interface. See the rpc_port under wallet in config.yaml",
