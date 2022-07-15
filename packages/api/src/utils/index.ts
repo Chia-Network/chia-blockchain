f1fd87d4ecf3788acf3241da0ceb224188c834ee
Zlatko Fedor <zfedor@gmail.com>
2022-07-15 20:16:56 +0200

    allow to use only valid (t)xch address for NFT transfer

--- packages/api/src/utils/index.ts
+++ packages/api/src/utils/index.ts
@@ -2,4 +2,4 @@ export { default as defaultsForPlotter } from './defaultsForPlotter';
 export { default as english } from './english';
 export { default as optionsForPlotter } from './optionsForPlotter';
 export { default as toCamelCase } from './toCamelCase';
-export { default as toBech32m, fromBech32m } from './toBech32m';
+export { default as toBech32m, fromBech32m, decodeBech32m } from './toBech32m';
--- packages/api/src/utils/toBech32m.ts
+++ packages/api/src/utils/toBech32m.ts
@@ -22,3 +22,10 @@ export function fromBech32m(value: string): string {
   const data = bech32m.decode(value);
   return Buffer.from(bech32m.fromWords(data.words)).toString('hex');
 }
+
+export function decodeBech32m(value: string): string {
+  const { words, prefix } = bech32m.decode(value);
+  const data = Buffer.from(bech32m.fromWords(words)).toString('hex');
+
+  return { prefix, data };
+}
