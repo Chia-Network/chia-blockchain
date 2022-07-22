edd60a105468012f59db5bfb60fc8627c611e43e
dustinface <35775977+xdustinface@users.noreply.github.com>
2022-07-22 18:15:00 +0200

    keychain: Drop `supports_keyring_passphrase` (#12479)
    * keychain: Drop `supports_keyring_passphrase`
    
    * Drop `PASSPHRASE_CLI_OPTION_NAMES`

--- chia/cmds/chia.py
+++ chia/cmds/chia.py
@@ -22,7 +22,6 @@ from chia.util.keychain import (
     Keychain,
     KeyringCurrentPassphraseIsInvalid,
     set_keys_root_path,
-    supports_keyring_passphrase,
 )
 from chia.util.ssl_check import check_ssl
 from typing import Optional
@@ -95,13 +94,6 @@ def cli(
     check_ssl(Path(root_path))
 
 
-if not supports_keyring_passphrase():
-    from chia.cmds.passphrase_funcs import remove_passphrase_options_from_cmd
-
-    # TODO: Remove once keyring passphrase management is rolled out to all platforms
-    remove_passphrase_options_from_cmd(cli)
-
-
 @cli.command("version", short_help="Show chia version")
 def version_cmd() -> None:
     print(__version__)
@@ -140,9 +132,7 @@ cli.add_command(netspace_cmd)
 cli.add_command(farm_cmd)
 cli.add_command(plotters_cmd)
 cli.add_command(db_cmd)
-
-if supports_keyring_passphrase():
-    cli.add_command(passphrase_cmd)
+cli.add_command(passphrase_cmd)
 
 
 def main() -> None:
