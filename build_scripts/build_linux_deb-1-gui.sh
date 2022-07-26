5f3a101cdbcc96cf2eb0be401f652fa6cbdbfe75
ChiaMineJP <admin@chiamine.jp>
2022-07-27 00:16:21 +0900

    Improved CI performance for linux

---
+++ build_scripts/build_linux_deb-1-gui.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+
+echo "Installing global npm packages"
+cd npm_linux_deb || exit
+npm ci
+PATH=$(npm bin):$PATH
+
+cd ../../chia-blockchain-gui || exit
+
+echo "npm build"
+lerna clean -y
+npm ci
+# Audit fix does not currently work with Lerna. See https://github.com/lerna/lerna/issues/1663
+# npm audit fix
+npm run build
+LAST_EXIT_CODE=$?
+if [ "$LAST_EXIT_CODE" -ne 0 ]; then
+	echo >&2 "npm run build failed!"
+	exit $LAST_EXIT_CODE
+fi
--- build_scripts/build_linux_deb-2-installer.sh
+++ build_scripts/build_linux_deb-2-installer.sh
@@ -57,23 +57,9 @@ dpkg-deb --build --root-owner-group "dist/$CLI_DEB_BASE"
 # CLI only .deb done
 
 cp -r dist/daemon ../chia-blockchain-gui/packages/gui
-cd .. || exit
-cd chia-blockchain-gui || exit
-
-echo "npm build"
-lerna clean -y
-npm ci
-# Audit fix does not currently work with Lerna. See https://github.com/lerna/lerna/issues/1663
-# npm audit fix
-npm run build
-LAST_EXIT_CODE=$?
-if [ "$LAST_EXIT_CODE" -ne 0 ]; then
-	echo >&2 "npm run build failed!"
-	exit $LAST_EXIT_CODE
-fi
 
 # Change to the gui package
-cd packages/gui || exit
+cd ../chia-blockchain-gui/packages/gui || exit
 
 # sets the version for chia-blockchain in package.json
 cp package.json package.json.orig
