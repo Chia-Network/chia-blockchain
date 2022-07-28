3d65755a626ebbad4eef77751dc94de783d51b5f
ChiaAutomation <automation@chia.net>
2022-07-28 21:27:17 +0000

    testnet config for test3

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
 
-DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
+DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/test3"))).resolve()
 STANDALONE_ROOT_PATH = Path(
     os.path.expanduser(os.getenv("CHIA_STANDALONE_WALLET_ROOT", "~/.chia/standalone_wallet"))
 ).resolve()
 
 DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
+)
