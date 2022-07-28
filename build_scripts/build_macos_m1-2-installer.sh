e80aa78fae635e5a85a0cb63308fb90afe40f2f8
ChiaMineJP <admin@chiamine.jp>
2022-07-29 00:18:26 +0900

    Improved CI performance for M1 Mac

--- build_scripts/build_macos_m1-2-installer.sh
+++ build_scripts/build_macos_m1-2-installer.sh
@@ -30,23 +30,7 @@ if [ "$LAST_EXIT_CODE" -ne 0 ]; then
 	exit $LAST_EXIT_CODE
 fi
 cp -r dist/daemon ../chia-blockchain-gui/packages/gui
-cd .. || exit
-cd chia-blockchain-gui || exit
-
-echo "npm build"
-lerna clean -y
-npm ci
-# Audit fix does not currently work with Lerna. See https://github.com/lerna/lerna/issues/1663
-# npm audit fix
-npm run build
-LAST_EXIT_CODE=$?
-if [ "$LAST_EXIT_CODE" -ne 0 ]; then
-	echo >&2 "npm run build failed!"
-	exit $LAST_EXIT_CODE
-fi
-
-# Change to the gui package
-cd packages/gui || exit
+cd ../chia-blockchain-gui/packages/gui || exit
 
 # sets the version for chia-blockchain in package.json
 brew install jq
