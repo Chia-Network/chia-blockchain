2487dd0efe71c7301e6c02a01c40bcd39108572d
Jeff Cruikshank <jeff@chia.net>
2022-07-30 19:18:39 -0700

    Few additional sets of test SSL certs/keys for CI

--- chia/simulator/ssl_certs.py
+++ chia/simulator/ssl_certs.py
@@ -8,6 +8,18 @@ from chia.simulator.ssl_certs_4 import SSL_TEST_NODE_CERTS_AND_KEYS_4, SSL_TEST_
 from chia.simulator.ssl_certs_5 import SSL_TEST_NODE_CERTS_AND_KEYS_5, SSL_TEST_PRIVATE_CA_CERT_AND_KEY_5
 from chia.simulator.ssl_certs_6 import SSL_TEST_NODE_CERTS_AND_KEYS_6, SSL_TEST_PRIVATE_CA_CERT_AND_KEY_6
 from chia.simulator.ssl_certs_7 import SSL_TEST_NODE_CERTS_AND_KEYS_7, SSL_TEST_PRIVATE_CA_CERT_AND_KEY_7
+from chia.simulator.ssl_certs_8 import SSL_TEST_NODE_CERTS_AND_KEYS_8, SSL_TEST_PRIVATE_CA_CERT_AND_KEY_8
+from chia.simulator.ssl_certs_9 import SSL_TEST_NODE_CERTS_AND_KEYS_9, SSL_TEST_PRIVATE_CA_CERT_AND_KEY_9
+from chia.simulator.ssl_certs_10 import SSL_TEST_NODE_CERTS_AND_KEYS_10, SSL_TEST_PRIVATE_CA_CERT_AND_KEY_10
+
+# ---------------------------------------------------------------------------
+# NOTE:
+# Use tests/util/gen_ssl_certs.py to generate additional SSL certs and keys
+#
+# EXAMPLE:
+# $ python3 tests/util/gen_ssl_certs.py --suffix 123 > ssl_certs_123.py
+# ---------------------------------------------------------------------------
+
 
 # ---------------------------------------------------------------------------
 # Private CA certs/keys
@@ -21,6 +33,9 @@ SSL_TEST_PRIVATE_CA_CERTS_AND_KEYS: List[Tuple[bytes, bytes]] = [
     SSL_TEST_PRIVATE_CA_CERT_AND_KEY_5,
     SSL_TEST_PRIVATE_CA_CERT_AND_KEY_6,
     SSL_TEST_PRIVATE_CA_CERT_AND_KEY_7,
+    SSL_TEST_PRIVATE_CA_CERT_AND_KEY_8,
+    SSL_TEST_PRIVATE_CA_CERT_AND_KEY_9,
+    SSL_TEST_PRIVATE_CA_CERT_AND_KEY_10,
 ]
 
 # ---------------------------------------------------------------------------
@@ -35,6 +50,9 @@ SSL_TEST_NODE_CERTS_AND_KEYS: List[Dict[str, Dict[str, Dict[str, bytes]]]] = [
     SSL_TEST_NODE_CERTS_AND_KEYS_5,
     SSL_TEST_NODE_CERTS_AND_KEYS_6,
     SSL_TEST_NODE_CERTS_AND_KEYS_7,
+    SSL_TEST_NODE_CERTS_AND_KEYS_8,
+    SSL_TEST_NODE_CERTS_AND_KEYS_9,
+    SSL_TEST_NODE_CERTS_AND_KEYS_10,
 ]
 
 
