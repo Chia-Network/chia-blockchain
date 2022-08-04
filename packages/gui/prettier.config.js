b259f29300a1e8c058cf868ed0df620b3422b2ef
Jeff <jeff@chia.net>
2022-08-04 08:55:54 -0700

    Prettier code formatting settings and VSCode integration (#875)
    * Prettier code styling and jshint + VSCode integration
    
    * Updates and reformatting by prettier
    
    * Allow running prettier against specific paths or everything

---
+++ packages/gui/prettier.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  printWidth: 80,
+  quoteProps: "as-needed",
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
