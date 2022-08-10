b642ca2d55c4e6c02b25b415db5a0f9024c3c350
Kyle Altendorf <sda@fstab.net>
2022-08-10 09:06:16 -0400

    actually skip the setproctitle test file

--- tests/core/test_setproctitle.py
+++ tests/core/test_setproctitle.py
@@ -1,4 +1,10 @@
+import pytest
+
 from chia.util.setproctitle import setproctitle
+
+pytestmark = pytest.mark.skip(
+    reason="this test ends up hanging frequently and needs to be rewritten with a subprocess and a title check",
+)
 
 
 def test_does_not_crash():
