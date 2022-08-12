627706b419357c9a9d8f826c058c5c27dfb5b8f1
Jeff <jeff@chia.net>
2022-08-12 09:00:35 -0700

    Show the image hash mismatch status for large files (#899)
    * Show the image hash mismatch status for large files
    
    * More specific fix. Prior change wasn't matching on hash mismatches.
    
    * Linter fix

--- packages/gui/src/hooks/useNFTHash.ts
+++ packages/gui/src/hooks/useNFTHash.ts
@@ -1,9 +1,9 @@
 import type NFTInfo from '@chia/api';
 import useVerifyURIHash from './useVerifyURIHash';
 
-export default function useNFTHash(nft: NFTInfo) {
+export default function useNFTHash(nft: NFTInfo, ignoreSizeLimit = false) {
   const { dataHash, dataUris } = nft;
   const uri = dataUris?.[0];
 
-  return useVerifyURIHash(uri, dataHash);
+  return useVerifyURIHash(uri, dataHash, ignoreSizeLimit);
 }
