ef0735e9195243e03c1ae3b4bdc741d5b468d537
Jeff <jeff@chia.net>
2022-08-12 10:15:00 -0700

    Force legacy keyring migration. Legacy keyring support is now deprecated. (#904)
    * Force legacy keyring migration. Legacy keyring support is now deprecated.
    
    * Fixed empty passphrase / min length enforcement

--- packages/core/src/hooks/useValidateChangePassphraseParams.tsx
+++ packages/core/src/hooks/useValidateChangePassphraseParams.tsx
@@ -12,7 +12,7 @@ export default function useValidateChangePassphraseParams() {
   async function validateChangePassphraseParams(
     currentPassphrase: string | null,
     newPassphrase: string,
-    confirmationPassphrase: string,
+    confirmationPassphrase: string
   ): Promise<boolean> {
     try {
       if (isLoading) {
@@ -22,36 +22,58 @@ export default function useValidateChangePassphraseParams() {
       }
 
       const {
-        allowEmptyPassphrase,
-        minPassphraseLength,
-      } = keyringState;
-      
+        isOptional: allowEmptyPassphrase,
+        minLength: minPassphraseLength,
+      } = keyringState.passphraseRequirements;
+
       if (newPassphrase != confirmationPassphrase) {
-        throw new Error(t`The provided passphrase and confirmation do not match`);
-      } else if ((newPassphrase.length == 0 && !allowEmptyPassphrase) || // Passphrase required, no passphrase provided
-                (newPassphrase.length > 0 && newPassphrase.length < minPassphraseLength)) { // Passphrase provided, not long enough
-        throw new Error(plural(minPassphraseLength, {
-          one: 'Passphrases must be at least # character in length',
-          other: 'Passphrases must be at least # characters in length'
-        }));
-      } else if (currentPassphrase !== null && (currentPassphrase == newPassphrase)) {
-        throw new Error(t`New passphrase is the same as your current passphrase`);
+        throw new Error(
+          t`The provided passphrase and confirmation do not match`
+        );
+      } else if (
+        (newPassphrase.length == 0 && !allowEmptyPassphrase) || // Passphrase required, no passphrase provided
+        (newPassphrase.length > 0 && newPassphrase.length < minPassphraseLength)
+      ) {
+        // Passphrase provided, not long enough
+        throw new Error(
+          plural(minPassphraseLength, {
+            one: 'Passphrases must be at least # character in length',
+            other: 'Passphrases must be at least # characters in length',
+          })
+        );
+      } else if (
+        currentPassphrase !== null &&
+        currentPassphrase == newPassphrase
+      ) {
+        throw new Error(
+          t`New passphrase is the same as your current passphrase`
+        );
       } else if (newPassphrase.length == 0) {
         // Warn about using an empty passphrase
         let alertTitle: React.ReactElement | string;
         let buttonTitle: React.ReactElement | string;
         let message: React.ReactElement | string;
-  
+
         if (currentPassphrase === null) {
-          alertTitle = (<Trans>Skip Passphrase Protection</Trans>);
-          buttonTitle = (<Trans>Skip</Trans>);
-          message = (<Trans>Setting a passphrase is strongly recommended to protect your keys. Are you sure you want to skip setting a passphrase?</Trans>);
+          alertTitle = <Trans>Skip Passphrase Protection</Trans>;
+          buttonTitle = <Trans>Skip</Trans>;
+          message = (
+            <Trans>
+              Setting a passphrase is strongly recommended to protect your keys.
+              Are you sure you want to skip setting a passphrase?
+            </Trans>
+          );
         } else {
-          alertTitle = (<Trans>Disable Passphrase Protection</Trans>);
-          buttonTitle = (<Trans>Disable</Trans>);
-          message = (<Trans>Using a passphrase is strongly recommended to protect your keys. Are you sure you want to disable passphrase protection?</Trans>);
+          alertTitle = <Trans>Disable Passphrase Protection</Trans>;
+          buttonTitle = <Trans>Disable</Trans>;
+          message = (
+            <Trans>
+              Using a passphrase is strongly recommended to protect your keys.
+              Are you sure you want to disable passphrase protection?
+            </Trans>
+          );
         }
-  
+
         const useEmptyPassphrase = await openDialog(
           <ConfirmDialog
             title={alertTitle}
@@ -63,20 +85,16 @@ export default function useValidateChangePassphraseParams() {
             {message}
           </ConfirmDialog>
         );
-  
+
         // @ts-ignore
         if (!useEmptyPassphrase) {
           return false;
         }
       }
-  
+
       return true;
     } catch (error: any) {
-      await openDialog(
-        <AlertDialog>
-          {error.message}
-        </AlertDialog>
-      );
+      await openDialog(<AlertDialog>{error.message}</AlertDialog>);
       return false;
     }
   }
