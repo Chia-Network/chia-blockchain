a79decb244200a2a26174c4b3fc791e66e712707
ChiaMineJP <admin@chiamine.jp>
2022-08-14 15:50:40 +0900

    Updated Linux(deb) installer CI job

--- build_scripts/build_linux_deb-2-installer.sh
+++ build_scripts/build_linux_deb-2-installer.sh
@@ -6,11 +6,9 @@ if [ ! "$1" ]; then
   echo "This script requires either amd64 of arm64 as an argument"
 	exit 1
 elif [ "$1" = "amd64" ]; then
-	PLATFORM="$1"
-	DIR_NAME="chia-blockchain-linux-x64"
+	PLATFORM="amd64"
 else
-	PLATFORM="$1"
-	DIR_NAME="chia-blockchain-linux-arm64"
+	PLATFORM="arm64"
 fi
 export PLATFORM
 
@@ -28,10 +26,10 @@ echo "Chia Installer Version is: $CHIA_INSTALLER_VERSION"
 export CHIA_INSTALLER_VERSION
 
 echo "Installing npm and electron packagers"
-cd npm_linux || exit
+cd npm_linux
 npm ci
 PATH=$(npm bin):$PATH
-cd .. || exit
+cd ..
 
 echo "Create dist/"
 rm -rf dist
@@ -62,49 +60,40 @@ dpkg-deb --build --root-owner-group "dist/$CLI_DEB_BASE"
 cp -r dist/daemon ../chia-blockchain-gui/packages/gui
 
 # Change to the gui package
-cd ../chia-blockchain-gui/packages/gui || exit
+cd ../chia-blockchain-gui/packages/gui
 
 # sets the version for chia-blockchain in package.json
 cp package.json package.json.orig
 jq --arg VER "$CHIA_INSTALLER_VERSION" '.version=$VER' package.json > temp.json && mv temp.json package.json
 
-echo electron-packager
-electron-packager . chia-blockchain --asar.unpack="**/daemon/**" --platform=linux \
---icon=src/assets/img/Chia.icns --overwrite --app-bundle-id=net.chia.blockchain \
---appVersion=$CHIA_INSTALLER_VERSION --executable-name=chia-blockchain \
---no-prune --no-deref-symlinks \
---ignore="/node_modules/(?!ws(/|$))(?!@electron(/|$))" --ignore="^/src$" --ignore="^/public$"
+echo "Building Linux(deb) Electron app"
+OPT_ARCH="--x64"
+if [ "$PLATFORM" = "arm64" ]; then
+  OPT_ARCH="--arm64"
+fi
+echo electron-builder build --linux deb "${OPT_ARCH}"
+electron-builder build --linux deb "${OPT_ARCH}"
 LAST_EXIT_CODE=$?
-# Note: `node_modules/ws` and `node_modules/@electron/remote` are dynamic dependencies
-# which GUI calls by `window.require('...')` at runtime.
-# So `ws` and `@electron/remote` cannot be ignored at this time.
-ls -l $DIR_NAME/resources
+ls -l dist/linux*-unpacked/resources
 
 # reset the package.json to the original
 mv package.json.orig package.json
 
 if [ "$LAST_EXIT_CODE" -ne 0 ]; then
-	echo >&2 "electron-packager failed!"
+	echo >&2 "electron-builder failed!"
 	exit $LAST_EXIT_CODE
 fi
 
-mv $DIR_NAME ../../../build_scripts/dist/
-cd ../../../build_scripts || exit
+GUI_DEB_NAME=chia-blockchain_${CHIA_INSTALLER_VERSION}_${PLATFORM}.deb
+mv dist/chia-${CHIA_INSTALLER_VERSION}.deb ../../../build_scripts/dist/${GUI_DEB_NAME}
+cd ../../../build_scripts
 
-echo "Create chia-$CHIA_INSTALLER_VERSION.deb"
+echo "Create final installer"
 rm -rf final_installer
 mkdir final_installer
-electron-installer-debian --src "dist/$DIR_NAME/" \
-  --arch "$PLATFORM" \
-  --options.version "$CHIA_INSTALLER_VERSION" \
-  --config deb-options.json
-LAST_EXIT_CODE=$?
-if [ "$LAST_EXIT_CODE" -ne 0 ]; then
-	echo >&2 "electron-installer-debian failed!"
-	exit $LAST_EXIT_CODE
-fi
 
+mv "dist/${GUI_DEB_NAME}" final_installer/
 # Move the cli only deb into final installers as well, so it gets uploaded as an artifact
-mv "dist/$CLI_DEB_BASE.deb" final_installer/
+mv "dist/${CLI_DEB_BASE}.deb" final_installer/
 
 ls final_installer/
