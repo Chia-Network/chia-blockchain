61fab1dc659886e8c41be3d3ca2ab35c5db251b4
Florin Chirica <fchirica96@gmail.com>
2022-08-18 20:14:07 +0300

    Checkpoint decouple atari server.

--- chia/data_layer/data_layer_server.py
+++ chia/data_layer/data_layer_server.py
@@ -1,4 +1,6 @@
+import asyncio
 import logging
+import sys
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Dict
@@ -6,8 +8,19 @@ from typing import Any, Dict
 from aiohttp import web
 
 from chia.data_layer.download_data import is_filename_valid
+from chia.server.start_service import async_run
 from chia.server.upnp import UPnP
+from chia.util.chia_logging import initialize_logging
+from chia.util.config import load_config, load_config_cli
+from chia.util.default_root import DEFAULT_ROOT_PATH
 from chia.util.path import path_from_root
+from chia.util.setproctitle import setproctitle
+
+# See: https://bugs.python.org/issue29288
+"".encode("idna")
+
+SERVICE_NAME = "data_layer"
+log = logging.getLogger(__name__)
 
 
 @dataclass
@@ -36,6 +49,8 @@ class DataLayerServer:
         self.site = web.TCPSite(self.runner, self.config["host_ip"], port=self.port)
         await self.site.start()
         self.log.info("Started Data Layer Server.")
+        while True:
+            await asyncio.sleep(1)
 
     async def stop(self) -> None:
         self.upnp.release(self.port)
@@ -58,3 +73,27 @@ class DataLayerServer:
             body=content,
         )
         return response
+
+
+async def async_main() -> int:
+    config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
+    service_config = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", SERVICE_NAME)
+    config[SERVICE_NAME] = service_config
+    setproctitle("data_layer_server")
+    initialize_logging(
+        service_name="data_layer_server",
+        logging_config=service_config["logging"],
+        root_path=DEFAULT_ROOT_PATH,
+    )
+    data_layer_server = DataLayerServer(DEFAULT_ROOT_PATH, config[SERVICE_NAME], log)
+    await data_layer_server.start()
+    return 0
+
+
+def main() -> int:
+    async_run(async_main())
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
