95de522a9c92f6a4e69463b392bc7778eccd50b6
Florin Chirica <fchirica96@gmail.com>
2022-08-19 16:39:32 +0300

    Add click cli.

--- chia/data_layer/data_layer_server.py
+++ chia/data_layer/data_layer_server.py
@@ -3,15 +3,16 @@ import logging
 import sys
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
+import click
 from aiohttp import web
 
+from chia.cmds.chia import monkey_patch_click
 from chia.data_layer.download_data import is_filename_valid
-from chia.server.start_service import async_run
 from chia.server.upnp import UPnP
 from chia.util.chia_logging import initialize_logging
-from chia.util.config import load_config, load_config_cli
+from chia.util.config import load_config
 from chia.util.default_root import DEFAULT_ROOT_PATH
 from chia.util.path import path_from_root
 from chia.util.setproctitle import setproctitle
@@ -29,9 +30,17 @@ class DataLayerServer:
     config: Dict[str, Any]
     log: logging.Logger
 
-    async def start(self) -> None:
+    async def start(self, ip: Optional[str], port: Optional[int]) -> None:
         self.log.info("Starting Data Layer Server.")
-        self.port = self.config["host_port"]
+        if ip is not None:
+            self.host_ip = ip
+        else:
+            self.host_ip = self.config["host_ip"]
+
+        if port is None:
+            self.port = self.config["host_port"]
+        else:
+            self.port = port
 
         # Setup UPnP for the data_layer_service port
         self.upnp: UPnP = UPnP()
@@ -46,11 +55,11 @@ class DataLayerServer:
         app.add_routes([web.get("/{filename}", self.file_handler)])
         self.runner = web.AppRunner(app)
         await self.runner.setup()
-        self.site = web.TCPSite(self.runner, self.config["host_ip"], port=self.port)
+        self.site = web.TCPSite(self.runner, self.host_ip, port=self.port)
         await self.site.start()
         self.log.info("Started Data Layer Server.")
-        while True:
-            await asyncio.sleep(1)
+        shutdown_event = asyncio.Event()
+        await shutdown_event.wait()
 
     async def stop(self) -> None:
         self.upnp.release(self.port)
@@ -75,23 +84,36 @@ class DataLayerServer:
         return response
 
 
-async def async_main() -> int:
-    config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
-    service_config = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", SERVICE_NAME)
-    config[SERVICE_NAME] = service_config
+async def async_start(ip: Optional[str], port: Optional[int]) -> int:
+    config = load_config(DEFAULT_ROOT_PATH, "config.yaml", SERVICE_NAME)
     setproctitle("data_layer_server")
     initialize_logging(
         service_name="data_layer_server",
-        logging_config=service_config["logging"],
+        logging_config=config["logging"],
         root_path=DEFAULT_ROOT_PATH,
     )
-    data_layer_server = DataLayerServer(DEFAULT_ROOT_PATH, config[SERVICE_NAME], log)
-    await data_layer_server.start()
-    return 0
+    data_layer_server = DataLayerServer(DEFAULT_ROOT_PATH, config, log)
+    await data_layer_server.start(ip, port)
+
+
+@click.option("-i", "--ip", type=str, default=None)
+@click.option("-p", "--port", type=int, default=None)
+@click.command("start")
+def start_cmd(ip: Optional[str], port: Optional[int]) -> None:
+    asyncio.run(async_start(ip, port))
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(start_cmd)
 
 
 def main() -> int:
-    async_run(async_main())
+    monkey_patch_click()
+    cli()
     return 0
 
 
