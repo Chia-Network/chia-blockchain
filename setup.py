0b81d9d79ecc5b25b61aa735a1cf3c591ef0f0fd
Kyle Altendorf <sda@fstab.net>
2022-08-21 16:56:24 -0400

    no setproctitle install

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependencies = [
     #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
     #  See https://github.com/frispete/keyrings.cryptfile/issues/15
     "PyYAML==6.0",  # Used for config file format
-    "setproctitle==1.2.3",  # Gives the chia processes readable names
+    #"setproctitle==1.2.3",  # Gives the chia processes readable names
     "sortedcontainers==2.4.0",  # For maintaining sorted mempools
     # TODO: when moving to click 8 remove the pinning of black noted below
     "click==7.1.2",  # For the CLI
