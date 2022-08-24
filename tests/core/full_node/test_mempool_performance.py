16036ef403e367827c94f4e3fd74c90c9636241e
Mariano Sorgente <sorgente711@gmail.com>
2022-08-24 10:19:56 -0400

    Fix test, and improve detection of synced

--- tests/core/full_node/test_mempool_performance.py
+++ tests/core/full_node/test_mempool_performance.py
@@ -14,7 +14,7 @@ from chia.simulator.time_out_assert import time_out_assert
 from tests.util.misc import assert_runtime
 
 
-def wallet_height_at_least(wallet_node, h):
+async def wallet_height_at_least(wallet_node, h):
     height = await wallet_node.wallet_state_manager.blockchain.get_finished_sync_up_to()
     if height == h:
         return True
