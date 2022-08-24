2276284c8ed4812194a213c1dd573e87a8df2a59
Arvid Norberg <arvid@libtorrent.org>
2022-08-24 08:36:25 +0200

    now that all uses of DBWrapper have been updated to DBWrapper2, remove the old one (#13116)

--- chia/util/db_wrapper.py
+++ chia/util/db_wrapper.py
@@ -13,32 +13,6 @@ else:
     SQLITE_MAX_VARIABLE_NUMBER = 32700
 
 
-class DBWrapper:
-    """
-    This object handles HeaderBlocks and Blocks stored in DB used by wallet.
-    """
-
-    db: aiosqlite.Connection
-    lock: asyncio.Lock
-
-    def __init__(self, connection: aiosqlite.Connection):
-        self.db = connection
-        self.lock = asyncio.Lock()
-
-    async def begin_transaction(self):
-        cursor = await self.db.execute("BEGIN TRANSACTION")
-        await cursor.close()
-
-    async def rollback_transaction(self):
-        # Also rolls back the coin store, since both stores must be updated at once
-        if self.db.in_transaction:
-            cursor = await self.db.execute("ROLLBACK")
-            await cursor.close()
-
-    async def commit_transaction(self) -> None:
-        await self.db.commit()
-
-
 async def execute_fetchone(
     c: aiosqlite.Connection, sql: str, parameters: Iterable[Any] = None
 ) -> Optional[sqlite3.Row]:
