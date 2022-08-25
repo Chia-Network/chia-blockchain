c393e434f59deac0bfa92a5f200b16a8b37144d3
danieljperry <d.perry@chia.net>
2022-08-25 09:23:42 +0800

    resolve merge conflicts with main

--- tests/wallet/sync/test_wallet_sync.py
+++ tests/wallet/sync/test_wallet_sync.py
@@ -6,6 +6,7 @@ from colorlog import getLogger
 from chia.consensus.block_record import BlockRecord
 from chia.consensus.block_rewards import calculate_base_farmer_reward, calculate_pool_reward
 from chia.full_node.full_node_api import FullNodeAPI
+from chia.full_node.weight_proof import WeightProofHandler
 from chia.protocols import full_node_protocol, wallet_protocol
 from chia.protocols.protocol_message_types import ProtocolMessageTypes
 from chia.protocols.shared_protocol import Capability
@@ -13,21 +14,25 @@ from chia.protocols.wallet_protocol import RequestAdditions, RespondAdditions, R
 from chia.server.outbound_message import Message
 from chia.simulator.simulator_protocol import FarmNewBlockProtocol
 from chia.simulator.time_out_assert import time_out_assert, time_out_assert_not_none
+from chia.types.blockchain_format.program import Program
 from chia.types.peer_info import PeerInfo
+from chia.util.block_cache import BlockCache
 from chia.util.hash import std_hash
 from chia.util.ints import uint16, uint32, uint64
-from chia.wallet.transaction_record import TransactionRecord
-from chia.wallet.util.wallet_types import AmountWithPuzzlehash
-from tests.connection_utils import disconnect_all_and_reconnect
-from tests.pools.test_pool_rpc import wallet_is_synced
-from tests.setup_nodes import test_constants
-from chia.wallet.wallet_coin_record import WalletCoinRecord
 from chia.wallet.nft_wallet.nft_wallet import NFTWallet
+from chia.wallet.transaction_record import TransactionRecord
 from chia.wallet.util.compute_memos import compute_memos
-from chia.types.blockchain_format.program import Program
+from chia.wallet.util.wallet_types import AmountWithPuzzlehash
+from chia.wallet.wallet_coin_record import WalletCoinRecord
+from chia.wallet.wallet_weight_proof_handler import get_wp_fork_point
+from tests.connection_utils import disconnect_all, disconnect_all_and_reconnect
+from tests.setup_nodes import test_constants
+from tests.util.wallet_is_synced import wallet_is_synced
+from tests.weight_proof.test_weight_proof import load_blocks_dont_validate
 
-def wallet_height_at_least(wallet_node, h):
-    height = wallet_node.wallet_state_manager.blockchain.get_peak_height()
+
+async def wallet_height_at_least(wallet_node, h):
+    height = await wallet_node.wallet_state_manager.blockchain.get_finished_sync_up_to()
     if height == h:
         return True
     return False
@@ -40,10 +45,10 @@ log = getLogger(__name__)
 
 class TestWalletSync:
     @pytest.mark.asyncio
-    async def test_request_block_headers(self, bt, wallet_node, default_1000_blocks):
+    async def test_request_block_headers(self, wallet_node, default_1000_blocks):
         # Tests the edge case of receiving funds right before the recent blocks  in weight proof
         full_node_api: FullNodeAPI
-        full_node_api, wallet_node, full_node_server, wallet_server = wallet_node
+        full_node_api, wallet_node, full_node_server, wallet_server, bt = wallet_node
 
         wallet = wallet_node.wallet_state_manager.main_wallet
         ph = await wallet.get_new_puzzlehash()
@@ -86,10 +91,10 @@ class TestWalletSync:
     #     [(10, 8, False, None)],
     # )
     @pytest.mark.asyncio
-    async def test_request_block_headers_rejected(self, bt, wallet_node, default_1000_blocks):
+    async def test_request_block_headers_rejected(self, wallet_node, default_1000_blocks):
         # Tests the edge case of receiving funds right before the recent blocks  in weight proof
         full_node_api: FullNodeAPI
-        full_node_api, wallet_node, full_node_server, wallet_server = wallet_node
+        full_node_api, wallet_node, full_node_server, wallet_server, bt = wallet_node
 
         # start_height, end_height, return_filter, expected_res = test_case
 
@@ -143,8 +148,8 @@ class TestWalletSync:
         indirect=True,
     )
     @pytest.mark.asyncio
-    async def test_basic_sync_wallet(self, bt, two_wallet_nodes, default_400_blocks, self_hostname):
-        full_nodes, wallets = two_wallet_nodes
+    async def test_basic_sync_wallet(self, two_wallet_nodes, default_400_blocks, self_hostname):
+        full_nodes, wallets, bt = two_wallet_nodes
         full_node_api = full_nodes[0]
         full_node_server = full_node_api.full_node.server
 
@@ -165,7 +170,8 @@ class TestWalletSync:
 
         # Tests a reorg with the wallet
         num_blocks = 30
-        blocks_reorg = bt.get_consecutive_blocks(num_blocks, block_list_input=default_400_blocks[:-5])
+        blocks_reorg = bt.get_consecutive_blocks(num_blocks - 1, block_list_input=default_400_blocks[:-5])
+        blocks_reorg = bt.get_consecutive_blocks(1, blocks_reorg, guarantee_transaction_block=True, current_time=True)
         for i in range(1, len(blocks_reorg)):
             await full_node_api.full_node.respond_block(full_node_protocol.RespondBlock(blocks_reorg[i]))
 
@@ -176,6 +182,9 @@ class TestWalletSync:
             await time_out_assert(
                 100, wallet_height_at_least, True, wallet_node, len(default_400_blocks) + num_blocks - 5 - 1
             )
+            await time_out_assert(20, wallet_node.wallet_state_manager.synced)
+            await disconnect_all(wallet_server)
+            assert not (await wallet_node.wallet_state_manager.synced())
 
     @pytest.mark.parametrize(
         "two_wallet_nodes",
@@ -190,9 +199,9 @@ class TestWalletSync:
         indirect=True,
     )
     @pytest.mark.asyncio
-    async def test_almost_recent(self, bt, two_wallet_nodes, default_400_blocks, self_hostname):
+    async def test_almost_recent(self, two_wallet_nodes, default_400_blocks, self_hostname):
         # Tests the edge case of receiving funds right before the recent blocks  in weight proof
-        full_nodes, wallets = two_wallet_nodes
+        full_nodes, wallets, bt = two_wallet_nodes
         full_node_api = full_nodes[0]
         full_node_server = full_node_api.full_node.server
 
@@ -230,7 +239,7 @@ class TestWalletSync:
 
     @pytest.mark.asyncio
     async def test_backtrack_sync_wallet(self, two_wallet_nodes, default_400_blocks, self_hostname):
-        full_nodes, wallets = two_wallet_nodes
+        full_nodes, wallets, _ = two_wallet_nodes
         full_node_api = full_nodes[0]
         full_node_server = full_node_api.full_node.server
 
@@ -252,7 +261,7 @@ class TestWalletSync:
     # Tests a reorg with the wallet
     @pytest.mark.asyncio
     async def test_short_batch_sync_wallet(self, two_wallet_nodes, default_400_blocks, self_hostname):
-        full_nodes, wallets = two_wallet_nodes
+        full_nodes, wallets, _ = two_wallet_nodes
         full_node_api = full_nodes[0]
         full_node_server = full_node_api.full_node.server
 
@@ -272,8 +281,8 @@ class TestWalletSync:
             await time_out_assert(100, wallet_height_at_least, True, wallet_node, 199)
 
     @pytest.mark.asyncio
-    async def test_long_sync_wallet(self, bt, two_wallet_nodes, default_1000_blocks, default_400_blocks, self_hostname):
-        full_nodes, wallets = two_wallet_nodes
+    async def test_long_sync_wallet(self, two_wallet_nodes, default_1000_blocks, default_400_blocks, self_hostname):
+        full_nodes, wallets, bt = two_wallet_nodes
         full_node_api = full_nodes[0]
         full_node_server = full_node_api.full_node.server
 
@@ -299,7 +308,9 @@ class TestWalletSync:
         for wallet_node, wallet_server in wallets:
             await disconnect_all_and_reconnect(wallet_server, full_node_server, self_hostname)
 
-            log.info(f"wallet node height is {wallet_node.wallet_state_manager.blockchain.get_peak_height()}")
+            log.info(
+                f"wallet node height is {await wallet_node.wallet_state_manager.blockchain.get_finished_sync_up_to()}"
+            )
             await time_out_assert(600, wallet_height_at_least, True, wallet_node, len(default_1000_blocks) - 1)
 
             await disconnect_all_and_reconnect(wallet_server, full_node_server, self_hostname)
@@ -317,9 +328,9 @@ class TestWalletSync:
             )
 
     @pytest.mark.asyncio
-    async def test_wallet_reorg_sync(self, bt, two_wallet_nodes, default_400_blocks, self_hostname):
+    async def test_wallet_reorg_sync(self, two_wallet_nodes, default_400_blocks, self_hostname):
         num_blocks = 5
-        full_nodes, wallets = two_wallet_nodes
+        full_nodes, wallets, bt = two_wallet_nodes
         full_node_api = full_nodes[0]
         full_node_server = full_node_api.full_node.server
 
@@ -357,8 +368,8 @@ class TestWalletSync:
 
         for wallet_node, wallet_server in wallets:
             wallet = wallet_node.wallet_state_manager.main_wallet
-            await time_out_assert(5, wallet.get_confirmed_balance, funds)
-            await time_out_assert(5, get_tx_count, 2 * (num_blocks - 1), wallet_node.wallet_state_manager, 1)
+            await time_out_assert(20, wallet.get_confirmed_balance, funds)
+            await time_out_assert(20, get_tx_count, 2 * (num_blocks - 1), wallet_node.wallet_state_manager, 1)
 
         # Reorg blocks that carry reward
         num_blocks = 30
@@ -369,12 +380,12 @@ class TestWalletSync:
 
         for wallet_node, wallet_server in wallets:
             wallet = wallet_node.wallet_state_manager.main_wallet
-            await time_out_assert(5, get_tx_count, 0, wallet_node.wallet_state_manager, 1)
-            await time_out_assert(5, wallet.get_confirmed_balance, 0)
+            await time_out_assert(20, get_tx_count, 0, wallet_node.wallet_state_manager, 1)
+            await time_out_assert(20, wallet.get_confirmed_balance, 0)
 
     @pytest.mark.asyncio
-    async def test_wallet_reorg_get_coinbase(self, bt, two_wallet_nodes, default_400_blocks, self_hostname):
-        full_nodes, wallets = two_wallet_nodes
+    async def test_wallet_reorg_get_coinbase(self, two_wallet_nodes, default_400_blocks, self_hostname):
+        full_nodes, wallets, bt = two_wallet_nodes
         full_node_api = full_nodes[0]
         full_node_server = full_node_api.full_node.server
 
@@ -403,7 +414,7 @@ class TestWalletSync:
             return len(txs)
 
         for wallet_node, wallet_server in wallets:
-            await time_out_assert(10, get_tx_count, 0, wallet_node.wallet_state_manager, 1)
+            await time_out_assert(30, get_tx_count, 0, wallet_node.wallet_state_manager, 1)
             await time_out_assert(30, wallet_is_synced, True, wallet_node, full_node_api)
 
         num_blocks_reorg_1 = 40
@@ -435,7 +446,7 @@ class TestWalletSync:
 
     @pytest.mark.asyncio
     async def test_request_additions_errors(self, wallet_node_sim_and_wallet, self_hostname):
-        full_nodes, wallets = wallet_node_sim_and_wallet
+        full_nodes, wallets, _ = wallet_node_sim_and_wallet
         wallet_node, wallet_server = wallets[0]
         wallet = wallet_node.wallet_state_manager.main_wallet
         ph = await wallet.get_new_puzzlehash()
@@ -476,7 +487,7 @@ class TestWalletSync:
 
     @pytest.mark.asyncio
     async def test_request_additions_success(self, wallet_node_sim_and_wallet, self_hostname):
-        full_nodes, wallets = wallet_node_sim_and_wallet
+        full_nodes, wallets, _ = wallet_node_sim_and_wallet
         wallet_node, wallet_server = wallets[0]
         wallet = wallet_node.wallet_state_manager.main_wallet
         ph = await wallet.get_new_puzzlehash()
@@ -560,6 +571,31 @@ class TestWalletSync:
         response = RespondAdditions.from_bytes(res4.data)
         assert response.proofs == []
         assert len(response.coins) == 0
+
+    @pytest.mark.asyncio
+    async def test_get_wp_fork_point(self, default_10000_blocks):
+        blocks = default_10000_blocks
+        header_cache, height_to_hash, sub_blocks, summaries = await load_blocks_dont_validate(blocks)
+        wpf = WeightProofHandler(test_constants, BlockCache(sub_blocks, header_cache, height_to_hash, summaries))
+        wp1 = await wpf.get_proof_of_weight(header_cache[height_to_hash[uint32(9000)]].header_hash)
+        wp2 = await wpf.get_proof_of_weight(header_cache[height_to_hash[uint32(9030)]].header_hash)
+        wp3 = await wpf.get_proof_of_weight(header_cache[height_to_hash[uint32(7500)]].header_hash)
+        wp4 = await wpf.get_proof_of_weight(header_cache[height_to_hash[uint32(8700)]].header_hash)
+        wp5 = await wpf.get_proof_of_weight(header_cache[height_to_hash[uint32(9700)]].header_hash)
+        fork12 = get_wp_fork_point(test_constants, wp1, wp2)
+        fork13 = get_wp_fork_point(test_constants, wp3, wp1)
+        fork14 = get_wp_fork_point(test_constants, wp4, wp1)
+        fork23 = get_wp_fork_point(test_constants, wp3, wp2)
+        fork24 = get_wp_fork_point(test_constants, wp4, wp2)
+        fork34 = get_wp_fork_point(test_constants, wp3, wp4)
+        fork45 = get_wp_fork_point(test_constants, wp4, wp5)
+        assert fork14 == 8700
+        assert fork24 == 8700
+        assert fork12 == 9000
+        assert fork13 in summaries.keys()
+        assert fork23 in summaries.keys()
+        assert fork34 in summaries.keys()
+        assert fork45 in summaries.keys()
 
     """
     This tests that a wallet filters out the dust properly.
