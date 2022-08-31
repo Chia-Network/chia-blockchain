82931582dea445ba177ffbd87615c5bc17f981f6
Kyle Altendorf <sda@fstab.net>
2022-08-31 08:55:01 -0400

    add poetry-dynamic-versioning

--- Setup-poetry.ps1
+++ Setup-poetry.ps1
@@ -9,5 +9,6 @@ py -$pythonVersion -m venv .penv
 .penv/Scripts/python -m pip install --upgrade pip setuptools wheel
 # TODO: maybe make our own zipapp/shiv/pex of poetry and download that?
 .penv/Scripts/python -m pip install poetry
+.penv/Scripts/poetry self add poetry-dynamic-versioning-plugin@latest
 
 # New-Item -ItemType SymbolicLink -Path "poetry.exe" -Target ".penv/Scripts/poetry.exe"
