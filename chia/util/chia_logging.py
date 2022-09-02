e3b012aa1372dd6bbf49a38d4cce236759989cbe
xdustinface <xdustinfacex@gmail.com>
2022-09-02 11:56:18 +0200

    Use a separate file logger for beta logs

--- chia/util/chia_logging.py
+++ chia/util/chia_logging.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import colorlog
 from concurrent_log_handler import ConcurrentRotatingFileHandler
@@ -22,12 +22,28 @@ def get_beta_logging_config() -> Dict[str, Any]:
     }
 
 
-def initialize_logging(service_name: str, logging_config: Dict, root_path: Path):
-    log_path = path_from_root(root_path, logging_config.get("log_filename", "log/debug.log"))
-    log_date_format = "%Y-%m-%dT%H:%M:%S"
+def get_file_logger(
+    formatter: logging.Formatter, log_path: Path, logging_config: Dict[str, object]
+) -> ConcurrentRotatingFileHandler:
+    maxrotation = logging_config.get("log_maxfilesrotation", 7)
+    maxbytesrotation = logging_config.get("log_maxbytesrotation", 50 * 1024 * 1024)
+    use_gzip = logging_config.get("log_use_gzip", False)
+    handler = ConcurrentRotatingFileHandler(
+        log_path, "a", maxBytes=maxbytesrotation, backupCount=maxrotation, use_gzip=use_gzip
+    )
+    handler.setFormatter(formatter)
+    return handler
 
+
+def initialize_logging(service_name: str, logging_config: Dict, root_path: Path, beta_root_path: Optional[Path] = None):
+    log_path = path_from_root(root_path, logging_config.get("log_filename", "log/debug.log"))
     log_path.parent.mkdir(parents=True, exist_ok=True)
     file_name_length = 33 - len(service_name)
+    log_date_format = "%Y-%m-%dT%H:%M:%S"
+    file_log_formatter = logging.Formatter(
+        fmt=f"%(asctime)s.%(msecs)03d {service_name} %(name)-{file_name_length}s: %(levelname)-8s %(message)s",
+        datefmt=log_date_format,
+    )
     if logging_config["log_stdout"]:
         handler = colorlog.StreamHandler()
         handler.setFormatter(
@@ -43,19 +59,7 @@ def initialize_logging(service_name: str, logging_config: Dict, root_path: Path)
         logger.addHandler(handler)
     else:
         logger = logging.getLogger()
-        maxrotation = logging_config.get("log_maxfilesrotation", 7)
-        maxbytesrotation = logging_config.get("log_maxbytesrotation", 50 * 1024 * 1024)
-        use_gzip = logging_config.get("log_use_gzip", False)
-        handler = ConcurrentRotatingFileHandler(
-            log_path, "a", maxBytes=maxbytesrotation, backupCount=maxrotation, use_gzip=use_gzip
-        )
-        handler.setFormatter(
-            logging.Formatter(
-                fmt=f"%(asctime)s.%(msecs)03d {service_name} %(name)-{file_name_length}s: %(levelname)-8s %(message)s",
-                datefmt=log_date_format,
-            )
-        )
-        logger.addHandler(handler)
+        logger.addHandler(get_file_logger(file_log_formatter, log_path, logging_config))
 
     if logging_config.get("log_syslog", False):
         log_syslog_host = logging_config.get("log_syslog_host", "localhost")
@@ -82,21 +86,26 @@ def initialize_logging(service_name: str, logging_config: Dict, root_path: Path)
     else:
         logger.setLevel(logging.INFO)
 
+    if beta_root_path is not None:
+        beta_logging_config = get_beta_logging_config()
+        beta_log_path = path_from_root(beta_root_path, beta_logging_config["log_filename"])
+        beta_log_path.parent.mkdir(parents=True, exist_ok=True)
+        logger.addHandler(get_file_logger(file_log_formatter, beta_log_path, get_beta_logging_config()))
+
 
 def initialize_service_logging(service_name: str, config: Dict[str, Any]) -> None:
-    if "beta" in config:
-        logging_root_path = config["beta"]["path"]
-        logging_config = get_beta_logging_config()
+    logging_root_path = DEFAULT_ROOT_PATH
+    if service_name == "daemon":
+        # TODO: Maybe introduce a separate `daemon` section in the config instead of having `daemon_port`, `logging`
+        #  and the daemon related stuff as top level entries.
+        logging_config = config["logging"]
     else:
-        logging_root_path = DEFAULT_ROOT_PATH
-        if service_name == "daemon":
-            # TODO: Maybe introduce a separate `daemon` section in the config instead of having `daemon_port`, `logging`
-            #  and the daemon related stuff as top level entries.
-            logging_config = config["logging"]
-        else:
-            logging_config = config[service_name]["logging"]
+        logging_config = config[service_name]["logging"]
+    beta_config = config.get("beta", {})
+    beta_config_path = beta_config.get("path") if beta_config.get("enabled", False) else None
     initialize_logging(
         service_name=service_name,
         logging_config=logging_config,
         root_path=logging_root_path,
+        beta_root_path=beta_config_path,
     )
