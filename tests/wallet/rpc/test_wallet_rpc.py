290ed25031ec8cf80ce556f874e069ed4210537d
almog <almogdepaz@gmail.com>
2022-09-06 17:14:10 +0300

    lint

--- tests/wallet/rpc/test_wallet_rpc.py
+++ tests/wallet/rpc/test_wallet_rpc.py
@@ -75,6 +75,7 @@ async def farm_transaction_block(full_node_api: FullNodeSimulator, wallet_node: 
 def check_mempool_spend_count(full_node_api: FullNodeSimulator, num_of_spends):
     return len(full_node_api.full_node.mempool_manager.mempool.sorted_spends) == num_of_spends
 
+
 async def farm_transaction(full_node_api: FullNodeSimulator, wallet_node: WalletNode, spend_bundle: SpendBundle):
     await time_out_assert(
         20, full_node_api.full_node.mempool_manager.get_spendbundle, spend_bundle, spend_bundle.name()
