5437992f22833b60e7cb9d144021e8978195edd6
arvidn <arvid@libtorrent.org>
2022-09-08 18:43:11 +0200

    reduce log noise

--- chia/wallet/wallet_node.py
+++ chia/wallet/wallet_node.py
@@ -711,9 +711,11 @@ class WalletNode:
 
         # Ensure the list is sorted
 
-        self.log.info(f"Filtering spam, total items: {len(items_input)}")
+        before = len(items_input)
         items = await self.wallet_state_manager.filter_spam(list(sorted(items_input, key=last_change_height_cs)))
-        self.log.info(f"Filtered spam. Total left: {len(items)}")
+        num_filtered = before - len(items)
+        if num_filtered > 0:
+            self.log.info(f"Filtered {num_filtered} spam transactions")
 
         async def receive_and_validate(inner_states: List[CoinState], inner_idx_start: int, cs_heights: List[uint32]):
             try:
