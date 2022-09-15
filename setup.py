e2573c4bd2c20b5e17c1c325c5ee61b812b5eb00
xdustinface <xdustinfacex@gmail.com>
2022-09-15 22:02:48 +0200

    Use fixed `chiapos`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ dependencies = [
     "blspy==1.0.13",  # Signature library
     "chiavdf==1.0.6",  # timelord and vdf verification
     "chiabip158==1.1",  # bip158-style wallet filters
-    "chiapos==1.0.10",  # proof of space
+    "chiapos @ git+https://github.com/Chia-Network/chiapos.git@c2-read-short-circuit",  # proof of space
     "clvm==0.9.7",
     "clvm_tools==0.4.5",  # Currying, Program.to, other conveniences
     "chia_rs==0.1.5",
