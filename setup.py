78d7c38497be3ccbf32359ad90734db228fd1cf9
Kyle Altendorf <sda@fstab.net>
2022-09-16 16:23:07 -0400

    include dev deps for snyk

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from setuptools import setup
 
@@ -151,6 +152,9 @@ kwargs = dict(
     },
 )
 
+if "setup_file" in sys.modules:
+    # include dev deps in regular deps when run in snyk
+    dependencies.extend(dev_dependencies)
 
 if len(os.environ.get("CHIA_SKIP_SETUP", "")) < 1:
     setup(**kwargs)  # type: ignore
