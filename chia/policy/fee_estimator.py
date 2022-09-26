4443e3014dca0c45e2c52103762f562fa746d433
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-09-26 09:48:15 -0700

    simplify new_block

--- chia/policy/fee_estimator.py
+++ chia/policy/fee_estimator.py
@@ -5,7 +5,7 @@ from typing_extensions import Protocol
 from chia.consensus.block_record import BlockRecord
 from chia.policy.fee_estimation import FeeMempoolInfo
 from chia.types.mempool_item import MempoolItem
-from chia.util.ints import uint64
+from chia.util.ints import uint64, uint32
 
 
 class FeeEstimatorConfig:
@@ -18,7 +18,7 @@ class FeeEstimatorConfig:
 
 class FeeEstimatorInterface(Protocol):
     #def new_block(self, block_info: FeeBlockInfo) -> None:
-    def new_block(self, block_info: BlockRecord, included_items: List[MempoolItem]) -> None:
+    def new_block(self, block_height: uint32, included_items: List[MempoolItem]) -> None:
         pass
 
     def add_mempool_item(self, mempool_item_info: FeeMempoolInfo, mempool_item: MempoolItem) -> None:
