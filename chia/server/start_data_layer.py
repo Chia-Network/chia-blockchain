d78c122505352adc0a791860bd410cfe24189aee
dustinface <35775977+xdustinface@users.noreply.github.com>
2022-09-26 18:16:25 +0200

    Drop redundant RPC server instances in DataLayer related RPC tests (#13518)

--- chia/server/start_data_layer.py
+++ chia/server/start_data_layer.py
@@ -10,6 +10,7 @@ from chia.rpc.data_layer_rpc_api import DataLayerRpcApi
 from chia.rpc.wallet_rpc_client import WalletRpcClient
 from chia.server.outbound_message import NodeType
 from chia.server.start_service import RpcInfo, Service, async_run
+from chia.server.start_wallet import WalletNode
 from chia.util.chia_logging import initialize_logging
 from chia.util.config import load_config, load_config_cli
 from chia.util.default_root import DEFAULT_ROOT_PATH
@@ -26,12 +27,19 @@ log = logging.getLogger(__name__)
 def create_data_layer_service(
     root_path: pathlib.Path,
     config: Dict[str, Any],
+    wallet_service: Optional[Service[WalletNode]] = None,
     connect_to_daemon: bool = True,
 ) -> Service[DataLayer]:
     service_config = config[SERVICE_NAME]
     self_hostname = config["self_hostname"]
     wallet_rpc_port = service_config["wallet_peer"]["port"]
-    wallet_rpc_init = WalletRpcClient.create(self_hostname, uint16(wallet_rpc_port), root_path, config)
+    if wallet_service is None:
+        wallet_root_path = root_path
+        wallet_config = config
+    else:
+        wallet_root_path = wallet_service.root_path
+        wallet_config = wallet_service.config
+    wallet_rpc_init = WalletRpcClient.create(self_hostname, uint16(wallet_rpc_port), wallet_root_path, wallet_config)
     data_layer = DataLayer(config=service_config, root_path=root_path, wallet_rpc_init=wallet_rpc_init)
     api = DataLayerAPI(data_layer)
     network_id = service_config["selected_network"]
