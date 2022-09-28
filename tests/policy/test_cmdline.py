8561a9e0ccfaa9f704171f06a758d4113e71db4c
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-09-28 16:07:06 -0700

    Test commandline while node is running

--- tests/policy/test_cmdline.py
+++ tests/policy/test_cmdline.py
@@ -1,6 +1,16 @@
 import os
+from click.testing import CliRunner
+from chia.cmds.chia import cli
 
 
-def test_print_fee_info_cmd() -> None:
+def test_print_fee_info_cmd(one_node_one_block) -> None:
+    full_node_1, server_1, bt = one_node_one_block
     exit_code = os.system("chia show -f")
     assert exit_code == 0
+
+
+def test_show_fee_info(one_node_one_block) -> None:
+    full_node_1, server_1, bt = one_node_one_block
+    runner = CliRunner()
+    result = runner.invoke(cli, ['show', '-f'])
+    assert result.exit_code == 0
