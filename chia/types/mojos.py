79be170bd48eb8c53b034ac5dcf243575925e859
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-10-03 12:37:26 -0700

    Improve class declaration

--- chia/types/mojos.py
+++ chia/types/mojos.py
@@ -2,15 +2,19 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from decimal import Decimal
+from typing import final, Type, TypeVar
 
 from chia.cmds.units import units
 from chia.util.ints import uint64
 
+_T_Mojos = TypeVar("_T_Mojos", bound="Mojos")
+
 
 @dataclass
+@final
 class Mojos:
     """
-    XCH converison: 1 Chia = 1 Trillion Mojos
+    XCH conversion: 1 Chia = 1 Trillion Mojos
     """
 
     mojos: uint64
@@ -19,7 +23,7 @@ class Mojos:
         self.mojos = mojos
 
     @classmethod
-    def from_chia(cls, chia_amount: int) -> Mojos:
+    def from_chia(cls: Type[_T_Mojos], chia_amount: int) -> Mojos:
         xch = Decimal(chia_amount)
         mojos = uint64(int(xch * units["chia"]))
-        return Mojos(mojos)
+        return cls(mojos)
