10075044beba62854bb1f4c1ac7ee8fc7237e2a1
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-10-04 15:07:16 -0700

    Use NewType instead of Mojos class to prevent accidental misuse

--- chia/types/mojos.py
+++ chia/types/mojos.py
@@ -1,29 +1,7 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from decimal import Decimal
-from typing import Type, TypeVar, final
+from typing import NewType
 
-from chia.cmds.units import units
 from chia.util.ints import uint64
 
-_T_Mojos = TypeVar("_T_Mojos", bound="Mojos")
-
-
-@dataclass
-@final
-class Mojos:
-    """
-    XCH conversion: 1 Chia = 1 Trillion Mojos
-    """
-
-    mojos: uint64
-
-    def __init__(self, mojos: uint64):
-        self.mojos = mojos
-
-    @classmethod
-    def from_chia(cls: Type[_T_Mojos], chia_amount: int) -> Mojos:
-        xch = Decimal(chia_amount)
-        mojos = uint64(int(xch * units["chia"]))
-        return cls(mojos)
+Mojos = NewType("Mojos", uint64)
