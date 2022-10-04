9a224d5dad655cd1328f8cf0d2518317ce24c7ca
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-10-04 14:04:22 -0700

    isort

--- chia/types/mojos.py
+++ chia/types/mojos.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from decimal import Decimal
-from typing import final, Type, TypeVar
+from typing import Type, TypeVar, final
 
 from chia.cmds.units import units
 from chia.util.ints import uint64
