1ec733cfc1e96bbb39a963e512f21f35ca9a8f74
Earle Lowe <e.lowe@chia.net>
2022-10-18 17:23:29 -0700

    Simplfied get_full_chia_simulator

--- chia/simulator/simulator_test_tools.py
+++ chia/simulator/simulator_test_tools.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import asyncio
 import sys
-import tempfile
 from pathlib import Path
 from typing import Any, AsyncGenerator, Dict, Optional, Tuple
 
@@ -24,11 +23,11 @@ from chia.simulator.start_simulator import async_main as start_simulator_main
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.bech32m import encode_puzzle_hash
 from chia.util.config import create_default_chia_config, load_config, save_config
+from chia.util.errors import KeychainFingerprintExists
 from chia.util.ints import uint32
 from chia.util.keychain import Keychain
 from chia.util.lock import Lockfile
 from chia.wallet.derive_keys import master_sk_to_wallet_sk
-from tests.util.keyring import TempKeyring
 
 """
 These functions are used to test the simulator.
@@ -42,13 +41,16 @@ def mnemonic_fingerprint(keychain: Keychain) -> Tuple[str, int]:
         "memory riot escape high dragon knock food blade"
     )
     # add key to keychain
-    sk = keychain.add_private_key(mnemonic)
+    try:
+        sk = keychain.add_private_key(mnemonic)
+    except KeychainFingerprintExists:
+        pass
     fingerprint = sk.get_g1().get_fingerprint()
     return mnemonic, fingerprint
 
 
-def get_puzzle_hash_from_key(fingerprint: int, key_id: int = 1) -> bytes32:
-    priv_key_and_entropy = Keychain().get_private_key_by_fingerprint(fingerprint)
+def get_puzzle_hash_from_key(keychain: Keychain, fingerprint: int, key_id: int = 1) -> bytes32:
+    priv_key_and_entropy = keychain.get_private_key_by_fingerprint(fingerprint)
     if priv_key_and_entropy is None:
         raise Exception("Fingerprint not found")
     private_key = priv_key_and_entropy[0]
@@ -62,6 +64,7 @@ def create_config(
     fingerprint: int,
     private_ca_crt_and_key: Tuple[bytes, bytes],
     node_certs_and_keys: Dict[str, Dict[str, Dict[str, bytes]]],
+    keychain: Keychain,
 ) -> Dict[str, Any]:
     # create chia directories
     create_default_chia_config(chia_root)
@@ -96,7 +99,7 @@ def create_config(
     config["full_node"]["rpc_port"] = find_available_listen_port("Node RPC")
     # simulator overrides
     config["simulator"]["key_fingerprint"] = fingerprint
-    config["simulator"]["farming_address"] = encode_puzzle_hash(get_puzzle_hash_from_key(fingerprint), "txch")
+    config["simulator"]["farming_address"] = encode_puzzle_hash(get_puzzle_hash_from_key(keychain, fingerprint), "txch")
     config["simulator"]["plot_directory"] = "test-simulator/plots"
     # save config
     save_config(chia_root, "config.yaml", config)
@@ -115,25 +118,24 @@ async def start_simulator(chia_root: Path, automated_testing: bool = False) -> A
 
 
 async def get_full_chia_simulator(
-    automated_testing: bool = False, chia_root: Optional[Path] = None, config: Optional[Dict[str, Any]] = None
-) -> AsyncGenerator[Tuple[FullNodeSimulator, Path, Dict[str, Any], str, int], None]:
+    chia_root: Path,
+    keychain: Optional[Keychain] = None,
+    automated_testing: bool = False,
+    config: Optional[Dict[str, Any]] = None,
+) -> AsyncGenerator[Tuple[FullNodeSimulator, Path, Dict[str, Any], str, int, Keychain], None]:
     """
-    A chia root directory can be provided, otherwise a temporary one is created.
+    A chia root Path is required.
+    The chia root Path can be a temporary directory (tempfile.TemporaryDirectory)
+    Passing in a Keychain prevents test keys from being added to the default key location
     This test can either be run in automated mode or not, which determines which mode block tools run in.
     This test is fully interdependent and can be used without the rest of the chia test suite.
     Please refer to the documentation for more information.
     """
-    # Create and setup temporary chia directories.
-    temp_dir: Optional[tempfile.TemporaryDirectory[str]] = None
-    if chia_root is None:
-        temp_dir = tempfile.TemporaryDirectory()
-        chia_root = Path(temp_dir.name)
 
-    # this always uses a temporary keyring that is cleaned up at the end.
-    # not clear this is always suitable
-    with Lockfile.create(daemon_launch_lock_path(chia_root)), TempKeyring(
-        user="user-chia-1.8", service="chia-user-chia-1.8"
-    ) as keychain:
+    if keychain is None:
+        keychain = Keychain()
+
+    with Lockfile.create(daemon_launch_lock_path(chia_root)):
 
         mnemonic, fingerprint = mnemonic_fingerprint(keychain)
 
@@ -149,6 +151,7 @@ async def get_full_chia_simulator(
                 fingerprint,
                 ssl_ca_cert_and_key_wrapper.collateral.cert_and_key,
                 ssl_nodes_certs_and_keys_wrapper.collateral.certs_and_keys,
+                keychain,
             )
         crt_path = chia_root / config["daemon_ssl"]["private_crt"]
         key_path = chia_root / config["daemon_ssl"]["private_key"]
@@ -161,9 +164,7 @@ async def get_full_chia_simulator(
         await ws_server.start()  # type: ignore[no-untyped-call]
 
         async for simulator in start_simulator(chia_root, automated_testing):
-            yield simulator, chia_root, config, mnemonic, fingerprint
+            yield simulator, chia_root, config, mnemonic, fingerprint, keychain
 
         await ws_server.stop()
         await shutdown_event.wait()  # wait till shutdown is complete
-        if temp_dir is not None:
-            temp_dir.cleanup()
