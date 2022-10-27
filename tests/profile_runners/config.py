90eae6e3b30a0bcf43a21ffcbf75c73f605b861c
Kyle Altendorf <sda@fstab.net>
2022-10-27 09:40:14 -0400

    avoid unnecessary ci setup via config.py

---
+++ tests/profile_runners/config.py
@@ -0,0 +1,8 @@
+# flake8: noqa: E501
+from __future__ import annotations
+
+parallel = False
+job_timeout = 40
+check_resource_usage = False
+checkout_blocks_and_plots = False
+install_timelord = False
