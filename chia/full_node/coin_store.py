ddd88ee6e1112825105f4c2f05dfc3f81316e6f6
xdustinface <xdustinfacex@gmail.com>
2022-11-17 22:43:57 +0100

    isort: Remove all `chia.full_node` exceptions and sort the related files

--- chia/full_node/coin_store.py
+++ chia/full_node/coin_store.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 import dataclasses
+import logging
 import sqlite3
-from typing import List, Optional, Set, Dict, Any, Tuple, Union
+import time
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
 import typing_extensions
 from aiosqlite import Cursor
@@ -11,12 +13,9 @@ from chia.protocols.wallet_protocol import CoinState
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.coin_record import CoinRecord
-from chia.util.db_wrapper import DBWrapper2, SQLITE_MAX_VARIABLE_NUMBER
-from chia.util.ints import uint32, uint64
 from chia.util.chunks import chunks
-import time
-import logging
-
+from chia.util.db_wrapper import SQLITE_MAX_VARIABLE_NUMBER, DBWrapper2
+from chia.util.ints import uint32, uint64
 from chia.util.lru_cache import LRUCache
 
 log = logging.getLogger(__name__)
