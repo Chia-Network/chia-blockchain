56f848f574bfc6dc70febd7856bb6120884719c7
Earle Lowe <30607889+emlowe@users.noreply.github.com>
2022-11-17 16:16:37 -0800

    Update tests/core/test_daemon_rpc.py
    use config value for heartbeat
    
    Co-authored-by: Kyle Altendorf <sda@fstab.net>

--- tests/core/test_daemon_rpc.py
+++ tests/core/test_daemon_rpc.py
@@ -10,7 +10,7 @@ class TestDaemonRpc:
         ws_server = get_daemon
         config = bt.config
         client = await connect_to_daemon(
-            config["self_hostname"], config["daemon_port"], 50 * 1000 * 1000, bt.get_daemon_ssl_context(), heartbeat=300
+            config["self_hostname"], config["daemon_port"], 50 * 1000 * 1000, bt.get_daemon_ssl_context(), heartbeat=config["daemon_heartbeat"]
         )
         response = await client.get_version()
 
