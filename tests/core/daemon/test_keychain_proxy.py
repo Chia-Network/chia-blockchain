87e00e0bfaf233deaf102a3e831c7c842214b96a
xdustinface <xdustinfacex@gmail.com>
2022-11-18 12:30:31 +0100

    Simplify tests and fix `mypy`

--- tests/core/daemon/test_keychain_proxy.py
+++ tests/core/daemon/test_keychain_proxy.py
@@ -1,113 +1,62 @@
 from __future__ import annotations
 
 import logging
+from dataclasses import replace
+from typing import AsyncGenerator
 
 import pytest
 import pytest_asyncio
 
-from chia.daemon.keychain_proxy import connect_to_keychain_and_validate
-from chia.simulator.setup_services import setup_daemon
-from chia.util.errors import KeychainSecretsMissing
+from chia.daemon.keychain_proxy import KeychainProxy, connect_to_keychain_and_validate
+from chia.simulator.setup_services import BlockTools, setup_daemon
+from chia.util.keychain import KeyData
 
-TEST_MNEMONIC_SEED_1 = (
-    "toilet honey metal act put album cave useless pen dust lumber "
-    "bird magnet trip unveil ranch canoe nation place since choose "
-    "allow network off"
-)
-
-TEST_MNEMONIC_SEED_2 = (
-    "captain broccoli humble dish sister fuel sunset horn tree couch "
-    "author diamond imitate brother dilemma venue powder shaft shrimp "
-    "boring lawsuit symptom uncover raven"
-)
-
-TEST_MNEMONIC_SEED_3 = (
-    "coffee candy pause ginger sadness lunch mom topic sadness space "
-    "around again door taxi pupil garage negative tell mutual cycle "
-    "process profit course basic"
-)
-
-
-TEST_FINGERPRINT_1 = 1328532914
-TEST_FINGERPRINT_2 = 801159606
-TEST_FINGERPRINT_3 = 2524590841
+TEST_KEY_1 = KeyData.generate(label="🚽🍯")
+TEST_KEY_2 = KeyData.generate(label="👨‍✈️🥦")
+TEST_KEY_3 = KeyData.generate(label="☕️🍬")
 
 
 @pytest_asyncio.fixture(scope="function")
-async def keychain_proxy(get_b_tools):
+async def keychain_proxy(get_b_tools: BlockTools) -> AsyncGenerator[KeychainProxy, None]:
     async for daemon in setup_daemon(btools=get_b_tools):
         log = logging.getLogger("keychain_proxy_fixture")
         keychain_proxy = await connect_to_keychain_and_validate(daemon.root_path, log)
+        assert keychain_proxy is not None
         yield keychain_proxy
         await keychain_proxy.close()
 
 
 @pytest_asyncio.fixture(scope="function")
-async def keychain_proxy_with_keys(keychain_proxy):
-    await keychain_proxy.add_private_key(TEST_MNEMONIC_SEED_1, "🚽🍯")
-    await keychain_proxy.add_private_key(TEST_MNEMONIC_SEED_2, "👨‍✈️🥦")
+async def keychain_proxy_with_keys(keychain_proxy: KeychainProxy) -> KeychainProxy:
+    await keychain_proxy.add_private_key(TEST_KEY_1.mnemonic_str(), TEST_KEY_1.label)
+    await keychain_proxy.add_private_key(TEST_KEY_2.mnemonic_str(), TEST_KEY_2.label)
     return keychain_proxy
 
 
 @pytest.mark.asyncio
-async def test_add_private_key(keychain_proxy):
+async def test_add_private_key(keychain_proxy: KeychainProxy) -> None:
     keychain = keychain_proxy
-    await keychain.add_private_key(TEST_MNEMONIC_SEED_3, "☕️🍬")
-    key = await keychain.get_key(TEST_FINGERPRINT_3, include_secrets=True)
-    assert key is not None
-    assert key.fingerprint == TEST_FINGERPRINT_3
-    assert key.mnemonic == TEST_MNEMONIC_SEED_3.split(" ")
-    assert key.label == "☕️🍬"
+    await keychain.add_private_key(TEST_KEY_3.mnemonic_str(), TEST_KEY_3.label)
+    key = await keychain.get_key(TEST_KEY_3.fingerprint, include_secrets=True)
+    assert key == TEST_KEY_3
 
 
+@pytest.mark.parametrize("include_secrets", [True, False])
 @pytest.mark.asyncio
-async def test_get_key(keychain_proxy_with_keys):
+async def test_get_key(keychain_proxy_with_keys: KeychainProxy, include_secrets: bool) -> None:
     keychain = keychain_proxy_with_keys
-    key = await keychain.get_key(TEST_FINGERPRINT_1)
-    assert key is not None
-    assert key.fingerprint == TEST_FINGERPRINT_1
-    assert key.label == "🚽🍯"
-    with pytest.raises(KeychainSecretsMissing):
-        _ = key.mnemonic
+    key = await keychain.get_key(TEST_KEY_1.fingerprint, include_secrets=include_secrets)
+    expected_key = TEST_KEY_1 if include_secrets else replace(TEST_KEY_1, secrets=None)
+    assert key == expected_key
 
 
+@pytest.mark.parametrize("include_secrets", [True, False])
 @pytest.mark.asyncio
-async def test_get_key_with_secrets(keychain_proxy_with_keys):
+async def test_get_keys(keychain_proxy_with_keys: KeychainProxy, include_secrets: bool) -> None:
     keychain = keychain_proxy_with_keys
-    key = await keychain.get_key(TEST_FINGERPRINT_1, include_secrets=True)
-    assert key is not None
-    assert key.fingerprint == TEST_FINGERPRINT_1
-    assert key.mnemonic == TEST_MNEMONIC_SEED_1.split(" ")
-    assert key.label == "🚽🍯"
-
-
-@pytest.mark.asyncio
-async def test_get_keys(keychain_proxy_with_keys):
-    keychain = keychain_proxy_with_keys
-    keys = await keychain.get_keys()
-    assert len(keys) == 2
-    assert keys[0].fingerprint == TEST_FINGERPRINT_1
-    assert keys[0].label == "🚽🍯"
-    assert keys[1].fingerprint == TEST_FINGERPRINT_2
-    assert keys[1].label == "👨‍✈️🥦"
-    with pytest.raises(KeychainSecretsMissing):
-        _ = keys[0].mnemonic
-    with pytest.raises(KeychainSecretsMissing):
-        _ = keys[1].mnemonic
-
-
-@pytest.mark.asyncio
-async def test_get_keys_with_secrets(keychain_proxy_with_keys):
-    keychain = keychain_proxy_with_keys
-    keys = await keychain.get_keys(include_secrets=True)
-    assert len(keys) == 2
-    assert keys[0].fingerprint == TEST_FINGERPRINT_1
-    assert keys[0].label == "🚽🍯"
-    assert keys[0].mnemonic == TEST_MNEMONIC_SEED_1.split(" ")
-    assert keys[0].entropy is not None
-    assert keys[0].private_key.get_g1().get_fingerprint() == TEST_FINGERPRINT_1
-    assert keys[1].fingerprint == TEST_FINGERPRINT_2
-    assert keys[1].label == "👨‍✈️🥦"
-    assert keys[1].mnemonic == TEST_MNEMONIC_SEED_2.split(" ")
-    assert keys[1].entropy is not None
-    assert keys[1].private_key.get_g1().get_fingerprint() == TEST_FINGERPRINT_2
+    keys = await keychain.get_keys(include_secrets=include_secrets)
+    if include_secrets:
+        expected_keys = [TEST_KEY_1, TEST_KEY_2]
+    else:
+        expected_keys = [replace(TEST_KEY_1, secrets=None), replace(TEST_KEY_2, secrets=None)]
+    assert keys == expected_keys
