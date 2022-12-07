a494105047f7522d65de6e3c5e4feb38cc6c949d
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-12-07 01:41:37 -0800

    Update tests

--- tests/type_checking/test_type_conversion.py
+++ tests/type_checking/test_type_conversion.py
@@ -2,27 +2,36 @@ from __future__ import annotations
 
 import pytest
 
-from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.types.borderlands import PrivateKeyBytes, bytes_to_PrivateKeyBytes
+from chia.types.blockchain_format.sized_bytes import bytes48
+from chia.types.borderlands import PublicKeyBytes, bytes_to_PublicKeyBytes
 
 
 def test_wrong_input_type() -> None:
     with pytest.raises(TypeError):
-        _ = bytes_to_PrivateKeyBytes("hi")  # type: ignore[arg-type]
+        _ = bytes_to_PublicKeyBytes("hi")  # type: ignore[arg-type]
 
 
 def test_unknown_type() -> None:
     with pytest.raises(ValueError):
-        _ = bytes_to_PrivateKeyBytes(b"")
+        _ = bytes_to_PublicKeyBytes(b"")
 
 
 def test_underlying_typecheck() -> None:
     with pytest.raises(ValueError):
-        _ = bytes_to_PrivateKeyBytes(b"")
+        _ = bytes_to_PublicKeyBytes(b"")
+
+
+def test_incorrect_number_of_bytes() -> None:
+    with pytest.raises(ValueError):
+        _ = bytes_to_PublicKeyBytes(b"a" * 32)
 
 
 def test_type_conversion() -> None:
-    a = bytes_to_PrivateKeyBytes(b"a" * 32)
-    assert a == PrivateKeyBytes(
-        bytes32(bytes.fromhex("6161616161616161616161616161616161616161616161616161616161616161"))
+    a = bytes_to_PublicKeyBytes(b"a" * 48)
+    assert a == PublicKeyBytes(
+        bytes48(
+            bytes.fromhex(
+                "6161616161616161616161616161616161616161616161616161616161616161" "61616161616161616161616161616161"
+            )
+        )
     )
