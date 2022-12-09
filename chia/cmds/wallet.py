0b99fc563ba4acd60e61a6b1bf88a53172126413
Earle Lowe <e.lowe@chia.net>
2022-12-09 14:34:24 -0800

    match click with parameter

--- chia/cmds/wallet.py
+++ chia/cmds/wallet.py
@@ -174,7 +174,7 @@ def get_transactions_cmd(
 )
 @click.option(
     "--exclude-coin",
-    "exclude_coins",
+    "coins_to_exclude",
     multiple=True,
     help="Exclude this coin from being spent.",
 )
