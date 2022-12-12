8a3830dc643fbc1eb98d3e128ba35f2562319fb0
almog <almogdepaz@gmail.com>
2022-12-12 11:58:33 +0200

    use monkeypatch

--- tests/core/server/test_server.py
+++ tests/core/server/test_server.py
@@ -4,7 +4,7 @@ import logging
 from typing import Tuple
 import pytest
 from chia.full_node.full_node_api import FullNodeAPI
-from chia.protocols.shared_protocol import Capability, capabilities
+from chia.protocols.shared_protocol import Capability
 from chia.server import ws_connection
 from chia.server.server import ChiaServer
 from chia.simulator.block_tools import BlockTools
@@ -24,26 +24,28 @@ async def test_duplicate_client_connection(
 
 
 @pytest.mark.asyncio
-async def test_capabilities_back_comp_before_17(
+async def test_capabilities_back_comp_before_17(  # type: ignore
     monkeypatch, two_nodes: Tuple[FullNodeAPI, FullNodeAPI, ChiaServer, ChiaServer, BlockTools], self_hostname: str
 ) -> None:
-    def getverold():
+    def getverold() -> str:
         return "1.6.0b657"
+
     monkeypatch.setattr(ws_connection, "chia_full_version_str", getverold)
     _, _, server_1, server_2, _ = two_nodes
     assert await server_2.start_client(PeerInfo(self_hostname, uint16(server_1._port)), None)
     con = server_2.get_connections()[0]
     assert con.has_capability(Capability.NONE_RESPONSE) is False
 
+
 @pytest.mark.asyncio
-async def test_capabilities_back_comp_fix_17_or_above(
+async def test_capabilities_back_comp_fix_17_or_above(  # type: ignore
     monkeypatch, two_nodes: Tuple[FullNodeAPI, FullNodeAPI, ChiaServer, ChiaServer, BlockTools], self_hostname: str
 ) -> None:
-    def getver17():
+    def getver17() -> str:
         return "1.7.0b657"
+
     monkeypatch.setattr(ws_connection, "chia_full_version_str", getver17)
     _, _, server_1, server_2, _ = two_nodes
     assert await server_2.start_client(PeerInfo(self_hostname, uint16(server_1._port)), None)
     con = server_2.get_connections()[0]
     assert con.has_capability(Capability.NONE_RESPONSE) is True
-
