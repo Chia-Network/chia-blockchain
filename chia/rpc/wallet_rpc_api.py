ffd02b2f807f771d0a3a146afd4f02a030e411cd
Matt Hauff <quexington@gmail.com>
2022-12-13 21:46:16 -0700

    Return existing CAT wallet instead of raising (#14101)
    * Return existing CAT wallet instead of raising
    
    There's no reason to raise here, all the caller is looking for is an instance of the CATWallet that they're looking for and to create it if it doesn't exist.
    
    * rename create_wallet_for_cat -> get_or_...
    
    * Move wallet created noti inside of function
    
    * remove redundant call

--- chia/rpc/wallet_rpc_api.py
+++ chia/rpc/wallet_rpc_api.py
@@ -588,10 +588,9 @@ class WalletRpcApi:
 
             elif request["mode"] == "existing":
                 async with self.service.wallet_state_manager.lock:
-                    cat_wallet = await CATWallet.create_wallet_for_cat(
+                    cat_wallet = await CATWallet.get_or_create_wallet_for_cat(
                         wallet_state_manager, main_wallet, request["asset_id"], name
                     )
-                self.service.wallet_state_manager.state_changed("wallet_created")
                 return {"type": cat_wallet.type(), "asset_id": request["asset_id"], "wallet_id": cat_wallet.id()}
 
             else:  # undefined mode
