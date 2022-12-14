fcbba8a81fc272123fbcffbcc9edfd78591b4e22
Adam Kelly <338792+aqk@users.noreply.github.com>
2022-12-14 13:01:04 -0800

    Typechecking and code organization

---
+++ chia/wallet/dao_wallet/dao_info.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Tuple, Optional
+
+from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.util.ints import uint64
+from chia.util.streamable import streamable, Streamable
+from chia.wallet.dao_wallet.dao_wallet import ProposalInfo
+from chia.wallet.lineage_proof import LineageProof
+
+
+@streamable
+@dataclass(frozen=True)
+class DAOInfo(Streamable):
+    treasury_id: bytes32
+    cat_wallet_id: uint64
+    proposals_list: List[ProposalInfo]
+    parent_info: List[Tuple[bytes32, Optional[LineageProof]]]  # {coin.name(): LineageProof}
