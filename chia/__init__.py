83e4352d769b13c1e9b5bb7049e393ca92d2963d
Kyle Altendorf <sda@fstab.net>
2022-12-14 15:24:33 -0500

    add some code for testing diff coverage reporting

--- chia/__init__.py
+++ chia/__init__.py
@@ -9,3 +9,16 @@ except DistributionNotFound:
     __version__ = "unknown"
 
 PYINSTALLER_SPEC_PATH = resource_filename("chia", "pyinstaller.spec")
+
+def f() -> None:
+    x = 0
+
+    if False:
+        # a not covered line
+        x = 1
+
+    if True:
+        # this line is covered, but the not-entering branch is not covered
+        x = 2
+
+f()
