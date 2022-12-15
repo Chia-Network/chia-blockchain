d166214e9b2802ad29c56ec56c0c39dfa6496ea2
Kyle Altendorf <sda@fstab.net>
2022-12-14 19:39:57 -0500

    Update __init__.py

--- chia/__init__.py
+++ chia/__init__.py
@@ -10,15 +10,17 @@ except DistributionNotFound:
 
 PYINSTALLER_SPEC_PATH = resource_filename("chia", "pyinstaller.spec")
 
+f = False
+t = True
 
 def f() -> int:
     x = 0
 
-    if False:
+    if f:
         # a not covered line
         x = 1
 
-    if True:
+    if t:
         # this line is covered, but the not-entering branch is not covered
         x = 2
 
