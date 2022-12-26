4e21a727a6500ab565dfbcfec40e0c6a3aaf7421
Kyle Altendorf <sda@fstab.net>
2022-12-26 13:44:49 -0500

    datalayer too

--- chia/data_layer/data_layer_api.py
+++ chia/data_layer/data_layer_api.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 import logging
+from typing import ClassVar
 
 from chia.data_layer.data_layer import DataLayer
+from chia.server.outbound_message import NodeType
 from chia.server.server import ChiaServer
+from chia.util.api_decorators import ApiNodeMetadata
 
 
 class DataLayerAPI:
+    metadata: ClassVar[ApiNodeMetadata] = ApiNodeMetadata(type=NodeType.DATA_LAYER)
     data_layer: DataLayer
 
     def __init__(self, data_layer: DataLayer) -> None:
