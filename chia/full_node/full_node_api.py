c6bff494682c5de86adc9ede5f65b19163ffc868
Kyle Altendorf <sda@fstab.net>
2023-01-04 10:19:22 -0500

    use config.get("trusted_peers", {}) for wallet

--- chia/full_node/full_node_api.py
+++ chia/full_node/full_node_api.py
@@ -1594,4 +1594,4 @@ class FullNodeAPI:
         return msg
 
     def is_trusted(self, peer: WSChiaConnection) -> bool:
-        return self.server.is_trusted_peer(peer, self.full_node.config["trusted_peers"])
+        return self.server.is_trusted_peer(peer, self.full_node.config.get("trusted_peers", {}))
