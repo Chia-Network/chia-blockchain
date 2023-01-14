ec427db3a286e366839c01031223c54085a19c11
ytx1991 <t.yu@chia.net>
2023-01-13 17:59:58 -0800

    Improve unit test

--- tests/wallet/nft_wallet/test_nft_wallet.py
+++ tests/wallet/nft_wallet/test_nft_wallet.py
@@ -1281,6 +1281,8 @@ async def test_nft_bulk_set_did(self_hostname: str, two_wallet_nodes: Any, trust
     nft_coin_list = [
         {"wallet_id": nft_wallet_0_id, "nft_coin_id": nft1.nft_coin_id.hex()},
         {"wallet_id": nft_wallet_1_id, "nft_coin_id": nft2.nft_coin_id.hex()},
+        {"wallet_id": nft_wallet_1_id},
+        {"nft_coin_id": nft2.nft_coin_id.hex()},
     ]
     resp = await api_0.nft_set_did_bulk(dict(did_id=hmr_did_id, nft_coin_list=nft_coin_list))
     coins_response = await wait_rpc_state_condition(
@@ -1422,6 +1424,8 @@ async def test_nft_bulk_transfer(two_wallet_nodes: Any, trusted: Any) -> None:
     nft_coin_list = [
         {"wallet_id": nft_wallet_0_id, "nft_coin_id": nft1.nft_coin_id.hex()},
         {"wallet_id": nft_wallet_1_id, "nft_coin_id": nft2.nft_coin_id.hex()},
+        {"wallet_id": nft_wallet_1_id},
+        {"nft_coin_id": nft2.nft_coin_id.hex()},
     ]
     resp = await api_0.nft_transfer_bulk(dict(target_address=address, nft_coin_list=nft_coin_list))
     sb = await make_new_block_with(resp, full_node_api, ph)
