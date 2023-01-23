722e110948552be2c0075c528419375df105e123
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-01-23 08:58:50 -0800

    Make test faster, more reliable

--- tests/util/test_logging_filter.py
+++ tests/util/test_logging_filter.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import time
 from re import split
 from time import sleep
 
@@ -11,17 +12,21 @@ from chia.util.logging import TimedDuplicateFilter
 
 def test_logging_filter(caplog: pytest.LogCaptureFixture) -> None:
 
-    log_interval_secs = 10
+    log_interval_secs = 1
     num_logs = 11
-    sleep_secs = 1
+    sleep_secs = 0.095
 
     log = logging.getLogger()
     log.addFilter(TimedDuplicateFilter("Filter this log message.*", log_interval_secs))
+    last_time = time.monotonic()
 
     for n in range(num_logs):
         with caplog.at_level(logging.WARNING):
             log.warning(f"Filter this log message {n}")
-            sleep(sleep_secs)
+            now = time.monotonic()
+            print(now - last_time)
+            sleep(min(0.0, now - last_time))
+            last_time = now
 
     assert len(split("\n", caplog.text)) <= ((num_logs * sleep_secs) / log_interval_secs) + 1
 
