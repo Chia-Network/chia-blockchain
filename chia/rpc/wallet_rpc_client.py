99fbb056e12a01bcb5971a021043b28fe0f2832f
Jack Nelson <j.nelson@chia.net>
2023-01-24 20:33:25 -0500

    Fix chia wallet coins cli bugs (#14388)
    * fix fee bug
    
    The fee should not be taken into account when dealing with CAT's or other non XCH coins.
    
    * fix broken CAT removals
    
    it was never implemented ...
    
    * Update wallet_rpc_api.py

--- chia/rpc/wallet_rpc_client.py
+++ chia/rpc/wallet_rpc_client.py
@@ -635,6 +635,7 @@ class WalletRpcClient(RpcClient):
         exclude_amounts: Optional[List[uint64]] = None,
         exclude_coin_ids: Optional[List[str]] = None,
         additions: Optional[List[Dict[str, Any]]] = None,
+        removals: Optional[List[Coin]] = None,
     ) -> TransactionRecord:
         send_dict: Dict[str, Any] = {
             "wallet_id": wallet_id,
@@ -657,6 +658,8 @@ class WalletRpcClient(RpcClient):
             send_dict["additions"] = additions_hex
         else:
             raise ValueError("Must specify either amount and inner_address or additions")
+        if removals is not None and len(removals) > 0:
+            send_dict["coins"] = [c.to_json_dict() for c in removals]
         res = await self.fetch("cat_spend", send_dict)
         return TransactionRecord.from_json_dict_convenience(res["transaction"])
 
