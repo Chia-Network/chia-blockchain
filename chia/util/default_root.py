11237dd0837f215662f7295e06b364f5c7312f9b
ChiaAutomation <automation@chia.net>
2023-02-10 22:35:50 +0000

    testnet config for ips-2023-02-10

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -3,6 +3,6 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
-DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
+DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/ips-2023-02-10"))).resolve()
 
 DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
