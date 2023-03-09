df7e1cacc71ad0c7de982a247accbda1397612b8
wallentx <william.allentx@gmail.com>
2023-03-09 02:49:02 -0600

    Cleanup completions command

--- chia/cmds/completion.py
+++ chia/cmds/completion.py
@@ -6,30 +6,38 @@ import subprocess
 import click
 
 SHELLS = ['bash', 'zsh', 'fish']
+shell = os.environ.get('SHELL', '').split('/')[-1]
+cmd = f'_CHIA_COMPLETE={shell}_source chia'
 
-@click.group()
+@click.group(
+    short_help="Generate shell completion",
+)
 def completion() -> None:
     pass
 
 
-@completion.command()
+@completion.command(
+    short_help='Generate shell completion code'
+)
 @click.option(
     "-s",
     "--shell",
     type=click.Choice(SHELLS),
-    default=os.environ.get('SHELL', '').split('/')[-1],
+    default=shell,
     show_default=False,
-    required=True,
-    help="Shell type to generate completion for",
+    required=False,
+    help="Shell type to generate for",
 )
 def generate(shell: str) -> None:
     """
-    Generate shell completion script
+    \b
+    Generate shell completion code for the current, or specified (-s)hell.
+    You will need to 'source' this code to enable shell completion.
+    You can source it directly (performs slower) by running:
+        \033[3;33meval "$(chia complete generate)"\033[0m
+    or you can save the output to a file:
+        \033[3;33mchia complete generate > ~/.chia-complete-bash\033[0m
+    and source that file with:
+        \033[3;33m. ~/.chia-complete-bash\033[0m
     """
-    if shell is None:
-        shell = os.environ.get('SHELL', '').split('/')[-1]
-    if shell not in SHELLS:
-        raise click.UsageError(f'Invalid shell type: {shell}')
-    cmd = f'_CHIA_COMPLETE={shell}_source chia'
-    print("Save the following output as '~/.chia-completion."+shell+"' and source it according to your shell:\n")
     subprocess.run(cmd, shell=True, check=True)
