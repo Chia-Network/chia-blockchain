b8c9ed8ec00c3b47ae260d85fb3425e9cf3009f4
xdustinface <xdustinfacex@gmail.com>
2023-03-15 12:25:18 +0100

    Drop list creation, `lookup_coin_ids` is already a list

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -1171,7 +1171,7 @@ class FullNode:
         lookup_coin_ids: List[bytes32],
     ) -> None:
         # Looks up coin records in DB for the coins that wallets are interested in
-        new_states: List[CoinRecord] = await self.coin_store.get_coin_records(list(lookup_coin_ids))
+        new_states: List[CoinRecord] = await self.coin_store.get_coin_records(lookup_coin_ids)
 
         # Re-arrange to a map, and filter out any non-ph sized hint
         coin_id_to_ph_hint: Dict[bytes32, bytes32] = {
