4c20dee9f615fb523a6a565ce89ad782cb67f079
xdustinface <xdustinfacex@gmail.com>
2023-03-15 11:43:47 +0100

    full_node: Just add `new_states` since the entries can't be `None`

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -1179,7 +1179,7 @@ class FullNode:
         }
 
         changes_for_peer: Dict[bytes32, Set[CoinState]] = {}
-        for coin_record in state_change_summary.rolled_back_records + [s for s in new_states if s is not None]:
+        for coin_record in state_change_summary.rolled_back_records + new_states:
             cr_name: bytes32 = coin_record.name
 
             for peer in self.subscriptions.peers_for_coin_id(cr_name):
