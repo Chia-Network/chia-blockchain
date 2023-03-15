76d452f5056246cdccf96f8d236dcfeba7ac0451
Kyle Altendorf <sda@fstab.net>
2023-03-15 16:05:30 -0400

    delayed formatting for debug logging

--- chia/full_node/full_node_api.py
+++ chia/full_node/full_node_api.py
@@ -132,7 +132,7 @@ class FullNodeAPI:
             async with self.full_node.new_peak_sem.acquire():
                 await self.full_node.new_peak(request, peer)
         except LimitedSemaphoreFullError:
-            self.log.debug(f"Ignoring NewPeak, limited semaphore full: {peer.get_peer_logging()} {request}")
+            self.log.debug(f"Ignoring NewPeak, limited semaphore full: %s %s", peer.get_peer_logging(), request)
             return None
 
         return None
@@ -1419,7 +1419,7 @@ class FullNodeAPI:
 
         name = std_hash(request_bytes)
         if name in self.full_node.compact_vdf_requests:
-            self.log.debug(f"Ignoring NewCompactVDF, already requested: {peer.get_peer_logging()} {request}")
+            self.log.debug(f"Ignoring NewCompactVDF, already requested: %s %s", peer.get_peer_logging(), request)
             return None
         self.full_node.compact_vdf_requests.add(name)
 
@@ -1432,7 +1432,7 @@ class FullNodeAPI:
                 finally:
                     self.full_node.compact_vdf_requests.remove(name)
         except LimitedSemaphoreFullError:
-            self.log.debug(f"Ignoring NewCompactVDF, limited semaphore full: {peer.get_peer_logging()} {request}")
+            self.log.debug(f"Ignoring NewCompactVDF, limited semaphore full: %s %s", peer.get_peer_logging(), request)
             return None
 
         return None
