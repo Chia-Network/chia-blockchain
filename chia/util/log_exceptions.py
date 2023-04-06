cd6fc22b30a6ab2c13482338b10f6fd74b1ef5ed
Kyle Altendorf <sda@fstab.net>
2023-04-06 08:54:28 -0400

    Add flexibility to `log_exceptions()`
    Allows configuration of the exception or exceptions to be caught, whether or not to show the traceback, the log level, and also adds the exception type to aid readability of errors like `KeyError` where the only text is the value of the key requested.

--- chia/util/log_exceptions.py
+++ chia/util/log_exceptions.py
@@ -3,13 +3,26 @@ from __future__ import annotations
 import logging
 import traceback
 from contextlib import contextmanager
+from typing import Union
 
 
 @contextmanager
-def log_exceptions(log: logging.Logger, *, consume: bool = False):
+def log_exceptions(
+    log: logging.Logger,
+    *, 
+    consume: bool = False,
+    level: int = logging.ERROR,
+    show_traceback: bool = True,
+    exceptions_to_catch: Union[Type[BaseException], Tuple[Type[BaseException], ...]] = Exception,
+ ):
     try:
         yield
-    except Exception as e:
-        log.error(f"Caught Exception: {e}. Traceback: {traceback.format_exc()}")
+    except exceptions_to_catch as e:
+        message = f"Caught exception: {type(e).__name__}: {e}"
+        if show_traceback:
+            message += f"\n{traceback.format_exc()}"
+
+        log.log(level, message)
+
         if not consume:
             raise
