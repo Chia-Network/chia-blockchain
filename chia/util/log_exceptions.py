c2e947c9ca929f92f57139b2e13b4f5ae1d0797f
Kyle Altendorf <sda@fstab.net>
2023-04-07 16:26:33 -0400

    add tests for `chia.util.log_exceptions()`

--- chia/util/log_exceptions.py
+++ chia/util/log_exceptions.py
@@ -3,22 +3,22 @@ from __future__ import annotations
 import logging
 import traceback
 from contextlib import contextmanager
-from typing import Union
+from typing import Tuple, Type, Union
 
 
 @contextmanager
 def log_exceptions(
     log: logging.Logger,
-    *, 
+    *,
     consume: bool = False,
     message: str = "Caught exception",
     level: int = logging.ERROR,
     show_traceback: bool = True,
-    exceptions_to_catch: Union[Type[BaseException], Tuple[Type[BaseException], ...]] = Exception,
- ):
+    exceptions_to_process: Union[Type[BaseException], Tuple[Type[BaseException], ...]] = Exception,
+):
     try:
         yield
-    except exceptions_to_catch as e:
+    except exceptions_to_process as e:
         message = f"{message}: {type(e).__name__}: {e}"
         if show_traceback:
             message += f"\n{traceback.format_exc()}"
