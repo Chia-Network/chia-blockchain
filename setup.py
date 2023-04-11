5919fd71662fc76e6ad312eaa3698064acfe97e4
Matt Hauff <quexington@gmail.com>
2023-04-11 12:04:28 -0700

    Fix setup.py package_data fields

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ kwargs = dict(
     },
     package_data={
         "chia": ["pyinstaller.spec"],
-        "": ["*.clsp", "*.clsp.hex", "*.clvm", "*.clib", "*.clinc", "*.clsp", "py.typed"],
+        "": ["*.clsp", "*.clsp.hex", "*.clvm", "*.clib", "py.typed"],
         "chia.util": ["initial-*.yaml", "english.txt"],
         "chia.ssl": ["chia_ca.crt", "chia_ca.key", "dst_root_ca.pem"],
         "mozilla-ca": ["cacert.pem"],
