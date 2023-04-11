6503b26ec326582fd6c0bd9fdef3cef71c1b3036
Matt Hauff <quexington@gmail.com>
2023-04-11 10:13:46 -0700

    debug

--- tools/manage_clvm.py
+++ tools/manage_clvm.py
@@ -356,7 +356,7 @@ def check(use_cache: bool, fix_hashfile_trailing_whitespace: bool) -> int:
         file_output = json.dumps(HASHES, indent=4, sort_keys=True) + '\n'
         # print(f"New File: {file_output[-10:]}")
         assert og_file == file_output
-        hashes_path.write_text(json.dumps(HASHES, indent=4, sort_keys=True) + "\n")
+        # hashes_path.write_text(json.dumps(HASHES, indent=4, sort_keys=True) + "\n")
 
     return 1 if overall_fail else 0
 
