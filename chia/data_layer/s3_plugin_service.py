e17958b73e85bcc3111565afd97c892dcdd6f963
Earle Lowe <e.lowe@chia.net>
2023-04-12 14:20:34 -0700

    add_store_id and remove_store_id endpoints

--- chia/data_layer/s3_plugin_service.py
+++ chia/data_layer/s3_plugin_service.py
@@ -4,7 +4,10 @@ import asyncio
 import concurrent.futures
 import functools
 import logging
+import os
+import shutil
 import sys
+import tempfile
 from pathlib import Path
 from typing import Any, Dict, List
 from urllib.parse import urlparse
@@ -17,6 +20,8 @@ from botocore.exceptions import ClientError
 from chia.types.blockchain_format.sized_bytes import bytes32
 
 log = logging.getLogger(__name__)
+plugin_id = "2bdf9ed6-f3bc-44f6-a69d-06602c0688d9"
+plugin_version = "Chia S3 Datalayer plugin 0.1.0"
 
 
 class S3Plugin:
@@ -50,6 +55,42 @@ class S3Plugin:
         self.buckets = buckets
         self.urls = urls
         self.instance_name = instance_name
+
+    async def add_store_id(self, request: web.Request) -> web.Response:
+        self.update_instance_from_config()
+        try:
+            data = await request.json()
+        except Exception as e:
+            print(f"failed parsing request {request} {e}")
+            return web.json_response({"success": False})
+        store_id = bytes32.from_hexstr(data["id"])
+        if store_id not in self.store_ids:
+            self.store_ids.append(store_id)
+            try:
+                self.update_config()
+            except Exception as e:
+                print(f"failed handling request {request} {e}")
+                return web.json_response({"success": False})
+
+        return web.json_response({"success": True, "id": store_id.hex()})
+
+    async def remove_store_id(self, request: web.Request) -> web.Response:
+        self.update_instance_from_config()
+        try:
+            data = await request.json()
+        except Exception as e:
+            print(f"failed parsing request {request} {e}")
+            return web.json_response({"success": False})
+        store_id = bytes32.from_hexstr(data["id"])
+        try:
+            self.store_ids.remove(store_id)
+            self.update_config()
+        except Exception as e:
+            if not isinstance(e, ValueError):
+                print(f"failed handling request {request} {e}")
+                return web.json_response({"success": False})
+
+        return web.json_response({"success": True, "id": store_id.hex()})
 
     async def check_store_id(self, request: web.Request) -> web.Response:
         self.update_instance_from_config()
@@ -86,6 +127,12 @@ class S3Plugin:
             print(f"failed handling request {request} {e}")
             return web.json_response({"handles_url": False})
         return web.json_response({"uploaded": True})
+
+    async def plugin_id(self, request: web.Request) -> web.Response:
+        return web.json_response({"id": plugin_id, "version": plugin_version})
+
+    async def healthz(self, request: web.Request) -> web.Response:
+        return web.json_response({"success": True})
 
     async def check_url(self, request: web.Request) -> web.Response:
         self.update_instance_from_config()
@@ -127,12 +174,37 @@ class S3Plugin:
 
     def update_instance_from_config(self) -> None:
         config = load_config(self.instance_name)
-        store_ids = config["store_ids"]
+        # TODO: dedup from startup code in make_app
+        store_ids = []
+        for store in config["store_ids"]:
+            store_ids.append(bytes32.from_hexstr(store))
         buckets: Dict[str, List[str]] = config["buckets"]
         urls = config["urls"]
         self.buckets = buckets
         self.store_ids = store_ids
         self.urls = urls
+
+    def update_config(self) -> None:
+        store_ids = []
+        for store_id in self.store_ids:
+            store_ids.append(store_id.hex())
+
+        with open("s3_plugin_config.yml", "r") as file:
+            full_config = yaml.safe_load(file)
+
+        full_config[self.instance_name]["store_ids"] = store_ids
+        self.save_config("s3_plugin_config.yml", full_config)
+
+    def save_config(self, filename: str, config_data: Any) -> None:
+        path: Path = Path(filename)
+        with tempfile.TemporaryDirectory(dir=path.parent) as tmp_dir:
+            tmp_path: Path = Path(tmp_dir) / Path(filename)
+            with open(tmp_path, "w") as f:
+                yaml.safe_dump(config_data, f)
+            try:
+                os.replace(str(tmp_path), path)
+            except PermissionError:
+                shutil.move(str(tmp_path), str(path))
 
 
 def make_app(config: Dict[str, Any], instance_name: str):  # type: ignore
@@ -150,6 +222,11 @@ def make_app(config: Dict[str, Any], instance_name: str):  # type: ignore
     app.add_routes([web.post("/upload", s3_client.upload)])
     app.add_routes([web.post("/check_url", s3_client.check_url)])
     app.add_routes([web.post("/download", s3_client.download)])
+    app.add_routes([web.post("/plugin_id", s3_client.plugin_id)])
+    app.add_routes([web.post("/healthz", s3_client.healthz)])
+    app.add_routes([web.post("/add_store_id", s3_client.add_store_id)])
+    app.add_routes([web.post("/remove_store_id", s3_client.remove_store_id)])
+
     return app
 
 
