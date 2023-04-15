9e6efba4472190bcd21e0dc9e6ecdf66688d6412
xdustinface <xdustinfacex@gmail.com>
2023-04-15 10:34:13 +0200

    Introduce sized int enums

--- chia/util/ints.py
+++ chia/util/ints.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from enum import ReprEnum
+from typing import Union
+
 from chia.util.struct_stream import StructStream, parse_metadata_from_name
 
 
@@ -61,3 +64,30 @@ class int512(StructStream):
     # [-INT512_MAX, INT512_MAX]
     MAXIMUM_EXCLUSIVE = 2**BITS
     MINIMUM = -(2**BITS) + 1
+
+
+class Int8Enum(int8, ReprEnum):
+    pass
+
+
+class Int16Enum(int16, ReprEnum):
+    pass
+
+
+class Int32Enum(int32, ReprEnum):
+    pass
+
+
+class UInt8Enum(uint8, ReprEnum):
+    pass
+
+
+class UInt16Enum(uint16, ReprEnum):
+    pass
+
+
+class UInt32Enum(uint32, ReprEnum):
+    pass
+
+
+SizedIntEnum = Union[Int8Enum, Int16Enum, Int32Enum, UInt8Enum, UInt16Enum, UInt32Enum]
