27c9d5c8eda6905f9936154edae322eb0e032ba4
arvidn <arvid@libtorrent.org>
2023-04-17 13:47:37 +0200

    remove unused run_generator_mempool() function

--- chia/full_node/generator.py
+++ chia/full_node/generator.py
@@ -71,11 +71,6 @@ def setup_generator_args(self: BlockGenerator) -> Tuple[SerializedProgram, Progr
     return self.program, args
 
 
-def run_generator_mempool(self: BlockGenerator, max_cost: int) -> Tuple[int, SerializedProgram]:
-    program, args = setup_generator_args(self)
-    return GENERATOR_MOD.run_mempool_with_cost(max_cost, program, args)
-
-
 def run_generator_unsafe(self: BlockGenerator, max_cost: int) -> Tuple[int, SerializedProgram]:
     """This mode is meant for accepting possibly soft-forked transactions into the mempool"""
     program, args = setup_generator_args(self)
