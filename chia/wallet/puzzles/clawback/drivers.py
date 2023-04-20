5c5f42c051be4a7d4902fd8187e51d7ede30d41b
geoffwalmsley <geoffwalmsley@gmail.com>
2023-04-21 11:49:49 +1200

    update drivers and add lifecycle tests

--- chia/wallet/puzzles/clawback/drivers.py
+++ chia/wallet/puzzles/clawback/drivers.py
@@ -14,12 +14,14 @@ from chia.wallet.puzzles.clawback.metadata import ClawbackMetadata
 from chia.wallet.puzzles.load_clvm import load_clvm_maybe_recompile
 from chia.wallet.puzzles.p2_delegated_puzzle_or_hidden_puzzle import MOD
 from chia.wallet.uncurried_puzzle import uncurry_puzzle
+from chia.wallet.util.curry_and_treehash import calculate_hash_of_quoted_mod_hash, curry_and_treehash
 from chia.wallet.util.merkle_utils import build_merkle_tree
 from chia.wallet.util.puzzle_decorator_type import PuzzleDecoratorType
 
 P2_1_OF_N = load_clvm_maybe_recompile("p2_1_of_n.clvm")
 P2_CURRIED_PUZZLE_HASH = load_clvm_maybe_recompile("p2_puzzle_hash.clvm")
 AUGMENTED_CONDITION = load_clvm_maybe_recompile("augmented_condition.clvm")
+AUGMENTED_CONDITION_HASH = AUGMENTED_CONDITION.get_tree_hash()
 
 
 def create_augmented_cond_puzzle(condition: List[Any], puzzle: Program) -> Program:
@@ -27,7 +29,9 @@ def create_augmented_cond_puzzle(condition: List[Any], puzzle: Program) -> Progr
 
 
 def create_augmented_cond_puzzle_hash(condition: List[Any], puzzle_hash: bytes32) -> bytes32:
-    return AUGMENTED_CONDITION.curry(condition, puzzle_hash).get_tree_hash_precalc(puzzle_hash)
+    hash_of_quoted_mod_hash = calculate_hash_of_quoted_mod_hash(AUGMENTED_CONDITION_HASH)
+    hashed_args = [Program.to(condition).get_tree_hash(), puzzle_hash]
+    return curry_and_treehash(hash_of_quoted_mod_hash, *hashed_args)
 
 
 def create_augmented_cond_solution(inner_solution: Program) -> Any:
@@ -45,7 +49,16 @@ def create_p2_puzzle_hash_solution(inner_puzzle: Program, inner_solution: Progra
 def create_clawback_merkle_tree(
     timelock: uint64, sender_ph: bytes32, recipient_ph: bytes32
 ) -> Tuple[bytes32, Dict[bytes32, Tuple[int, List[bytes32]]]]:
-    timelock_condition = [80, timelock]
+    """
+    Returns (merkle_root, dict_of_proofs)
+    The full type for a merkle tree is taken from wallet.util.merkle_utils.build_merkle_tree
+    For clawbacks there are only 2 puzzles in the merkle tree, so the dict_of_proofs is just:
+    {claim_ph: (0, [sha256(prefix, claw_ph)]), claw_ph: (1, [sha256(prefix, claim_ph)])}
+    where prefix = bytes([2])
+    """
+    if timelock < 1:
+        raise ValueError("Timelock must be at least 1 second")
+    timelock_condition = [ConditionOpcode.ASSERT_SECONDS_RELATIVE, timelock]
     augmented_cond_puz_hash = create_augmented_cond_puzzle_hash(timelock_condition, recipient_ph)
     p2_puzzle_hash_puz = create_p2_puzzle_hash_puzzle(sender_ph)
     merkle_tree = build_merkle_tree([augmented_cond_puz_hash, p2_puzzle_hash_puz.get_tree_hash()])
@@ -55,6 +68,12 @@ def create_clawback_merkle_tree(
 def create_merkle_proof(
     merkle_tree: Tuple[bytes32, Dict[bytes32, Tuple[int, List[bytes32]]]], puzzle_hash: bytes32
 ) -> Any:
+    """
+    To spend a p2_1_of_n clawback we recreate the full merkle tree: (root, dict_of_proofs)
+    The required proof is then selected from dict_of_proofs based on the puzzle_hash of the puzzle we
+    want to execute
+    Returns a proof: (int, List[bytes32]) which can be provided to the p2_1_of_n solution
+    """
     return Program.to(merkle_tree[1][puzzle_hash])
 
 
@@ -70,12 +89,21 @@ def create_merkle_solution(
     inner_puzzle: Program,
     inner_solution: Program,
 ) -> Any:
+    """
+    Recreates the full merkle tree of a p2_1_of_n clawback coin. It uses the timelock and each party's
+    puzhash to create the tree.
+    The provided inner puzzle must hash to match either the sender or recipient puzhash
+    If it's the sender, then create the clawback solution. If it's the recipient then create the claim
+    solution.
+    Returns a program which is the solution to a p2_1_of_n clawback.
+    """
     merkle_tree = create_clawback_merkle_tree(timelock, sender_ph, recipient_ph)
-    if inner_puzzle.get_tree_hash() == sender_ph:
+    inner_puzzle_hash = inner_puzzle.get_tree_hash()
+    if inner_puzzle_hash == sender_ph:
         cb_inner_puz = create_p2_puzzle_hash_puzzle(sender_ph)
         merkle_proof = create_merkle_proof(merkle_tree, cb_inner_puz.get_tree_hash())
         cb_inner_solution = create_p2_puzzle_hash_solution(inner_puzzle, inner_solution)
-    elif inner_puzzle.get_tree_hash() == recipient_ph:
+    elif inner_puzzle_hash == recipient_ph:
         condition = [80, timelock]
         cb_inner_puz = create_augmented_cond_puzzle(condition, inner_puzzle)
         merkle_proof = create_merkle_proof(merkle_tree, cb_inner_puz.get_tree_hash())
@@ -90,7 +118,7 @@ def match_clawback_puzzle(inner_puzzle: Program, inner_solution: Program, max_co
     if MOD != uncurry_puzzle(inner_puzzle).mod:
         return None
     # Fetch Remark condition
-    error, conditions, cost = conditions_for_solution(
+    _, conditions, _ = conditions_for_solution(
         inner_puzzle,
         inner_solution,
         max_cost,
