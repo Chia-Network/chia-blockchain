6ae28aaef8732433ec218b7eda26e7b7c8a12b21
arvidn <arvid@libtorrent.org>
2023-04-28 11:12:30 +0200

    add new constants for soft-fork3, hard-fork and the other plot-filter adjustments

--- chia/consensus/constants.py
+++ chia/consensus/constants.py
@@ -66,6 +66,18 @@ class ConsensusConstants:
     # soft fork initiated in 1.8.0 release
     SOFT_FORK2_HEIGHT: uint32
 
+    # soft fork initiated in 2.0 release
+    SOFT_FORK3_HEIGHT: uint32
+
+    # the hard fork planned with the 2.0 release
+    # this is the block with the first plot filter adjustment
+    HARD_FORK_HEIGHT: uint32
+
+    # the plot filter adjustment heights
+    PLOT_FILTER_128_HEIGHT: uint32
+    PLOT_FILTER_64_HEIGHT: uint32
+    PLOT_FILTER_32_HEIGHT: uint32
+
     def replace(self, **changes: object) -> "ConsensusConstants":
         return dataclasses.replace(self, **changes)
 
