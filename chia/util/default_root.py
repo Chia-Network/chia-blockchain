3a8382f6fe61e4f78608c6f9cc496156ec23335a
Jack Nelson <jack@jacknelson.xyz>
2023-04-28 20:06:23 -0400

    fix parent path issue

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -10,3 +10,6 @@ DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/
 SIMULATOR_ROOT_PATH = Path(
     os.path.expanduser(os.getenv("CHIA_SIMULATOR_ROOT", f"{DEFAULT_ROOT_PATH.parent}/simulator"))
 ).resolve()
+if SIMULATOR_ROOT_PATH.parent == "simulator":
+    # This is in case the user runs the sim command with the default root path set to the sim folder
+    SIMULATOR_ROOT_PATH = SIMULATOR_ROOT_PATH.parent
