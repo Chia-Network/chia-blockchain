9c820bd6e724f19ecb4f6c05c8eff72f96e893e9
xdustinface <xdustinfacex@gmail.com>
2023-05-02 08:03:29 +0200

    Comment unused `cse1` creation

--- tests/generator/test_compression.py
+++ tests/generator/test_compression.py
@@ -247,9 +247,10 @@ class TestDecompression:
     def test_block_program_zero(self) -> None:
         "Decompress a list of CSEs"
         self.maxDiff = None
-        cse1 = binutils.assemble(  # type: ignore[no-untyped-call]
-            "(((0x0000000000000000000000000000000000000000000000000000000000000000 0x0186a0) (0xb081963921826355dcb6c355ccf9c2637c18adf7d38ee44d803ea9ca41587e48c913d8d46896eb830aeadfc13144a8eac3 (() (q (51 0x6b7a83babea1eec790c947db4464ab657dbe9b887fe9acc247062847b8c2a8a9 0x0186a0)) ()))))"  # noqa: E501
-        )
+        # TODO: Should cse1 be used in a test run below?
+        # cse1 = binutils.assemble(  # type: ignore[no-untyped-call]
+        #     "(((0x0000000000000000000000000000000000000000000000000000000000000000 0x0186a0) (0xb081963921826355dcb6c355ccf9c2637c18adf7d38ee44d803ea9ca41587e48c913d8d46896eb830aeadfc13144a8eac3 (() (q (51 0x6b7a83babea1eec790c947db4464ab657dbe9b887fe9acc247062847b8c2a8a9 0x0186a0)) ()))))"  # noqa: E501
+        # )
         cse2 = binutils.assemble(  # type: ignore[no-untyped-call]
             """
 (
@@ -300,9 +301,10 @@ class TestDecompression:
 
     def test_block_program_zero_with_curry(self) -> None:
         self.maxDiff = None
-        cse1 = binutils.assemble(  # type: ignore[no-untyped-call]
-            "(((0x0000000000000000000000000000000000000000000000000000000000000000 0x0186a0) (0xb081963921826355dcb6c355ccf9c2637c18adf7d38ee44d803ea9ca41587e48c913d8d46896eb830aeadfc13144a8eac3 (() (q (51 0x6b7a83babea1eec790c947db4464ab657dbe9b887fe9acc247062847b8c2a8a9 0x0186a0)) ()))))"  # noqa: E501
-        )
+        # TODO: Should cse1 be used in a test run below?
+        # cse1 = binutils.assemble(  # type: ignore[no-untyped-call]
+        #     "(((0x0000000000000000000000000000000000000000000000000000000000000000 0x0186a0) (0xb081963921826355dcb6c355ccf9c2637c18adf7d38ee44d803ea9ca41587e48c913d8d46896eb830aeadfc13144a8eac3 (() (q (51 0x6b7a83babea1eec790c947db4464ab657dbe9b887fe9acc247062847b8c2a8a9 0x0186a0)) ()))))"  # noqa: E501
+        # )
         cse2 = binutils.assemble(  # type: ignore[no-untyped-call]
             """
 (
