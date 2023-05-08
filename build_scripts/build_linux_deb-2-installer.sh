09f3c8ee00cc5d9182179dd3c0a234cd462670da
Patrick Maslana <pmaslana3@gmail.com>
2023-05-08 12:30:13 -0700

    Change word from library to licenses

--- build_scripts/build_linux_deb-2-installer.sh
+++ build_scripts/build_linux_deb-2-installer.sh
@@ -58,7 +58,7 @@ CLI_DEB_BASE="chia-blockchain-cli_$CHIA_INSTALLER_VERSION-1_$PLATFORM"
 mkdir -p "dist/$CLI_DEB_BASE/opt/chia"
 mkdir -p "dist/$CLI_DEB_BASE/usr/bin"
 mkdir -p "dist/$CLI_DEB_BASE/DEBIAN"
-cp -r dist/library "dist/$CLI_DEB_BASE/opt/chia"
+cp -r dist/licenses "dist/$CLI_DEB_BASE/opt/chia"
 j2 -o "dist/$CLI_DEB_BASE/DEBIAN/control" assets/deb/control.j2
 cp -r dist/daemon/* "dist/$CLI_DEB_BASE/opt/chia/"
 
