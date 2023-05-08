571c68556e2caeae84b4526cec4fa19ce63a2b4e
Joseph Chiocchi <input.output@gmail.com>
2023-05-07 21:34:40 -0500

    revert literal "xch"

--- chia/cmds/wallet_funcs.py
+++ chia/cmds/wallet_funcs.py
@@ -651,7 +651,7 @@ async def take_offer(args: dict, wallet_client: WalletRpcClient, fingerprint: in
     if royalty_asset_dict != {}:
         fungible_asset_dict: Dict[Any, uint64] = {}
         for fungible_asset_id in fungible_assets_from_offer(offer):
-            fungible_asset_id_str = fungible_asset_id.hex() if fungible_asset_id is not None else network_xch.lower()
+            fungible_asset_id_str = fungible_asset_id.hex() if fungible_asset_id is not None else "xch"
             if fungible_asset_id_str in requested:
                 nft_royalty_currency: str = "Unknown CAT"
                 if fungible_asset_id is None:
