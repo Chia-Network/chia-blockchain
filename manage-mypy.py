26a57145969686f4a02f1ed97d46b172ca4db52f
xdustinface <xdustinfacex@gmail.com>
2023-05-15 23:30:49 +0200

    Only `read_text` once

--- manage-mypy.py
+++ manage-mypy.py
@@ -31,19 +31,21 @@ def main() -> None:
 def build_mypy_ini(check_exclusions: bool = False) -> None:
     if not exclusion_file.exists():
         raise click.ClickException(f"{exclusion_file.name} missing, run with `{file_path.name}`")
+    exclusion_file_content = None
     if check_exclusions:
         updated_exclusions = build_exclusion_list()
         # Compare the old content with the new content and fail if some file without issues is excluded.
-        old_exclusions = exclusion_file.read_text(encoding="utf-8").splitlines()[1:]
+        exclusion_file_content = exclusion_file.read_text(encoding="utf-8").splitlines()[1:]
+        old_exclusions = exclusion_file_content[1:]
         if updated_exclusions != old_exclusions:
             fixed = "\n".join(f"  -> {entry}" for entry in sorted(set(old_exclusions) - set(updated_exclusions)))
             if len(fixed) > 0:
                 raise click.ClickException(
                     f"The following fixed files need to be dropped from {exclusion_file.name}:\n{fixed}"
                 )
-
     # Create the `mypy.ini` with all entries from `mypy-exclusions.txt`
-    exclusion_file_content = exclusion_file.read_text(encoding="utf-8").splitlines()
+    if exclusion_file_content is None:
+        exclusion_file_content = exclusion_file.read_text(encoding="utf-8").splitlines()
     exclusion_lines = [line for line in exclusion_file_content if not line.startswith("#") and len(line.strip()) > 0]
     exclusion_section = f"[mypy-{','.join(exclusion_lines)}]"
     mypy_config_data = (
