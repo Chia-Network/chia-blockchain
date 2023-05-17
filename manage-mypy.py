9a2db56c1d8a17f1c944a6d2cff095864f0dc3f2
xdustinface <xdustinfacex@gmail.com>
2023-05-18 00:08:45 +0200

    Apply suggestions

--- manage-mypy.py
+++ manage-mypy.py
@@ -31,22 +31,20 @@ def main() -> None:
 def build_mypy_ini(check_exclusions: bool = False) -> None:
     if not exclusion_file.exists():
         raise click.ClickException(f"{exclusion_file.name} missing, run `{file_path.name} build-exclusions`")
-    exclusion_file_content = None
+    exclusion_file_content = exclusion_file.read_text(encoding="utf-8").splitlines()
+    exclusion_lines = [line for line in exclusion_file_content if not line.startswith("#") and len(line.strip()) > 0]
     if check_exclusions:
         updated_exclusions = build_exclusion_list()
         # Compare the old content with the new content and fail if some file without issues is excluded.
-        exclusion_file_content = exclusion_file.read_text(encoding="utf-8").splitlines()[1:]
-        old_exclusions = exclusion_file_content[1:]
-        if updated_exclusions != old_exclusions:
-            fixed = "\n".join(f"  -> {entry}" for entry in sorted(set(old_exclusions) - set(updated_exclusions)))
+        updated_set = set(updated_exclusions)
+        old_set = set(exclusion_lines)
+        if updated_set != old_set:
+            fixed = "\n".join(f"  -> {entry}" for entry in sorted(old_set - updated_set))
             if len(fixed) > 0:
                 raise click.ClickException(
                     f"The following fixed files need to be dropped from {exclusion_file.name}:\n{fixed}"
                 )
     # Create the `mypy.ini` with all entries from `mypy-exclusions.txt`
-    if exclusion_file_content is None:
-        exclusion_file_content = exclusion_file.read_text(encoding="utf-8").splitlines()
-    exclusion_lines = [line for line in exclusion_file_content if not line.startswith("#") and len(line.strip()) > 0]
     exclusion_section = f"[mypy-{','.join(exclusion_lines)}]"
     mypy_config_data = (
         here.joinpath("mypy.ini.template")
