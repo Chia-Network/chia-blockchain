8a75a7a883aed45fa9b86880962c9a1ccc4afa67
Earle Lowe <e.lowe@chia.net>
2023-05-23 10:50:17 -0700

    return false if peak is none per code review

--- tests/core/node_height.py
+++ tests/core/node_height.py
@@ -7,22 +7,22 @@ from chia.util.ints import uint32
 def node_height_at_least(node: FullNodeAPI, h: uint32) -> bool:
     if node.full_node.blockchain.get_peak() is not None:
         peak = node.full_node.blockchain.get_peak()
-        assert peak is not None
-        return peak.height >= h
+        if peak is not None:
+            return peak.height >= h
     return False
 
 
 def node_height_exactly(node: FullNodeAPI, h: uint32) -> bool:
     if node.full_node.blockchain.get_peak() is not None:
         peak = node.full_node.blockchain.get_peak()
-        assert peak is not None
-        return peak.height == h
+        if peak is not None:
+            return peak.height == h
     return False
 
 
 def node_height_between(node: FullNodeAPI, h1: uint32, h2: uint32) -> bool:
     if node.full_node.blockchain.get_peak() is not None:
         peak = node.full_node.blockchain.get_peak()
-        assert peak is not None
-        return h1 <= peak.height <= h2
+        if peak is not None:
+            return h1 <= peak.height <= h2
     return False
