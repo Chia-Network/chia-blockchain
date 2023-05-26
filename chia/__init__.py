53d81b9436b47ac0e72471c2bf01f7c2b165aa9e
Kyle Altendorf <sda@fstab.net>
2023-05-26 13:33:23 -0400

    upload to coveralls from each job - take 2

--- chia/__init__.py
+++ chia/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+# TODO: remove this test code
+import random
+
 from pkg_resources import DistributionNotFound, get_distribution, resource_filename
 
 try:
@@ -9,3 +12,7 @@ except DistributionNotFound:
     __version__ = "unknown"
 
 PYINSTALLER_SPEC_PATH = resource_filename("chia", "pyinstaller.spec")
+
+# TODO: remove this test code
+if random.random() < -1:
+    print()
