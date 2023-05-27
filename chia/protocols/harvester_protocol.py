5063f4963d9478c9358059265ded8dfd285f28ff
ChiaMineJP <admin@chiamine.jp>
2023-05-27 19:46:44 +0900

    Fixed tests

--- chia/protocols/harvester_protocol.py
+++ chia/protocols/harvester_protocol.py
@@ -184,4 +184,4 @@ class PlotSyncResponse(Streamable):
 @dataclass(frozen=True)
 class HarvestingModeUpdate(Streamable):
     identifier: PlotSyncIdentifier
-    harvesting_mode: int8
+    harvesting_mode: uint8
