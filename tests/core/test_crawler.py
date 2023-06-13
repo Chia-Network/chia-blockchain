2728e77b1fa17aa9d763dcdcea89692980044ffe
xdustinface <xdustinfacex@gmail.com>
2023-06-13 20:27:25 +0200

    Use a real crawler service for the test

--- tests/core/test_crawler.py
+++ tests/core/test_crawler.py
@@ -9,8 +9,8 @@ from chia.full_node.full_node_api import FullNodeAPI
 from chia.protocols.protocol_message_types import ProtocolMessageTypes
 from chia.protocols.wallet_protocol import RequestChildren
 from chia.seeder.crawler import Crawler
-from chia.seeder.crawler_api import CrawlerAPI
 from chia.server.outbound_message import make_msg
+from chia.server.start_service import Service
 from chia.simulator.setup_nodes import SimulatorsAndWalletsServices
 from chia.simulator.time_out_assert import time_out_assert
 from chia.types.blockchain_format.sized_bytes import bytes32
@@ -21,23 +21,22 @@ from chia.util.ints import uint16
 @pytest.mark.asyncio
 async def test_unknown_messages(
     self_hostname: str,
-    one_wallet_and_one_simulator_services: SimulatorsAndWalletsServices,
+    one_node: SimulatorsAndWalletsServices,
+    crawler_service: Service[Crawler],
     caplog: pytest.LogCaptureFixture,
 ) -> None:
-    [full_node_service], [wallet_service], bt = one_wallet_and_one_simulator_services
-    wallet_node = wallet_service._node
+    [full_node_service], _, _ = one_node
+    crawler = crawler_service._node
     full_node = full_node_service._node
-    await wallet_node.server.start_client(
+    assert await crawler.server.start_client(
         PeerInfo(self_hostname, uint16(cast(FullNodeAPI, full_node_service._api).server._port)), None
     )
-    send_connection = full_node.server.all_connections[wallet_node.server.node_id]
-    receive_connection = wallet_node.server.all_connections[full_node.server.node_id]
-    receive_connection.api = CrawlerAPI(Crawler(bt.config["seeder"], bt.root_path, bt.constants))
-    msg = make_msg(ProtocolMessageTypes.request_children, RequestChildren(bytes32(b"\0" * 32)))
+    connection = full_node.server.all_connections[crawler.server.node_id]
 
     def receiving_failed() -> bool:
         return "Non existing function: request_children" in caplog.text
 
     with caplog.at_level(logging.ERROR):
-        assert await send_connection.send_message(msg)
+        msg = make_msg(ProtocolMessageTypes.request_children, RequestChildren(bytes32(b"\0" * 32)))
+        assert await connection.send_message(msg)
         await time_out_assert(10, receiving_failed)
