849166361dbeee971ec3e04b22a16a3bc99dcfc2
Arvid Norberg <arvid@libtorrent.org>
2023-07-14 06:19:46 +0200

    bump chia_rs dependency to 0.2.9 (#15760)

--- tests/generator/test_rom.py
+++ tests/generator/test_rom.py
@@ -133,7 +133,9 @@ class TestROM:
 
         spend = Spend(
             coin_id=bytes32.fromhex("e8538c2d14f2a7defae65c5c97f5d4fae7ee64acef7fec9d28ad847a0880fd03"),
+            parent_id=bytes32.fromhex("0000000000000000000000000000000000000000000000000000000000000000"),
             puzzle_hash=bytes32.fromhex("9dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2"),
+            coin_amount=50000,
             height_relative=None,
             seconds_relative=None,
             before_height_relative=None,
@@ -142,6 +144,12 @@ class TestROM:
             birth_seconds=None,
             create_coin=[(bytes([0] * 31 + [1]), 500, None)],
             agg_sig_me=[],
+            agg_sig_parent=[],
+            agg_sig_puzzle=[],
+            agg_sig_amount=[],
+            agg_sig_puzzle_amount=[],
+            agg_sig_parent_amount=[],
+            agg_sig_parent_puzzle=[],
             flags=ELIGIBLE_FOR_DEDUP,
         )
 
