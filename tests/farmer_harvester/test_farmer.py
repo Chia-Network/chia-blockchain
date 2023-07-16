fc2b48516c3fe988465403cc558629ddfe9a1bc4
ChiaMineJP <admin@chiamine.jp>
2023-07-17 04:51:11 +0900

    Added test case

--- tests/farmer_harvester/test_farmer.py
+++ tests/farmer_harvester/test_farmer.py
@@ -2,11 +2,10 @@ from __future__ import annotations
 
 import logging
 from dataclasses import dataclass
-from secrets import token_bytes
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import pytest
-from blspy import G1Element, G2Element
+from blspy import AugSchemeMPL, G1Element, G2Element, PrivateKey
 
 from chia.consensus.constants import ConsensusConstants
 from chia.consensus.default_constants import DEFAULT_CONSTANTS, default_kwargs
@@ -14,10 +13,15 @@ from chia.farmer.farmer import increment_pool_stats, strip_old_entries
 from chia.pools.pool_config import PoolWalletConfig
 from chia.protocols import farmer_protocol, harvester_protocol
 from chia.protocols.harvester_protocol import NewProofOfSpace, RespondSignatures
-from chia.types.blockchain_format.proof_of_space import ProofOfSpace, verify_and_get_quality_string
+from chia.protocols.pool_protocol import PoolErrorCode
+from chia.types.blockchain_format.proof_of_space import (
+    ProofOfSpace,
+    generate_plot_public_key,
+    verify_and_get_quality_string,
+)
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.hash import std_hash
-from chia.util.ints import uint8, uint32, uint64
+from chia.util.ints import uint8, uint16, uint32, uint64
 from tests.conftest import HarvesterFarmerEnvironment
 from tests.util.misc import Marks
 
@@ -72,7 +76,7 @@ class IncrementPoolStatsCase:
 
 class DummyHarvesterPeer:
     return_invalid_response: bool
-    peer_node_id: bytes32 = bytes32(token_bytes(32))
+    peer_node_id: bytes32 = std_hash(b"1")
 
     def __init__(self, return_valid_response: bool):
         self.return_invalid_response = return_valid_response
@@ -83,13 +87,25 @@ class DummyHarvesterPeer:
     async def call_api(self, arg1: Any, request: harvester_protocol.RequestSignatures) -> Any:
         if self.return_invalid_response:
             return 0
+
+        local_sk: PrivateKey = PrivateKey.from_bytes(
+            bytes.fromhex("185c61579e152bbd3b2face3245951e7c67f67f8d2fb2aa4a56e178186c1e5fa")
+        )
+        farmer_public_key = G1Element.from_bytes(
+            bytes.fromhex(
+                "af59ca047f2f34a4db6e4fa313764f6e71d4c53bc316517e0fa486b8e3d3f36f112ee078e3226854b97791452e3a9fd4"
+            )
+        )
+        agg_pk = generate_plot_public_key(local_sk.get_g1(), farmer_public_key, True)
+        message = request.messages[0]
+        signature: G2Element = AugSchemeMPL.sign(local_sk, message, agg_pk)
         return RespondSignatures(
             plot_identifier=request.plot_identifier,
             challenge_hash=request.challenge_hash,
             sp_hash=request.sp_hash,
-            local_pk=G1Element(),
-            farmer_pk=G1Element(),
-            message_signatures=[(bytes32.from_bytes(b"ff" * 32), G2Element())],
+            local_pk=local_sk.get_g1(),
+            farmer_pk=farmer_public_key,
+            message_signatures=[(message, signature)],
         )
 
 
@@ -112,6 +128,8 @@ class NewProofOfSpaceCase:
     pool_config: PoolWalletConfig
     pool_difficulty: Optional[uint64]
     authentication_token_timeout: Optional[uint8]
+    farmer_private_keys: List[PrivateKey]
+    authentication_keys: Dict[bytes32, PrivateKey]
     use_invalid_peer_response: bool
     expected_pool_state: Dict[str, Any]
     marks: Marks = ()
@@ -126,6 +144,7 @@ class NewProofOfSpaceCase:
         pool_difficulty: Optional[uint64],
         authentication_token_timeout: Optional[uint8],
         use_invalid_peer_response: bool,
+        has_valid_authentication_keys: bool,
         expected_pool_stats: Dict[str, Any],
     ) -> NewProofOfSpaceCase:
         p2_singleton_puzzle_hash = bytes32.fromhex("302e05a1e6af431c22043ae2a9a8f71148c955c372697cb8ab348160976283df")
@@ -141,6 +160,7 @@ class NewProofOfSpaceCase:
                 )
             ),
         )
+
         expected_pool_state = expected_pool_stats.copy()
         expected_pool_state["p2_singleton_puzzle_hash"] = p2_singleton_puzzle_hash.hex()
         expected_pool_state["pool_config"] = pool_config
@@ -151,7 +171,7 @@ class NewProofOfSpaceCase:
             challenge_hash=bytes32.fromhex("9dcc155cafa8723627d01d00118767269a15f5f0d496dc9c2940a45b63c0c00c"),
             sp_hash=bytes32.fromhex("bca3ac17a0a8fc072188b3738455a3f006528d181c39e31c88e8755492575272"),
             plot_identifier="test",
-            signage_point_index=uint8(0),
+            signage_point_index=uint8(1),
             plot_id=bytes32.fromhex("baaa6780c53d4b3739b8807b4ae79a76644ddf0d9e03dc7d0a6a0e613e764d9f"),
             plot_size=uint8(32),
             plot_challenge=bytes32.fromhex("7580e4c366dc2c94c37ce44943f9629a3cd6e027d7b24cd014adeaa578d4b0a2"),
@@ -177,6 +197,16 @@ class NewProofOfSpaceCase:
             pool_config=pool_config,
             pool_difficulty=pool_difficulty,
             authentication_token_timeout=authentication_token_timeout,
+            farmer_private_keys=[
+                PrivateKey.from_bytes(bytes.fromhex("3cb188d8b2469bb8414a3ec68857959e231a8bec836df199a896ff523b9d2f7d"))
+            ],
+            authentication_keys={
+                p2_singleton_puzzle_hash: PrivateKey.from_bytes(
+                    bytes.fromhex("11ed596eb95b31364a9185e948f6b66be30415f816819449d5d40751dc70e786")
+                ),
+            }
+            if has_valid_authentication_keys
+            else {},
             use_invalid_peer_response=use_invalid_peer_response,
             expected_pool_state=expected_pool_state,
         )
@@ -298,6 +328,7 @@ def test_increment_pool_stats(case: IncrementPoolStatsCase) -> None:
                 pool_difficulty=uint64(1),
                 authentication_token_timeout=uint8(10),
                 use_invalid_peer_response=False,
+                has_valid_authentication_keys=True,
                 expected_pool_stats={
                     "points_found_since_start": 0,
                     # Original item format here is (timestamp, value) but we'll ignore timestamp part
@@ -322,10 +353,11 @@ def test_increment_pool_stats(case: IncrementPoolStatsCase) -> None:
             NewProofOfSpaceCase.create_verified_quality_case(
                 difficulty=uint64(1),
                 sub_slot_iters=uint64(1000000000000),
-                pool_url="localhost",
+                pool_url="http://localhost",
                 pool_difficulty=None,
                 authentication_token_timeout=uint8(10),
                 use_invalid_peer_response=False,
+                has_valid_authentication_keys=True,
                 expected_pool_stats={
                     "points_found_since_start": 0,
                     # Original item format here is (timestamp, value) but we'll ignore timestamp part
@@ -350,10 +382,11 @@ def test_increment_pool_stats(case: IncrementPoolStatsCase) -> None:
             NewProofOfSpaceCase.create_verified_quality_case(
                 difficulty=uint64(1),
                 sub_slot_iters=uint64(64),
-                pool_url="localhost",
+                pool_url="http://localhost",
                 pool_difficulty=uint64(1),
                 authentication_token_timeout=uint8(10),
                 use_invalid_peer_response=False,
+                has_valid_authentication_keys=True,
                 expected_pool_stats={
                     "points_found_since_start": 0,
                     # Original item format here is (timestamp, value) but we'll ignore timestamp part
@@ -378,10 +411,11 @@ def test_increment_pool_stats(case: IncrementPoolStatsCase) -> None:
             NewProofOfSpaceCase.create_verified_quality_case(
                 difficulty=uint64(1),
                 sub_slot_iters=uint64(1000000000000),
-                pool_url="localhost",
+                pool_url="http://localhost",
                 pool_difficulty=uint64(1),
                 authentication_token_timeout=None,
                 use_invalid_peer_response=False,
+                has_valid_authentication_keys=True,
                 expected_pool_stats={
                     "points_found_since_start": 0,
                     # Original item format here is (timestamp, value) but we'll ignore timestamp part
@@ -406,10 +440,11 @@ def test_increment_pool_stats(case: IncrementPoolStatsCase) -> None:
             NewProofOfSpaceCase.create_verified_quality_case(
                 difficulty=uint64(1),
                 sub_slot_iters=uint64(1000000000000),
-                pool_url="localhost",
+                pool_url="http://localhost",
                 pool_difficulty=uint64(1),
                 authentication_token_timeout=uint8(10),
                 use_invalid_peer_response=True,
+                has_valid_authentication_keys=True,
                 expected_pool_stats={
                     "points_found_since_start": 0,
                     # Original item format here is (timestamp, value) but we'll ignore timestamp part
@@ -429,6 +464,64 @@ def test_increment_pool_stats(case: IncrementPoolStatsCase) -> None:
                 },
             ),
             id="invalid_peer_response",
+        ),
+        pytest.param(
+            NewProofOfSpaceCase.create_verified_quality_case(
+                difficulty=uint64(1),
+                sub_slot_iters=uint64(1000000000000),
+                pool_url="http://localhost",
+                pool_difficulty=uint64(1),
+                authentication_token_timeout=uint8(10),
+                use_invalid_peer_response=False,
+                has_valid_authentication_keys=False,
+                expected_pool_stats={
+                    "points_found_since_start": 0,
+                    # Original item format here is (timestamp, value) but we'll ignore timestamp part
+                    # so every `xxx_24h` item in this dict will be List[Any].
+                    "points_found_24h": [],
+                    "points_acknowledged_since_start": 0,
+                    "points_acknowledged_24h": [],
+                    "pool_errors_24h": [],
+                    "valid_partials_since_start": 0,
+                    "valid_partials_24h": [],
+                    "invalid_partials_since_start": 0,
+                    "invalid_partials_24h": [],
+                    "stale_partials_since_start": 0,
+                    "stale_partials_24h": [],
+                    "missing_partials_since_start": 1,
+                    "missing_partials_24h": [1],
+                },
+            ),
+            id="no_valid_auth_keys",
+        ),
+        pytest.param(
+            NewProofOfSpaceCase.create_verified_quality_case(
+                difficulty=uint64(1),
+                sub_slot_iters=uint64(1000000000000),
+                pool_url="http://192.168.0.256",
+                pool_difficulty=uint64(1),
+                authentication_token_timeout=uint8(10),
+                use_invalid_peer_response=False,
+                has_valid_authentication_keys=True,
+                expected_pool_stats={
+                    "points_found_since_start": 1,
+                    # Original item format here is (timestamp, value) but we'll ignore timestamp part
+                    # so every `xxx_24h` item in this dict will be List[Any].
+                    "points_found_24h": [1],
+                    "points_acknowledged_since_start": 0,
+                    "points_acknowledged_24h": [],
+                    "pool_errors_24h": [{"error_code": uint16(PoolErrorCode.REQUEST_FAILED.value)}],
+                    "valid_partials_since_start": 0,
+                    "valid_partials_24h": [],
+                    "invalid_partials_since_start": 1,
+                    "invalid_partials_24h": [1],
+                    "stale_partials_since_start": 0,
+                    "stale_partials_24h": [],
+                    "missing_partials_since_start": 0,
+                    "missing_partials_24h": [],
+                },
+            ),
+            id="error_connecting_pool_server",
         ),
     ],
 )
@@ -469,6 +562,8 @@ async def test_farmer_new_proof_of_space_for_pool_stats(
     constant_kwargs = default_kwargs.copy()
     constant_kwargs["POOL_SUB_SLOT_ITERS"] = case.sub_slot_iters
     farmer_api.farmer.constants = ConsensusConstants(**constant_kwargs)  # type: ignore
+    farmer_api.farmer._private_keys = case.farmer_private_keys
+    farmer_api.farmer.authentication_keys = case.authentication_keys
     farmer_api.farmer.sps[case.sp_hash] = [sp]
     farmer_api.farmer.pool_state[p2_singleton_puzzle_hash] = {
         "p2_singleton_puzzle_hash": p2_singleton_puzzle_hash.hex(),
@@ -510,11 +605,18 @@ async def test_farmer_new_proof_of_space_for_pool_stats(
         for i, stat in enumerate(farmer_api.farmer.pool_state[p2_singleton_puzzle_hash][name]):
             assert stat[1] == case.expected_pool_state[name][i]
 
+    def assert_pool_errors_24h() -> None:
+        assert len(farmer_api.farmer.pool_state[p2_singleton_puzzle_hash]["pool_errors_24h"]) == len(
+            case.expected_pool_state["pool_errors_24h"]
+        )
+        for i, stat in enumerate(farmer_api.farmer.pool_state[p2_singleton_puzzle_hash]["pool_errors_24h"]):
+            assert stat[1]["error_code"] == case.expected_pool_state["pool_errors_24h"][i]["error_code"]
+
     assert_stats_since_start("points_found_since_start")
     assert_stats_24h("points_found_24h")
     assert_stats_since_start("points_acknowledged_since_start")
     assert_stats_24h("points_acknowledged_24h")
-    assert_stats_24h("pool_errors_24h")
+    assert_pool_errors_24h()
     assert_stats_since_start("valid_partials_since_start")
     assert_stats_24h("valid_partials_24h")
     assert_stats_since_start("invalid_partials_since_start")
