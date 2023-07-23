fedb77d41e6cb33e24fc9193678d65644b356fc0
almog <almogdepaz@gmail.com>
2023-07-23 18:48:51 +0300

    skip if known heavier unfinished block

--- chia/timelord/timelord_api.py
+++ chia/timelord/timelord_api.py
@@ -37,6 +37,20 @@ class TimelordAPI:
             if self.timelord.bluebox_mode:
                 return None
             self.timelord.max_allowed_inactivity_time = 60
+
+            # if there is a heavier unfinished block which does not point to this peak skip
+            first_block = None
+            for unf_block in self.timelord.unfinished_blocks:
+                if unf_block.reward_chain_block.total_iters > new_peak.reward_chain_block.total_iters:
+                    if first_block is None:
+                        first_block = unf_block
+                    elif unf_block.reward_chain_block.total_iters < first_block.reward_chain_block.total_iters:
+                        first_block = unf_block
+
+            if unf_block.rc_prev != new_peak.reward_chain_block.get_hash():
+                log.info("there is a heavier unfinished block that does not belong to this chain- skip peak")
+                return None
+
             if new_peak.reward_chain_block.weight > self.timelord.last_state.get_weight():
                 log.info("Not skipping peak, don't have. Maybe we are not the fastest timelord")
                 log.info(
