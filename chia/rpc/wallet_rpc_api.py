9022a101cf5e1cad4af61ff9e925c0ec6e2486f2
Kronus91 <t.yu@chia.net>
2023-07-27 10:31:31 -0700

    Add force option for spend_clawback_coin (#15832)

--- chia/rpc/wallet_rpc_api.py
+++ chia/rpc/wallet_rpc_api.py
@@ -1079,12 +1079,24 @@ class WalletRpcApi:
             try:
                 coins[coin_record.coin] = coin_record.parsed_metadata()
                 if len(coins) >= batch_size:
-                    tx_id_list.extend((await self.service.wallet_state_manager.spend_clawback_coins(coins, tx_fee)))
+                    tx_id_list.extend(
+                        (
+                            await self.service.wallet_state_manager.spend_clawback_coins(
+                                coins, tx_fee, request.get("force", False)
+                            )
+                        )
+                    )
                     coins = {}
             except Exception as e:
                 log.error(f"Failed to spend clawback coin {coin_id.hex()}: %s", e)
         if len(coins) > 0:
-            tx_id_list.extend((await self.service.wallet_state_manager.spend_clawback_coins(coins, tx_fee)))
+            tx_id_list.extend(
+                (
+                    await self.service.wallet_state_manager.spend_clawback_coins(
+                        coins, tx_fee, request.get("force", False)
+                    )
+                )
+            )
         return {
             "success": True,
             "transaction_ids": [tx.hex() for tx in tx_id_list],
