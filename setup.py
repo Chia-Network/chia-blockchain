5a13edde62f9be84060835052b7abb5636f269d3
Amine Khaldi <amine.khaldi@reactos.org>
2023-08-10 20:01:48 +0100

    Pin mypy to v1.4.1.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ dev_dependencies = [
     "twine",
     "isort",
     "flake8",
-    "mypy",
+    "mypy==1.4.1",
     "black==23.3.0",
     "aiohttp_cors",  # For blackd
     "ipython",  # For asyncio debugging
