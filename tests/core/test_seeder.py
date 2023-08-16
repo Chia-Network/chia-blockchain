7da62b3a721614358b925f88e0ee7e217e5fdcca
Jack Nelson <j.nelson@chia.net>
2023-08-16 13:32:46 -0400

    rewrite db test

--- tests/core/test_seeder.py
+++ tests/core/test_seeder.py
@@ -4,7 +4,7 @@ import time
 from dataclasses import dataclass
 from ipaddress import IPv4Address, IPv6Address
 from socket import AF_INET, AF_INET6, SOCK_STREAM
-from typing import List, Tuple, cast
+from typing import Dict, List, Tuple, cast
 
 import dns
 import pytest
@@ -57,6 +57,34 @@ class FakeDnsPacket:
     def __getattr__(self, item: object) -> None:
         # This is definitely cheating, but it works
         return None
+
+
+@pytest.fixture(scope="module")
+def database_peers() -> Dict[str, PeerReliability]:
+    """
+    We override the values in the class with these dbs, to save time.
+    """
+    host_to_reliability = {}
+    ipv4, ipv6 = get_addresses(2)  # get 1000 addresses
+    # add peers to the in memory part of the db.
+    for peer in [str(peer) for pair in zip(ipv4, ipv6) for peer in pair]:
+        new_peer = PeerRecord(
+            peer,
+            peer,
+            uint32(58444),
+            False,
+            uint64(0),
+            uint32(0),
+            uint64(0),
+            uint64(int(time.time())),
+            uint64(0),
+            "undefined",
+            uint64(0),
+            tls_version="unknown",
+        )
+        new_peer_reliability = PeerReliability(peer, tries=3, successes=3)  # make sure the peer starts as reliable.
+        host_to_reliability[new_peer.peer_id] = new_peer_reliability
+    return host_to_reliability
 
 
 async def make_dns_query(
@@ -244,34 +272,25 @@ async def test_dns_queries(
 
 
 @pytest.mark.asyncio
-async def test_db_processing(seeder_service: DNSServer) -> None:
+@pytest.mark.parametrize("use_tcp, target_address, request_type", all_test_combinations)
+async def test_db_processing(
+    seeder_service: DNSServer,
+    database_peers: Dict[str, PeerReliability],
+    use_tcp: bool,
+    target_address: str,
+    request_type: dns.rdatatype.RdataType,
+) -> None:
     """
     We add 1000 peers through the db, then try every kind of query over both the TCP and UDP protocols.
     """
+    port = get_dns_port(seeder_service, use_tcp, target_address)
     domain = seeder_service.domain  # default is: seeder.example.com
     num_ns = len(seeder_service.ns_records)
     crawl_store = seeder_service.crawl_store
     assert crawl_store is not None
 
-    ipv4, ipv6 = get_addresses(2)  # get 1000 addresses
-    # add peers to the in memory part of the db.
-    for peer in [str(peer) for pair in zip(ipv4, ipv6) for peer in pair]:
-        new_peer = PeerRecord(
-            peer,
-            peer,
-            uint32(58444),
-            False,
-            uint64(0),
-            uint32(0),
-            uint64(0),
-            uint64(int(time.time())),
-            uint64(0),
-            "undefined",
-            uint64(0),
-            tls_version="unknown",
-        )
-        new_peer_reliability = PeerReliability(peer, tries=3, successes=3)  # make sure the peer starts as reliable.
-        crawl_store.maybe_add_peer(new_peer, new_peer_reliability)  # we don't fully add it because we don't need to.
+    # override host_to_reliability with the pre-generated db peers
+    crawl_store.host_to_reliability = database_peers
 
     # Write these new peers to db.
     await crawl_store.load_reliable_peers_to_db()
@@ -279,10 +298,8 @@ async def test_db_processing(seeder_service: DNSServer) -> None:
     # wait for the new db to be read.
     await time_out_assert(30, lambda: seeder_service.reliable_peers_v4 != [])
 
-    # now we check all the combinations once (not a stupid amount of times)
-    for use_tcp, target_address, request_type in all_test_combinations:
-        port = get_dns_port(seeder_service, use_tcp, target_address)
-        query = dns.message.make_query(domain, request_type, use_edns=True)  # we need to generate a new request id.
-        std_query_response = await make_dns_query(use_tcp, target_address, port, query)
-        assert std_query_response.rcode() == dns.rcode.NOERROR
-        assert_standard_results(std_query_response.answer, request_type, num_ns)
+    # now we check that the db peers are being used.
+    query = dns.message.make_query(domain, request_type, use_edns=True)  # we need to generate a new request id.
+    std_query_response = await make_dns_query(use_tcp, target_address, port, query)
+    assert std_query_response.rcode() == dns.rcode.NOERROR
+    assert_standard_results(std_query_response.answer, request_type, num_ns)
