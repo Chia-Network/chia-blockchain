a7fb495ca6b871b2a643ac04bf97a991779983df
Florin Chirica <fchirica96@gmail.com>
2023-08-18 19:37:03 +0300

    Call tests with verbose.

--- tests/core/data_layer/test_data_rpc.py
+++ tests/core/data_layer/test_data_rpc.py
@@ -712,7 +712,7 @@ async def offer_setup_fixture(
             )
             data_rpc_api = DataLayerRpcApi(data_layer)
 
-            create_response = await data_rpc_api.create_data_store({})
+            create_response = await data_rpc_api.create_data_store({"verbose": True})
             await full_node_api.process_transaction_records(records=create_response["txs"])
 
             store_setups.append(
