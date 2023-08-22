2b7e2a3ceca6bac8113f79ec5790f7c09c0669b1
ytx1991 <t.yu@chia.net>
2023-08-22 10:06:58 -0700

    Revert Clawback fee change

--- chia/wallet/wallet_state_manager.py
+++ chia/wallet/wallet_state_manager.py
@@ -637,36 +637,6 @@ class WalletStateManager:
         wallet_identifier = await self.get_wallet_identifier_for_coin(coin, hint_dict)
         return wallet_identifier is not None and wallet_identifier.id == wallet_id
 
-    async def get_fee_for_coin(
-        self,
-        coin: Coin,
-        peer: WSChiaConnection,
-    ) -> uint64:
-        """
-        Calculate the tx fee of a coin
-        :param coin: The coin includes the fee
-        :param peer: Peer node
-        :return: Transaction fee
-        """
-        # Get all children coins
-        children: List[CoinState] = await self.wallet_node.fetch_children(coin.name(), peer=peer)
-        # Get parent coin amount
-        parent_coin_record: Optional[WalletCoinRecord] = await self.coin_store.get_coin_record(coin.name())
-        parent_coin: Optional[Coin] = None
-        if parent_coin_record is None:
-            coin_states: List[CoinState] = await self.wallet_node.get_coin_state([coin.name()], peer)
-            if len(coin_states) > 0:
-                parent_coin = coin_states[0].coin
-        else:
-            parent_coin = parent_coin_record.coin
-        if parent_coin is None:
-            return uint64(0)
-        children_amount: uint64 = uint64(0)
-        for child_coin in children:
-            children_amount = uint64(children_amount + child_coin.coin.amount)
-
-        return uint64(parent_coin.amount - children_amount)
-
     async def get_confirmed_balance_for_wallet(
         self,
         wallet_id: int,
@@ -1317,7 +1287,7 @@ class WalletStateManager:
                         if clawback_spend_bundle.additions()[0].puzzle_hash == metadata.sender_puzzle_hash
                         else metadata.recipient_puzzle_hash,
                         amount=uint64(coin_state.coin.amount),
-                        fee_amount=await self.get_fee_for_coin(coin_state.coin, peer),
+                        fee_amount=uint64(0),
                         confirmed=True,
                         sent=uint32(0),
                         spend_bundle=clawback_spend_bundle,
