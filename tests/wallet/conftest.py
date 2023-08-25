f8fbc89e3806321839454836f94b0bab2a89d109
Matt <quexington@gmail.com>
2023-08-25 10:58:07 -0700

    Wait for wallets to sync

--- tests/wallet/conftest.py
+++ tests/wallet/conftest.py
@@ -176,6 +176,7 @@ async def wallet_environments(trusted_full_node: bool, request: Any) -> AsyncIte
                 await full_node[0]._api.farm_blocks_to_wallet(
                     count=blocks_needed, wallet=service._node.wallet_state_manager.main_wallet
                 )
+                await full_node[0]._api.wait_for_wallet_synced(wallet_node=service._node, timeout=20)
                 wallet_states.append(
                     WalletState(
                         Balance(
