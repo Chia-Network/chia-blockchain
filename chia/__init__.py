8cafdcd0901b96d41380d9bef2deb506605979ff
Kyle Altendorf <sda@fstab.net>
2023-09-16 21:23:16 -0400

    Update __init__.py

--- chia/__init__.py
+++ chia/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import random
+
 from pkg_resources import DistributionNotFound, get_distribution, resource_filename
 
 try:
@@ -9,3 +11,6 @@ except DistributionNotFound:
     __version__ = "unknown"
 
 PYINSTALLER_SPEC_PATH = resource_filename("chia", "pyinstaller.spec")
+
+if random.random() < -1:
+    print("this is not covered")
