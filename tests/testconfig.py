8f84e7ac8c7a3fd657320f6842f28174475af7de
William Blanke <wjb98672@gmail.com>
2023-09-23 22:49:15 -0700

    90

--- tests/testconfig.py
+++ tests/testconfig.py
@@ -12,6 +12,6 @@ parallel: Union[bool, int, Literal["auto"]] = True
 checkout_blocks_and_plots = False
 install_timelord = False
 check_resource_usage = False
-job_timeout = 60
+job_timeout = 90
 custom_vars: List[str] = []
 os_skip: List[Oses] = []
