75672ea29874a4b7eff519168e233c31e7a09ba5
Kyle Altendorf <sda@fstab.net>
2023-09-27 13:12:24 -0400

    pyupgrade==3.13.0

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ dev_dependencies = [
     "pytest-mock==3.11.1",
     "pytest-monitor==1.6.6; sys_platform == 'linux'",
     "pytest-xdist==3.3.1",
-    "pyupgrade==3.10.1",
+    "pyupgrade==3.13.0",
     "twine==4.0.2",
     "isort==5.12.0",
     "flake8==6.1.0",
