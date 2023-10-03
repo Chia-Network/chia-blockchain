abe9a806db5dd1f1bb3737fdeee107c2229d4642
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-10-03 09:19:16 -0700

    CI test 13

--- chia/simulator/full_node_simulator.py
+++ chia/simulator/full_node_simulator.py
@@ -32,7 +32,7 @@ from chia.wallet.wallet import Wallet
 from chia.wallet.wallet_node import WalletNode
 from chia.wallet.wallet_state_manager import WalletStateManager
 
-# CI test 10
+# CI test 13
 
 
 class _Default:
