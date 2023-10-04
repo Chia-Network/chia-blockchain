bac47a97a889b8b47f949754918c460e39e0b15f
Florin Chirica <fchirica96@gmail.com>
2023-10-05 01:23:48 +0300

    Change port values.

--- chia/simulator/socket.py
+++ chia/simulator/socket.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import filelock
 import secrets
 import socket
 from typing import Set
@@ -11,21 +10,18 @@ recent_ports: Set[int] = set()
 def find_available_listen_port(name: str = "free") -> int:
     global recent_ports
 
-    lock = filelock.FileLock("bind.lock")
-    with lock:
-        while True:
-            port = secrets.randbelow(0xFFFF - 1024) + 1024
-            if port in recent_ports:
+    while True:
+        port = secrets.randbelow(65535 - 49152 + 1) + 49152
+        if port in recent_ports:
+            continue
+
+        with socket.socket() as s:
+            try:
+                s.bind(("127.0.0.1", port))
+            except OSError:
+                recent_ports.add(port)
                 continue
 
-            with socket.socket() as s:
-                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                try:
-                    s.bind(("127.0.0.1", port))
-                except OSError:
-                    recent_ports.add(port)
-                    continue
-
-            recent_ports.add(port)
-            print(f"{name} port: {port}")
-            return port
+        recent_ports.add(port)
+        print(f"{name} port: {port}")
+        return port
