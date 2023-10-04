57d781a83d90171a0dd96170c59c0c0d8064997d
Florin Chirica <fchirica96@gmail.com>
2023-10-05 01:45:41 +0300

    Retry multiple times.

--- chia/simulator/socket.py
+++ chia/simulator/socket.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import secrets
 import socket
+import time
 from typing import Set
 
 recent_ports: Set[int] = set()
@@ -11,17 +12,23 @@ def find_available_listen_port(name: str = "free") -> int:
     global recent_ports
 
     while True:
-        port = secrets.randbelow(65535 - 49152 + 1) + 49152
+        port = secrets.randbelow(0xFFFF - 1024) + 1024
         if port in recent_ports:
             continue
 
-        with socket.socket() as s:
-            try:
-                s.bind(("127.0.0.1", port))
-            except OSError:
-                recent_ports.add(port)
-                continue
+        errored = False
+        for _ in range(10):
+            with socket.socket() as s:
+                try:
+                    s.bind(("127.0.0.1", port))
+                except OSError:
+                    recent_ports.add(port)
+                    errored = True
+                    break
+                time.sleep(0.5)
 
+        if errored:
+            continue
         recent_ports.add(port)
         print(f"{name} port: {port}")
         return port
