15e29ad6dfd021e87c257f085c00b51868095312
Florin Chirica <fchirica96@gmail.com>
2023-10-06 23:17:16 +0300

    Increase timeout.

--- tests/core/mempool/test_mempool_performance.py
+++ tests/core/mempool/test_mempool_performance.py
@@ -78,7 +78,7 @@ class TestMempoolPerformance:
             if idx >= len(blocks) - 3:
                 duration = 0.17
             else:
-                duration = 0.006
+                duration = 0.009
 
             with benchmark_runner.assert_runtime(seconds=duration):
                 await full_node_api_1.full_node.add_block(block)
