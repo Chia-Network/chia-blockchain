aa12716f2424d5503813de61ff671aef6e1f804d
Florin Chirica <fchirica96@gmail.com>
2023-10-06 22:42:31 +0300

    Increase timeouts.

--- tests/core/mempool/test_mempool_performance.py
+++ tests/core/mempool/test_mempool_performance.py
@@ -76,7 +76,7 @@ class TestMempoolPerformance:
 
         for idx, block in enumerate(blocks):
             if idx >= len(blocks) - 3:
-                duration = 0.16
+                duration = 0.17
             else:
                 duration = 0.006
 
