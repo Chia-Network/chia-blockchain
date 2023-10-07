5995c0db6559344a67a1b06e545ad31c833069b0
Chris Marslender <chrismarslender@gmail.com>
2023-10-06 23:19:21 -0500

    Split core install to be first, to avoid electron race condition

--- build_scripts/build_windows-1-gui.ps1
+++ build_scripts/build_windows-1-gui.ps1
@@ -22,6 +22,9 @@ $Env:NODE_OPTIONS = "--max-old-space-size=3000"
 
 Write-Output "lerna clean -y"
 npx lerna clean -y
+# Do core first, so that electron is already done there to avoid a race condition
+Write-Output "npm ci --workspace @chia-network/core"
+npm ci --workspace @chia-network/core
 Write-Output "npm ci"
 npm ci
 # Audit fix does not currently work with Lerna. See https://github.com/lerna/lerna/issues/1663
