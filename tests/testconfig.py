d3bca0d7ec3f21a4ccc9eead2010384c6ccee71a
Kyle Altendorf <sda@fstab.net>
2023-10-12 09:09:23 -0400

    import `Literal` from `typing`

--- tests/testconfig.py
+++ tests/testconfig.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-from typing import Union
-
-from typing_extensions import Literal
+from typing import Literal, Union
 
 # Defaults are conservative.
 parallel: Union[bool, int, Literal["auto"]] = True
