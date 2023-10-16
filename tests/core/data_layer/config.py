9de746c08eeba639e9b1fdf3de8e11500ea73f51
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-10-16 08:44:18 -0700

    After testing job-lengths, increase timeouts for certain groups of tests

--- tests/core/data_layer/config.py
+++ tests/core/data_layer/config.py
@@ -2,3 +2,4 @@ from __future__ import annotations
 
 parallel = 4
 checkout_blocks_and_plots = True
+job_timeout = 90
--- tests/tools/config.py
+++ tests/tools/config.py
@@ -2,4 +2,5 @@ from __future__ import annotations
 
 import sys
 
+job_timeout = 60
 legacy_keyring_required = sys.platform == "linux"
