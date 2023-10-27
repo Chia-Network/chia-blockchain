c47d769c840c71b370118b818ca2ea978cf386ef
Chris Marslender <chrismarslender@gmail.com>
2023-10-26 23:16:12 -0500

    Update to support looking up by just the first 4 letters of each word when adding a key by mnemonic

--- chia/util/keychain.py
+++ chia/util/keychain.py
@@ -87,8 +87,28 @@ def bytes_to_mnemonic(mnemonic_bytes: bytes) -> str:
     return " ".join(mnemonics)
 
 
-def bytes_from_mnemonic(mnemonic_str: str) -> bytes:
+def mnemonic_from_short_words(mnemonic_str: str) -> str:
+    """
+    Since the first 4 letters of each word is unique (or the full word, if less than 4 characters), and its common
+    practice to only store the first 4 letters of each word in many offline storage solutions, also support looking
+    up words by the first 4 characters
+    """
     mnemonic: List[str] = mnemonic_str.split(" ")
+    four_char_dict = {word[:min(4, len(word))]: word for word in bip39_word_list().splitlines()}
+    full_words: List[str] = []
+    for i in range(0, len(mnemonic)):
+        word = mnemonic[i]
+        full_word = four_char_dict.get(word[:min(4, len(word))])
+        if not full_word:
+            raise ValueError(f"'{word}' is not in the mnemonic dictionary; may be misspelled")
+        full_words.append(full_word)
+
+    return " ".join(full_words)
+
+
+def bytes_from_mnemonic(mnemonic_str: str) -> bytes:
+    full_mnemonic_str = mnemonic_from_short_words(mnemonic_str)
+    mnemonic: List[str] = full_mnemonic_str.split(" ")
     if len(mnemonic) not in [12, 15, 18, 21, 24]:
         raise ValueError("Invalid mnemonic length")
 
@@ -123,6 +143,10 @@ def mnemonic_to_seed(mnemonic: str) -> bytes:
     """
     Uses BIP39 standard to derive a seed from entropy bytes.
     """
+    # If there are only ASCII characters (as typically expected in a seed phrase), we can check if its just shortened
+    # 4 letter versions of each word
+    if not any(ord(c) >= 128 for c in mnemonic):
+        mnemonic = mnemonic_from_short_words(mnemonic)
     salt_str: str = "mnemonic"
     salt = unicodedata.normalize("NFKD", salt_str).encode("utf-8")
     mnemonic_normalized = unicodedata.normalize("NFKD", mnemonic).encode("utf-8")
