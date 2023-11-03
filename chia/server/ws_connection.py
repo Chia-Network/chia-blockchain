9e14c1dd04deca2a182e32c11a5e132f29c64a38
Harold Brenes <h.brenes@chia.net>
2023-11-03 17:51:32 -0400

    Fix target protocol version check givein incoming node type

--- chia/server/ws_connection.py
+++ chia/server/ws_connection.py
@@ -223,13 +223,14 @@ class WSChiaConnection:
             if inbound_handshake.network_id != network_id:
                 raise ProtocolError(Err.INCOMPATIBLE_NETWORK_ID)
 
-            if inbound_handshake.protocol_version != protocol_version:
+            inbound_node_type = NodeType(inbound_handshake.node_type)
+            if inbound_handshake.protocol_version != protocol_version[inbound_node_type]:
                 raise ProtocolError(Err.INCOMPATIBLE_PROTOCOL_VERSION)
 
             self.version = inbound_handshake.software_version
             self.protocol_version = Version(inbound_handshake.protocol_version)
             self.peer_server_port = inbound_handshake.server_port
-            self.connection_type = NodeType(inbound_handshake.node_type)
+            self.connection_type = inbound_node_type
             # "1" means capability is enabled
             self.peer_capabilities = known_active_capabilities(inbound_handshake.capabilities)
         else:
@@ -254,14 +255,15 @@ class WSChiaConnection:
             if inbound_handshake.network_id != network_id:
                 raise ProtocolError(Err.INCOMPATIBLE_NETWORK_ID)
 
-            if inbound_handshake.protocol_version != protocol_version:
+            inbound_node_type = NodeType(inbound_handshake.node_type)
+            if inbound_handshake.protocol_version != protocol_version[inbound_node_type]:
                 raise ProtocolError(Err.INCOMPATIBLE_PROTOCOL_VERSION)
 
             await self._send_message(outbound_handshake)
             self.version = inbound_handshake.software_version
             self.protocol_version = Version(inbound_handshake.protocol_version)
             self.peer_server_port = inbound_handshake.server_port
-            self.connection_type = NodeType(inbound_handshake.node_type)
+            self.connection_type = inbound_node_type
             # "1" means capability is enabled
             self.peer_capabilities = known_active_capabilities(inbound_handshake.capabilities)
 
