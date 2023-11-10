d0a6f580e3db37dae7669ee6cca5489ae2274422
arvidn <arvid@libtorrent.org>
2023-11-10 09:52:30 +0100

    add type annotations to test_conditions.py

--- tests/core/full_node/test_conditions.py
+++ tests/core/full_node/test_conditions.py
@@ -16,6 +16,7 @@ from chia.simulator.block_tools import BlockTools
 from chia.simulator.keyring import TempKeyring
 from chia.types.announcement import Announcement
 from chia.types.blockchain_format.program import Program
+from chia.types.blockchain_format.serialized_program import SerializedProgram
 from chia.types.coin_record import CoinRecord
 from chia.types.coin_spend import CoinSpend
 from chia.types.condition_opcodes import ConditionOpcode
@@ -29,7 +30,7 @@ from ...blockchain.blockchain_test_utils import _validate_and_add_block
 from .ram_db import create_ram_blockchain
 
 
-def cleanup_keyring(keyring: TempKeyring):
+def cleanup_keyring(keyring: TempKeyring) -> None:
     keyring.cleanup()
 
 
@@ -39,17 +40,17 @@ log = logging.getLogger(__name__)
 # This puzzle simply returns the solution as conditions.
 # We call it the `EASY_PUZZLE` because it's pretty easy to solve.
 
-EASY_PUZZLE = Program.to(assemble("1"))
+EASY_PUZZLE = SerializedProgram.from_bytes(b"\x01")
 EASY_PUZZLE_HASH = EASY_PUZZLE.get_tree_hash()
 
 
-async def initial_blocks(bt, block_count: int = 4) -> List[FullBlock]:
+async def initial_blocks(bt: BlockTools, block_count: int = 4) -> List[FullBlock]:
     blocks = bt.get_consecutive_blocks(
         block_count,
         guarantee_transaction_block=True,
         farmer_reward_puzzle_hash=EASY_PUZZLE_HASH,
         pool_reward_puzzle_hash=EASY_PUZZLE_HASH,
-        genesis_timestamp=10000,
+        genesis_timestamp=uint64(10000),
         time_per_block=10,
     )
     return blocks
@@ -110,7 +111,7 @@ async def check_conditions(
     blocks = await initial_blocks(bt)
     coin = list(blocks[spend_reward_index].get_included_reward_coins())[0]
 
-    coin_spend = CoinSpend(coin, EASY_PUZZLE, condition_solution)
+    coin_spend = CoinSpend(coin, EASY_PUZZLE, SerializedProgram.from_program(condition_solution))
     spend_bundle = SpendBundle([coin_spend], G2Element())
 
     # now let's try to create a block with the spend bundle and ensure that it doesn't validate
@@ -142,9 +143,9 @@ class TestConditions:
         ],
     )
     async def test_unknown_conditions_with_cost(
-        self, opcode: int, expected_cost: int, bt, consensus_mode: ConsensusMode
-    ):
-        conditions = Program.to(assemble(f"(({opcode} 1337))"))
+        self, opcode: int, expected_cost: int, bt: BlockTools, consensus_mode: ConsensusMode
+    ) -> None:
+        conditions = Program.to(assemble(f"(({opcode} 1337))"))  # type: ignore[no-untyped-call]
         additions, removals, new_block = await check_conditions(bt, conditions)
 
         if consensus_mode != ConsensusMode.HARD_FORK_2_0:
@@ -168,8 +169,10 @@ class TestConditions:
             ("((90 30000))", 300000000),
         ],
     )
-    async def test_softfork_condition(self, condition: str, expected_cost: int, bt, consensus_mode: ConsensusMode):
-        conditions = Program.to(assemble(condition))
+    async def test_softfork_condition(
+        self, condition: str, expected_cost: int, bt: BlockTools, consensus_mode: ConsensusMode
+    ) -> None:
+        conditions = Program.to(assemble(condition))  # type: ignore[no-untyped-call]
         additions, removals, new_block = await check_conditions(bt, conditions)
 
         if consensus_mode != ConsensusMode.HARD_FORK_2_0:
@@ -271,28 +274,36 @@ class TestConditions:
             (co.ASSERT_BEFORE_SECONDS_ABSOLUTE, 0x100000000, None),
         ],
     )
-    async def test_condition(self, opcode, value, expected, bt):
-        conditions = Program.to(assemble(f"(({opcode[0]} {value}))"))
+    async def test_condition(self, opcode: ConditionOpcode, value: int, expected: Err, bt: BlockTools) -> None:
+        conditions = Program.to(assemble(f"(({opcode[0]} {value}))"))  # type: ignore[no-untyped-call]
         await check_conditions(bt, conditions, expected_err=expected)
 
     @pytest.mark.anyio
-    async def test_invalid_my_id(self, bt):
+    async def test_invalid_my_id(self, bt: BlockTools) -> None:
         blocks = await initial_blocks(bt)
         coin = list(blocks[-2].get_included_reward_coins())[0]
         wrong_name = bytearray(coin.name())
         wrong_name[-1] ^= 1
-        conditions = Program.to(assemble(f"(({ConditionOpcode.ASSERT_MY_COIN_ID[0]} 0x{wrong_name.hex()}))"))
+        conditions = Program.to(
+            assemble(
+                f"(({ConditionOpcode.ASSERT_MY_COIN_ID[0]} 0x{wrong_name.hex()}))"
+            )  # type: ignore[no-untyped-call]
+        )
         await check_conditions(bt, conditions, expected_err=Err.ASSERT_MY_COIN_ID_FAILED)
 
     @pytest.mark.anyio
-    async def test_valid_my_id(self, bt):
+    async def test_valid_my_id(self, bt: BlockTools) -> None:
         blocks = await initial_blocks(bt)
         coin = list(blocks[-2].get_included_reward_coins())[0]
-        conditions = Program.to(assemble(f"(({ConditionOpcode.ASSERT_MY_COIN_ID[0]} 0x{coin.name().hex()}))"))
+        conditions = Program.to(
+            assemble(
+                f"(({ConditionOpcode.ASSERT_MY_COIN_ID[0]} 0x{coin.name().hex()}))"
+            )  # type: ignore[no-untyped-call]
+        )
         await check_conditions(bt, conditions)
 
     @pytest.mark.anyio
-    async def test_invalid_coin_announcement(self, bt):
+    async def test_invalid_coin_announcement(self, bt: BlockTools) -> None:
         blocks = await initial_blocks(bt)
         coin = list(blocks[-2].get_included_reward_coins())[0]
         announce = Announcement(coin.name(), b"test_bad")
@@ -300,12 +311,12 @@ class TestConditions:
             assemble(
                 f"(({ConditionOpcode.CREATE_COIN_ANNOUNCEMENT[0]} 'test')"
                 f"({ConditionOpcode.ASSERT_COIN_ANNOUNCEMENT[0]} 0x{announce.name().hex()}))"
-            )
+            )  # type: ignore[no-untyped-call]
         )
         await check_conditions(bt, conditions, expected_err=Err.ASSERT_ANNOUNCE_CONSUMED_FAILED)
 
     @pytest.mark.anyio
-    async def test_valid_coin_announcement(self, bt):
+    async def test_valid_coin_announcement(self, bt: BlockTools) -> None:
         blocks = await initial_blocks(bt)
         coin = list(blocks[-2].get_included_reward_coins())[0]
         announce = Announcement(coin.name(), b"test")
@@ -313,29 +324,29 @@ class TestConditions:
             assemble(
                 f"(({ConditionOpcode.CREATE_COIN_ANNOUNCEMENT[0]} 'test')"
                 f"({ConditionOpcode.ASSERT_COIN_ANNOUNCEMENT[0]} 0x{announce.name().hex()}))"
-            )
+            )  # type: ignore[no-untyped-call]
         )
         await check_conditions(bt, conditions)
 
     @pytest.mark.anyio
-    async def test_invalid_puzzle_announcement(self, bt):
+    async def test_invalid_puzzle_announcement(self, bt: BlockTools) -> None:
         announce = Announcement(EASY_PUZZLE_HASH, b"test_bad")
         conditions = Program.to(
             assemble(
                 f"(({ConditionOpcode.CREATE_PUZZLE_ANNOUNCEMENT[0]} 'test')"
                 f"({ConditionOpcode.ASSERT_PUZZLE_ANNOUNCEMENT[0]} 0x{announce.name().hex()}))"
-            )
+            )  # type: ignore[no-untyped-call]
         )
         await check_conditions(bt, conditions, expected_err=Err.ASSERT_ANNOUNCE_CONSUMED_FAILED)
 
     @pytest.mark.anyio
-    async def test_valid_puzzle_announcement(self, bt):
+    async def test_valid_puzzle_announcement(self, bt: BlockTools) -> None:
         announce = Announcement(EASY_PUZZLE_HASH, b"test")
         conditions = Program.to(
             assemble(
                 f"(({ConditionOpcode.CREATE_PUZZLE_ANNOUNCEMENT[0]} 'test')"
                 f"({ConditionOpcode.ASSERT_PUZZLE_ANNOUNCEMENT[0]} 0x{announce.name().hex()}))"
-            )
+            )  # type: ignore[no-untyped-call]
         )
         await check_conditions(bt, conditions)
 
@@ -371,7 +382,7 @@ class TestConditions:
         num: int,
         expect_err: Optional[Err],
         bt: BlockTools,
-    ):
+    ) -> None:
         """
         Test that the condition checker accepts more announcements than the new per puzzle limit
         pre-v2-softfork, and rejects more than the announcement limit afterward.
@@ -389,7 +400,7 @@ class TestConditions:
 
         conditions = b""
         if prefix != "":
-            conditions += b"\xff" + assemble(prefix).as_bin()
+            conditions += b"\xff" + assemble(prefix).as_bin()  # type: ignore[no-untyped-call]
 
         cond = condition.format(
             coin="0x" + coin.name().hex(),
@@ -398,7 +409,7 @@ class TestConditions:
             pann="0x" + puzzle_announcement.name().hex(),
         )
 
-        conditions += (b"\xff" + assemble(cond).as_bin()) * num
+        conditions += (b"\xff" + assemble(cond).as_bin()) * num  # type: ignore[no-untyped-call]
         conditions += b"\x80"
         conditions_program = Program.from_bytes(conditions)
 
