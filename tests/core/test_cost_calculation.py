d1273530302f3cd8769eb3d0e7d5b769aa6b0e25
Arvid Norberg <arvid@libtorrent.org>
2023-11-14 16:28:30 +0100

    Fix mypy types (#16807)

--- tests/core/test_cost_calculation.py
+++ tests/core/test_cost_calculation.py
@@ -13,24 +13,25 @@ from chia.consensus.cost_calculator import NPCResult
 from chia.consensus.default_constants import DEFAULT_CONSTANTS
 from chia.full_node.bundle_tools import simple_solution_generator
 from chia.full_node.mempool_check_conditions import get_name_puzzle_conditions, get_puzzle_and_solution_for_coin
-from chia.simulator.block_tools import test_constants
+from chia.simulator.block_tools import BlockTools, test_constants
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.serialized_program import SerializedProgram
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.generator_types import BlockGenerator
+from chia.util.ints import uint32, uint64
 from chia.wallet.puzzles import p2_delegated_puzzle_or_hidden_puzzle
 from tests.util.misc import BenchmarkRunner
 
 from .make_block_generator import make_block_generator
 
-BURN_PUZZLE_HASH = b"0" * 32
+BURN_PUZZLE_HASH = bytes32(b"0" * 32)
 SMALL_BLOCK_GENERATOR = make_block_generator(1)
 
 log = logging.getLogger(__name__)
 
 
-def large_block_generator(size):
+def large_block_generator(size: int) -> bytes:
     # make a small block and hash it
     # use this in the name for the cached big block
     # the idea is, if the algorithm for building the big block changes,
@@ -54,7 +55,7 @@ def large_block_generator(size):
 
 
 @pytest.mark.anyio
-async def test_basics(softfork_height, bt):
+async def test_basics(softfork_height: int, bt: BlockTools) -> None:
     wallet_tool = bt.get_pool_wallet_tool()
     ph = wallet_tool.get_new_puzzlehash()
     num_blocks = 3
@@ -68,7 +69,7 @@ async def test_basics(softfork_height, bt):
             break
     assert coinbase is not None
     spend_bundle = wallet_tool.generate_signed_transaction(
-        coinbase.amount,
+        uint64(coinbase.amount),
         BURN_PUZZLE_HASH,
         coinbase,
     )
@@ -79,7 +80,7 @@ async def test_basics(softfork_height, bt):
         program,
         bt.constants.MAX_BLOCK_COST_CLVM,
         mempool_mode=False,
-        height=softfork_height,
+        height=uint32(softfork_height),
         constants=bt.constants,
     )
 
@@ -87,6 +88,7 @@ async def test_basics(softfork_height, bt):
     assert len(bytes(program.program)) == 433
 
     coin_spend = spend_bundle.coin_spends[0]
+    assert npc_result.conds is not None
     assert coin_spend.coin.name() == npc_result.conds.spends[0].coin_id
     spend_info = get_puzzle_and_solution_for_coin(program, coin_spend.coin, softfork_height, bt.constants)
     assert spend_info.puzzle == coin_spend.puzzle_reveal
@@ -112,7 +114,7 @@ async def test_basics(softfork_height, bt):
 
 
 @pytest.mark.anyio
-async def test_mempool_mode(softfork_height, bt):
+async def test_mempool_mode(softfork_height: int, bt: BlockTools) -> None:
     wallet_tool = bt.get_pool_wallet_tool()
     ph = wallet_tool.get_new_puzzlehash()
 
@@ -128,7 +130,7 @@ async def test_mempool_mode(softfork_height, bt):
             break
     assert coinbase is not None
     spend_bundle = wallet_tool.generate_signed_transaction(
-        coinbase.amount,
+        uint64(coinbase.amount),
         BURN_PUZZLE_HASH,
         coinbase,
     )
@@ -144,14 +146,14 @@ async def test_mempool_mode(softfork_height, bt):
         binutils.assemble(
             f"(q ((0x3d2331635a58c0d49912bc1427d7db51afe3f20a7b4bcaffa17ee250dcbcbfaa {disassembly} 300"
             f"  (() (q . (({unknown_opcode} '00000000000000000000000000000000' 0x0cbba106e000))) ()))))"
-        ).as_bin()
+        ).as_bin()  # type: ignore[no-untyped-call]
     )
     generator = BlockGenerator(program, [], [])
     npc_result: NPCResult = get_name_puzzle_conditions(
         generator,
         bt.constants.MAX_BLOCK_COST_CLVM,
         mempool_mode=True,
-        height=softfork_height,
+        height=uint32(softfork_height),
         constants=bt.constants,
     )
     assert npc_result.error is not None
@@ -159,7 +161,7 @@ async def test_mempool_mode(softfork_height, bt):
         generator,
         bt.constants.MAX_BLOCK_COST_CLVM,
         mempool_mode=False,
-        height=softfork_height,
+        height=uint32(softfork_height),
         constants=bt.constants,
     )
     assert npc_result.error is None
@@ -174,20 +176,22 @@ async def test_mempool_mode(softfork_height, bt):
 
 
 @pytest.mark.anyio
-async def test_clvm_mempool_mode(softfork_height):
+async def test_clvm_mempool_mode(softfork_height: int) -> None:
     block = Program.from_bytes(bytes(SMALL_BLOCK_GENERATOR.program))
     disassembly = binutils.disassemble(block)
     # this is a valid generator program except the first clvm
     # if-condition, that depends on executing an unknown operator
     # ("0xfe"). In mempool mode, this should fail, but in non-mempool
     # mode, the unknown operator should be treated as if it returns ().
-    program = SerializedProgram.from_bytes(binutils.assemble(f"(i (0xfe (q . 0)) (q . ()) {disassembly})").as_bin())
+    program = SerializedProgram.from_bytes(
+        binutils.assemble(f"(i (0xfe (q . 0)) (q . ()) {disassembly})").as_bin()  # type: ignore[no-untyped-call]
+    )
     generator = BlockGenerator(program, [], [])
     npc_result: NPCResult = get_name_puzzle_conditions(
         generator,
         test_constants.MAX_BLOCK_COST_CLVM,
         mempool_mode=True,
-        height=softfork_height,
+        height=uint32(softfork_height),
         constants=test_constants,
     )
     assert npc_result.error is not None
@@ -195,14 +199,14 @@ async def test_clvm_mempool_mode(softfork_height):
         generator,
         test_constants.MAX_BLOCK_COST_CLVM,
         mempool_mode=False,
-        height=softfork_height,
+        height=uint32(softfork_height),
         constants=test_constants,
     )
     assert npc_result.error is None
 
 
 @pytest.mark.anyio
-async def test_tx_generator_speed(softfork_height, benchmark_runner: BenchmarkRunner):
+async def test_tx_generator_speed(softfork_height: int, benchmark_runner: BenchmarkRunner) -> None:
     LARGE_BLOCK_COIN_CONSUMED_COUNT = 687
     generator_bytes = large_block_generator(LARGE_BLOCK_COIN_CONSUMED_COUNT)
     program = SerializedProgram.from_bytes(generator_bytes)
@@ -213,7 +217,7 @@ async def test_tx_generator_speed(softfork_height, benchmark_runner: BenchmarkRu
             generator,
             test_constants.MAX_BLOCK_COST_CLVM,
             mempool_mode=False,
-            height=softfork_height,
+            height=uint32(softfork_height),
             constants=test_constants,
         )
 
@@ -223,7 +227,7 @@ async def test_tx_generator_speed(softfork_height, benchmark_runner: BenchmarkRu
 
 
 @pytest.mark.anyio
-async def test_clvm_max_cost(softfork_height):
+async def test_clvm_max_cost(softfork_height: int) -> None:
     block = Program.from_bytes(bytes(SMALL_BLOCK_GENERATOR.program))
     disassembly = binutils.disassemble(block)
     # this is a valid generator program except the first clvm
@@ -232,13 +236,15 @@ async def test_clvm_max_cost(softfork_height):
     # mode, the unknown operator should be treated as if it returns ().
     # the CLVM program has a cost of 391969
     program = SerializedProgram.from_bytes(
-        binutils.assemble(f"(i (softfork (q . 10000000)) (q . ()) {disassembly})").as_bin()
+        binutils.assemble(
+            f"(i (softfork (q . 10000000)) (q . ()) {disassembly})"
+        ).as_bin()  # type: ignore[no-untyped-call]
     )
 
     # ensure we fail if the program exceeds the cost
     generator = BlockGenerator(program, [], [])
     npc_result = get_name_puzzle_conditions(
-        generator, 10000000, mempool_mode=False, height=softfork_height, constants=test_constants
+        generator, 10000000, mempool_mode=False, height=uint32(softfork_height), constants=test_constants
     )
 
     assert npc_result.error is not None
@@ -247,7 +253,7 @@ async def test_clvm_max_cost(softfork_height):
     # raise the max cost to make sure this passes
     # ensure we pass if the program does not exceeds the cost
     npc_result = get_name_puzzle_conditions(
-        generator, 23000000, mempool_mode=False, height=softfork_height, constants=test_constants
+        generator, 23000000, mempool_mode=False, height=uint32(softfork_height), constants=test_constants
     )
 
     assert npc_result.error is None
@@ -255,7 +261,7 @@ async def test_clvm_max_cost(softfork_height):
 
 
 @pytest.mark.anyio
-async def test_standard_tx(benchmark_runner: BenchmarkRunner):
+async def test_standard_tx(benchmark_runner: BenchmarkRunner) -> None:
     # this isn't a real public key, but we don't care
     public_key = bytes.fromhex(
         "af949b78fa6a957602c3593a3d6cb7711e08720415dad831ab18adacaa9b27ec3dda508ee32e24bc811c0abc5781ae21"
@@ -266,7 +272,7 @@ async def test_standard_tx(benchmark_runner: BenchmarkRunner):
     conditions = binutils.assemble(
         "((51 0x699eca24f2b6f4b25b16f7a418d0dc4fc5fce3b9145aecdda184158927738e3e 10)"
         " (51 0x847bb2385534070c39a39cc5dfdc7b35e2db472dc0ab10ab4dec157a2178adbf 0x00cbba106df6))"
-    )
+    )  # type: ignore[no-untyped-call]
     solution_program = SerializedProgram.from_bytes(
         bytes(p2_delegated_puzzle_or_hidden_puzzle.solution_for_conditions(conditions))
     )
@@ -279,7 +285,7 @@ async def test_standard_tx(benchmark_runner: BenchmarkRunner):
 
 
 @pytest.mark.anyio
-async def test_get_puzzle_and_solution_for_coin_performance(benchmark_runner: BenchmarkRunner):
+async def test_get_puzzle_and_solution_for_coin_performance(benchmark_runner: BenchmarkRunner) -> None:
     from clvm.casts import int_from_bytes
 
     from chia.full_node.mempool_check_conditions import DESERIALIZE_MOD
