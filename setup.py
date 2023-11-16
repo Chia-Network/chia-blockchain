c5c4a746ae43749158728e16bd25c2328c7ff805
Kyle Altendorf <sda@fstab.net>
2023-11-16 07:26:48 -0500

    --reruns 5

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ dev_dependencies = [
     "pytest==7.4.3",
     "pytest-cov==4.1.0",
     "pytest-mock==3.12.0",
+    "pytest-rerunfailures==12.0",
     "pytest-xdist==3.4.0",
     "pyupgrade==3.15.0",
     "twine==4.0.2",
