05b2fd49ee4dc2572c72a5fc95979fff6418652a
Kyle Altendorf <sda@fstab.net>
2023-11-30 22:17:31 -0500

    also make gui package own `/opt/chia`

--- build_scripts/build_linux_rpm-2-installer.sh
+++ build_scripts/build_linux_rpm-2-installer.sh
@@ -93,11 +93,13 @@ PRODUCT_NAME="chia"
 echo npx electron-builder build --linux rpm "${OPT_ARCH}" \
   --config.extraMetadata.name=chia-blockchain \
   --config.productName="${PRODUCT_NAME}" --config.linux.desktop.Name="Chia Blockchain" \
-  --config.rpm.packageName="chia-blockchain"
+  --config.rpm.packageName="chia-blockchain" \
+  --config.rpm.fpm='["--directory=/opt/chia"]'
 npx electron-builder build --linux rpm "${OPT_ARCH}" \
   --config.extraMetadata.name=chia-blockchain \
   --config.productName="${PRODUCT_NAME}" --config.linux.desktop.Name="Chia Blockchain" \
-  --config.rpm.packageName="chia-blockchain"
+  --config.rpm.packageName="chia-blockchain" \
+  --config.rpm.fpm='["--directory=/opt/chia"]'
 LAST_EXIT_CODE=$?
 ls -l dist/linux*-unpacked/resources
 
