8d8e5e8b1a37435039c7b94f415a8f427de86620
Kyle Altendorf <sda@fstab.net>
2023-12-01 20:11:19 -0500

    again

--- build_scripts/build_linux_rpm-2-installer.sh
+++ build_scripts/build_linux_rpm-2-installer.sh
@@ -118,6 +118,8 @@ if [ "$LAST_EXIT_CODE" -ne 0 ]; then
 	exit $LAST_EXIT_CODE
 fi
 
+mv *.spec "../../../build_scripts/dist/"
+
 GUI_RPM_NAME="chia-blockchain-${CHIA_INSTALLER_VERSION}-1.${REDHAT_PLATFORM}.rpm"
 mv "dist/${PRODUCT_NAME}-${CHIA_INSTALLER_VERSION}.rpm" "../../../build_scripts/dist/${GUI_RPM_NAME}"
 cd ../../../build_scripts || exit 1
