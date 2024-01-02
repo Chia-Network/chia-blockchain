ba901b95db33e7c6b0853988406211598ae8b8c5
Matt <quexington@gmail.com>
2024-01-02 08:23:29 -0800

    Remove coin_solutions from SpendBundle (nearly) entirely

--- chia/types/spend_bundle.py
+++ chia/types/spend_bundle.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-import warnings
 from dataclasses import dataclass
-from typing import Any, Dict, List, cast
+from typing import Any, Dict, List
 
 from chia_rs import AugSchemeMPL, G2Element
 
@@ -10,7 +9,7 @@ from chia.consensus.default_constants import DEFAULT_CONSTANTS
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.errors import Err, ValidationError
-from chia.util.streamable import Streamable, recurse_jsonify, streamable, streamable_from_dict
+from chia.util.streamable import Streamable, streamable, streamable_from_dict
 from chia.wallet.util.debug_spend_bundle import debug_spend_bundle
 
 from .coin_spend import CoinSpend, compute_additions_with_cost
@@ -59,37 +58,13 @@ class SpendBundle(Streamable):
     def debug(self, agg_sig_additional_data: bytes = DEFAULT_CONSTANTS.AGG_SIG_ME_ADDITIONAL_DATA) -> None:
         debug_spend_bundle(self, agg_sig_additional_data)
 
-    # Note that `coin_spends` used to have the bad name `coin_solutions`, prior
-    # to Jul 12, 2021
-    # Some API still expects this name. For now, we accept both names.
-    #
-    # TODO: continue this deprecation. Eventually, all code below here should be removed.
-    #  1. [x] set `exclude_modern_keys` to `False` (and manually set to `True` where necessary)
-    #  2. [ ] set `include_legacy_keys` to `False` (and manually set to `False` where necessary)
-    #  3. [ ] remove all references to `include_legacy_keys=True`
-    #  4. [ ] remove all code below this point
-
     @classmethod
     def from_json_dict(cls, json_dict: Dict[str, Any]) -> SpendBundle:
-        if "coin_solutions" in json_dict:
-            if "coin_spends" not in json_dict:
-                json_dict = dict(
-                    aggregated_signature=json_dict["aggregated_signature"], coin_spends=json_dict["coin_solutions"]
-                )
-                warnings.warn("`coin_solutions` is now `coin_spends` in `SpendBundle.from_json_dict`")
-            else:
-                warnings.warn("JSON contains both `coin_solutions` and `coin_spends`, just use `coin_spends`")
+        if "coin_solutions" in json_dict and "coin_spends" not in json_dict:
+            json_dict = dict(
+                aggregated_signature=json_dict["aggregated_signature"], coin_spends=json_dict["coin_solutions"]
+            )
         return streamable_from_dict(cls, json_dict)
-
-    def to_json_dict(self, include_legacy_keys: bool = True, exclude_modern_keys: bool = False) -> Dict[str, Any]:
-        if include_legacy_keys is False and exclude_modern_keys is True:
-            raise ValueError("`coin_spends` not included in legacy or modern outputs")
-        d = recurse_jsonify(self)
-        if include_legacy_keys:
-            d["coin_solutions"] = d["coin_spends"]
-        if exclude_modern_keys:
-            del d["coin_spends"]
-        return cast(Dict[str, Any], recurse_jsonify(d))
 
 
 # This function executes all the puzzles to compute the difference between
