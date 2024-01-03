03d927b36b3fcc1fa8d14466f58d816885580944
Amine Khaldi <amine.khaldi@reactos.org>
2024-01-03 21:25:19 +0100

    No need to return None in dl_track_new and dl_stop_tracking (#17145)
    No need to return None in dl_track_new and dl_stop_tracking.

--- chia/rpc/wallet_rpc_client.py
+++ chia/rpc/wallet_rpc_client.py
@@ -1140,12 +1140,10 @@ class WalletRpcClient(RpcClient):
     async def dl_track_new(self, launcher_id: bytes32) -> None:
         request = {"launcher_id": launcher_id.hex()}
         await self.fetch("dl_track_new", request)
-        return None
 
     async def dl_stop_tracking(self, launcher_id: bytes32) -> None:
         request = {"launcher_id": launcher_id.hex()}
         await self.fetch("dl_stop_tracking", request)
-        return None
 
     async def dl_latest_singleton(
         self, launcher_id: bytes32, only_confirmed: bool = False
