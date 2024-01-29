832dfe727a7bba1a25221158c269e071e818add4
Matt Hauff <quexington@gmail.com>
2024-01-29 08:20:38 -0800

    Update singleton.py

--- chia/clvm/singleton.py
+++ chia/clvm/singleton.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from chia.wallet.puzzles.load_clvm import load_clvm_maybe_recompile
 
+# blah
+
 P2_SINGLETON_MOD = load_clvm_maybe_recompile("p2_singleton.clsp")
 SINGLETON_TOP_LAYER_MOD = load_clvm_maybe_recompile("singleton_top_layer.clsp")
 SINGLETON_LAUNCHER = load_clvm_maybe_recompile("singleton_launcher.clsp")
