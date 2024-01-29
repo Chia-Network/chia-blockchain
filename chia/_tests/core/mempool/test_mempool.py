a3a28c7474e97fbbbd685285ac411a405388bdab
arvidn <arvid@libtorrent.org>
2024-01-29 23:03:06 +0100

    use rust version of SpendBundle

--- chia/_tests/core/mempool/test_mempool.py
+++ chia/_tests/core/mempool/test_mempool.py
@@ -671,7 +671,7 @@ class TestMempoolManager:
 
         sb: SpendBundle = generate_test_spend_bundle(wallet_a, coin1)
         assert sb.aggregated_signature != G2Element.generator()
-        sb = dataclasses.replace(sb, aggregated_signature=G2Element.generator())
+        sb = sb.replace(aggregated_signature=G2Element.generator())
         res: Optional[Message] = await send_sb(full_node_1, sb)
         assert res is not None
         ack: TransactionAck = TransactionAck.from_bytes(res.data)
