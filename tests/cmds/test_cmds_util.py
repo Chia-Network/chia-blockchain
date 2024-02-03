2e172d0724afba24504f0ae07e28b59f4d5dcceb
Kyle Altendorf <sda@fstab.net>
2024-02-02 20:42:26 -0500

    fix

--- tests/cmds/test_cmds_util.py
+++ tests/cmds/test_cmds_util.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import pytest
 
 from chia.cmds.cmds_util import get_any_service_client
@@ -8,12 +10,16 @@ from tests.util.misc import RecordingWebServer
 
 
 @pytest.mark.anyio
-async def test_get_any_service_client_works_without_ssl(recording_web_server: RecordingWebServer) -> None:
+async def test_get_any_service_client_works_without_ssl(
+    root_path_populated_with_config: Path,
+    recording_web_server: RecordingWebServer,
+) -> None:
     expected_result = {"success": True, "keepy": "uppy"}
 
     async with get_any_service_client(
         client_type=RpcClient,
         rpc_port=recording_web_server.web_server.listen_port,
+        root_path=root_path_populated_with_config,
         use_ssl=False,
     ) as [rpc_client, _]:
         result = await rpc_client.fetch(path="", request_json={"response": expected_result})
