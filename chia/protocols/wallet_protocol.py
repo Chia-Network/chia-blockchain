d95e5cfac129dc1ac6e5e1aa3ef716603f2d91d6
Rigidity <me@rigidnetwork.com>
2024-02-05 10:25:39 -0500

    Implementation of TransactionAddedUpdate

--- chia/protocols/wallet_protocol.py
+++ chia/protocols/wallet_protocol.py
@@ -385,3 +385,13 @@ class RespondCoinState(Streamable):
 @dataclass(frozen=True)
 class RejectCoinState(Streamable):
     header_hash: Optional[bytes32]
+
+
+@streamable
+@dataclass(frozen=True)
+class TransactionAddedUpdate(Streamable):
+    transaction_id: bytes32
+    height_added: uint32
+    cost: uint64
+    additions: List[Coin]
+    removals: List[Coin]
