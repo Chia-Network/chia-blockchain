9f2b1659762671ddc6b1ddbafcdaad582876acec
Florin Chirica <fchirica96@gmail.com>
2024-02-12 19:11:25 +0200

    Remove short option.

--- chia/cmds/data.py
+++ chia/cmds/data.py
@@ -119,7 +119,6 @@ def create_page_option() -> Callable[[FC], FC]:
 
 def create_max_page_size_option() -> Callable[[FC], FC]:
     return click.option(
-        "-mp",
         "--max-page-size",
         help="Set how many bytes to be included in a page, if pagination is enabled.",
         type=int,
