dc470a9aa58bf8aa9ed671d8aaa394f9e1106cdc
Matt <quexington@gmail.com>
2024-02-29 10:38:09 -0800

    Fix simulation test

--- tests/simulation/test_simulation.py
+++ tests/simulation/test_simulation.py
@@ -290,10 +290,11 @@ class TestSimulation:
         rewards = await full_node_api.farm_blocks_to_wallet(count=count, wallet=wallet)
 
         # The requested number of blocks had been processed plus 1 to handle the final reward
-        # transactions in the case of a non-zero count.
-        expected_height = count + 1  # +1 for genesis block (those rewards don't go to wallet)
+        # transactions in the case of a non-zero count. An additional +1 is added for the genesis block which does not
+        # farm rewards to the wallet.
+        expected_height = count
         if count > 0:
-            expected_height += 1
+            expected_height += 2
 
         peak_height = full_node_api.full_node.blockchain.get_peak_height()
         if peak_height is None:
