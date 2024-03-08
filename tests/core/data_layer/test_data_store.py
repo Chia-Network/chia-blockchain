0d351d8de7f982affc70d6e3278fe0c92e22d468
Florin Chirica <fchirica96@gmail.com>
2024-03-08 23:51:08 +0200

    Optimize DL get key by node.

--- tests/core/data_layer/test_data_store.py
+++ tests/core/data_layer/test_data_store.py
@@ -1797,3 +1797,35 @@ async def test_delete_store_data_protects_pending_roots(raw_data_store: DataStor
         start_index = index * keys_per_pending_root
         end_index = (index + 1) * keys_per_pending_root
         assert {pair.key for pair in kv} == set(original_keys[start_index:end_index])
+
+
+@pytest.mark.anyio
+async def test_get_node_by_key_with_overlapping_keys(raw_data_store: DataStore) -> None:
+    num_stores = 5
+    num_keys = 20
+    values_offset = 10000
+    repetitions = 10
+
+    tree_ids = [bytes32(i.to_bytes(32, byteorder="big")) for i in range(num_stores)]
+    for tree_id in tree_ids:
+        await raw_data_store.create_tree(tree_id=tree_id, status=Status.COMMITTED)
+    keys = [key.to_bytes(4, byteorder="big") for key in range(num_keys)]
+    for repetition in range(repetitions):
+        for index, tree_id in enumerate(tree_ids):
+            values = [
+                (value + values_offset * repetition).to_bytes(4, byteorder="big")
+                for value in range(index * num_keys, (index + 1) * num_keys)
+            ]
+            batch = []
+            for key, value in zip(keys, values):
+                batch.append({"action": "upsert", "key": key, "value": value})
+            await raw_data_store.insert_batch(tree_id, batch, status=Status.COMMITTED)
+
+        for index, tree_id in enumerate(tree_ids):
+            values = [
+                (value + values_offset * repetition).to_bytes(4, byteorder="big")
+                for value in range(index * num_keys, (index + 1) * num_keys)
+            ]
+            for key, value in zip(keys, values):
+                node = await raw_data_store.get_node_by_key(tree_id=tree_id, key=key)
+                assert node.value == value
