48703fa08469e46c7444025273b490a11c9540cf
Kyle Altendorf <sda@fstab.net>
2024-03-07 19:53:28 -0500

    chiapos==2.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ dependencies = [
     "boto3==1.34.46",  # AWS S3 for DL s3 plugin
     "chiavdf==1.1.2",  # timelord and vdf verification
     "chiabip158==1.4",  # bip158-style wallet filters
-    "chiapos==2.0.4",  # proof of space
+    "chiapos==2.0.3",  # proof of space
+    # "chiapos==2.0.4",  # proof of space
     "clvm==0.9.9",
     "clvm_tools==0.4.9",  # Currying, Program.to, other conveniences
     "chia_rs==0.5.2",
