2ae93d421208b533107eb457acc20d60777bced0
Adam Kelly <338792+aqk@users.noreply.github.com>
2024-03-15 10:59:04 -0700

    Show tool versions in CI

---
+++ tools/tool-versions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "OS: "
+uname -a
+echo -n "sqlite3: "
+sqlite3 --version
+echo -n "black: "
+black --version
+echo -n "mypy: "
+mypy --version
+echo -n "flake8: "
+flake8 --version
+echo -n "isort: "
+isort --version
+
