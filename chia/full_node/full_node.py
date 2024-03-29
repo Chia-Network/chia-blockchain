23e88632d56b1f567c06f9f3d35e234f330b0e20
Rigidity <35380458+Rigidity@users.noreply.github.com>
2024-03-29 15:56:20 -0400

    New subscription and wallet sync protocol (#17340)
    * New wallet sync protocol
    
    * New updates to protocol and initial tests
    
    * Bump new message ids
    
    * Bump protocol version
    
    * Update existing messages
    
    * Implementation of TransactionAddedUpdate
    
    * Improve unit tests
    
    * Update protocol and tests to use previous height
    
    * Testing tweaks
    
    * Remove transaction subscriptions
    
    * Update protocol tests
    
    * Fix request_coin_state test
    
    * Improve test suite, add min_amount, fix bugs
    
    * Update protocol message serialization tests
    
    * Resolve conflicting file
    
    * Remove old hint store code
    
    * Remove transaction added protocol type
    
    * Make height optional for genesis challenge
    
    * Test subscribe after initial state and dedupe ids in that case
    
    * Forcing an empty commit.
    
    * Fix new protocol tests
    
    * Improve performance of clearing subscriptions
    
    * Add more amounts to coin store test
    
    * Lower max items for coin store test
    
    * Fix test coverage, set lower max response items
    
    * Remove add_puzzle_subscriptions and remove_puzzle_subscriptions from protocol
    
    * Update protocol message list test
    
    * Fix valid reply len
    
    * Fix performance issue in query and add error codes
    
    * Add missing message to regression tes
    
    * Add rate limits
    
    * Add test for hint store get_coin_ids_multi
    
    * Increase max puzzle hash batch size and make it a constant
    
    * Use actual params on to_bytes
    
    * Fix test
    
    * Actually check output of no puzzle hash coin state test

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -2363,8 +2363,10 @@ class FullNode:
                     await self.server.send_to_all([msg], NodeType.FULL_NODE)
                 else:
                     await self.server.send_to_all([msg], NodeType.FULL_NODE, peer.peer_node_id)
+
                 if self.simulator_transaction_callback is not None:  # callback
                     await self.simulator_transaction_callback(spend_name)  # pylint: disable=E1102
+
             else:
                 self.mempool_manager.remove_seen(spend_name)
                 self.log.debug(f"Wasn't able to add transaction with id {spend_name}, status {status} error: {error}")
