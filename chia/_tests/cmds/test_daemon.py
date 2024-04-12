71f76f7d791537ba4500582e4a41ba21fa1e5306
Izumi Hoshino <admin@chiamine.jp>
2024-04-13 02:36:58 +0900

    Fixed a test

--- chia/_tests/cmds/test_daemon.py
+++ chia/_tests/cmds/test_daemon.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from typing import Any
-
 import pytest
+from _pytest.capture import CaptureFixture
 from pytest_mock import MockerFixture
 
 from chia.cmds.start_funcs import create_start_daemon_connection
@@ -11,13 +10,13 @@ from chia.simulator.block_tools import BlockTools
 
 @pytest.mark.anyio
 @pytest.mark.parametrize("skip_keyring", [False, True])
-async def test_daemon(skip_keyring: bool, mocker: MockerFixture, bt: BlockTools, capsys: Any) -> None:
+async def test_daemon(skip_keyring: bool, mocker: MockerFixture, bt: BlockTools, capsys: CaptureFixture[str]) -> None:
     mocker.patch("sys.argv", ["chia", "start", "daemon"])
     daemon = await create_start_daemon_connection(bt.root_path, bt.config, skip_keyring=skip_keyring)
     assert daemon is not None
     captured = capsys.readouterr()
     assert captured.err == ""
     if skip_keyring:
-        assert captured.out == "Daemon not started yet\nStarting daemon\nSkipping to unlock keyring\n"
+        assert captured.out.endswith("Skipping to unlock keyring\n")
     else:
-        assert captured.out == "Daemon not started yet\nStarting daemon\n"
+        assert not captured.out.endswith("Skipping to unlock keyring\n")
