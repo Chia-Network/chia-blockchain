d860b07879d29b957a9d21cfe71fe60e1955db3a
Matt <quexington@gmail.com>
2024-04-15 07:34:47 -0700

    Ban instead of skipping

--- chia/wallet/wallet_node.py
+++ chia/wallet/wallet_node.py
@@ -1635,7 +1635,8 @@ class WalletNode:
             valid_list = []
             for coin in coin_state.coin_states:
                 if coin.coin.name() not in coin_names:
-                    continue
+                    await peer.close(9999)
+                    self.log.warning(f"Peer {peer.peer_node_id} sent us an unrequested coin state. Banning.")
                 valid = await self.validate_received_state_from_peer(
                     coin, peer, self.get_cache_for_peer(peer), fork_height
                 )
