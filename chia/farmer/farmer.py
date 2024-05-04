40a5be3f11f5c9f0e2675323cdf1b66b852916ed
Arvid Norberg <arvid@libtorrent.org>
2024-05-04 16:44:11 +0200

    add ability to profile the farmer process (#17953)

--- chia/farmer/farmer.py
+++ chia/farmer/farmer.py
@@ -4,6 +4,7 @@ import asyncio
 import contextlib
 import json
 import logging
+import sys
 import time
 import traceback
 from dataclasses import dataclass
@@ -47,6 +48,7 @@ from chia.util.hash import std_hash
 from chia.util.ints import uint8, uint16, uint32, uint64
 from chia.util.keychain import Keychain
 from chia.util.logging import TimedDuplicateFilter
+from chia.util.profiler import profile_task
 from chia.wallet.derive_keys import (
     find_authentication_sk,
     find_owner_sk,
@@ -190,6 +192,12 @@ class Farmer:
                     self.started = True
                     return
                 await asyncio.sleep(1)
+
+        if self.config.get("enable_profiler", False):
+            if sys.getprofile() is not None:
+                self.log.warning("not enabling profiler, getprofile() is already set")
+            else:
+                asyncio.create_task(profile_task(self._root_path, "farmer", self.log))
 
         asyncio.create_task(start_task())
         try:
