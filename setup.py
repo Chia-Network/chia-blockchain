dd11254390b646a3ef4a333a9ce158db0bcee569
Kyle Altendorf <sda@fstab.net>
2024-05-21 15:29:51 -0400

    setuptools<70 for now (#18048)
    (cherry picked from commit 525d30a996a0e016eefa12116480d032378cb4c1)

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ dev_dependencies = [
     "lxml==5.1.0",
     "aiohttp_cors==0.7.0",  # For blackd
     "pyinstaller==6.5.0",
+    "setuptools<70",  # TODO: remove - https://github.com/pypa/setuptools/issues/4374
     "types-aiofiles==23.2.0.20240311",
     "types-cryptography==3.3.23.2",
     "types-pyyaml==6.0.12.12",
