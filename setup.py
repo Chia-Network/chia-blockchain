1623e752bc176666be2f4e5174978a8f2b091b90
Kyle Altendorf <sda@fstab.net>
2024-05-21 14:04:19 -0400

    try with pyinstaller develop

--- setup.py
+++ setup.py
@@ -64,7 +64,8 @@ dev_dependencies = [
     "black==24.4.2",
     "lxml==5.2.2",
     "aiohttp_cors==0.7.0",  # For blackd
-    "pyinstaller==6.6.0",
+    "pyinstaller @ git+https://github.com/pyinstaller/pyinstaller@676584885f2dfa1f885ab6155a5eda9150892c03",
+    "setuptools==70.0.0",
     "types-aiofiles==23.2.0.20240311",
     "types-cryptography==3.3.23.2",
     "types-pyyaml==6.0.12.20240311",
