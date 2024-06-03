40d26b506aa0b4b7f11001fef1eb887ae3eac148
Matt <quexington@gmail.com>
2024-06-03 13:14:24 -0700

    The goes with wallet action scope commit

--- chia/_tests/wallet/test_wallet_action_scope.py
+++ chia/_tests/wallet/test_wallet_action_scope.py
@@ -6,6 +6,7 @@ from typing import List, Optional, Tuple
 import pytest
 
 from chia._tests.cmds.wallet.test_consts import STD_TX
+from chia.wallet.signer_protocol import SigningResponse
 from chia.wallet.transaction_record import TransactionRecord
 from chia.wallet.wallet_action_scope import WalletActionScope, WalletSideEffects
 
@@ -21,19 +22,26 @@ def test_back_and_forth_serialization() -> None:
 
 @dataclass
 class MockWalletStateManager:
-    most_recent_call: Optional[Tuple[List[TransactionRecord], bool]] = None
+    most_recent_call: Optional[Tuple[List[TransactionRecord], bool, bool, bool, List[SigningResponse]]] = None
 
     async def add_pending_transactions(
-        self, txs: List[TransactionRecord], merge_spends: bool
+        self,
+        txs: List[TransactionRecord],
+        push: bool,
+        merge_spends: bool,
+        sign: bool,
+        additional_signing_responses: List[SigningResponse],
     ) -> List[TransactionRecord]:
-        self.most_recent_call = (txs, merge_spends)
+        self.most_recent_call = (txs, push, merge_spends, sign, additional_signing_responses)
         return txs
 
 
 @pytest.mark.anyio
 async def test_wallet_action_scope() -> None:
     wsm = MockWalletStateManager()
-    async with WalletActionScope.new(wsm, push=True, merge_spends=False) as action_scope:  # type: ignore[arg-type]
+    async with WalletActionScope.new(
+        wsm, push=True, merge_spends=False, sign=True, additional_signing_responses=[]  # type: ignore[arg-type]
+    ) as action_scope:
         async with action_scope.use() as interface:
             interface.side_effects.transactions = [STD_TX]
 
@@ -41,11 +49,13 @@ async def test_wallet_action_scope() -> None:
             action_scope.side_effects
 
     assert action_scope.side_effects.transactions == [STD_TX]
-    assert wsm.most_recent_call == ([STD_TX], False)
+    assert wsm.most_recent_call == ([STD_TX], True, False, True, [])
 
-    async with WalletActionScope.new(wsm, push=False, merge_spends=True) as action_scope:  # type: ignore[arg-type]
+    async with WalletActionScope.new(
+        wsm, push=False, merge_spends=True, sign=True, additional_signing_responses=[]  # type: ignore[arg-type]
+    ) as action_scope:
         async with action_scope.use() as interface:
             interface.side_effects.transactions = []
 
     assert action_scope.side_effects.transactions == []
-    assert wsm.most_recent_call == ([STD_TX], False)
+    assert wsm.most_recent_call == ([], False, True, True, [])
