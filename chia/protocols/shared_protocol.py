860876abd04d918ba4748698977ef69da5a06cee
Rigidity <me@rigidnetwork.com>
2024-06-05 10:57:52 -0400

    Bump wallet protocol version

--- chia/protocols/shared_protocol.py
+++ chia/protocols/shared_protocol.py
@@ -14,7 +14,7 @@ protocol_version = {
     NodeType.FARMER: "0.0.36",
     NodeType.TIMELORD: "0.0.36",
     NodeType.INTRODUCER: "0.0.36",
-    NodeType.WALLET: "0.0.37",
+    NodeType.WALLET: "0.0.38",
     NodeType.DATA_LAYER: "0.0.36",
 }
 
