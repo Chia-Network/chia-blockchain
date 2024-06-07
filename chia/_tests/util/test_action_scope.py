a50dd8f96a5d9f1c7b5a2b412442f8f0ef7dc1fc
Matt <quexington@gmail.com>
2024-06-07 11:33:19 -0700

    Address comments by @altendky

--- chia/_tests/util/test_action_scope.py
+++ chia/_tests/util/test_action_scope.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import AsyncIterator
+from typing import AsyncIterator, final
 
 import pytest
 
 from chia.util.action_scope import ActionScope, StateInterface
 
 
+@final
 @dataclass
 class TestSideEffects:
     buf: bytes = b""
@@ -27,53 +28,56 @@ async def default_async_callback(interface: StateInterface[TestSideEffects]) -> 
 # Test adding a callback
 def test_add_callback() -> None:
     state_interface = StateInterface({}, TestSideEffects(), True)
-    initial_count = len(state_interface._new_callbacks)
+    initial_callbacks = list(state_interface._new_callbacks)
     state_interface.add_callback(default_async_callback)
-    assert len(state_interface._new_callbacks) == initial_count + 1
+    assert state_interface._new_callbacks == [*initial_callbacks, default_async_callback]
+    initial_callbacks.append(default_async_callback)
+    state_interface.add_callback(default_async_callback)
+    assert state_interface._new_callbacks == [*initial_callbacks, default_async_callback]
 
 
-# Fixture to create an ActionScope with a mocked DBWrapper2
-@pytest.fixture
-async def action_scope() -> AsyncIterator[ActionScope[TestSideEffects]]:
+@pytest.fixture(name="action_scope")
+async def action_scope_fixture() -> AsyncIterator[ActionScope[TestSideEffects]]:
     async with ActionScope.new_scope(TestSideEffects) as scope:
         yield scope
 
 
-# Test creating a new ActionScope and ensuring tables are created
 @pytest.mark.anyio
 async def test_new_action_scope(action_scope: ActionScope[TestSideEffects]) -> None:
+    """
+    Assert we can immediately check out some initial state
+    """
     async with action_scope.use() as interface:
         assert interface == StateInterface({}, TestSideEffects(), True)
 
 
 @pytest.mark.anyio
 async def test_scope_persistence(action_scope: ActionScope[TestSideEffects]) -> None:
+    """ """
     async with action_scope.use() as interface:
         interface.memos[b"foo"] = b"bar"
-        interface.side_effects.buf = b"bar"
+        interface.side_effects.buf = b"baz"
 
     async with action_scope.use() as interface:
         assert interface.memos[b"foo"] == b"bar"
-        assert interface.side_effects.buf == b"bar"
+        assert interface.side_effects.buf == b"baz"
 
 
 @pytest.mark.anyio
 async def test_transactionality(action_scope: ActionScope[TestSideEffects]) -> None:
     async with action_scope.use() as interface:
         interface.memos[b"foo"] = b"bar"
-        interface.side_effects.buf = b"bar"
+        interface.side_effects.buf = b"baz"
 
-    try:
+    with pytest.raises(Exception, match="Going to be caught"):
         async with action_scope.use() as interface:
             interface.memos[b"foo"] = b"qux"
             interface.side_effects.buf = b"qat"
             raise RuntimeError("Going to be caught")
-    except RuntimeError:
-        pass
 
     async with action_scope.use() as interface:
         assert interface.memos[b"foo"] == b"bar"
-        assert interface.side_effects.buf == b"bar"
+        assert interface.side_effects.buf == b"baz"
 
 
 @pytest.mark.anyio
@@ -103,7 +107,7 @@ async def test_callback_in_callback_error() -> None:
 
 @pytest.mark.anyio
 async def test_no_callbacks_if_error() -> None:
-    try:
+    with pytest.raises(Exception, match="This should prevent the callbacks from being called"):
         async with ActionScope.new_scope(TestSideEffects) as action_scope:
             async with action_scope.use() as interface:
 
@@ -114,5 +118,14 @@ async def test_no_callbacks_if_error() -> None:
 
             async with action_scope.use() as interface:
                 raise RuntimeError("This should prevent the callbacks from being called")
-    except RuntimeError:
-        pass
+
+    with pytest.raises(Exception, match="This should prevent the callbacks from being called"):
+        async with ActionScope.new_scope(TestSideEffects) as action_scope:
+            async with action_scope.use() as interface:
+
+                async def callback(interface: StateInterface[TestSideEffects]) -> None:
+                    raise NotImplementedError("Should not get here")  # pragma: no cover
+
+                interface.add_callback(callback)
+
+            raise RuntimeError("This should prevent the callbacks from being called")
