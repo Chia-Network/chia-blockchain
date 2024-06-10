04c8034bb74bfb3a04e395b1bc0cbf3e4c4f1ccd
arvidn <arvid@libtorrent.org>
2024-06-10 12:43:22 +0200

    NO_RELATIVE_CONDITIONS_ON_EPHEMERAL is always enabled in chia_rs 0.10.0

--- chia/full_node/mempool_check_conditions.py
+++ chia/full_node/mempool_check_conditions.py
@@ -12,7 +12,6 @@ from chia_rs import (
     ENABLE_MESSAGE_CONDITIONS,
     ENABLE_SOFTFORK_CONDITION,
     MEMPOOL_MODE,
-    NO_RELATIVE_CONDITIONS_ON_EPHEMERAL,
 )
 from chia_rs import get_puzzle_and_solution_for_coin as get_puzzle_and_solution_for_coin_rust
 from chia_rs import run_block_generator, run_block_generator2, run_chia_program
@@ -41,7 +40,7 @@ log = logging.getLogger(__name__)
 
 
 def get_flags_for_height_and_constants(height: int, constants: ConsensusConstants) -> int:
-    flags = NO_RELATIVE_CONDITIONS_ON_EPHEMERAL
+    flags = 0
 
     if height >= constants.SOFT_FORK4_HEIGHT:
         flags = flags | ENABLE_MESSAGE_CONDITIONS
