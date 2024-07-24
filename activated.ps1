8e26a1977a9e0bd8fd942ae397b53c1244aa2764
Kyle Altendorf <sda@fstab.net>
2024-07-24 09:57:27 -0400

    add poetry check to pre-commit

--- activated.ps1
+++ activated.ps1
@@ -2,11 +2,13 @@ $ErrorActionPreference = "Stop"
 
 $script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-$command = $args[0]
+$env_director = $args[0]
+$command = $args[1]
 $parameters = [System.Collections.ArrayList]$args
 $parameters.RemoveAt(0)
+$parameters.RemoveAt(1)
 
-& $script_directory/.venv/Scripts/Activate.ps1
+& $script_directory/$env_director/Scripts/Activate.ps1
 & $command @parameters
 
 exit $LASTEXITCODE
--- activated.sh
+++ activated.sh
@@ -6,7 +6,10 @@ SCRIPT_DIRECTORY=$(
   cd -- "$(dirname -- "$0")"
   pwd
 )
+
+ENV_DIRECTORY="$1"
+shift
 # shellcheck disable=SC1091
-. "${SCRIPT_DIRECTORY}/.venv/bin/activate"
+. "${SCRIPT_DIRECTORY}/${ENV_DIRECTORY}/bin/activate"
 
 "$@"
