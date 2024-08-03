68e587124715e0a705219b02efbab2f1ade82769
Geoff Walmsley <geoffwalmsley@gmail.com>
2024-08-03 16:04:45 +0100

    add vault parameterisation to wallet_environments

--- chia/_tests/wallet/conftest.py
+++ chia/_tests/wallet/conftest.py
@@ -5,18 +5,26 @@ from dataclasses import replace
 from typing import Any, AsyncIterator, Awaitable, Callable, Dict, List, Literal, Optional, Tuple
 
 import pytest
+from chia_rs import G1Element
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 
-from chia._tests.environments.wallet import WalletEnvironment, WalletState, WalletTestFramework
+from chia._tests.conftest import ConsensusMode
+from chia._tests.environments.wallet import WalletEnvironment, WalletState, WalletStateTransition, WalletTestFramework
 from chia._tests.util.setup_nodes import setup_simulators_and_wallets_service
 from chia._tests.wallet.wallet_block_tools import WalletBlockTools
 from chia.consensus.constants import ConsensusConstants
 from chia.consensus.cost_calculator import NPCResult
 from chia.full_node.full_node import FullNode
+from chia.rpc.wallet_request_types import VaultCreate
 from chia.rpc.wallet_rpc_client import WalletRpcClient
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.peer_info import PeerInfo
 from chia.util.ints import uint32, uint64, uint128
 from chia.wallet.util.tx_config import DEFAULT_TX_CONFIG, TXConfig
+from chia.wallet.vault.vault_root import VaultRoot
+from chia.wallet.vault.vault_wallet import Vault
 from chia.wallet.wallet_node import Balance
 from chia.wallet.wallet_state_manager import WalletStateManager
 
@@ -112,6 +120,7 @@ async def wallet_environments(
     trusted_full_node: bool,
     tx_config: TXConfig,
     blockchain_constants: ConsensusConstants,
+    consensus_mode: ConsensusMode,
     request: pytest.FixtureRequest,
 ) -> AsyncIterator[WalletTestFramework]:
     if "trusted" in request.param:
@@ -125,6 +134,9 @@ async def wallet_environments(
         config_overrides: Dict[str, Any] = request.param["config_overrides"]
     else:  # pragma: no cover
         config_overrides = {}
+    as_vault = request.param["as_vault"] if "as_vault" in request.param else False
+    if as_vault and (consensus_mode != ConsensusMode.HARD_FORK_2_0):
+        pytest.skip("Skipping vault tests for consensus modes other than HARD_FORK_2_0")
     async with setup_simulators_and_wallets_service(
         1,
         request.param["num_environments"],
@@ -184,7 +196,7 @@ async def wallet_environments(
                     )
                 )
 
-            yield WalletTestFramework(
+            wallet_environments = WalletTestFramework(
                 full_node[0]._api,
                 trusted_full_node,
                 [
@@ -197,3 +209,89 @@ async def wallet_environments(
                 ],
                 tx_config,
             )
+
+            if as_vault:
+                for index, env in enumerate(wallet_environments.environments):
+                    seed = 0x1A62C9636D1C9DB2E7D564D0C11603BF456AAD25AA7B12BDFD762B4E38E7EDC6 * (index + 1)
+                    SECP_SK = ec.derive_private_key(seed, ec.SECP256R1(), default_backend())
+                    SECP_PK = SECP_SK.public_key().public_bytes(Encoding.X962, PublicFormat.CompressedPoint)
+                    client = env.rpc_client
+                    fingerprint = (await client.get_public_keys())[0]
+                    bls_pk_hex = (await client.get_private_key(fingerprint))["pk"]
+                    bls_pk = G1Element.from_bytes(bytes.fromhex(bls_pk_hex))
+                    timelock = uint64(10)
+                    hidden_puzzle_index = uint32(0)
+                    res = await client.vault_create(
+                        VaultCreate(
+                            secp_pk=SECP_PK,
+                            hp_index=hidden_puzzle_index,
+                            bls_pk=bls_pk,
+                            timelock=timelock,
+                            push=True,
+                        ),
+                        tx_config=tx_config,
+                    )
+                    all_removals = [coin for tx in res.transactions for coin in tx.removals]
+                    eve_coin = [
+                        item
+                        for tx in res.transactions
+                        for item in tx.additions
+                        if item not in all_removals and item.amount == 1
+                    ][0]
+                    launcher_id = eve_coin.parent_coin_info
+                    vault_root = VaultRoot.from_bytes(launcher_id)
+
+                    await wallet_environments.process_pending_states(
+                        [
+                            WalletStateTransition(
+                                pre_block_balance_updates={
+                                    1: {
+                                        "init": True,
+                                        "set_remainder": True,
+                                    }
+                                },
+                                post_block_balance_updates={
+                                    1: {
+                                        "set_remainder": True,
+                                    }
+                                },
+                            )
+                            for _ in wallet_environments.environments
+                        ]
+                    )
+                    await env.node.keychain_proxy.add_key(launcher_id.hex(), label="vault", private=False)
+                    await env.restart(vault_root.get_fingerprint())
+                    await wallet_environments.full_node.wait_for_wallet_synced(env.node, 20)
+                    assert isinstance(env.xch_wallet, Vault)  # mypy
+                    await env.xch_wallet.sync_vault_launcher()
+
+                for env, blocks_needed in zip(wallet_environments.environments, request.param["blocks_needed"]):
+                    if blocks_needed > 0:
+                        assert isinstance(env.xch_wallet, Vault)  # mypy
+                        farm_to = env.xch_wallet.get_p2_singleton_puzzle_hash()
+                        await wallet_environments.full_node.farm_blocks_to_puzzlehash(
+                            count=blocks_needed,
+                            farm_to=farm_to,
+                            guarantee_transaction_blocks=True,
+                        )
+                        await full_node[0]._api.wait_for_wallet_synced(wallet_node=service._node, timeout=20)
+                        await wallet_environments.process_pending_states(
+                            [
+                                WalletStateTransition(
+                                    pre_block_balance_updates={
+                                        1: {
+                                            "init": True,
+                                            "set_remainder": True,
+                                        }
+                                    },
+                                    post_block_balance_updates={
+                                        1: {
+                                            "set_remainder": True,
+                                        }
+                                    },
+                                )
+                                for _ in wallet_environments.environments
+                            ]
+                        )
+
+            yield wallet_environments
