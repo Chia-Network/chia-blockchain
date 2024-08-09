5b63171483789365bf95e6f2b8fc97801ff1673a
Matthew Howard <m.o.howard97@gmail.com>
2024-08-09 16:29:50 +0100

    initial commit

--- chia/types/spend_bundle_conditions.py
+++ chia/types/spend_bundle_conditions.py
@@ -3,5 +3,9 @@ from __future__ import annotations
 import chia_rs
 
 ELIGIBLE_FOR_DEDUP = chia_rs.ELIGIBLE_FOR_DEDUP
-Spend = chia_rs.Spend
+try:
+    SpendConditions = chia_rs.SpendConditions
+except AttributeError:
+    # Fallback to the old name if the new name does not exist
+    SpendConditions = chia_rs.Spend
 SpendBundleConditions = chia_rs.SpendBundleConditions
