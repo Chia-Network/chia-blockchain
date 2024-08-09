d784c70dad2e3b06a08175cc59d17c7dad1fa3b5
Matthew Howard <m.o.howard97@gmail.com>
2024-08-09 16:54:29 +0100

    black

--- chia/types/spend_bundle_conditions.py
+++ chia/types/spend_bundle_conditions.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import chia_rs
 
 ELIGIBLE_FOR_DEDUP = chia_rs.ELIGIBLE_FOR_DEDUP
-if hasattr(chia_rs, 'SpendConditions'):
+if hasattr(chia_rs, "SpendConditions"):
     SpendConditions = chia_rs.SpendConditions
 else:
     # Fallback to the old name if the new name does not exist
