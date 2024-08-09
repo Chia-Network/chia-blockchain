c290703391db2a0653a2efc29fff6d928e28026c
Matthew Howard <m.o.howard97@gmail.com>
2024-08-09 16:51:32 +0100

    avoid using try except

--- chia/types/spend_bundle_conditions.py
+++ chia/types/spend_bundle_conditions.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 import chia_rs
 
 ELIGIBLE_FOR_DEDUP = chia_rs.ELIGIBLE_FOR_DEDUP
-try:
+if hasattr(chia_rs, 'SpendConditions'):
     SpendConditions = chia_rs.SpendConditions
-except AttributeError:
+else:
     # Fallback to the old name if the new name does not exist
     SpendConditions = chia_rs.Spend
 SpendBundleConditions = chia_rs.SpendBundleConditions
