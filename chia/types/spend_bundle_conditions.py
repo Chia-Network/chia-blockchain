09d329d7bf61c09443866905d5613020325e6580
Matthew Howard <m.o.howard97@gmail.com>
2024-08-13 17:16:05 +0100

    only support current version of chia_rs

--- chia/types/spend_bundle_conditions.py
+++ chia/types/spend_bundle_conditions.py
@@ -3,9 +3,5 @@ from __future__ import annotations
 import chia_rs
 
 ELIGIBLE_FOR_DEDUP = chia_rs.ELIGIBLE_FOR_DEDUP
-if hasattr(chia_rs, "SpendConditions"):
-    SpendConditions = chia_rs.SpendConditions
-else:
-    # Fallback to the old name if the new name does not exist
-    SpendConditions = chia_rs.Spend
+SpendConditions = chia_rs.Spend
 SpendBundleConditions = chia_rs.SpendBundleConditions
