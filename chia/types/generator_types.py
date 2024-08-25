95deb9caa06252ba8650a622f1518cadfe75f66c
arvidn <arvid@libtorrent.org>
2024-08-25 15:20:18 +0200

    remove unused GeneratorBlockCacheInterface and BlockDict

--- chia/types/generator_types.py
+++ chia/types/generator_types.py
@@ -4,15 +4,7 @@ from dataclasses import dataclass
 from typing import List
 
 from chia.types.blockchain_format.serialized_program import SerializedProgram
-from chia.util.ints import uint32
 from chia.util.streamable import Streamable, streamable
-
-
-class GeneratorBlockCacheInterface:
-    def get_generator_for_block_height(self, height: uint32) -> SerializedProgram:
-        # Requested block must be a transaction block
-        # ignoring hinting error until we handle our interfaces more formally
-        return  # type: ignore[return-value]
 
 
 @streamable
