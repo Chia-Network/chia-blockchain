d5df5701708f63d9fef01c0c4d105e19b5603ee8
Richard Kiss <him@richardkiss.com>
2024-09-06 17:12:48 -0700

    Move a file to a subproject

---
+++ subprojects/chia_core/chia/core/types/ints.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import chia_rs.sized_ints
+
+int8 = chia_rs.sized_ints.int8
+uint8 = chia_rs.sized_ints.uint8
+
+int16 = chia_rs.sized_ints.int16
+uint16 = chia_rs.sized_ints.uint16
+
+int32 = chia_rs.sized_ints.int32
+uint32 = chia_rs.sized_ints.uint32
+
+int64 = chia_rs.sized_ints.int64
+uint64 = chia_rs.sized_ints.uint64
+
+uint128 = chia_rs.sized_ints.uint128
+
+int512 = chia_rs.sized_ints.int512
