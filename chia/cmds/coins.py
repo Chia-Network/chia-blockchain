025fac6085b3958b77f9284655a17686b858525f
Matt <quexington@gmail.com>
2024-09-26 09:21:01 -0700

    Forgot to delete old split command

--- chia/cmds/coins.py
+++ chia/cmds/coins.py
@@ -1,15 +1,13 @@
 from __future__ import annotations
 
-import asyncio
 import dataclasses
 import sys
 from typing import List, Optional, Sequence, Tuple
 
 import click
 
-from chia.cmds import options
 from chia.cmds.cmd_classes import NeedsCoinSelectionConfig, NeedsWalletRPC, TransactionEndpoint, chia_command, option
-from chia.cmds.cmds_util import cli_confirm, tx_config_args, tx_out_cmd
+from chia.cmds.cmds_util import cli_confirm
 from chia.cmds.param_types import AmountParamType, Bytes32ParamType, CliAmount
 from chia.cmds.wallet_funcs import get_mojo_per_unit, get_wallet_type, print_balance
 from chia.rpc.wallet_request_types import CombineCoins, SplitCoins
@@ -18,7 +16,6 @@ from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.bech32m import encode_puzzle_hash
 from chia.util.config import selected_network_address_prefix
 from chia.util.ints import uint16, uint32, uint64
-from chia.wallet.conditions import ConditionValidTimes
 from chia.wallet.transaction_record import TransactionRecord
 from chia.wallet.util.wallet_types import WalletType
 
@@ -27,74 +24,6 @@ from chia.wallet.util.wallet_types import WalletType
 @click.pass_context
 def coins_cmd(ctx: click.Context) -> None:
     pass
-
-
-@coins_cmd.command("split", help="Split up larger coins")
-@click.option(
-    "-p",
-    "--wallet-rpc-port",
-    help="Set the port where the Wallet is hosting the RPC interface. See the rpc_port under wallet in config.yaml",
-    type=int,
-    default=None,
-)
-@options.create_fingerprint()
-@click.option("-i", "--id", help="Id of the wallet to use", type=int, default=1, show_default=True, required=True)
-@click.option(
-    "-n",
-    "--number-of-coins",
-    type=int,
-    help="The number of coins we are creating.",
-    required=True,
-)
-@options.create_fee()
-@click.option(
-    "-a",
-    "--amount-per-coin",
-    help="The amount of each newly created coin, in XCH or CAT units",
-    type=AmountParamType(),
-    required=True,
-)
-@click.option(
-    "-t", "--target-coin-id", type=Bytes32ParamType(), required=True, help="The coin id of the coin we are splitting."
-)
-@tx_config_args
-@tx_out_cmd()
-def split_cmd(
-    wallet_rpc_port: Optional[int],
-    fingerprint: int,
-    id: int,
-    number_of_coins: int,
-    fee: uint64,
-    amount_per_coin: CliAmount,
-    target_coin_id: bytes32,
-    min_coin_amount: CliAmount,
-    max_coin_amount: CliAmount,
-    amounts_to_exclude: Sequence[CliAmount],
-    coins_to_exclude: Sequence[bytes32],
-    reuse: bool,
-    push: bool,
-    condition_valid_times: ConditionValidTimes,
-) -> List[TransactionRecord]:
-    from .coin_funcs import async_split
-
-    return asyncio.run(
-        async_split(
-            wallet_rpc_port=wallet_rpc_port,
-            fingerprint=fingerprint,
-            wallet_id=id,
-            fee=fee,
-            max_coin_amount=max_coin_amount,
-            min_coin_amount=min_coin_amount,
-            excluded_amounts=amounts_to_exclude,
-            coins_to_exclude=coins_to_exclude,
-            reuse_puzhash=reuse,
-            number_of_coins=number_of_coins,
-            amount_per_coin=amount_per_coin,
-            target_coin_id=target_coin_id,
-            push=push,
-            condition_valid_times=condition_valid_times,
-        )
-    )
 
 
 def print_coins(
