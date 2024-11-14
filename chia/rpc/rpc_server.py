359ff89f8ac519c590a081961c308699754100b4
Kyle Altendorf <sda@fstab.net>
2024-11-14 10:33:42 -0500

    fill in missing rpc client methods (#18851)
    * fill in missing rpc client methods
    
    * no cover
    
    (cherry picked from commit 96a8be8f635b5800a55a855773864bb42c477a29)

--- chia/rpc/rpc_server.py
+++ chia/rpc/rpc_server.py
@@ -8,7 +8,8 @@ import traceback
 from dataclasses import dataclass
 from pathlib import Path
 from ssl import SSLContext
-from typing import Any, AsyncIterator, Awaitable, Callable, Dict, Generic, List, Optional, TypeVar
+from types import MethodType
+from typing import Any, AsyncIterator, Awaitable, Callable, ClassVar, Dict, Generic, List, Optional, TypeVar
 
 from aiohttp import ClientConnectorError, ClientSession, ClientWebSocketResponse, WSMsgType, web
 from typing_extensions import Protocol, final
@@ -236,14 +237,7 @@ class RpcServer(Generic[_T_RpcApiProtocol]):
     def _get_routes(self) -> Dict[str, Endpoint]:
         return {
             **self.rpc_api.get_routes(),
-            "/get_network_info": self.get_network_info,
-            "/get_connections": self.get_connections,
-            "/open_connection": self.open_connection,
-            "/close_connection": self.close_connection,
-            "/stop_node": self.stop_node,
-            "/get_routes": self.get_routes,
-            "/get_version": self.get_version,
-            "/healthz": self.healthz,
+            **{path: MethodType(handler, self) for path, handler in self._routes.items()},
         }
 
     async def get_routes(self, request: Dict[str, Any]) -> EndpointResult:
@@ -408,6 +402,17 @@ class RpcServer(Generic[_T_RpcApiProtocol]):
                 await asyncio.sleep(2)
 
         self.daemon_connection_task = asyncio.create_task(inner())
+
+    _routes: ClassVar[dict[str, Callable[..., Awaitable[object]]]] = {
+        "/get_network_info": get_network_info,
+        "/get_connections": get_connections,
+        "/open_connection": open_connection,
+        "/close_connection": close_connection,
+        "/stop_node": stop_node,
+        "/get_routes": get_routes,
+        "/get_version": get_version,
+        "/healthz": healthz,
+    }
 
 
 async def start_rpc_server(
