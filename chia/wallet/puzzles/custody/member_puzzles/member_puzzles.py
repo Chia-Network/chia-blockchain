95ba042b5dfa38b698c64c9b641a1fce43d3d34e
Matthew Howard <m.o.howard97@gmail.com>
2024-11-14 15:05:59 +0000

    initial commit

--- chia/wallet/puzzles/custody/member_puzzles/member_puzzles.py
+++ chia/wallet/puzzles/custody/member_puzzles/member_puzzles.py
@@ -24,6 +24,10 @@ SECPR1_MEMBER_MOD = load_clvm_maybe_recompile(
     "secp256r1_member.clsp", package_or_requirement="chia.wallet.puzzles.custody.member_puzzles"
 )
 
+SECPK1_MEMBER_MOD = load_clvm_maybe_recompile(
+    "secp256k1_member.clsp", package_or_requirement="chia.wallet.puzzles.custody.member_puzzles"
+)
+
 
 @dataclass(frozen=True)
 class BLSMember(Puzzle):
@@ -77,6 +81,19 @@ class SECPR1Member(Puzzle):
 
     def puzzle(self, nonce: int) -> Program:
         return SECPR1_MEMBER_MOD.curry(self.secp_pk)
+
+    def puzzle_hash(self, nonce: int) -> bytes32:
+        return self.puzzle(nonce).get_tree_hash()
+
+@dataclass(frozen=True)
+class SECPK1Member(Puzzle):
+    secp_pk: bytes
+
+    def memo(self, nonce: int) -> Program:
+        return Program.to(0)
+
+    def puzzle(self, nonce: int) -> Program:
+        return SECPK1_MEMBER_MOD.curry(self.secp_pk)
 
     def puzzle_hash(self, nonce: int) -> bytes32:
         return self.puzzle(nonce).get_tree_hash()
