9765bf86462ecc1c999867e93d5dfcf3337954f4
Chris Marslender <chrismarslender@gmail.com>
2024-11-15 11:52:39 -0600

    twofourfouraio network

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
-DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/twofourfour"))).resolve()
+DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/twofourfouraio"))).resolve()
 
 DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
 
