e7d12c7cb162c569bc74ad0bbe9f1a34e82c9da5
Kyle Altendorf <sda@fstab.net>
2024-12-03 16:32:34 -0500

    add `root_path` parameter to `initialize_service_logging()` (#18961)

--- chia/server/start_farmer.py
+++ chia/server/start_farmer.py
@@ -17,7 +17,7 @@ from chia.server.start_service import RpcInfo, Service, async_run
 from chia.types.aliases import FarmerService
 from chia.util.chia_logging import initialize_service_logging
 from chia.util.config import get_unresolved_peer_infos, load_config, load_config_cli
-from chia.util.default_root import DEFAULT_ROOT_PATH
+from chia.util.default_root import resolve_root_path
 from chia.util.keychain import Keychain
 from chia.util.task_timing import maybe_manage_task_instrumentation
 
@@ -68,16 +68,16 @@ def create_farmer_service(
     )
 
 
-async def async_main() -> int:
+async def async_main(root_path: pathlib.Path) -> int:
     # TODO: refactor to avoid the double load
-    config = load_config(DEFAULT_ROOT_PATH, "config.yaml")
-    service_config = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", SERVICE_NAME)
+    config = load_config(root_path, "config.yaml")
+    service_config = load_config_cli(root_path, "config.yaml", SERVICE_NAME)
     config[SERVICE_NAME] = service_config
-    config_pool = load_config_cli(DEFAULT_ROOT_PATH, "config.yaml", "pool")
+    config_pool = load_config_cli(root_path, "config.yaml", "pool")
     config["pool"] = config_pool
-    initialize_service_logging(service_name=SERVICE_NAME, config=config)
+    initialize_service_logging(service_name=SERVICE_NAME, config=config, root_path=root_path)
 
-    service = create_farmer_service(DEFAULT_ROOT_PATH, config, config_pool, DEFAULT_CONSTANTS)
+    service = create_farmer_service(root_path, config, config_pool, DEFAULT_CONSTANTS)
     async with SignalHandlers.manage() as signal_handlers:
         await service.setup_process_global_state(signal_handlers=signal_handlers)
         await service.run()
@@ -86,10 +86,12 @@ async def async_main() -> int:
 
 
 def main() -> int:
+    root_path = resolve_root_path(override=None)
+
     with maybe_manage_task_instrumentation(
         enable=os.environ.get(f"CHIA_INSTRUMENT_{SERVICE_NAME.upper()}") is not None
     ):
-        return async_run(coro=async_main())
+        return async_run(coro=async_main(root_path=root_path))
 
 
 if __name__ == "__main__":
