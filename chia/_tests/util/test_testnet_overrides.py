e3c6d8f3286773d3e15f27124d1144995b382e3f
Arvid Norberg <arvid@libtorrent.org>
2024-12-04 21:55:53 +0100

    bump chia_rs to 0.16.0 and introduce soft-fork6

--- chia/_tests/util/test_testnet_overrides.py
+++ chia/_tests/util/test_testnet_overrides.py
@@ -9,7 +9,7 @@ def test_testnet11() -> None:
     overrides: Dict[str, Any] = {}
     update_testnet_overrides("testnet11", overrides)
     assert overrides == {
-        "SOFT_FORK5_HEIGHT": 1340000,
+        "SOFT_FORK6_HEIGHT": 2000000,
     }
 
 
