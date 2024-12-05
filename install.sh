06750ea060086c8771f08f25b0947f99bd742488
Earle Lowe <e.lowe@chia.net>
2024-12-04 17:10:29 -0800

    go back to the check for 7 which is wrong, but wrong in a consistent way for main

--- install.sh
+++ install.sh
@@ -137,7 +137,7 @@ if ! command -v "$INSTALL_PYTHON_PATH" >/dev/null; then
   exit 1
 fi
 
-if [ "$PYTHON_MAJOR_VER" -ne "3" ] || [ "$PYTHON_MINOR_VER" -lt "8" ] || [ "$PYTHON_MINOR_VER" -ge "13" ]; then
+if [ "$PYTHON_MAJOR_VER" -ne "3" ] || [ "$PYTHON_MINOR_VER" -lt "7" ] || [ "$PYTHON_MINOR_VER" -ge "13" ]; then
   echo "Chia requires Python version >= 3.9 and  < 3.13.0" >&2
   echo "Current Python version = $INSTALL_PYTHON_VERSION" >&2
   # If Arch, direct to Arch Wiki
