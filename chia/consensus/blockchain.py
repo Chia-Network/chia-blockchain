7c84f8ef05e10383a07fea9e47e3c9f901ae14b6
Amine Khaldi <amine.khaldi@reactos.org>
2025-01-07 16:34:45 +0100

    Clarify cases when transactions filter is computed with just reward coins.

--- chia/consensus/blockchain.py
+++ chia/consensus/blockchain.py
@@ -902,18 +902,22 @@ class Blockchain:
                 raise ValueError(f"Block at {block.header_hash} is no longer in the blockchain (it's in a fork)")
             if tx_filter is False:
                 header = get_block_header(block)
-            elif block.transactions_generator is None:
-                # There is no point in getting additions and removals for
-                # blocks that do not have transactions.
-                header = get_block_header(block)
-            else:
+            elif block.transactions_generator is not None:
                 added_coins_records, removed_coins_records = await asyncio.gather(
                     self.coin_store.get_coins_added_at_height(block.height),
                     self.coin_store.get_coins_removed_at_height(block.height),
                 )
                 tx_additions = [cr.coin for cr in added_coins_records if not cr.coinbase]
                 removed = [cr.coin.name() for cr in removed_coins_records]
-                header = get_block_header(block, (tx_additions, removed))
+                header = get_block_header(block, (removed, tx_additions))
+            elif block.is_transaction_block():
+                # This is a transaction block with just reward coins.
+                # We're sending empty additions and removals to signal that we
+                # want the transactions filter to be computed.
+                header = get_block_header(block, ([], []))
+            else:
+                # Non transaction block.
+                header = get_block_header(block)
             header_blocks[header.header_hash] = header
 
         return header_blocks
