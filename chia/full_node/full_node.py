36128e3b839cfdc71ed9c193a7f3c3924154a009
William Blanke <wjb98672@gmail.com>
2025-01-10 05:21:51 -0800

    ruff

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -638,7 +638,7 @@ class FullNode:
                     vs = ValidationState(ssi, diff, None)
 
                     # Wrap add_block_batch with writer to ensure all writes and reads are on same connection.
-                    async with self.block_store.db_wrapper.writer() as conn:
+                    async with self.block_store.db_wrapper.writer():
                         success, state_change_summary = await self.add_block_batch(
                             response.blocks, peer_info, fork_info, vs
                         )
@@ -1357,7 +1357,7 @@ class FullNode:
                 pre_validation_results = list(await asyncio.gather(*futures))
 
                 # Wrap add_prevalidated_blocks with writer to ensure all writes and reads are on same connection.
-                async with self.block_store.db_wrapper.writer() as conn:
+                async with self.block_store.db_wrapper.writer():
                     # The ValidationState object (vs) is an in-out parameter. the add_block_batch()
                     # call will update it
                     state_change_summary, err = await self.add_prevalidated_blocks(
