1f66b2f6fe6dc90139de7e4e3d83837e731477c6
Kyle Altendorf <sda@fstab.net>
2025-01-13 08:03:31 -0500

    some more

--- install.sh
+++ install.sh
@@ -137,8 +137,8 @@ if ! command -v "$INSTALL_PYTHON_PATH" >/dev/null; then
   exit 1
 fi
 
-if [ "$PYTHON_MAJOR_VER" -ne "3" ] || [ "$PYTHON_MINOR_VER" -lt "7" ] || [ "$PYTHON_MINOR_VER" -ge "13" ]; then
-  echo "Chia requires Python version >= 3.9 and  < 3.13.0" >&2
+if [ "$PYTHON_MAJOR_VER" -ne "3" ] || [ "$PYTHON_MINOR_VER" -lt "7" ] || [ "$PYTHON_MINOR_VER" -ge "14" ]; then
+  echo "Chia requires Python version >= 3.9 and  < 3.14.0" >&2
   echo "Current Python version = $INSTALL_PYTHON_VERSION" >&2
   # If Arch, direct to Arch Wiki
   if type pacman >/dev/null 2>&1 && [ -f "/etc/arch-release" ]; then
