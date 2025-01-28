9ea70e3777e66de2488d1aaab5417f86e500bc47
Matthew Howard <m.o.howard97@gmail.com>
2025-01-28 12:22:12 +0000

    initial commit

--- chia/harvester/harvester_api.py
+++ chia/harvester/harvester_api.py
@@ -6,9 +6,9 @@ import time
 from pathlib import Path
 from typing import TYPE_CHECKING, ClassVar, Optional, cast
 
-from chia_rs import AugSchemeMPL, G1Element, G2Element
+from chia_rs import AugSchemeMPL, G1Element, G2Element, calculate_sp_interval_iters
 
-from chia.consensus.pot_iterations import calculate_iterations_quality, calculate_sp_interval_iters
+from chia.consensus.pot_iterations import calculate_iterations_quality
 from chia.harvester.harvester import Harvester
 from chia.plotting.util import PlotInfo, parse_plot_info
 from chia.protocols import harvester_protocol
@@ -151,7 +151,9 @@ class HarvesterAPI:
                             difficulty,
                             new_challenge.sp_hash,
                         )
-                        sp_interval_iters = calculate_sp_interval_iters(self.harvester.constants, sub_slot_iters)
+                        sp_interval_iters = calculate_sp_interval_iters(
+                            self.harvester.constants.NUM_SPS_SUB_SLOT, sub_slot_iters
+                        )
                         if required_iters < sp_interval_iters:
                             # Found a very good proof of space! will fetch the whole proof from disk,
                             # then send to farmer
