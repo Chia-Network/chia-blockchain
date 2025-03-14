8376e74ce5ed1f731b1ae1f68810a9f786e89479
Matthew Howard <m.o.howard97@gmail.com>
2025-03-14 15:47:36 +0000

    first version of new schema

--- chia/server/node_discovery.py
+++ chia/server/node_discovery.py
@@ -11,6 +11,7 @@ from random import Random
 from secrets import randbits
 from typing import Any, Optional
 
+import aiosqlite
 import dns.asyncresolver
 from chia_rs.sized_ints import uint16, uint64
 
@@ -18,7 +19,7 @@ from chia.protocols.full_node_protocol import RequestPeers, RespondPeers
 from chia.protocols.introducer_protocol import RequestPeersIntroducer
 from chia.protocols.protocol_message_types import ProtocolMessageTypes
 from chia.server.address_manager import AddressManager, ExtendedPeerInfo
-from chia.server.address_manager_store import AddressManagerStore
+from chia.server.address_manager_store_sql import AddressManagerStore
 from chia.server.outbound_message import Message, NodeType, make_msg
 from chia.server.server import ChiaServer
 from chia.server.ws_connection import WSChiaConnection
@@ -73,6 +74,7 @@ class FullNodeDiscovery:
         self.log = log
         self.relay_queue: Optional[asyncio.Queue[tuple[TimestampedPeerInfo, int]]] = None
         self.address_manager: Optional[AddressManager] = None
+        self.peers_db_connection: Optional[aiosqlite.Connection] = None
         self.connection_time_pretest: dict[str, Any] = {}
         self.received_count_from_peers: dict[str, Any] = {}
         self.lock = asyncio.Lock()
@@ -92,6 +94,8 @@ class FullNodeDiscovery:
             self.default_port = NETWORK_ID_DEFAULT_PORTS[selected_network]
 
     async def initialize_address_manager(self) -> None:
+        self.peers_db_connection = aiosqlite.connect(self.peers_file_path)
+        AddressManagerStore.initialise(self.peers_db_connection)
         self.address_manager = await AddressManagerStore.create_address_manager(self.peers_file_path)
         if self.enable_private_networks:
             self.address_manager.make_private_subnets_valid()
