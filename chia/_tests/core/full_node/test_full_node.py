7082e819fb659dc083e71a9b598f10e306549430
Matthew Howard <m.o.howard97@gmail.com>
2025-03-31 16:59:31 +0100

    ruff check

--- chia/_tests/core/full_node/test_full_node.py
+++ chia/_tests/core/full_node/test_full_node.py
@@ -51,7 +51,6 @@ from chia.protocols.full_node_protocol import NewTransaction, RespondTransaction
 from chia.protocols.protocol_message_types import ProtocolMessageTypes
 from chia.protocols.shared_protocol import Capability, default_capabilities
 from chia.protocols.wallet_protocol import SendTransaction, TransactionAck
-from chia.server.address_manager import AddressManager
 from chia.server.node_discovery import FullNodePeers
 from chia.server.outbound_message import Message, NodeType
 from chia.server.server import ChiaServer
