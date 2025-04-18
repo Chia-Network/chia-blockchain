4afc1c0e018acf14fa3490652b199ede8a1b0dc5
Kyle Altendorf <sda@fstab.net>
2025-04-18 09:50:30 -0400

    links

--- chia/data_layer/data_store.py
+++ chia/data_layer/data_store.py
@@ -255,6 +255,8 @@ class DataStore:
             async with self.db_wrapper.reader() as reader:
                 cursor = await reader.execute(
                     # TODO: the INDEXED BY seems like it shouldn't be needed, figure out why it is
+                    #       https://sqlite.org/lang_indexedby.html: admonished to omit all use of INDEXED BY
+                    #       https://sqlite.org/queryplanner-ng.html#howtofix
                     "SELECT MAX(generation) FROM nodes INDEXED BY nodes_generation_index WHERE store_id = ?",
                     (store_id,),
                 )
