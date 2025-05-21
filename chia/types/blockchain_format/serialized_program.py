248734dba3280f65aa83e820419a74951ec00b1f
Matthew Howard <m.o.howard97@gmail.com>
2025-05-21 15:06:14 +0100

    re-implement to_program and from_program

--- chia/types/blockchain_format/serialized_program.py
+++ chia/types/blockchain_format/serialized_program.py
@@ -2,4 +2,23 @@ from __future__ import annotations
 
 import chia_rs
 
-SerializedProgram = chia_rs.Program
+from chia.types.blockchain_format.program import Program
+
+class SerializedProgram(chia_rs.Program):
+    """
+    SerializedProgram is a wrapper around the chia_rs.Program class.
+    It provides additional methods and properties specific to the Chia blockchain.
+    """
+
+    def to_program(self) -> Program:
+        """
+        Convert the SerializedProgram to a Program object.
+        """
+        return Program.from_bytes(self.to_bytes())
+    
+    @classmethod
+    def from_program(cls, program: Program) -> SerializedProgram:
+        """
+        Convert a Program object to a SerializedProgram.
+        """
+        return SerializedProgram.from_bytes(program.to_bytes())
