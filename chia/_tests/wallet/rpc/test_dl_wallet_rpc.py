f52e94b5903c06b74dd421365f6120bcbcffda50
Matt <quexington@gmail.com>
2025-06-12 09:30:13 +0200

    Fix test

--- chia/_tests/wallet/rpc/test_dl_wallet_rpc.py
+++ chia/_tests/wallet/rpc/test_dl_wallet_rpc.py
@@ -347,7 +347,7 @@ class TestWalletRpc:
         )
 
         with pytest.raises(ValueError, match="No peer connected"):
-            await wallet_service.rpc_server.rpc_api.dl_verify_proof(fake_gpr)
+            await wallet_service.rpc_server.rpc_api.dl_verify_proof(fake_gpr.to_json_dict())
 
         await wallet_node.server.start_client(PeerInfo(self_hostname, full_node_server.get_port()), None)
         await validate_get_routes(client, wallet_service.rpc_server.rpc_api)
