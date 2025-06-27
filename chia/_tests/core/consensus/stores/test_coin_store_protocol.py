58c3af76b38087251174b2dc6a386ea077cd56e4
Richard Kiss <him@richardkiss.com>
2025-06-27 11:25:25 -0700

    Improve test fixture stuff

--- chia/_tests/core/consensus/stores/test_coin_store_protocol.py
+++ chia/_tests/core/consensus/stores/test_coin_store_protocol.py
@@ -8,18 +8,22 @@ from chia._tests.util.db_connection import DBConnection
 from chia.consensus.coinbase import create_farmer_coin, create_pool_coin
 from chia.full_node.coin_store import CoinStore
 
+# black box tests from `chia/_tests/core/full_node/stores/test_coin_store.py`
+# should be moved here
+
 
 @pytest.mark.anyio
-async def test_is_empty_when_empty(db_version: int = 2) -> None:
+async def test_is_empty_when_empty(db_version: int) -> None:
     async with DBConnection(db_version) as db_wrapper:
         coin_store = await CoinStore.create(db_wrapper)
         assert await coin_store.is_empty() is True
 
 
 @pytest.mark.anyio
-async def test_is_empty_when_not_empty(db_version: int = 2) -> None:
+async def test_is_empty_when_not_empty(db_version: int) -> None:
     async with DBConnection(db_version) as db_wrapper:
         coin_store = await CoinStore.create(db_wrapper)
+        assert await coin_store.is_empty() is True
         height = uint32(1)
         genesis_challenge = bytes32(b"\0" * 32)
         pool_puzzle_hash = bytes32(b"\x01" * 32)
