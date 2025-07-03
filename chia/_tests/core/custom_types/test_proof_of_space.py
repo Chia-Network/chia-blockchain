3491f1aeaea1efc998d80a1824e2a182027521fc
Arvid Norberg <arvid@libtorrent.org>
2025-07-03 18:21:07 +0200

    Plot v2 quality string (#19769)
    * add shortcuts for bytes32 and G1Element to test_proof_of_space.py to make it a bit easier to read
    
    * introduce calculate_plot_difficulty(), for incrementing plot difficulty over time. Tweak the placeholder cut-over block heights to make it possible to test
    
    * update verify_and_get_quality_string() to support more aspects of v2 plots. update the v2 proof verification stub to take plot difficulty. add plot filter test cases for v2 plots
    
    * move some tests from weight-proof to proof-of-space

--- chia/_tests/core/custom_types/test_proof_of_space.py
+++ chia/_tests/core/custom_types/test_proof_of_space.py
@@ -5,13 +5,18 @@ from dataclasses import dataclass
 from typing import Optional
 
 import pytest
-from chia_rs import G1Element, ProofOfSpace
+from chia_rs import G1Element, PlotSize, ProofOfSpace
 from chia_rs.sized_bytes import bytes32, bytes48
 from chia_rs.sized_ints import uint8, uint32
 
 from chia._tests.util.misc import Marks, datacases
 from chia.consensus.default_constants import DEFAULT_CONSTANTS
-from chia.types.blockchain_format.proof_of_space import passes_plot_filter, verify_and_get_quality_string
+from chia.types.blockchain_format.proof_of_space import (
+    calculate_plot_difficulty,
+    calculate_prefix_bits,
+    passes_plot_filter,
+    verify_and_get_quality_string,
+)
 
 
 @dataclass
@@ -27,6 +32,15 @@ class ProofOfSpaceCase:
     marks: Marks = ()
 
 
+def g1(key: str) -> G1Element:
+    return G1Element.from_bytes_unchecked(bytes48.from_hexstr(key))
+
+
+def b32(key: str) -> bytes32:
+    return bytes32.from_hexstr(key)
+
+
+# TODO: todo_v2_plots more test cases
 @datacases(
     ProofOfSpaceCase(
         id="Neither pool public key nor pool contract puzzle hash",
@@ -70,36 +84,56 @@ class ProofOfSpaceCase:
     ),
     ProofOfSpaceCase(
         id="Not passing the plot filter with size 9",
-        pos_challenge=bytes32.from_hexstr("08b23cc2844dfb92d2eedaa705a1ce665d571ee753bd81cbb67b92caa6d34722"),
+        pos_challenge=b32("08b23cc2844dfb92d2eedaa705a1ce665d571ee753bd81cbb67b92caa6d34722"),
         plot_size=uint8(42),
-        pool_public_key=G1Element.from_bytes_unchecked(
-            bytes48.from_hexstr(
-                "b6449c2c68df97c19e884427e42ee7350982d4020571ead08732615ff39bd216bfd630b6460784982bec98b49fea79d0"
-            )
+        pool_public_key=g1(
+            "b6449c2c68df97c19e884427e42ee7350982d4020571ead08732615ff39bd216bfd630b6460784982bec98b49fea79d0"
         ),
-        plot_public_key=G1Element.from_bytes_unchecked(
-            bytes48.from_hexstr(
-                "b17d368f5400230b2b01464807825bf4163c5c159bd7d4465f935912e538ac9fb996dd9a9c479bd8aa6256bdca1fed96"
-            )
+        plot_public_key=g1(
+            "b17d368f5400230b2b01464807825bf4163c5c159bd7d4465f935912e538ac9fb996dd9a9c479bd8aa6256bdca1fed96"
         ),
         height=uint32(5495999),
         expected_error="Did not pass the plot filter",
     ),
     ProofOfSpaceCase(
         id="Passing the plot filter with size 8",
-        pos_challenge=bytes32.from_hexstr("08b23cc2844dfb92d2eedaa705a1ce665d571ee753bd81cbb67b92caa6d34722"),
+        pos_challenge=b32("08b23cc2844dfb92d2eedaa705a1ce665d571ee753bd81cbb67b92caa6d34722"),
         plot_size=uint8(42),
-        pool_public_key=G1Element.from_bytes_unchecked(
-            bytes48.from_hexstr(
-                "b6449c2c68df97c19e884427e42ee7350982d4020571ead08732615ff39bd216bfd630b6460784982bec98b49fea79d0"
-            )
+        pool_public_key=g1(
+            "b6449c2c68df97c19e884427e42ee7350982d4020571ead08732615ff39bd216bfd630b6460784982bec98b49fea79d0"
         ),
-        plot_public_key=G1Element.from_bytes_unchecked(
-            bytes48.from_hexstr(
-                "b17d368f5400230b2b01464807825bf4163c5c159bd7d4465f935912e538ac9fb996dd9a9c479bd8aa6256bdca1fed96"
-            )
+        plot_public_key=g1(
+            "b17d368f5400230b2b01464807825bf4163c5c159bd7d4465f935912e538ac9fb996dd9a9c479bd8aa6256bdca1fed96"
         ),
         height=uint32(5496000),
+    ),
+    ProofOfSpaceCase(
+        id="v2 plot size 0",
+        pos_challenge=bytes32(b"1" * 32),
+        plot_size=uint8(0x80),
+        plot_public_key=G1Element(),
+        pool_public_key=G1Element(),
+        expected_error="Plot size is lower than the minimum",
+    ),
+    ProofOfSpaceCase(
+        id="v2 plot size 34",
+        pos_challenge=bytes32(b"1" * 32),
+        plot_size=uint8(0x80 | 34),
+        plot_public_key=G1Element(),
+        pool_public_key=G1Element(),
+        expected_error="Plot size is higher than the maximum",
+    ),
+    ProofOfSpaceCase(
+        id="Not passing the plot filter v2",
+        pos_challenge=b32("3d29ea79d19b3f7e99ebf764ae53697cbe143603909873946af6ab1ece606861"),
+        plot_size=uint8(0x80 | 32),
+        pool_public_key=g1(
+            "b6449c2c68df97c19e884427e42ee7350982d4020571ead08732615ff39bd216bfd630b6460784982bec98b49fea79d0"
+        ),
+        plot_public_key=g1(
+            "879526b4e7b616cfd64984d8ad140d0798b048392a6f11e2faf09054ef467ea44dc0dab5e5edb2afdfa850c5c8b629cc"
+        ),
+        expected_error="Did not pass the plot filter",
     ),
 )
 def test_verify_and_get_quality_string(caplog: pytest.LogCaptureFixture, case: ProofOfSpaceCase) -> None:
@@ -114,41 +148,25 @@ def test_verify_and_get_quality_string(caplog: pytest.LogCaptureFixture, case: P
     quality_string = verify_and_get_quality_string(
         pos=pos,
         constants=DEFAULT_CONSTANTS,
-        original_challenge_hash=bytes32.from_hexstr(
-            "0x73490e166d0b88347c37d921660b216c27316aae9a3450933d3ff3b854e5831a"
-        ),
-        signage_point=bytes32.from_hexstr("0x7b3e23dbd438f9aceefa9827e2c5538898189987f49b06eceb7a43067e77b531"),
+        original_challenge_hash=b32("0x73490e166d0b88347c37d921660b216c27316aae9a3450933d3ff3b854e5831a"),
+        signage_point=b32("0x7b3e23dbd438f9aceefa9827e2c5538898189987f49b06eceb7a43067e77b531"),
         height=case.height,
     )
     assert quality_string is None
     assert len(caplog.text) == 0 if case.expected_error is None else case.expected_error in caplog.text
 
 
-# TODO: todo_v2_plots more test cases
 @datacases(
     ProofOfSpaceCase(
-        id="v2 plot size 0",
-        pos_challenge=bytes32(b"1" * 32),
-        plot_size=uint8(0x80),
-        plot_public_key=G1Element(),
-        pool_public_key=G1Element(),
-        pool_contract_puzzle_hash=bytes32(b"1" * 32),
-        expected_error="Plot size is lower than the minimum",
-    ),
-    ProofOfSpaceCase(
-        id="v2 plot size 34",
-        pos_challenge=bytes32(b"1" * 32),
-        plot_size=uint8(0x80 | 34),
-        plot_public_key=G1Element(),
-        pool_public_key=G1Element(),
-        expected_error="Plot size is higher than the maximum",
-    ),
-    ProofOfSpaceCase(
         id="v2 plot are not implemented",
-        pos_challenge=bytes32(b"1" * 32),
         plot_size=uint8(0x80 | 30),
-        plot_public_key=G1Element(),
-        pool_public_key=G1Element(),
+        pos_challenge=b32("47deb938e145d25d7b3b3c85ca9e3972b76c01aeeb78a02fe5d3b040d282317e"),
+        plot_public_key=g1(
+            "afa3aaf09c03885154be49216ee7fb2e4581b9c4a4d7e9cc402e27280bf0cfdbdf1b9ba674e301fd1d1450234b3b1868"
+        ),
+        pool_public_key=g1(
+            "b6449c2c68df97c19e884427e42ee7350982d4020571ead08732615ff39bd216bfd630b6460784982bec98b49fea79d0"
+        ),
         expected_error="NotImplementedError",
     ),
 )
@@ -168,18 +186,39 @@ def test_verify_and_get_quality_string_v2(caplog: pytest.LogCaptureFixture, case
         quality_string = verify_and_get_quality_string(
             pos=pos,
             constants=DEFAULT_CONSTANTS,
-            original_challenge_hash=bytes32.from_hexstr(
-                "0x73490e166d0b88347c37d921660b216c27316aae9a3450933d3ff3b854e5831a"
-            ),
-            signage_point=bytes32.from_hexstr("0x7b3e23dbd438f9aceefa9827e2c5538898189987f49b06eceb7a43067e77b531"),
+            original_challenge_hash=b32("0x73490e166d0b88347c37d921660b216c27316aae9a3450933d3ff3b854e5831a"),
+            signage_point=b32("0x7b3e23dbd438f9aceefa9827e2c5538898189987f49b06eceb7a43067e77b531"),
             height=case.height,
         )
-    except Exception as e:
+    except NotImplementedError as e:
         assert case.expected_error is not None
         assert case.expected_error in repr(e)
     else:
         assert quality_string is None
         assert len(caplog.text) == 0 if case.expected_error is None else case.expected_error in caplog.text
+
+
+@pytest.mark.parametrize(
+    "height, difficulty",
+    [
+        (0, 2),
+        (DEFAULT_CONSTANTS.HARD_FORK_HEIGHT, 2),
+        (DEFAULT_CONSTANTS.HARD_FORK2_HEIGHT, 2),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_4_HEIGHT - 1, 2),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_4_HEIGHT, 4),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_5_HEIGHT - 1, 4),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_5_HEIGHT, 5),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_6_HEIGHT - 1, 5),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_6_HEIGHT, 6),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_7_HEIGHT - 1, 6),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_7_HEIGHT, 7),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_8_HEIGHT - 1, 7),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_8_HEIGHT, 8),
+        (DEFAULT_CONSTANTS.PLOT_DIFFICULTY_8_HEIGHT + 1000000, 8),
+    ],
+)
+def test_calculate_plot_difficulty(height: uint32, difficulty: uint8) -> None:
+    assert calculate_plot_difficulty(DEFAULT_CONSTANTS, height) == difficulty
 
 
 class TestProofOfSpace:
@@ -200,3 +239,34 @@ class TestProofOfSpace:
                 success_count += 1
 
         assert abs((success_count * target_filter / num_trials) - 1) < 0.35
+
+
+@pytest.mark.parametrize("height,expected", [(0, 3), (5496000, 2), (10542000, 1), (15592000, 0), (20643000, 0)])
+@pytest.mark.parametrize("plot_size", [PlotSize.make_v1(32), PlotSize.make_v2(28)])
+def test_calculate_prefix_bits_clamp_zero(height: uint32, expected: int, plot_size: PlotSize) -> None:
+    constants = DEFAULT_CONSTANTS.replace(NUMBER_ZERO_BITS_PLOT_FILTER_V1=uint8(3))
+    if plot_size.size_v2 is not None:
+        expected = constants.NUMBER_ZERO_BITS_PLOT_FILTER_V2
+    assert calculate_prefix_bits(constants, height, plot_size) == expected
+
+
+@pytest.mark.parametrize(
+    argnames=["height", "expected"],
+    argvalues=[
+        (0, 9),
+        (5495999, 9),
+        (5496000, 8),
+        (10541999, 8),
+        (10542000, 7),
+        (15591999, 7),
+        (15592000, 6),
+        (20642999, 6),
+        (20643000, 5),
+    ],
+)
+@pytest.mark.parametrize("plot_size", [PlotSize.make_v1(32), PlotSize.make_v2(28)])
+def test_calculate_prefix_bits_default(height: uint32, expected: int, plot_size: PlotSize) -> None:
+    constants = DEFAULT_CONSTANTS
+    if plot_size.size_v2 is not None:
+        expected = DEFAULT_CONSTANTS.NUMBER_ZERO_BITS_PLOT_FILTER_V2
+    assert calculate_prefix_bits(constants, height, plot_size) == expected
--- chia/types/blockchain_format/proof_of_space.py
+++ chia/types/blockchain_format/proof_of_space.py
@@ -4,7 +4,7 @@ import logging
 from typing import Optional, cast
 
 from bitstring import BitArray
-from chia_rs import AugSchemeMPL, ConsensusConstants, G1Element, PrivateKey, ProofOfSpace
+from chia_rs import AugSchemeMPL, ConsensusConstants, G1Element, PlotSize, PrivateKey, ProofOfSpace
 from chia_rs.sized_bytes import bytes32
 from chia_rs.sized_ints import uint8, uint32
 from chiapos import Verifier
@@ -22,29 +22,38 @@ def get_plot_id(pos: ProofOfSpace) -> bytes32:
     return calculate_plot_id_pk(pos.pool_public_key, pos.plot_public_key)
 
 
-def validate_proof_v2(plot_id: bytes32, size: uint8, challenge: bytes32, proof: bytes) -> Optional[bytes]:
+# returns quality string for v2 plot, or None if invalid
+def validate_proof_v2(
+    plot_id: bytes32, size: uint8, difficulty: uint8, challenge: bytes32, proof: bytes
+) -> Optional[bytes32]:
+    # TODO: todo_v2_plots call into new chiapos library
     raise NotImplementedError
 
 
+def check_plot_size(constants: ConsensusConstants, ps: PlotSize) -> bool:
+    size_v1 = ps.size_v1
+    if size_v1 is not None:
+        assert ps.size_v2 is None
+        if size_v1 < constants.MIN_PLOT_SIZE_V1:
+            log.error("Plot size is lower than the minimum")
+            return False
+        if size_v1 > constants.MAX_PLOT_SIZE_V1:
+            log.error("Plot size is higher than the maximum")
+            return False
+        return True
+
+    size_v2 = ps.size_v2
+    assert size_v2 is not None
+    if size_v2 < constants.MIN_PLOT_SIZE_V2:
+        log.error("Plot size is lower than the minimum")
+        return False
+    if size_v2 > constants.MAX_PLOT_SIZE_V2:
+        log.error("Plot size is higher than the maximum")
+        return False
+    return True
+
+
 def verify_and_get_quality_string(
-    pos: ProofOfSpace,
-    constants: ConsensusConstants,
-    original_challenge_hash: bytes32,
-    signage_point: bytes32,
-    *,
-    height: uint32,
-) -> Optional[bytes32]:
-    # todo: use new proof to get format
-    plot_size = pos.size()
-    if plot_size.size_v1 is not None:
-        assert plot_size.size_v2 is None
-        return verify_and_get_quality_string_v1(pos, constants, original_challenge_hash, signage_point, height=height)
-    else:
-        assert plot_size.size_v2 is not None
-        return verify_and_get_quality_string_v2(pos, constants, original_challenge_hash, signage_point, height=height)
-
-
-def verify_and_get_quality_string_v1(
     pos: ProofOfSpace,
     constants: ConsensusConstants,
     original_challenge_hash: bytes32,
@@ -59,13 +68,9 @@ def verify_and_get_quality_string_v1(
     if (pos.pool_public_key is not None) and (pos.pool_contract_puzzle_hash is not None):
         log.error("Expected pool public key or pool contract puzzle hash but got both")
         return None
-    size = pos.size().size_v1
-    assert size is not None
-    if size < constants.MIN_PLOT_SIZE_V1:
-        log.error("Plot size is lower than the minimum")
-        return None
-    if size > constants.MAX_PLOT_SIZE_V1:
-        log.error("Plot size is higher than the maximum")
+
+    plot_size = pos.size()
+    if not check_plot_size(constants, plot_size):
         return None
 
     plot_id: bytes32 = get_plot_id(pos)
@@ -74,38 +79,27 @@ def verify_and_get_quality_string_v1(
     if new_challenge != pos.challenge:
         log.error("Calculated pos challenge doesn't match the provided one")
         return None
-    prefix_bits = calculate_prefix_bits(constants, height)
+
+    # we use different plot filter prefix sizes depending on v1 or v2 plots
+    prefix_bits = calculate_prefix_bits(constants, height, plot_size)
     if not passes_plot_filter(prefix_bits, plot_id, original_challenge_hash, signage_point):
         log.error("Did not pass the plot filter")
         return None
 
-    quality_str = Verifier().validate_proof(plot_id, size, pos.challenge, bytes(pos.proof))
-    if not quality_str:
-        return None
+    if plot_size.size_v1 is not None:
+        # === V1 plots ===
+        assert plot_size.size_v2 is None
 
-    return bytes32(quality_str)
+        quality_str = Verifier().validate_proof(plot_id, plot_size.size_v1, pos.challenge, bytes(pos.proof))
+        if not quality_str:
+            return None
+        return bytes32(quality_str)
+    else:
+        # === V2 plots ===
+        assert plot_size.size_v2 is not None
 
-
-def verify_and_get_quality_string_v2(
-    pos: ProofOfSpace,
-    constants: ConsensusConstants,
-    original_challenge_hash: bytes32,
-    signage_point: bytes32,
-    *,
-    height: uint32,
-) -> Optional[bytes32]:
-    # TODO: todo_v2_plots complete function
-
-    size = pos.size().size_v2
-    assert size is not None
-    if size < constants.MIN_PLOT_SIZE_V2:
-        log.error("Plot size is lower than the minimum")
-        return None
-    if size > constants.MAX_PLOT_SIZE_V2:
-        log.error("Plot size is higher than the maximum")
-        return None
-
-    raise NotImplementedError
+        plot_difficulty = calculate_plot_difficulty(constants, height)
+        return validate_proof_v2(plot_id, plot_size.size_v2, plot_difficulty, pos.challenge, bytes(pos.proof))
 
 
 def passes_plot_filter(
@@ -125,7 +119,11 @@ def passes_plot_filter(
     return cast(bool, plot_filter[:prefix_bits].uint == 0)
 
 
-def calculate_prefix_bits(constants: ConsensusConstants, height: uint32) -> int:
+def calculate_prefix_bits(constants: ConsensusConstants, height: uint32, plot_size: PlotSize) -> int:
+    # v2 plots have a constant plot filter size
+    if plot_size.size_v2 is not None:
+        return constants.NUMBER_ZERO_BITS_PLOT_FILTER_V2
+
     prefix_bits = int(constants.NUMBER_ZERO_BITS_PLOT_FILTER_V1)
     if height >= constants.PLOT_FILTER_32_HEIGHT:
         prefix_bits -= 4
@@ -137,6 +135,21 @@ def calculate_prefix_bits(constants: ConsensusConstants, height: uint32) -> int:
         prefix_bits -= 1
 
     return max(0, prefix_bits)
+
+
+def calculate_plot_difficulty(constants: ConsensusConstants, height: uint32) -> uint8:
+    if height < constants.PLOT_DIFFICULTY_4_HEIGHT:
+        return constants.PLOT_DIFFICULTY_INITIAL
+    if height < constants.PLOT_DIFFICULTY_5_HEIGHT:
+        return uint8(4)
+    if height < constants.PLOT_DIFFICULTY_6_HEIGHT:
+        return uint8(5)
+    if height < constants.PLOT_DIFFICULTY_7_HEIGHT:
+        return uint8(6)
+    if height < constants.PLOT_DIFFICULTY_8_HEIGHT:
+        return uint8(7)
+    else:
+        return uint8(8)
 
 
 def calculate_plot_filter_input(plot_id: bytes32, challenge_hash: bytes32, signage_point: bytes32) -> bytes32:
