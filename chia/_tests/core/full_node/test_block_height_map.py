c735695176c9c7617874a614a397eb8e1a52181b
Richard Kiss <him@richardkiss.com>
2025-07-10 23:44:06 -0700

    `BlockHeightMapProtocol`

--- chia/_tests/core/full_node/test_block_height_map.py
+++ chia/_tests/core/full_node/test_block_height_map.py
@@ -11,7 +11,7 @@ from chia_rs.sized_bytes import bytes32
 from chia_rs.sized_ints import uint8, uint32
 
 from chia._tests.util.db_connection import DBConnection
-from chia.full_node.block_height_map import BlockHeightMap, SesCache
+from chia.consensus.block_height_map import BlockHeightMap, SesCache
 from chia.util.db_wrapper import DBWrapper2
 from chia.util.files import write_file_async
 
