eccba6dd5657a243ae56b1a93c29fd613f143468
Kyle Altendorf <sda@fstab.net>
2025-07-16 19:27:57 -0400

    do a poetry lock in pre-commit poetry check

---
+++ poetry-check.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import subprocess
+import sys
+
+
+def main() -> int:
+    subprocess.run(
+        [
+            "poetry",
+            "check",
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "poetry",
+            "lock",
+        ],
+        check=True,
+    )
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
