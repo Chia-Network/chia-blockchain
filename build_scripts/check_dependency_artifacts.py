588ecd41a2d4318357b9a227a214af4345e68faa
Kyle Altendorf <sda@fstab.net>
2025-07-16 19:03:08 -0400

    and request all

--- build_scripts/check_dependency_artifacts.py
+++ build_scripts/check_dependency_artifacts.py
@@ -37,7 +37,7 @@ def main() -> int:
         artifact_directory_path = directory_path.joinpath("artifacts")
         artifact_directory_path.mkdir()
 
-        extras = ["upnp"]
+        extras = ["dev", "legacy-keyring", "upnp"]
 
         print("Downloading packages for Python version:")
         lines = [
