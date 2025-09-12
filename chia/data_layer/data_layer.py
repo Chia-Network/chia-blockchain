00a44155ef865fcb7f28ec947a400eb007919106
Matt <quexington@gmail.com>
2025-09-12 10:55:15 -0700

    Port `take_offer` to `@marshal`

--- chia/data_layer/data_layer.py
+++ chia/data_layer/data_layer.py
@@ -81,6 +81,7 @@ from chia.wallet.wallet_request_types import (
     DLUpdateRoot,
     LauncherRootPair,
     LogIn,
+    TakeOffer,
 )
 from chia.wallet.wallet_rpc_client import WalletRpcClient
 
@@ -1241,22 +1242,26 @@ class DataLayer:
                         )
                     )
 
-            solver: dict[str, Any] = {
-                "proofs_of_inclusion": proofs_of_inclusion,
-                **{
-                    "0x" + our_offer_store.store_id.hex(): {
-                        "new_root": "0x" + root.hex(),
-                        "dependencies": [
-                            {
-                                "launcher_id": "0x" + their_offer_store.store_id.hex(),
-                                "values_to_prove": ["0x" + entry.node_hash.hex() for entry in their_offer_store.proofs],
-                            }
-                            for their_offer_store in maker
-                        ],
-                    }
-                    for our_offer_store in taker
-                },
-            }
+            solver = Solver(
+                {
+                    "proofs_of_inclusion": proofs_of_inclusion,
+                    **{
+                        "0x" + our_offer_store.store_id.hex(): {
+                            "new_root": "0x" + root.hex(),
+                            "dependencies": [
+                                {
+                                    "launcher_id": "0x" + their_offer_store.store_id.hex(),
+                                    "values_to_prove": [
+                                        "0x" + entry.node_hash.hex() for entry in their_offer_store.proofs
+                                    ],
+                                }
+                                for their_offer_store in maker
+                            ],
+                        }
+                        for our_offer_store in taker
+                    },
+                }
+            )
 
         # Excluding wallet from transaction since failures in the wallet may occur
         # after the transaction is submitted to the chain.  If we roll back data we
@@ -1264,9 +1269,7 @@ class DataLayer:
 
         trade_record = (
             await self.wallet_rpc.take_offer(
-                offer=offer,
-                solver=solver,
-                fee=fee,
+                TakeOffer(offer=offer.to_bech32(), solver=solver, fee=fee, push=True),
                 # TODO: probably shouldn't be default but due to peculiarities in the RPC, we're using a stop gap.
                 # This is not a change in behavior, the default was already implicit.
                 tx_config=DEFAULT_TX_CONFIG,
