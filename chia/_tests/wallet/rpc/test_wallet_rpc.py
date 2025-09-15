e9f27321828ea29262325abb9871bd149f36b250
Matt <quexington@gmail.com>
2025-09-15 12:47:01 -0700

    Port `cancel_offers` to `@marshal`

--- chia/_tests/wallet/rpc/test_wallet_rpc.py
+++ chia/_tests/wallet/rpc/test_wallet_rpc.py
@@ -107,6 +107,7 @@ from chia.wallet.wallet_protocol import WalletProtocol
 from chia.wallet.wallet_request_types import (
     AddKey,
     CancelOffer,
+    CancelOffers,
     CATAssetIDToName,
     CATGetAssetID,
     CATGetName,
@@ -1783,7 +1784,9 @@ async def test_offer_endpoints(wallet_environments: WalletTestFramework, wallet_
         )
         == 2
     )
-    await env_1.rpc_client.cancel_offers(wallet_environments.tx_config, batch_size=1)
+    await env_1.rpc_client.cancel_offers(
+        CancelOffers(secure=True, batch_size=uint16(1)), tx_config=wallet_environments.tx_config
+    )
     assert (
         len(
             [
@@ -1838,7 +1841,9 @@ async def test_offer_endpoints(wallet_environments: WalletTestFramework, wallet_
         )
         == 2
     )
-    await env_1.rpc_client.cancel_offers(wallet_environments.tx_config, cancel_all=True)
+    await env_1.rpc_client.cancel_offers(
+        CancelOffers(secure=True, cancel_all=True), tx_config=wallet_environments.tx_config
+    )
     assert (
         len(
             [
@@ -1900,7 +1905,9 @@ async def test_offer_endpoints(wallet_environments: WalletTestFramework, wallet_
         )
         == 1
     )
-    await env_1.rpc_client.cancel_offers(wallet_environments.tx_config, asset_id=bytes32.zeros)
+    await env_1.rpc_client.cancel_offers(
+        CancelOffers(secure=True, asset_id=bytes32.zeros.hex()), tx_config=wallet_environments.tx_config
+    )
     assert (
         len(
             [
@@ -1911,7 +1918,9 @@ async def test_offer_endpoints(wallet_environments: WalletTestFramework, wallet_
         )
         == 1
     )
-    await env_1.rpc_client.cancel_offers(wallet_environments.tx_config, asset_id=cat_asset_id)
+    await env_1.rpc_client.cancel_offers(
+        CancelOffers(secure=True, asset_id=cat_asset_id.hex()), tx_config=wallet_environments.tx_config
+    )
     assert (
         len(
             [
