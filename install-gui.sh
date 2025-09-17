a3e35b72c05b24d350a90118206a7bcef8924467
Earle Lowe <e.lowe@chia.net>
2025-09-17 10:56:52 -0700

    shellcheck workaround for super linter

--- install-gui.sh
+++ install-gui.sh
@@ -166,6 +166,7 @@ elif [ "$(uname)" = "Darwin" ] && type brew >/dev/null 2>&1; then
     # Download and install nvm:
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
     # in lieu of restarting the shell
+    # shellcheck source=/dev/null
     source "$HOME/.nvm/nvm.sh"
     # Download and install Node.js:
     nvm install 20
