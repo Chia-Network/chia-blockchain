3652e609bdcdb199ece68bcfd60665d19594e5f0
Earle Lowe <30607889+emlowe@users.noreply.github.com>
2025-09-18 15:09:32 -0700

    Remove scripting that attempts to find and install node (#20082)
    * Remove scripting that attempts to find and install node
    
    * Remove using  direction in start-gui.sh
    
    * Remove unneeded npm_global files - only used to install node
    
    * Handle guess where node or npm is not installed
    
    * Add setup-node to test scripts workflow
    
    * make sure to checkout submodules

--- start-gui.sh
+++ start-gui.sh
@@ -11,13 +11,6 @@ SCRIPT_DIR=$(
 
 echo "### Checking GUI dependencies"
 
-if [ -d "${SCRIPT_DIR}/.n" ]; then
-  export N_PREFIX="${SCRIPT_DIR}/.n"
-  export PATH="${N_PREFIX}/bin:${PATH}"
-  echo "Loading nodejs/npm from"
-  echo "  ${N_PREFIX}"
-fi
-
 if [ -z "$VIRTUAL_ENV" ]; then
   echo "This requires the chia python virtual environment."
   echo "Execute '. ./activate' before running."
