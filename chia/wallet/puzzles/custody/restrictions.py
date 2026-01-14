74ec5744f5780193d554dab4330aea698f25b8b4
Matt <quexington@gmail.com>
2026-01-15 09:41:01 +1300

    Make a heightlock instead of timelock restriction

--- chia/wallet/puzzles/custody/restrictions.py
+++ chia/wallet/puzzles/custody/restrictions.py
@@ -2,25 +2,24 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from chia_puzzles_py import programs as puzzle_mods
 from chia_rs.sized_bytes import bytes32
-from chia_rs.sized_ints import uint64
+from chia_rs.sized_ints import uint32
 
 from chia.types.blockchain_format.program import Program
 from chia.wallet.puzzles.load_clvm import load_clvm_maybe_recompile
 
-TIMELOCK_WRAPPER = Program.from_bytes(puzzle_mods.TIMELOCK)
 FIXED_CREATE_COIN_DESTINATIONS = load_clvm_maybe_recompile(
     "fixed_create_coin_destinations.clsp", package_or_requirement="chia.wallet.puzzles.custody"
 )
 SEND_MESSAGE_BANNED = load_clvm_maybe_recompile(
     "send_message_banned.clsp", package_or_requirement="chia.wallet.puzzles.custody"
 )
+HEIGHTLOCK_WRAPPER = load_clvm_maybe_recompile("heightlock.clsp", package_or_requirement="chia.wallet.puzzles.custody")
 
 
 @dataclass(frozen=True)
-class Timelock:
-    timelock: uint64
+class Heightlock:
+    heightlock: uint32
 
     @property
     def member_not_dpuz(self) -> bool:
@@ -30,7 +29,7 @@ class Timelock:
         return Program.to(None)
 
     def puzzle(self, nonce: int) -> Program:
-        return TIMELOCK_WRAPPER.curry(self.timelock)
+        return HEIGHTLOCK_WRAPPER.curry(self.heightlock)
 
     def puzzle_hash(self, nonce: int) -> bytes32:
         return self.puzzle(nonce).get_tree_hash()
