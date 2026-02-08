b5a449f0f6bdf84f78e125eba7d5259cbad74f03
almogdepaz <almogdepaz@gmail.com>
2026-02-08 15:34:41 +0200

    fix overflow handling in test helper

--- chia/_tests/core/full_node/test_prev_tx_block.py
+++ chia/_tests/core/full_node/test_prev_tx_block.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from chia_rs import FullBlock
+from chia_rs import ConsensusConstants, FullBlock
 from chia_rs.sized_ints import uint8, uint32
 
 from chia.consensus.get_block_challenge import pre_sp_tx_block_height
+from chia.consensus.pot_iterations import is_overflow_block
 from chia.simulator.block_tools import BlockTools, load_block_list, test_constants
 from chia.util.block_cache import BlockCache
 
@@ -35,11 +36,11 @@ def test_prev_tx_block_blockrecord_tx(bt: BlockTools) -> None:
     )
     _, _, blocks = load_block_list(block_list, bt.constants)
     block = block_list[-1]
-    latest_tx_before_sp = find_tx_before_sp(block_list)
+    latest_tx_before_sp = find_tx_before_sp(block_list, bt.constants)
     assert latest_tx_before_sp is not None
     assert (
         pre_sp_tx_block_height(
-            constants=test_constants,
+            constants=bt.constants,
             blocks=BlockCache(blocks),
             prev_b_hash=block.prev_header_hash,
             sp_index=block.reward_chain_block.signage_point_index,
@@ -48,11 +49,11 @@ def test_prev_tx_block_blockrecord_tx(bt: BlockTools) -> None:
         == latest_tx_before_sp.height
     )
     block = block_list[-2]
-    latest_tx_before_sp = find_tx_before_sp(block_list[:-1])
+    latest_tx_before_sp = find_tx_before_sp(block_list[:-1], bt.constants)
     assert latest_tx_before_sp is not None
     assert (
         pre_sp_tx_block_height(
-            constants=test_constants,
+            constants=bt.constants,
             blocks=BlockCache(blocks),
             prev_b_hash=block.prev_header_hash,
             sp_index=block.reward_chain_block.signage_point_index,
@@ -61,11 +62,11 @@ def test_prev_tx_block_blockrecord_tx(bt: BlockTools) -> None:
         == latest_tx_before_sp.height
     )
     block = block_list[-3]
-    latest_tx_before_sp = find_tx_before_sp(block_list[:-2])
+    latest_tx_before_sp = find_tx_before_sp(block_list[:-2], bt.constants)
     assert latest_tx_before_sp is not None
     assert (
         pre_sp_tx_block_height(
-            constants=test_constants,
+            constants=bt.constants,
             blocks=BlockCache(blocks),
             prev_b_hash=block.prev_header_hash,
             sp_index=block.reward_chain_block.signage_point_index,
@@ -88,10 +89,10 @@ def test_prev_tx_block_blockrecord_not_tx(bt: BlockTools) -> None:
     )
     _, _, blocks = load_block_list(block_list, bt.constants)
     block = block_list[-1]
-    latest_tx_before_sp = find_tx_before_sp(block_list)
+    latest_tx_before_sp = find_tx_before_sp(block_list, bt.constants)
     assert latest_tx_before_sp is not None
     assert pre_sp_tx_block_height(
-        constants=test_constants,
+        constants=bt.constants,
         blocks=BlockCache(blocks),
         prev_b_hash=block.prev_header_hash,
         sp_index=block.reward_chain_block.signage_point_index,
@@ -100,21 +101,23 @@ def test_prev_tx_block_blockrecord_not_tx(bt: BlockTools) -> None:
 
 
 # get the latest infused transaction block before the signage point of the last block in the list
-def find_tx_before_sp(block_list: list[FullBlock]) -> FullBlock | None:
-    before_slot = False
-    before_sp = False
-    if len(block_list[-1].finished_sub_slots) > 0:
-        before_slot = True
+def find_tx_before_sp(block_list: list[FullBlock], constants: ConsensusConstants) -> FullBlock | None:
     sp_index = block_list[-1].reward_chain_block.signage_point_index
+    overflow = is_overflow_block(constants, sp_index)
+    slots_crossed = len(block_list[-1].finished_sub_slots)
     idx = len(block_list) - 2
     curr = None
     while idx > 0:
         curr = block_list[idx]
-        if curr.reward_chain_block.signage_point_index < sp_index:
-            before_sp = True
-        if curr.foliage_transaction_block is not None and (before_slot or before_sp):
+        if not overflow:
+            before_sp = curr.reward_chain_block.signage_point_index < sp_index or slots_crossed > 0
+        else:
+            before_sp = slots_crossed >= 2 or (
+                slots_crossed == 1 and curr.reward_chain_block.signage_point_index < sp_index
+            )
+        if curr.foliage_transaction_block is not None and before_sp:
             break
         if len(curr.finished_sub_slots) > 0:
-            before_slot = True
+            slots_crossed += 1
         idx -= 1
     return curr
