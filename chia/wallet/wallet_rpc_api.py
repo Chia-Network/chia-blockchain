fb7226bb692ec1dcfbb543daeaab2d3a35cd8604
Adam Kelly <338792+aqk@users.noreply.github.com>
2026-04-28 19:16:59 -0400

    remove redundant sync check. reduce blocking.

--- chia/wallet/wallet_rpc_api.py
+++ chia/wallet/wallet_rpc_api.py
@@ -1526,9 +1526,6 @@ class WalletRpcApi:
         action_scope: WalletActionScope,
         extra_conditions: tuple[Condition, ...] = tuple(),
     ) -> SendTransactionMultiResponse:
-        if await self.service.wallet_state_manager.synced() is False:
-            raise ValueError("Wallet needs to be fully synced before sending transactions")
-
         wallet = self.service.wallet_state_manager.wallets[request.wallet_id]
 
         async with self.service.wallet_state_manager.lock:
