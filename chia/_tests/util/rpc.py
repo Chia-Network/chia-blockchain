a61fbdcb62f3f0710a73860c2b54d0af205588b9
Noa Levi <275430404+lphuc2250gma@users.noreply.github.com>
2026-06-04 20:08:35 +0000

    chore: improve chia-blockchain maintenance path

--- chia/_tests/util/rpc.py
+++ chia/_tests/util/rpc.py
@@ -6,6 +6,8 @@ from chia.rpc.rpc_server import RpcApiProtocol
 
 async def validate_get_routes(client: RpcClient, api: RpcApiProtocol) -> None:
     routes_client = (await client.fetch("get_routes", {}))["routes"]
+    assert isinstance(routes_client, list)
+    assert all(isinstance(route, str) for route in routes_client)
     assert len(routes_client) > 0
     routes_api = list(api.get_routes().keys())
     # TODO: avoid duplication of RpcServer.get_routes()
