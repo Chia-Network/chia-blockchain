e94774d9babcc7b62a7e54f807371d7ed4eb8aaa
Matt <quexington@gmail.com>
2026-08-04 12:53:22 +1200

    Exempt pool tests

--- chia/_tests/pools/test_pool_cmdline.py
+++ chia/_tests/pools/test_pool_cmdline.py
@@ -76,6 +76,7 @@ class StateUrlCase:
         {
             "num_environments": 1,
             "blocks_needed": [1],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -229,6 +230,7 @@ async def test_plotnft_cli_create_errors(
         {
             "num_environments": 1,
             "blocks_needed": [1],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -309,6 +311,7 @@ async def test_plotnft_cli_show(
         {
             "num_environments": 1,
             "blocks_needed": [1],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -381,6 +384,7 @@ async def test_plotnft_cli_show_with_farmer(
         {
             "num_environments": 1,
             "blocks_needed": [10],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -472,6 +476,7 @@ async def test_plotnft_cli_leave(
         {
             "num_environments": 1,
             "blocks_needed": [10],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -704,6 +709,7 @@ async def test_plotnft_cli_join(
         {
             "num_environments": 1,
             "blocks_needed": [10],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -829,6 +835,7 @@ async def test_plotnft_cli_claim(wallet_environments: WalletTestFramework, versi
             "num_environments": 1,
             "blocks_needed": [10],
             "reuse_puzhash": False,
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -918,6 +925,7 @@ async def test_plotnft_cli_inspect(
         {
             "num_environments": 1,
             "blocks_needed": [10],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
@@ -990,6 +998,7 @@ async def test_plotnft_cli_change_payout(
         {
             "num_environments": 1,
             "blocks_needed": [10],
+            "reorg_exempt": True,
         }
     ],
     indirect=True,
